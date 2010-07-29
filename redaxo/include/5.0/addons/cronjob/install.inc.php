0582d8ab1b2d4bb99f6695ab4f9f54f97b923802
gharlan <gharlan@web.de>
2010-07-29 11:13:19 +0200

    Cronjob: Schreibrechte werden beim Installieren überprüft

--- redaxo/include/5.0/addons/cronjob/install.inc.php
+++ redaxo/include/5.0/addons/cronjob/install.inc.php
@@ -9,4 +9,19 @@
  * @version svn:$Id$
  */
  
-$REX['ADDON']['install']['cronjob'] = 1;
+$error = '';
+
+$config_file = $REX['INCLUDE_PATH'] .'/addons/cronjob/config.inc.php';
+
+if(($state = rex_is_writable($config_file)) !== true)
+  $error .= $state;
+
+$log_folder = $REX['INCLUDE_PATH'] .'/addons/cronjob/logs/';
+
+if(($state = rex_is_writable($log_folder)) !== true)
+  $error .= $state;
+
+if ($error != '')
+  $REX['ADDON']['installmsg']['cronjob'] = $error;
+else
+  $REX['ADDON']['install']['cronjob'] = true;
