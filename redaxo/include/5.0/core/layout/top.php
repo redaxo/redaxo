682cbf59f2433c46faa63d2dd8d026583bdf9f81
tbaddade <thomas@redaxo.de>
2010-08-05 17:22:12 +0200

    Stylesheets gelöscht

--- redaxo/include/5.0/core/layout/top.php
+++ redaxo/include/5.0/core/layout/top.php
@@ -34,18 +34,6 @@ $body_attr["onunload"] = array('closeAll();');
   <title><?php echo htmlspecialchars($page_title) ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $I18N->msg('htmlcharset'); ?>" />
   <meta http-equiv="Content-Language" content="<?php echo $I18N->msg('htmllang'); ?>" />
-  <link rel="stylesheet" type="text/css" href="../redaxo_media/css_import.css" media="screen, projection, print" />
-  <!--[if lte IE 7]>
-		<link rel="stylesheet" href="../redaxo_media/css_ie_lte_7.css" type="text/css" media="screen, projection, print" />
-	<![endif]-->
-			
-	<!--[if IE 7]>
-		<link rel="stylesheet" href="../redaxo_media/css_ie_7.css" type="text/css" media="screen, projection, print" />
-	<![endif]-->
-	
-	<!--[if lte IE 6]>
-		<link rel="stylesheet" href="../redaxo_media/css_ie_lte_6.css" type="text/css" media="screen, projection, print" />
-	<![endif]-->
 
   <!-- jQuery immer nach den Stylesheets! -->
   <script src="../redaxo_media/jquery.min.js" type="text/javascript"></script>
