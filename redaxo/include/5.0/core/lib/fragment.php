200e6e7a47c61ca7b414f27949359145a5f2d8f9
Markus Staab <markus.staab@redaxo.de>
2010-08-19 20:22:44 +0200

    added fragment::url() methode for use in fragments

--- redaxo/include/5.0/core/lib/fragment.php
+++ redaxo/include/5.0/core/lib/fragment.php
@@ -10,11 +10,18 @@ class rex_fragment
    * 
    * @param array $params A array of key-value pairs to pass as local parameters
    */
-  public function rex_fragment($vars = array())
+  public function rex_fragment(array $vars = array())
   {
     $this->vars = $vars;
   }
   
+  /**
+   * Set the variable $name to the given value.
+   * 
+   * @param $name string The name of the variable.
+   * @param $value string The value for the variable
+   * @param $escape Flag which indicates if the value should be escaped or not.
+   */
   public function setVar($name, $value, $escape = true)
   {
     if(!is_string($name))
@@ -32,6 +39,11 @@ class rex_fragment
     }
   }
   
+  /**
+   * Parses the variables of the fragment into the file $filename
+   *  
+   * @param string $filename the filename of the fragment to parse.
+   */
   public function parse($filename)
   {
     global $REX;
@@ -56,6 +68,8 @@ class rex_fragment
       throw new Exception(sprintf('Fragmentfile "%s" not found!', $template));
     }
   }
+  
+  // -------------------------- in fragment helpers
 
   /**
    * Include a Subfragment from within a fragment.
@@ -65,7 +79,7 @@ class rex_fragment
    * @param string $filename The filename of the fragment to use
    * @param array $params A array of key-value pairs to pass as local parameters
    */
-  protected function subfragment($filename, $params = array())
+  protected function subfragment($filename, array $params = array())
   {
     $fragment = new rex_fragment(array_merge($this->vars, $params));
     echo $fragment->parse($filename);
@@ -79,19 +93,72 @@ class rex_fragment
   protected function i18n($key)
   {
     global $I18N;
+    
+    if(!is_string($key))
+    {
+      throw new Exception(sprintf('Expecting $key to be a string, %s given!', gettype($key)));
+    }
+    
     return $I18N->msg($key);
   }
   
+  /**
+   * Returns the config for key $key.
+   * Enter description here ...
+   * @param $key
+   */
   protected function config($key)
   {
     global $REX;
+    
+    if(!is_string($key))
+    {
+      throw new Exception(sprintf('Expecting $key to be a string, %s given!', gettype($key)));
+    }
     
     if(isset($REX[$key]))
     {
       return $REX[$key];
     }
+    
     return null;
   }
+  
+  /**
+   * Generates a url with the given parameters
+   */
+  protected function url(array $params = array())
+  {
+    if(!is_array($params))
+    {
+      throw new Exception(sprintf('Expecting $params to be a array, %s given!', gettype($filename)));
+    }
+    
+    if(!isset($params['page']))
+    {
+      $page = rex_request('page');
+      if($page != null)
+      {
+        $params['page'] = $page;
+      }
+    }
+    if(!isset($params['subpage']))
+    {
+      $subpage = rex_request('subpage');
+      if($subpage != null)
+      {
+        $params['subpage'] = $subpage;
+      }
+    }
+    
+    $url = 'index.php?';
+    foreach($params as $key => $value)
+    {
+      $url .= $key .'='. urlencode($value) .'&';
+    }
+    return substr($url, 0, -1);
+  }
+  
   
   /**
    * Magic getter to reference variables from within the fragment.
@@ -109,4 +176,6 @@ class rex_fragment
     
     return null;
   }
+  
+  // /-------------------------- in fragment helpers
 }
