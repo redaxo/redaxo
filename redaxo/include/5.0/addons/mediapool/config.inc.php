271858c8c48369d7e22cd6185ddbbb804f2f7314
Markus Staab <markus.staab@redaxo.de>
2010-08-19 21:27:55 +0200

    adjusted addon page

--- redaxo/include/5.0/addons/mediapool/config.inc.php
+++ redaxo/include/5.0/addons/mediapool/config.inc.php
@@ -11,16 +11,20 @@
 
 $mypage = 'mediapool';
 
-$REX['ADDON']['name'][$mypage] = $I18N->msg('mediapool');
-$REX['ADDON']['perm'][$mypage] = ''; // hasStructurePerm
+//$REX['ADDON']['name'][$mypage] = $I18N->msg('mediapool');
+//$REX['ADDON']['perm'][$mypage] = ''; // hasStructurePerm
 $REX['ADDON']['version'][$mypage] = "0.1";
 $REX['ADDON']['author'][$mypage] = "Jan Kristinus";
 $REX['ADDON']['supportpage'][$mypage] = '';
 $REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
 
+$mpool = new rex_be_page_popup($I18N->msg('mediapool'), 'openMediaPool(); return false;');
+$mpool->setRequiredPermissions('hasMediaPerm');
+$REX['ADDON']['page'][$mypage] = $mpool; 
+
 if ($REX['REDAXO'])
 {
   $I18N->appendFile($REX['SRC_PATH'] . '/addons/' . $mypage . '/lang');
   include_once $REX['SRC_PATH'] . '/addons/' . $mypage . '/functions/function_rex_mediapool.inc.php';
-  $loader->addDirectory($REX['SRC_PATH'] . '/addons/' . $mypage . '/classes/');
+  rex_autoload::getInstance()->addDirectory($REX['SRC_PATH'] . '/addons/' . $mypage . '/classes/');
 }
