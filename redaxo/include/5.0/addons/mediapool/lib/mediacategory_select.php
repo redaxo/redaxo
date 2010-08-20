1056a5e8d336a482cb19a0e7c5516ef2ac9e7ef7
Markus Staab <markus.staab@redaxo.de>
2010-08-20 15:47:30 +0200

    renamed classes folder to lib folder

---
+++ redaxo/include/5.0/addons/mediapool/lib/mediacategory_select.php
@@ -0,0 +1,97 @@
+<?php
+
+################ Class MediaKategorie Select
+class rex_mediacategory_select extends rex_select
+{
+  var $check_perms;
+  var $rootId;
+  
+  public function rex_mediacategory_select($check_perms = true)
+  {
+    $this->check_perms = $check_perms;
+    $this->rootId = null;
+    
+    parent::rex_select();
+  }
+  
+  /**
+   * Kategorie-Id oder ein Array von Kategorie-Ids als Wurzelelemente der Select-Box.
+   * 
+   * @param $rootId mixed Kategorie-Id oder Array von Kategorie-Ids zur Identifikation der Wurzelelemente. 
+   */
+  public function setRootId($rootId)
+  {
+    $this->rootId = $rootId;
+  }
+  
+  protected function addCatOptions()
+  {
+    if($this->rootId !== null)
+    {
+      if(is_array($this->rootId))
+      {
+        foreach($this->rootId as $rootId)
+        {
+          if($rootCat = OOMediaCategory::getCategoryById($rootId))
+          {
+            $this->addCatOption($rootCat);
+          }
+        }
+      }
+      else
+      {
+        if($rootCat = OOMediaCategory::getCategoryById($this->rootId))
+        {
+          $this->addCatOption($rootCat);
+        }
+      }
+    }
+    else
+    {
+      if ($rootCats = OOMediaCategory::getRootCategories())
+      {
+        foreach($rootCats as $rootCat)
+        {
+          $this->addCatOption($rootCat);
+        }
+      }
+    }
+  }
+  
+  protected function addCatOption(/*OOMediaCategory*/ $mediacat)
+  {
+    global $REX;
+    
+    if(!$this->check_perms ||
+        $this->check_perms && $REX['USER']->hasMediaCategoryPerm($mediacat->getId()))
+    {
+      $mid = $mediacat->getId();
+      $mname = $mediacat->getName();
+      
+      if($REX['USER']->hasPerm('advancedMode[]'))
+        $mname .= ' ['. $mid .']';
+        
+      $this->addOption($mname, $mid, $mid, $mediacat->getParentId());
+      $childs = $mediacat->getChildren();
+      if (is_array($childs))
+      {
+        foreach ($childs as $child)
+        {
+          $this->addCatOption($child);
+        }
+      }
+    }
+  }
+  
+  public function get()
+  {
+    static $loaded = false;
+    
+    if(!$loaded)
+    {
+      $this->addCatOptions();
+    }
+    
+    return parent::get();
+  }
+}
