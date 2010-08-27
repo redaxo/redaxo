d3edc67cd631e48337ddefb34765e0e9aadfca37
Markus Staab <markus.staab@redaxo.de>
2010-08-27 17:47:40 +0200

    added missing linkmap files

---
+++ redaxo/include/5.0/addons/structure/plugins/linkmap/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Site Structure Addon
+ *
+ * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ *
+ * @package redaxo4
+ * @version svn:$Id$
+ */
+
+$mypage = 'linkmap';
+
+// $REX['ADDON']['rxid'][$mypage] = '62';
+//$page = new rex_be_page($I18N->msg('linkmap'), array('page' => 'linkmap'));
+//$page->setRequiredPermissions('hasStructurePerm');
+//$REX['ADDON']['page'][$mypage] = $page;
+
+$REX['ADDON']['name'][$mypage] = $I18N->msg('linkmap');
+$REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
+$REX['ADDON']['version'][$mypage] = "1.3";
+$REX['ADDON']['author'][$mypage] = "Markus Staab";
+$REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
+$REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
+
+if ($REX['REDAXO'])
+{
+  $I18N->appendFile(dirname(__FILE__) .'/lang');
+}
