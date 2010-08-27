0f57751b50294d99a67331f47b5122af8c06ffe1
Markus Staab <markus.staab@redaxo.de>
2010-08-27 18:21:25 +0200

    moved linkmap functions into linkmap plugin; made linkmap plugin registering as a root page

--- redaxo/include/5.0/addons/structure/plugins/linkmap/config.inc.php
+++ redaxo/include/5.0/addons/structure/plugins/linkmap/config.inc.php
@@ -12,16 +12,16 @@
 $mypage = 'linkmap';
 
 // $REX['ADDON']['rxid'][$mypage] = '62';
-//$page = new rex_be_page($I18N->msg('linkmap'), array('page' => 'linkmap'));
-//$page->setRequiredPermissions('hasStructurePerm');
-//$REX['ADDON']['page'][$mypage] = $page;
+$page = new rex_be_page_popup($I18N->msg('linkmap'), array('page' => 'linkmap'));
+$page->setRequiredPermissions('hasStructurePerm');
+$REX['ADDON']['page'][$mypage] = new rex_be_page_main('system', $page);
 
-$REX['ADDON']['name'][$mypage] = $I18N->msg('linkmap');
-$REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
+//$REX['ADDON']['name'][$mypage] = $I18N->msg('linkmap');
+//$REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
 $REX['ADDON']['version'][$mypage] = "1.3";
 $REX['ADDON']['author'][$mypage] = "Markus Staab";
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-$REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
+//$REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
 
 if ($REX['REDAXO'])
 {
