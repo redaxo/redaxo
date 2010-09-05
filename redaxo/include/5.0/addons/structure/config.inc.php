802cc5ac6e7b917a43f101fcee725525d607328c
Markus Staab <markus.staab@redaxo.de>
2010-09-05 17:11:36 +0200

    fixed I18N errors in frontend

--- redaxo/include/5.0/addons/structure/config.inc.php
+++ redaxo/include/5.0/addons/structure/config.inc.php
@@ -11,19 +11,19 @@
 
 $mypage = 'structure';
 
-// $REX['ADDON']['rxid'][$mypage] = '62';
-$REX['ADDON']['name'][$mypage] = $I18N->msg('structure');
-$REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
-$REX['ADDON']['version'][$mypage] = "1.3";
-$REX['ADDON']['author'][$mypage] = "Markus Staab";
-$REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-$REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
-
 if ($REX['REDAXO'])
 {
-  $I18N->appendFile(dirname(__FILE__) .'/lang');
-}
+  // $REX['ADDON']['rxid'][$mypage] = '62';
+  $REX['ADDON']['name'][$mypage] = $I18N->msg('structure');
+  $REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
+  $REX['ADDON']['version'][$mypage] = "1.3";
+  $REX['ADDON']['author'][$mypage] = "Markus Staab";
+  $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
+  $REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
 
-$REX['VARIABLES'][] = 'rex_var_globals';
-$REX['VARIABLES'][] = 'rex_var_article';
-$REX['VARIABLES'][] = 'rex_var_category';
+  $I18N->appendFile(dirname(__FILE__) .'/lang');
+  
+  $REX['VARIABLES'][] = 'rex_var_globals';
+  $REX['VARIABLES'][] = 'rex_var_article';
+  $REX['VARIABLES'][] = 'rex_var_category';
+}
