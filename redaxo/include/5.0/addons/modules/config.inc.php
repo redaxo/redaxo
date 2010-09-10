800536e747f4c268e0cd350a14bf3a1cd5e758e0
Markus Staab <markus.staab@redaxo.de>
2010-09-10 18:03:16 +0200

    cosmetic changes

--- redaxo/include/5.0/addons/modules/config.inc.php
+++ redaxo/include/5.0/addons/modules/config.inc.php
@@ -13,6 +13,8 @@ $mypage = 'modules';
 
 if ($REX['REDAXO'])
 {
+  // -------------------- register the addon
+  
   // $REX['ADDON']['rxid'][$mypage] = '62';
   $REX['ADDON']['name'][$mypage] = $I18N->msg('modules');
   $REX['ADDON']['perm'][$mypage] = 'isAdmin';
@@ -21,13 +23,9 @@ if ($REX['REDAXO'])
   $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
   $REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
 
-  $I18N->appendFile(dirname(__FILE__) .'/lang');
-}
-
-
-if($REX['REDAXO'])
-{
-	rex_register_extension('PAGE_CHECKED', 'rex_subpage_modules');
+  // -------------------- register extensions
+  
+  rex_register_extension('PAGE_CHECKED', 'rex_subpage_modules');
 	
 	function rex_subpage_modules($params)
 	{
@@ -35,19 +33,16 @@ if($REX['REDAXO'])
 	
 		if(($REX['PAGES']['modules']))
 		{
+	    $modules = new rex_be_page($I18N->msg('modules'), array('page'=>'modules', 'subpage' => ''));
+	    $modules->setRequiredPermissions('isAdmin');
+	    $modules->setHref('index.php?page=modules&subpage=');
 	
-		    $modules = new rex_be_page($I18N->msg('modules'), array('page'=>'modules', 'subpage' => ''));
-		    $modules->setRequiredPermissions('isAdmin');
-		    $modules->setHref('index.php?page=modules&subpage=');
-		
-		    $actions = new rex_be_page($I18N->msg('actions'), array('page'=>'modules', 'subpage' => 'actions'));
-		    $actions->setRequiredPermissions('isAdmin');
-		    $actions->setHref('index.php?page=modules&subpage=actions');
+	    $actions = new rex_be_page($I18N->msg('actions'), array('page'=>'modules', 'subpage' => 'actions'));
+	    $actions->setRequiredPermissions('isAdmin');
+	    $actions->setHref('index.php?page=modules&subpage=actions');
 		
 			$REX['PAGES']['modules']->page->addSubPage($modules);
 			$REX['PAGES']['modules']->page->addSubPage($actions);
 		}
-	
 	}
-
 }
