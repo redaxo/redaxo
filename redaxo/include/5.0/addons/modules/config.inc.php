6ea4890c3efaada1fb17e80e88fb72984f7b64f5
Markus Staab <markus.staab@redaxo.de>
2010-09-12 18:32:51 +0200

    simplified subpage handling

--- redaxo/include/5.0/addons/modules/config.inc.php
+++ redaxo/include/5.0/addons/modules/config.inc.php
@@ -15,34 +15,22 @@ if ($REX['REDAXO'])
 {
   // -------------------- register the addon
   
-  // $REX['ADDON']['rxid'][$mypage] = '62';
-  $REX['ADDON']['name'][$mypage] = $I18N->msg('modules');
-  $REX['ADDON']['perm'][$mypage] = 'isAdmin';
+  $page = new rex_be_page($I18N->msg('modules'), array('page' => 'modules'));
+  $page->setRequiredPermissions('isAdmin');
+  
+  $modules = new rex_be_page($I18N->msg('modules'), array('page'=>'modules', 'subpage' => ''));
+  $modules->setRequiredPermissions('isAdmin');
+  $modules->setHref('index.php?page=modules&subpage=');
+
+  $actions = new rex_be_page($I18N->msg('actions'), array('page'=>'modules', 'subpage' => 'actions'));
+  $actions->setRequiredPermissions('isAdmin');
+  $actions->setHref('index.php?page=modules&subpage=actions');
+
+  $page->addSubPage($modules);
+  $page->addSubPage($actions);
+    
+  $REX['ADDON']['page'][$mypage] = new rex_be_page_main('system', $page);
   $REX['ADDON']['version'][$mypage] = "1.3";
   $REX['ADDON']['author'][$mypage] = "Markus Staab";
   $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-  $REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
-
-  // -------------------- register extensions
-  
-  rex_register_extension('PAGE_CHECKED', 'rex_subpage_modules');
-	
-	function rex_subpage_modules($params)
-	{
-		global $REX,$I18N;
-	
-		if(($REX['PAGES']['modules']))
-		{
-	    $modules = new rex_be_page($I18N->msg('modules'), array('page'=>'modules', 'subpage' => ''));
-	    $modules->setRequiredPermissions('isAdmin');
-	    $modules->setHref('index.php?page=modules&subpage=');
-	
-	    $actions = new rex_be_page($I18N->msg('actions'), array('page'=>'modules', 'subpage' => 'actions'));
-	    $actions->setRequiredPermissions('isAdmin');
-	    $actions->setHref('index.php?page=modules&subpage=actions');
-		
-			$REX['PAGES']['modules']->page->addSubPage($modules);
-			$REX['PAGES']['modules']->page->addSubPage($actions);
-		}
-	}
 }
--- redaxo/include/5.0/addons/users/config.inc.php
+++ redaxo/include/5.0/addons/users/config.inc.php
@@ -13,31 +13,24 @@ $mypage = 'users';
 
 if($REX['REDAXO'])
 {
-  $REX['ADDON']['name'][$mypage] = $I18N->msg('user_management');
-  $REX['ADDON']['perm'][$mypage] = 'admin[]';
+  // -------------------- register the addon
+  
+  $page = new rex_be_page($I18N->msg('user_management'), array('page' => 'users'));
+  $page->setRequiredPermissions('isAdmin');
+  
+  $roles = new rex_be_page($I18N->msg('roles'), array('page'=>'users', 'subpage' => ''));
+  $roles->setRequiredPermissions('isAdmin');
+  $roles->setHref('index.php?page=users&subpage=');
+
+  $users = new rex_be_page($I18N->msg('users'), array('page'=>'users', 'subpage' => 'users'));
+  $users->setRequiredPermissions('isAdmin');
+  $users->setHref('index.php?page=users&subpage=users');
+    
+  $page->addSubPage($roles);
+  $page->addSubPage($users);
+  
+  $REX['ADDON']['page'][$mypage] = new rex_be_page_main('system', $page);
   $REX['ADDON']['version'][$mypage] = "1.0";
   $REX['ADDON']['author'][$mypage] = "Jan Kristinus";
   $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-  $REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
-  
-	rex_register_extension('PAGE_CHECKED', 'rex_subpage_users');
-	
-	function rex_subpage_users($params)
-	{
-		global $REX,$I18N;
-	
-		if($REX['PAGES']['users'])
-		{
-		    $roles = new rex_be_page($I18N->msg('roles'), array('page'=>'users', 'subpage' => ''));
-		    $roles->setRequiredPermissions('isAdmin');
-		    $roles->setHref('index.php?page=users&subpage=');
-		
-		    $users = new rex_be_page($I18N->msg('users'), array('page'=>'users', 'subpage' => 'users'));
-		    $users->setRequiredPermissions('isAdmin');
-		    $users->setHref('index.php?page=users&subpage=users');
-		
-			$REX['PAGES']['users']->page->addSubPage($roles);
-			$REX['PAGES']['users']->page->addSubPage($users);
-		}
-	}
 }
