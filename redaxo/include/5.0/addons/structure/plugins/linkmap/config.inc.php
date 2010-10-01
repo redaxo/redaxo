ffd4baeeb99bd09ee752b335ea5845ae55018554
Markus Staab <markus.staab@redaxo.de>
2010-10-01 14:35:15 +0200

    use setHidden instead of css class to set the visibity

--- redaxo/include/5.0/addons/structure/plugins/linkmap/config.inc.php
+++ redaxo/include/5.0/addons/structure/plugins/linkmap/config.inc.php
@@ -14,17 +14,14 @@ $mypage = 'linkmap';
 if ($REX['REDAXO'])
 {
   // $REX['ADDON']['rxid'][$mypage] = '62';
-  $page = new rex_be_page_popup($I18N->msg('linkmap'), array('page' => 'linkmap'));
-  $page->addItemClass('rex-hidden');
+  $page = new rex_be_page_popup($I18N->msg('linkmap'), '', array('page' => 'linkmap'));
+  $page->setHidden(true);
   $page->setRequiredPermissions('hasStructurePerm');
+
   $REX['ADDON']['page'][$mypage] = new rex_be_page_main('system', $page);
-  
-  //$REX['ADDON']['name'][$mypage] = $I18N->msg('linkmap');
-  //$REX['ADDON']['perm'][$mypage] = 'hasStructurePerm';
   $REX['ADDON']['version'][$mypage] = "1.3";
   $REX['ADDON']['author'][$mypage] = "Markus Staab";
   $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-  //$REX['ADDON']['navigation'][$mypage] = array('block'=>'system');
 }
 
 $REX['VARIABLES'][] = 'rex_var_link';
