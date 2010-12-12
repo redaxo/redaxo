2b40840a1b68eb387c9eec8a6b11367882ceb8b4
Markus Staab <markus.staab@redaxo.de>
2010-12-12 01:58:50 +0100

    added dependencies

--- redaxo/include/5.0/addons/cronjob/plugins/article_status/config.inc.php
+++ redaxo/include/5.0/addons/cronjob/plugins/article_status/config.inc.php
@@ -9,17 +9,6 @@
  * @version svn:$Id$
  */
 
-if($REX['REDAXO'])
-{
-  $REX['ADDON']['rxid']['article_status'] = '630';
-  
-  // Credits
-  $REX['ADDON']['version']['article_status'] = '1.0';
-  $REX['ADDON']['author']['article_status'] = 'Gregor Harlan';
-  $REX['ADDON']['supportpage']['article_status'] = 'forum.redaxo.de';
-  
-}
-
 // Config
 $REX['ADDON']['config']['article_status']['from'] = array(
   'field' => 'art_online_from',
