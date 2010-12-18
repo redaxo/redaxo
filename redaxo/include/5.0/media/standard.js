3bf3e7cda606b11c8f491aa2d79b7318a5e13869
Markus Staab <markus.staab@redaxo.de>
2010-12-18 14:52:39 +0100

    moved medialist javascript into mediapool addon

--- redaxo/include/5.0/media/standard.js
+++ redaxo/include/5.0/media/standard.js
@@ -188,67 +188,7 @@ function deleteREXLink(id)
   link.obj.value = "";
 }
 
-function openREXMedialist(id,param)
-{
-  var medialist = 'REX_MEDIALIST_' + id;
-  var mediaselect = 'REX_MEDIALIST_SELECT_' + id;
-  var needle = new getObj(mediaselect);
-  var source = needle.obj;
-  var sourcelength = source.options.length;
-  if ( typeof(param) == 'undefined')
-  {
-    param = '';
-  }
-  for (ii = 0; ii < sourcelength; ii++) {
-    if (source.options[ii].selected) {
-      param += '&subpage=detail&file_name='+ source.options[ii].value;
-      break;
-    }
-  }
-  newPoolWindow('index.php?page=mediapool'+ param +'&opener_input_field='+ medialist);
-}
 
-function viewREXMedialist(id,param)
-{
-  var medialist = 'REX_MEDIALIST_' + id;
-  var mediaselect = 'REX_MEDIALIST_SELECT_' + id;
-  var needle = new getObj(mediaselect);
-  var source = needle.obj;
-  var sourcelength = source.options.length;
-  if ( typeof(param) == 'undefined')
-  {
-    param = '';
-  }
-  for (ii = 0; ii < sourcelength; ii++) {
-    if (source.options[ii].selected) {
-      param += '&subpage=detail&file_name='+ source.options[ii].value;
-      break;
-    }
-  }
-  if(param != '')
-    newPoolWindow('index.php?page=mediapool' + param + '&opener_input_field=' + medialist);
-}
-
-function addREXMedialist(id,params)
-{
-  if (typeof(params) == 'undefined')
-  {
-    params = '';  
-  }
-  newPoolWindow('index.php?page=mediapool&action=media_upload&subpage=add_file&opener_input_field=REX_MEDIALIST_'+id+params);
-}
-
-function deleteREXMedialist(id){
-  deleteREX(id, 'REX_MEDIALIST_', 'REX_MEDIALIST_SELECT_');
-}
-
-function moveREXMedialist(id, direction){
-  moveREX(id, 'REX_MEDIALIST_', 'REX_MEDIALIST_SELECT_', direction);
-}
-
-function writeREXMedialist(id){
-  writeREX(id, 'REX_MEDIALIST_', 'REX_MEDIALIST_SELECT_');
-}
 
 function openREXLinklist(id, param)
 {
