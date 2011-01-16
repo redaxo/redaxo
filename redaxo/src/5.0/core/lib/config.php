bffa3c03d19d93b11a43002c678c4f68118bc5e9
Gregor Harlan <gharlan@web.de>
2011-01-16 15:45:21 +0100

    added rex_core_config

--- redaxo/src/5.0/core/lib/config.php
+++ redaxo/src/5.0/core/lib/config.php
@@ -3,166 +3,160 @@
 /**
  * Class for handling configurations.
  * The configuration is persisted between requests.
- * 
+ *
  * @author staabm
  */
 class rex_config
 {
   /**
-   * Constant for the redaxo core namespace
-   * @var string
-   */
-  const CORE_NS = 'rex-core';
-  
-  /**
    * Flag to indicate if the config was initialized
    * @var boolean
    */
   private static $initialized = false;
-  
+
   /**
    * Flag which indicates if database needs an update, because settings have changed.
    * @var boolean
    */
   private static $changed = false;
-  
+
   /**
    * data read from database
    * @var array
    */
   private static $data = array();
-  
+
   /**
    * data which is modified during this request
    * @var array
    */
   private static $changedData = array();
-  
+
   /**
    * data which was deleted durint this request
    * @var array
    */
   private static $deletedData = array();
-  
+
   /**
    * Method which saves an arbitary value associated to the given key.
    * The value may also be saved into a separate $namespace which preserves it from beeing overriden.
-   * 
+   *
    * The set-methode returns TRUE when an existing value was overridden, otherwise FALSE is returned.
-   * 
+   *
+   * @param string $namespace A namespace e.g. an addon name
    * @param string $key The associated key
    * @param mixed $value The value to save
-   * @param string $namespace A namespace e.g. an addon name
-   * 
+   *
    * @return boolean TRUE when an existing value was overridden, otherwise FALSE
-   * 
+   *
    * @throws rexException on invalid parameters
    */
-  public static function set($key, $value, $namespace = self::CORE_NS)
+  public static function set($namespace, $key, $value)
   {
     self::init();
-    
+
+    if(!is_string($namespace))
+    {
+      throw new rexException('rex_config: expecting $namespace to be a string');
+    }
     if(!is_string($key))
     {
       throw new rexException('rex_config: expecting $key to be a string');
     }
-    if($namespace !== null && !is_string($namespace))
-    {
-      throw new rexException('rex_config: expecting $namespace to be a string');
-    }
-    
+
     if(!isset(self::$data[$namespace]))
       self::$data[$namespace] = array();
-    
+
     $existed = isset(self::$data[$namespace][$key]);
     if(!$existed || $existed && self::$data[$namespace][$key] != $value)
     {
       // keep track of changed data
       if(!isset(self::$changedData[$namespace]))
         self::$changedData[$namespace] = array();
-      self::$changedData[$namespace][$key] = true;
+      self::$changedData[$namespace][$key] = $value;
 
       // since it was re-added, do not longer mark as deleted
       if(isset(self::$deletedData[$namespace]) && isset(self::$deletedData[$namespace][$key]))
         unset(self::$deletedData[$namespace][$key]);
 
-      // re-set the data in the container 
-      self::$data[$namespace][$key] = $value;    
+      // re-set the data in the container
+      self::$data[$namespace][$key] = $value;
       self::$changed = true;
     }
-    
+
     return $existed;
   }
-  
+
   /**
    * Method which returns an associated value for the given key.
    * The key might also be associated to a given namespace.
-   * 
+   *
    * If no value can be found for the given key/namespace combination $default is returned.
-   * 
-   * @param string $key The associated key
+   *
    * @param string $namespace A namespace e.g. an addon name
+   * @param string $key The associated key
    * @param mixed $default Default return value if no associated-value can be found
-   * 
+   *
    * @return the value for $key or $default if $key cannot be found in the given $namespace
-   * 
+   *
    * @throws rexException on invalid parameters
    */
-  public static function get($key, $namespace = self::CORE_NS, $default = null)
+  public static function get($namespace, $key, $default = null)
   {
     self::init();
-    
+
+    if(!is_string($namespace))
+    {
+      throw new rexException('rex_config: expecting $namespace to be a string');
+    }
     if(!is_string($key))
     {
       throw new rexException('rex_config: expecting $key to be a string');
     }
-    if($namespace !== null && !is_string($namespace))
-    {
-      throw new rexException('rex_config: expecting $namespace to be a string');
-    }
-    
+
     if(isset(self::$data[$namespace]) && isset(self::$data[$namespace][$key]))
     {
       return self::$data[$namespace][$key];
     }
     return $default;
   }
-  
+
   /**
    * Removes the setting associated with the given key.
    * The key might also be associated to a given namespace.
-   * 
-   * @param string $key The associated key
+   *
    * @param string $namespace A namespace e.g. an addon name
-   * 
+   * @param string $key The associated key
+   *
    * @return TRUE if the value was found and removed, otherwise FALSE
-   * 
+   *
    * @throws rexException on invalid parameters
    */
-  public static function remove($key, $namespace = self::CORE_NS)
+  public static function remove($namespace, $key)
   {
     self::init();
-    
+
+    if(!is_string($namespace))
+    {
+      throw new rexException('rex_config: expecting $namespace to be a string');
+    }
     if(!is_string($key))
     {
       throw new rexException('rex_config: expecting $key to be a string');
     }
-    if($namespace !== null && !is_string($namespace))
-    {
-      throw new rexException('rex_config: expecting $namespace to be a string');
-    }
-    
+
     if(isset(self::$data[$namespace]) && isset(self::$data[$namespace][$key]))
     {
       // keep track of deleted data
       if(!isset(self::$deletedData[$namespace]))
         self::$deletedData[$namespace] = array();
       self::$deletedData[$namespace][$key] = true;
-      
+
       // since it will be deleted, do not longer mark as changed
       if(isset(self::$changedData[$namespace]) && isset(self::$changedData[$namespace][$key]))
         unset(self::$changedData[$namespace][$key]);
-      
+
       // delete the data from the container
       unset(self::$data[$namespace][$key]);
       self::$changed = true;
@@ -170,51 +164,51 @@ class rex_config
     }
     return false;
   }
-  
+
   /**
    * Removes all settings associated with the given namespace
-   * 
+   *
    * @param string $namespace A namespace e.g. an addon name
-   * 
+   *
    * @return TRUE if the namespace was found and removed, otherwise FALSE
-   * 
+   *
    * @throws rexException
    */
   public static function removeNamespace($namespace)
   {
     self::init();
-    
+
     if(!is_string($namespace))
     {
       throw new rexException('rex_config: expecting $namespace to be a string');
     }
-    
+
     if(isset(self::$data[$namespace]))
     {
       foreach(self::$data[$namespace] as $key => $value)
       {
         self::remove($key, $namespace);
       }
-      
+
       unset(self::$data[$namespace]);
       self::$changed = true;
       return true;
     }
     return false;
   }
-  
+
   /**
    * initilizes the rex_config class
    */
   protected static function init()
   {
     global $REX;
-    
+
     if(self::$initialized)
       return;
-      
+
     define('REX_CONFIG_FILE_CACHE', $REX['INCLUDE_PATH'] .'/generated/files/config.cache');
-    
+
     // take care, so we are able to write a cache file on shutdown
     // (check here, since exceptions in shutdown functions are not visible to the user)
     if(!is_writable(dirname(REX_CONFIG_FILE_CACHE)))
@@ -224,11 +218,11 @@ class rex_config
 
     // save cache on shutdown
     register_shutdown_function(array(__CLASS__, 'save'));
-    
+
     self::load();
     self::$initialized = true;
   }
-  
+
   /**
    * load the config-data
    */
@@ -243,10 +237,10 @@ class rex_config
       self::generateCache();
     }
   }
-  
+
   /**
    * load the config-data from a file-cache
-   * 
+   *
    * @return Returns TRUE, if the data was successfully loaded from the file-cache, otherwise FALSE.
    */
   private static function loadFromFile()
@@ -259,17 +253,17 @@ class rex_config
     }
     return false;
   }
-  
+
   /**
    * load the config-data from database
    */
   private static function loadFromDb()
   {
     global $REX;
-    
+
     $sql = rex_sql::factory();
     $sql->setQuery('SELECT * FROM '. $REX['TABLE_PREFIX']. 'config');
-    
+
     self::$data = array();
     while($sql->hasNext())
     {
@@ -277,7 +271,7 @@ class rex_config
       $sql->next();
     }
   }
-  
+
   /**
    * save config to file-cache
    */
@@ -288,7 +282,7 @@ class rex_config
       throw new rexException('rex-config: unable to write cache file '. REX_CONFIG_FILE_CACHE);
     }
   }
-  
+
   /**
    * persists the config-data and truncates the file-cache
    */
@@ -297,43 +291,43 @@ class rex_config
     // save cache only if changes happened
     if(!self::$changed)
       return;
-      
+
     // after all no data needs to be deleted or update, so skip save
     if(empty(self::$deletedData) && empty(self::$changedData))
       return;
-      
+
     // delete cache-file; will be regenerated on next request
     if(file_exists(REX_CONFIG_FILE_CACHE))
     {
       unlink(REX_CONFIG_FILE_CACHE);
     }
-    
+
     // save all data to the db
     self::saveToDb();
     self::$changed = false;
   }
-  
+
   /**
    * save the config-data into the db
    */
   private static function saveToDb()
   {
     global $REX;
-    
+
     $sql = rex_sql::factory();
     // $sql->debugsql = true;
-    
+
     // remove all deleted data
     foreach(self::$deletedData as $namespace => $nsData)
     {
       foreach($nsData as $key => $value)
       {
         $sql->setTable($REX['TABLE_PREFIX']. 'config');
-        $sql->setWhere('namespace=`' .$namespace.'` and key=`'.  $key .'`');
+        $sql->setWhere('namespace="'. $namespace .'" and `key`="'. $key .'"');
         $sql->delete();
       }
     }
-    
+
     // update all changed data
     foreach(self::$changedData as $namespace => $nsData)
     {
