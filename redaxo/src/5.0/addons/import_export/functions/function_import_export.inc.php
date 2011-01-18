fddbe11e29df81f5097093bcd3a64337d0b14b55
Gregor Harlan <gharlan@web.de>
2011-01-18 19:38:31 +0100

    also replace table prefix in lock statements

--- redaxo/src/5.0/addons/import_export/functions/function_import_export.inc.php
+++ redaxo/src/5.0/addons/import_export/functions/function_import_export.inc.php
@@ -93,7 +93,7 @@ function rex_a1_import_db($filename)
   {
     // Hier case-insensitiv ersetzen, damit alle möglich Schreibweisen (TABLE TablE, tAblE,..) ersetzt werden
     // Dies ist wichtig, da auch SQLs innerhalb von Ein/Ausgabe der Module vom rex-admin verwendet werden
-    $conts = preg_replace('/(TABLE `?)' . preg_quote($prefix, '/') .'/i', '$1'. $REX['TABLE_PREFIX'], $conts);
+    $conts = preg_replace('/(TABLES? `?)' . preg_quote($prefix, '/') .'/i', '$1'. $REX['TABLE_PREFIX'], $conts);
     $conts = preg_replace('/(INTO `?)'  . preg_quote($prefix, '/') .'/i', '$1'. $REX['TABLE_PREFIX'], $conts);
     $conts = preg_replace('/(EXISTS `?)'. preg_quote($prefix, '/') .'/i', '$1'. $REX['TABLE_PREFIX'], $conts);
   }
