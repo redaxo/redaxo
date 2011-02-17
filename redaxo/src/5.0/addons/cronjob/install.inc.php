4d6e8017cd4d5049c4c332c0c8f1592b2e2623ea
Gregor Harlan <gharlan@web.de>
2011-02-17 22:13:30 +0100

    check if log folder already exists

--- redaxo/src/5.0/addons/cronjob/install.inc.php
+++ redaxo/src/5.0/addons/cronjob/install.inc.php
@@ -13,7 +13,8 @@ $error = '';
 
 $log_folder = rex_path::addonData('cronjob');
 
-rex_createDir($log_folder);
+if(!file_exists($log_folder))
+  rex_createDir($log_folder);
 
 if(($state = rex_is_writable($log_folder)) !== true)
   $error .= $state;
