3b5b5ad2ed73d30a4e7340e742e9239452e80536
Gregor Harlan <gharlan@web.de>
2011-03-23 23:40:07 +0100

    don't use global $mypage in EPs because other addons could have changed the variable until the EP is called

--- redaxo/src/5.0/addons/metainfo/functions/function_metainfo.inc.php
+++ redaxo/src/5.0/addons/metainfo/functions/function_metainfo.inc.php
@@ -216,8 +216,7 @@ function a62_extensions_handler($params)
   // additional javascripts
   if($page == 'metainfo' || ($page == 'content' && $mode == 'meta'))
   {
-    rex_register_extension('PAGE_HEADER', function($params) {
-      global $mypage;
+    rex_register_extension('PAGE_HEADER', function($params) use ($mypage) {
       return $params['subject'] ."\n".'  <script src="'. rex_path::addonAssets($mypage, 'metainfo.js', rex_path::RELATIVE) .'" type="text/javascript"></script>';
     });
   }
