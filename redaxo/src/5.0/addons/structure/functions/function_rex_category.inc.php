cb6b16879f164b6780869966e2e1f2eca25f95cf
Markus Staab <markus.staab@redaxo.de>
2011-04-17 16:16:35 +0200

    WIP: simplified rex_category function a bit more

--- redaxo/src/5.0/addons/structure/functions/function_rex_category.inc.php
+++ redaxo/src/5.0/addons/structure/functions/function_rex_category.inc.php
@@ -18,7 +18,7 @@ $KAT = rex_sql::factory();
 // $KAT->debugsql = true;
 $KAT->setQuery("SELECT * FROM ".$REX['TABLE_PREFIX']."article WHERE id=$category_id AND startpage=1 AND clang=$clang");
 
-if ($KAT->getRows()!=1)
+if ($KAT->getRows()!=1 || !$REX['USER']->hasCategoryPerm($category_id))
 {
   // kategorie existiert nicht
   if($category_id != 0)
@@ -30,36 +30,16 @@ if ($KAT->getRows()!=1)
 else
 {
   // kategorie existiert
-
-  $KPATH = explode('|',$KAT->getValue('path'));
-
-  $KATebene = count($KPATH)-1;
-  for ($ii=1;$ii<$KATebene;$ii++)
+  
+  $ooCat = rex_ooCategory::getCategoryById($category_id, $clang);
+  foreach($ooCat->getParentTree() as $parent)
   {
-    $SKAT = rex_sql::factory();
-    $SKAT->setQuery('SELECT * FROM '. $REX['TABLE_PREFIX'] .'article WHERE id='. $KPATH[$ii] .' AND startpage=1 AND clang='. $clang);
-
-    if ($SKAT->getRows()==1)
+    $catid = $parent->getId();
+    if ($REX['USER']->hasCategoryPerm($catid))
     {
-      $catname = str_replace(' ', '&nbsp;', htmlspecialchars($SKAT->getValue('catname')));
-      $catid = $SKAT->getValue('id');
-      if ($REX['USER']->hasCategoryPerm($catid))
-      {
-        $KAToutARR[]['content'] = '<a href="index.php?page=structure&amp;category_id='. $catid .'&amp;clang='. $clang .'"'. rex_tabindex() .'>'. $catname .'</a>';
-      }
+      $catname = str_replace(' ', '&nbsp;', htmlspecialchars($parent->getName()));
+      $KAToutARR[]['content'] = '<a href="index.php?page=structure&amp;category_id='. $catid .'&amp;clang='. $clang .'"'. rex_tabindex() .'>'. $catname .'</a>';
     }
-  }
-
-  if ($REX['USER']->hasCategoryPerm($category_id))
-  {
-    $catname = str_replace(' ', '&nbsp;', htmlspecialchars($KAT->getValue('catname')));
-
-    $KAToutARR[]['content'] = '<a href="index.php?page=structure&amp;category_id='. $category_id .'&amp;clang='. $clang .'"'. rex_tabindex() .'>'. $catname .'</a>';
-  }
-  else
-  {
-    $category_id = 0;
-    $article_id = 0;
   }
 }
 
@@ -69,6 +49,7 @@ $KATout = '
 /*	ul-Liste erstellen */
 $list = array();
 $list[1]['items'] = $KAToutARR;
+unset($KAToutARR);
 
 /*	dl-Liste erstellen  */
 $navi = array();
@@ -79,6 +60,7 @@ $fragment->setVar('list', $navi, false);
 $dl_list = $fragment->parse('core_navi_path');
 //$dl_list = preg_replace('/(?:(?<=\>)|(?<=\/\>))(\s+)(?=\<\/?)/', '', $dl_list);
 unset($fragment);
+unset($navi);
 
 $KATout .= $dl_list;
 $KATout .= '
