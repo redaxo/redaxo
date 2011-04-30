ed228a8559ce3ed606dc758c246776740df26d14
Gregor Harlan <gharlan@web.de>
2011-04-30 14:21:40 +0200

    moved compat vars into compat addon

--- redaxo/src/5.0/addons/compat/plugins/4.x/config.inc.php
+++ redaxo/src/5.0/addons/compat/plugins/4.x/config.inc.php
@@ -1,14 +1,52 @@
 <?php
 
+/**
+ * @deprecated 5.0
+ */
 $REX['INCLUDE_PATH'] = rex_path::src();
+
+/**
+ * @deprecated 5.0
+ */
 $REX['FRONTEND_PATH'] = rex_path::frontend();
+
+/**
+ * @deprecated 5.0
+ */
 $REX['MEDIAFOLDER']   = rex_path::media();
+
+/**
+ * @deprecated 5.0
+ */
 $REX['FRONTEND_FILE'] = 'index.php';
 
 if($REX['REDAXO'])
 {
+  /**
+	 * @deprecated 5.0
+	 */
   $I18N = new i18n($REX['LANG']);
 }
+
+/**
+ * @deprecated 4.2
+ */
+$REX_USER =& $REX["USER"];
+
+/**
+ * @deprecated 4.2
+ */
+$REX_LOGIN = &$REX["LOGIN"];
+
+/**
+ * @deprecated 4.2
+ */
+$article_id =& $REX['ARTICLE_ID'];
+
+/**
+ * @deprecated 4.2
+ */
+$clang =& $REX['CUR_CLANG'];
 
 require_once dirname(__FILE__) .'/functions/function_rex_file.inc.php';
 require_once dirname(__FILE__) .'/functions/function_rex_file.inc.php';
