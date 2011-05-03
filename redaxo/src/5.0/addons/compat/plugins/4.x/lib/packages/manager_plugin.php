f3d61e0af85a9f9a63d92f86f939585bdb733fce
Gregor Harlan <gharlan@web.de>
2011-05-03 10:00:09 +0200

    added compat for $REX['ADDON']

--- redaxo/src/5.0/addons/compat/plugins/4.x/lib/packages/manager_plugin.php
+++ redaxo/src/5.0/addons/compat/plugins/4.x/lib/packages/manager_plugin.php
@@ -2,5 +2,29 @@
 
 class rex_pluginManagerCompat extends rex_pluginManager
 {
+  public function includeFile($file)
+  {
+    global $REX;
 
+    $ADDONsic = $REX['ADDON'];
+    $REX['ADDON'] = array();
+
+    $this->package->includeFile($file, array('REX_USER', 'REX_LOGIN', 'I18N', 'article_id', 'clang'));
+
+    if(isset($REX['ADDON']) && is_array($REX['ADDON']))
+    {
+      foreach($REX['ADDON'] as $property => $propertyArray)
+      {
+        foreach($propertyArray as $pluginName => $value)
+        {
+          if($pluginName == $this->package->getName())
+          {
+            $this->package->setProperty($property, $value);
+            $ADDONsic['plugins'][$this->package->getAddon()->getName()][$property][$pluginName] = $value;
+          }
+        }
+      }
+    }
+    $REX['ADDON'] = $ADDONsic;
+  }
 }
