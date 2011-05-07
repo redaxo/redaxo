656d46b80f5205ad7f99c7436f460d42cc092bf8
tbaddade <thomas@redaxo.de>
2011-05-07 15:24:19 +0200

    edit ep function

--- redaxo/src/5.0/addons/be_style/plugins/classic/config.inc.php
+++ redaxo/src/5.0/addons/be_style/plugins/classic/config.inc.php
@@ -11,7 +11,7 @@
  * @version svn:$Id$
  */
 
-$mypage = 'classic_skin';
+$mypage = 'classic';
 
 $REX['ADDON']['version'][$mypage] = '1.3';
 $REX['ADDON']['author'][$mypage] = 'Umsetzung: Thomas Blum';
@@ -20,27 +20,24 @@ $REX['ADDON']['supportpage'][$mypage] = 'www.redaxo.org/de/forum/';
 if($REX["REDAXO"])
 {
 
-	function rex_be_style_classic_skin_css_add($params)
+	rex_extension::register('PAGE_HEADER', function ($params) use ($mypage)
 	{
 	  $params["subject"] .= '
-    <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', 'classic', 'css_import.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
+    <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', $mypage, 'css_import.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
 	  <!--[if lte IE 7]>
-	      <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', 'classic', 'css_ie_lte_7.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
+	      <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', $mypage, 'css_ie_lte_7.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
 	    <![endif]-->
 	    <!--[if lte IE 6]>
-	      <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', 'classic', 'css_ie_lte_6.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
+	      <link rel="stylesheet" href="'. rex_path::pluginAssets('be_style', $mypage, 'css_ie_lte_6.css', rex_path::RELATIVE) .'" type="text/css" media="screen, projection, print" />
 	    <![endif]-->';
 	  return $params["subject"];
-	}
+	});
 
-	rex_extension::register('PAGE_HEADER', 'rex_be_style_classic_skin_css_add');
 
-	function rex_be_style_classic_skin_css_body($params)
+	rex_extension::register('PAGE_BODY_ATTR', function ($params)
 	{
 	  $params["subject"]["class"][] = "be-style-classic-skin";
 	  return $params["subject"];
-	}
-
-	rex_extension::register('PAGE_BODY_ATTR', 'rex_be_style_classic_skin_css_body');
+	});
 
 }
