40acb1ff220081da19870fc18c8885c95dd2f98a
Markus Staab <markus.staab@redaxo.de>
2011-05-11 21:32:07 +0200

    added prototype of debug extension for rex_sql

--- redaxo/src/5.0/core/master.inc.php
+++ redaxo/src/5.0/core/master.inc.php
@@ -27,6 +27,10 @@ foreach($config as $key => $value)
   $REX[strtoupper($key)] = $value;
 }
 
+// --------- Debug extensions
+rex_sql::setFactoryClass(new rex_sql_debug());
+// rex_extension::setFactoryClass(new rex_extension_debug());
+
 $REX['FILEPERM'] = octdec($REX['FILEPERM']);
 $REX['DIRPERM'] = octdec($REX['DIRPERM']);
 
