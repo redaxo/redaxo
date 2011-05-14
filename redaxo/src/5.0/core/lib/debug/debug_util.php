195fdc890e52319b5fcaa9bd6544439c2acf0577
Markus Staab <markus.staab@redaxo.de>
2011-05-14 23:19:50 +0200

    added docs

--- redaxo/src/5.0/core/lib/debug/debug_util.php
+++ redaxo/src/5.0/core/lib/debug/debug_util.php
@@ -2,6 +2,12 @@
 
 class rex_debug_util {
   
+  /**
+   * Injects some content into the page-content of a backend page
+   *
+   * @param string $toInject the html-string to inject
+   * @param string $pageOutput the source of the current page
+   */
   static public function injectHtml($toInject, $pageOutput)
   {
     return str_replace('<div id="sidebar">', '<div>'. $toInject .'</div><div id="sidebar">', $pageOutput);
