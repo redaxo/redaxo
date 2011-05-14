04d550833adbeb17081b12c384077cac1b3ff001
Markus Staab <markus.staab@redaxo.de>
2011-05-14 23:12:43 +0200

    added precision parameter to timer

--- redaxo/src/5.0/core/lib/util/timer.php
+++ redaxo/src/5.0/core/lib/util/timer.php
@@ -32,12 +32,13 @@ class rex_timer extends rex_singleton
    * Stops the timer and returns the formatted time difference
    *
    * @param int $decimals Number of decimals points
+   * @param int $precision Factor which will be multiplied, for convertion into different units (e.g. 1000 for milli,...)
    *
    * @return string Formatted time difference
    */
-  public function stop($decimals = 3)
+  public function stop($decimals = 3, $precision = 1)
   {
-    $time = microtime(true) - $this->start;
+    $time = (microtime(true) - $this->start) * $precision;
     return rex_formatter::format($time, 'number', array($decimals));
   }
 }
