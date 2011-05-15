5d2b59d5fdd87ff44452b62ebaff0bf4e2faacb6
Gregor Harlan <gharlan@web.de>
2011-05-15 22:46:01 +0200

    added class rex_core

---
+++ redaxo/src/5.0/core/lib/core.php
@@ -0,0 +1,75 @@
+<?php
+
+class rex_core
+{
+  const CONFIG_NAMESPACE = 'rex-core';
+
+  static private $properties = array();
+
+  static public function setConfig($key, $value)
+  {
+    return rex_config::set(self::CONFIG_NAMESPACE, $key, $value);
+  }
+
+  static public function getConfig($key, $default = null)
+  {
+    return rex_config::get(self::CONFIG_NAMESPACE, $key, $default);
+  }
+
+  static public function hasConfig($key)
+  {
+    return rex_config::has(self::CONFIG_NAMESPACE, $key);
+  }
+
+  static public function setProperty($key, $value)
+  {
+    if(!is_string($key))
+    {
+      throw new rexException('Expecting $key to be string, but '. gettype($key) .' given!');
+    }
+    self::$properties[$key] = $value;
+  }
+
+  static public function getProperty($key, $default = null)
+  {
+    if(!is_string($key))
+    {
+      throw new rexException('Expecting $key to be string, but '. gettype($key) .' given!');
+    }
+    if(isset(self::$properties[$key]))
+    {
+      return self::$properties[$key];
+    }
+    return $default;
+  }
+
+  static public function hasProperty($key)
+  {
+    return is_string($key) && isset(self::$properties[$key]);
+  }
+
+  static public function isSetup()
+  {
+    return (boolean) self::getProperty('setup', false);
+  }
+
+  static public function isBackend()
+  {
+    return (boolean) self::getProperty('redaxo', false);
+  }
+
+  static public function getTablePrefix()
+  {
+    return self::getProperty('table_prefix');
+  }
+
+  static public function getTempPrefix()
+  {
+    return self::getProperty('temp_prefix');
+  }
+
+  static public function getUser()
+  {
+    return self::getProperty('user');
+  }
+}
