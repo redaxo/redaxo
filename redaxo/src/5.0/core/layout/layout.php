a9a97ad98005b298a858309e8c7bf7e0dd565f9d
tbaddade <thomas@redaxo.de>
2011-05-24 09:33:54 +0200

    layout tpl

---
+++ redaxo/src/5.0/core/layout/layout.php
@@ -0,0 +1,11 @@
+<?php
+  require rex_path::core('layout/top.php');
+
+  $params = array();
+  $params['key'] = 'test';
+  $fragment = new rex_fragment();
+  $fragment->decorate('layout', $params);
+  echo $fragment->parse('output');
+  unset($fragment);
+
+  require rex_path::core('layout/bottom.php');
