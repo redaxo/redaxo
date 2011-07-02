aae3a4a9cc286ae4ae964ccd299b41203c99ed5c
Markus Staab <markus.staab@redaxo.de>
2011-07-02 17:17:32 +0200

    added helpfile

---
+++ redaxo/src/5.0/addons/debug/help.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Debug Addon
+ *
+ * @author markus.staab[at]redaxo[dot]de Markus Staab
+ *
+ * @package redaxo5
+ * @version svn:$Id$
+ */
+
+?>
+<h3>Debug Addon</h3>
+
+<p>
+Dieses erleichtert die Entwicklung im Backend durch zusätzliche Informationen über <a onclick="window.open(this.href); return false;" href="http://www.firephp.org">FirePHP</a>.
+Dazu ist es nötig, dass die FireFox Erweiterung FirePHP installiert ist. 
+</p>
