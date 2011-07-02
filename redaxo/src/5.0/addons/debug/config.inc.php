3b3f94bc9613fa3c374600488efbf5568f774b6d
Markus Staab <markus.staab@redaxo.de>
2011-07-02 16:31:44 +0200

    added api function debug handler

--- redaxo/src/5.0/addons/debug/config.inc.php
+++ redaxo/src/5.0/addons/debug/config.inc.php
@@ -3,3 +3,4 @@
 rex_sql::setFactoryClass('rex_sql_debug');
 rex_extension::setFactoryClass('rex_extension_debug');
 rex_logger::setFactoryClass('rex_logger_debug');
+rex_api_function::setFactoryClass('rex_api_function_debug');
---
+++ redaxo/src/5.0/addons/debug/lib/api_function_debug.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class rex_api_function_debug extends rex_api_function
+{
+  static public function handleCall()
+  {
+    $apiFunc = self::factory();
+
+    if($apiFunc != null)
+    {
+      $firephp = FirePHP::getInstance(true);
+      $firephp->group(__CLASS__);
+      $firephp->log('called api function "'. get_class(self::factory()) .'"');
+      $firephp->groupEnd();
+    }
+    return parent::handleCall();
+  }
+}
