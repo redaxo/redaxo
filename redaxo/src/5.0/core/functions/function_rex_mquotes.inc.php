6044d010aeb25a945284caef8e25c92893e183ce
Jan Kristinus <jan@kristinus.de>
2011-07-06 19:21:26 +0200

    if magic quotes are on, now stripslahes works with arrays and newline ..

--- redaxo/src/5.0/core/functions/function_rex_mquotes.inc.php
+++ redaxo/src/5.0/core/functions/function_rex_mquotes.inc.php
@@ -9,8 +9,13 @@
 
 if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
 {
-  $_GET = json_decode(stripslashes(json_encode($_GET, JSON_HEX_APOS)), true);
-  $_POST = json_decode(stripslashes(json_encode($_POST, JSON_HEX_APOS)), true);
-  $_COOKIE = json_decode(stripslashes(json_encode($_COOKIE, JSON_HEX_APOS)), true);
-  $_REQUEST = json_decode(stripslashes(json_encode($_REQUEST, JSON_HEX_APOS)), true);
+	function rex_magic_quotes_gpc_strip(&$value)
+	{
+		$value = is_array($value) ? array_map('rex_magic_quotes_gpc_strip', $value) : stripslashes($value);
+		return $value;
+	}
+	$_GET = rex_magic_quotes_gpc_strip($_GET);
+	$_POST = rex_magic_quotes_gpc_strip($_POST);
+	$_COOKIE = rex_magic_quotes_gpc_strip($_COOKIE);
+	$_REQUEST = rex_magic_quotes_gpc_strip($_REQUEST);
 }
