c756bf561852c1181b63523284c4d59059ec248a
Gregor Harlan <gharlan@web.de>
2011-08-27 22:45:33 +0200

    added compat for $REX['HTDOCS_PATH']

--- redaxo/src/5.0/addons/compat/plugins/4.x/config.inc.php
+++ redaxo/src/5.0/addons/compat/plugins/4.x/config.inc.php
@@ -8,7 +8,8 @@ global $REX, $I18N;
 
 $REX = new rex_compat_array;
 
-$REX['INCLUDE_PATH'] = rex_path::version();
+$REX['HTDOCS_PATH']   = rex_path::frontend();
+$REX['INCLUDE_PATH']  = rex_path::version();
 $REX['FRONTEND_PATH'] = rex_path::frontend('', rex_path::ABSOLUTE);
 $REX['MEDIAFOLDER']   = rex_path::media('', rex_path::ABSOLUTE);
 $REX['FRONTEND_FILE'] = 'index.php';
