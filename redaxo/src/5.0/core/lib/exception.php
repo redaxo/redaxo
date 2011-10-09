1d9f6768e3c62fa70e1eaa98c57fc04288ee2708
Gregor Harlan <gharlan@web.de>
2011-10-09 21:19:25 +0200

    changed default code for rex_exceptions to E_USER_ERROR

--- redaxo/src/5.0/core/lib/exception.php
+++ redaxo/src/5.0/core/lib/exception.php
@@ -1,5 +1,11 @@
 <?php
 
-class rex_exception extends Exception{}
+class rex_exception extends Exception
+{
+  public function __construct($message, $code = E_USER_ERROR, Exception $previous = null)
+  {
+    parent::__construct($message, $code, $previous);
+  }
+}
 
 class rex_sql_exception extends rex_exception{}
