bb032451e4030731505a96ae1d75d852c6e8df64
Gregor Harlan <gharlan@web.de>
2011-10-31 00:18:12 +0100

    changed some i18n values

--- redaxo/src/5.0/addons/install/plugins/packages/pages/update.inc.php
+++ redaxo/src/5.0/addons/install/plugins/packages/pages/update.inc.php
@@ -70,7 +70,7 @@ else
   			<th>'. rex_i18n::msg('install_packages_key') .'</th>
   			<th>'. rex_i18n::msg('install_packages_name') .'</th>
   			<th>'. rex_i18n::msg('install_packages_existing_version') .'</th>
-  			<th>'. rex_i18n::msg('install_packages_available_version') .'</th>
+  			<th>'. rex_i18n::msg('install_packages_available_versions') .'</th>
   		</tr>';
 
   foreach($addons as $key => $addon)
