597f256d0c08d65b5e92bb89e97aecc6682ae4a4
Gregor Harlan <gharlan@web.de>
2011-12-04 11:52:01 +0100

    changed class order, base class must be on top

--- redaxo/src/addons/install/plugins/packages/lib/packages_api.php
+++ redaxo/src/addons/install/plugins/packages/lib/packages_api.php
@@ -1,5 +1,67 @@
 <?php
 
+abstract class rex_api_install_packages_base extends rex_api_function
+{
+  protected
+    $addonkey,
+    $archive;
+
+  public function execute()
+  {
+    $message = '';
+    $this->addonkey = rex_request('addonkey', 'string');
+    if(!$this->checkPreConditions())
+    {
+      return null;
+    }
+    $archivefile = rex_install_webservice::getArchive(rex_request('file', 'string'));
+    if(!$archivefile)
+    {
+      $message = rex_i18n::msg('install_packages_warning_zip_not_found');
+    }
+    else
+    {
+      $this->archive = "phar://$archivefile/". $this->addonkey;
+      if(!file_exists($this->archive))
+      {
+        $message = rex_i18n::msg('install_packages_warning_zip_wrong_format');
+      }
+      elseif(is_string($msg = $this->doAction()))
+      {
+        $message = $msg;
+      }
+      rex_file::delete($archivefile);
+    }
+    if($message)
+    {
+      $message = rex_i18n::msg('install_packages_warning_not_'. static::VERB, $this->addonkey) .'<br />'. $message;
+      $success = false;
+    }
+    else
+    {
+      $message = rex_i18n::msg('install_packages_info_addon_'. static::VERB, $this->addonkey)
+               . (static::SHOW_LINK ? ' <a href="index.php?page=addon">'. rex_i18n::msg('install_packages_to_addon_page') .'</a>' : '');
+      $success = true;
+      unset($_REQUEST['addonkey']);
+    }
+    return new rex_api_result($success, $message);
+  }
+
+  protected function extractArchiveTo($dir)
+  {
+    if(!rex_dir::copy($this->archive, $dir))
+    {
+      rex_dir::delete($dir);
+      return rex_i18n::msg('install_packages_warning_zip_not_extracted');
+    }
+    return true;
+  }
+
+  abstract protected function checkPreConditions();
+
+  abstract protected function doAction();
+}
+
 class rex_api_install_packages_download extends rex_api_install_packages_base
 {
   const
@@ -151,66 +213,4 @@ class rex_api_install_packages_update extends rex_api_install_packages_base
   {
     rex_dir::delete(rex_path::addon('_new_'. $this->addonkey));
   }
-}
-
-abstract class rex_api_install_packages_base extends rex_api_function
-{
-  protected
-    $addonkey,
-    $archive;
-
-  public function execute()
-  {
-    $message = '';
-    $this->addonkey = rex_request('addonkey', 'string');
-    if(!$this->checkPreConditions())
-    {
-      return null;
-    }
-    $archivefile = rex_install_webservice::getArchive(rex_request('file', 'string'));
-    if(!$archivefile)
-    {
-      $message = rex_i18n::msg('install_packages_warning_zip_not_found');
-    }
-    else
-    {
-      $this->archive = "phar://$archivefile/". $this->addonkey;
-      if(!file_exists($this->archive))
-      {
-        $message = rex_i18n::msg('install_packages_warning_zip_wrong_format');
-      }
-      elseif(is_string($msg = $this->doAction()))
-      {
-        $message = $msg;
-      }
-      rex_file::delete($archivefile);
-    }
-    if($message)
-    {
-      $message = rex_i18n::msg('install_packages_warning_not_'. static::VERB, $this->addonkey) .'<br />'. $message;
-      $success = false;
-    }
-    else
-    {
-      $message = rex_i18n::msg('install_packages_info_addon_'. static::VERB, $this->addonkey)
-               . (static::SHOW_LINK ? ' <a href="index.php?page=addon">'. rex_i18n::msg('install_packages_to_addon_page') .'</a>' : '');
-      $success = true;
-      unset($_REQUEST['addonkey']);
-    }
-    return new rex_api_result($success, $message);
-  }
-
-  protected function extractArchiveTo($dir)
-  {
-    if(!rex_dir::copy($this->archive, $dir))
-    {
-      rex_dir::delete($dir);
-      return rex_i18n::msg('install_packages_warning_zip_not_extracted');
-    }
-    return true;
-  }
-
-  abstract protected function checkPreConditions();
-
-  abstract protected function doAction();
 }
