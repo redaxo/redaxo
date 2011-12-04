261988437ca60bf772100ad309a70792c534e60d
Gregor Harlan <gharlan@web.de>
2011-12-04 10:36:11 +0100

    added test for rex_version_compare()

--- redaxo/src/addons/tests/lib/tests/core/functions/rex_func_other_test.php
+++ redaxo/src/addons/tests/lib/tests/core/functions/rex_func_other_test.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class rex_func_other_test extends PHPUnit_Framework_TestCase
 {
@@ -6,7 +6,7 @@ class rex_func_other_test extends PHPUnit_Framework_TestCase
   {
     parent::setUp();
   }
-  
+
   public function tearDown()
   {
     parent::tearDown();
@@ -16,11 +16,31 @@ class rex_func_other_test extends PHPUnit_Framework_TestCase
   {
     $s1 = 'a b c';
     $this->assertEquals(array('a', 'b', 'c'), rex_split_string($s1), 'splits string by space');
-    
+
     $s1 = '"a b" cdef \'ghi kl\'';
     $this->assertEquals(array('a b', 'cdef', 'ghi kl'), rex_split_string($s1), 'supports quoted strings');
-    
+
     $s1 = 'a=1 b=xyz c="hu hu" 123=\'he he\'';
     $this->assertEquals(array('a' => 1, 'b' => 'xyz', 'c' => 'hu hu', '123' => 'he he'), rex_split_string($s1), 'supports key value pairs');
+  }
+
+  public function testVersionCompare()
+  {
+    $this->assertTrue(rex_version_compare('1', '1', '='), '1 is equal to 1');
+    $this->assertTrue(rex_version_compare('1.0', '1.0', '='), '1.0 is equal to 1.0');
+    $this->assertTrue(rex_version_compare('1', '1.0', '='), '1 is equal to 1.0');
+    $this->assertTrue(rex_version_compare('1.0 a1', '1.0.a1', '='), '1.0 a1 is equal to 1.0.a1');
+
+    $this->assertTrue(rex_version_compare('1', '2', '<'), '1 is less than 2');
+    $this->assertTrue(rex_version_compare('1', '1.1', '<'), '1 is less than 1.1');
+    $this->assertTrue(rex_version_compare('1.0', '1.1', '<'), '1.0 is less than 1.1');
+    $this->assertTrue(rex_version_compare('1.1', '1.2', '<'), '1.1 is less than 1.2');
+    $this->assertTrue(rex_version_compare('1.2', '1.10', '<'), '1.2 is less than 1.10');
+    $this->assertTrue(rex_version_compare('1.a1', '1', '<'), '1.a1 is less than 1');
+    $this->assertTrue(rex_version_compare('1.a1', '1.0', '<'), '1.a1 is less than 1.0');
+    $this->assertTrue(rex_version_compare('1.a1', '1.a2', '<'), '1.a1 is less than 1.a2');
+    $this->assertTrue(rex_version_compare('1.a1', '1.b1', '<'), '1.a1 is less than 1.b1');
+    $this->assertTrue(rex_version_compare('1.0.a1', '1', '<'), '1.0.a1 is less than 1');
+    $this->assertTrue(rex_version_compare('1.0.a1', '1.0.0.0', '<'), '1.0.a1 is less than 1.0.0.0');
   }
 }
