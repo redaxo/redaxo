3af8f32a3ee076da508cfea54b91f4ee7dcb46eb
Gregor Harlan <gharlan@web.de>
2011-12-08 17:02:54 +0100

    added rex_functional_exception for user-friendly error messages

--- redaxo/src/core/lib/exception.php
+++ redaxo/src/core/lib/exception.php
@@ -8,10 +8,9 @@ class rex_exception extends Exception
   }
 }
 
-class rex_sql_exception extends rex_exception
-{
-  public function __construct($message, $code = E_USER_ERROR, Exception $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
-}
+class rex_sql_exception extends rex_exception {}
+
+/**
+ * Exception class for user-friendly error messages
+ */
+class rex_functional_exception extends rex_exception {}
