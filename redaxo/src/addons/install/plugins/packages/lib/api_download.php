09ce2a7eb85d0a049747b8c23b42ea0ea08523db
Gregor Harlan <gharlan@web.de>
2011-12-16 00:20:32 +0100

    fixed archive path

--- redaxo/src/addons/install/plugins/packages/lib/api_download.php
+++ redaxo/src/addons/install/plugins/packages/lib/api_download.php
@@ -197,7 +197,7 @@ class rex_api_install_packages_update extends rex_api_install_packages_download
     $path = rex_path::addon($this->addonkey);
     $archivePath = rex_path::pluginData('install', 'packages', $this->addonkey .'/');
     rex_dir::create($archivePath);
-    $archive = rex_path::pluginData('install', 'packages', 'developer/'. strtolower(preg_replace("/[^a-z0-9-_.]/i", "_", $this->addon->getVersion('0'))) .'.zip');
+    $archive = $archivePath . strtolower(preg_replace("/[^a-z0-9-_.]/i", "_", $this->addon->getVersion('0'))) .'.zip';
     rex_install_helper::copyDirToArchive($path, $archive);
     $assets = $this->addon->getAssetsPath('', rex_path::ABSOLUTE);
     if(is_dir($assets))
