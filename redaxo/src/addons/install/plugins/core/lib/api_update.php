ba6e1e03c80bc864395b7713958459b4dc2f635b
Gregor Harlan <gharlan@web.de>
2011-12-23 00:26:20 +0100

    added core update functionality

---
+++ redaxo/src/addons/install/plugins/core/lib/api_update.php
@@ -0,0 +1,90 @@
+<?php
+
+class rex_api_install_core_update extends rex_api_function
+{
+  static public function getVersions()
+  {
+    return rex_install_webservice::getJson('core');
+  }
+
+  public function execute()
+  {
+    if(!rex::getUser()->isAdmin())
+    {
+      throw new rex_api_exception('You do not have the permission!');
+    }
+    $plugin = rex_plugin::get('install', 'core');
+    $versions = self::getVersions();
+    $versionId = rex_request('version_id', 'int');
+    if(!isset($versions[$versionId]))
+    {
+      return null;
+    }
+    $version = $versions[$versionId];
+    if(!rex_version_compare($version['version'], rex::getVersion(), '>'))
+    {
+      throw new rex_api_exception(sprintf('Existing version of Core (%s) is newer than %s', rex::getVersion(), $version['version']));
+    }
+    try
+    {
+      $archivefile = rex_install_webservice::getArchive($version['path']);
+    }
+    catch(rex_functional_exception $e)
+    {
+      throw new rex_api_exception($e->getMessage());
+    }
+    $message = '';
+    $archive = "phar://$archivefile/core";
+    $temppath = rex_path::src('_new_core/');
+    if($version['checksum'] != md5_file($archivefile))
+    {
+      $message = $plugin->i18n('warning_zip_wrong_checksum');
+    }
+    elseif(!file_exists($archive))
+    {
+      $message = $plugin->i18n('warning_zip_wrong_format');
+    }
+    elseif(!rex_dir::copy($archive, $temppath))
+    {
+      $message = $plugin->i18n('warning_zip_not_extracted');
+    }
+    elseif(file_exists($temppath . 'update.inc.php'))
+    {
+      try
+      {
+        include $temppath . 'update.inc.php';
+      }
+      catch(rex_functional_exception $e)
+      {
+        $message = $e->getMessage();
+      }
+    }
+    rex_file::delete($archivefile);
+    if(!$message)
+    {
+      $path = rex_path::core();
+      rex_dir::create($plugin->getDataPath());
+      $archive = $plugin->getDataPath(strtolower(preg_replace("/[^a-z0-9-_.]/i", "_", rex::getVersion())) .'.zip');
+      rex_install_helper::copyDirToArchive($path, $archive);
+      rex_dir::delete($path);
+      rename($temppath, $path);
+      if(is_dir(rex_path::core('assets')))
+      {
+        rex_dir::copy(rex_path::core('assets'), rex_path::assets('', rex_path::ABSOLUTE));
+      }
+    }
+    rex_dir::delete($temppath);
+    if($message)
+    {
+      $message = $plugin->i18n('warning_not_updated') .'<br />'. $message;
+      $success = false;
+    }
+    else
+    {
+      $message = $plugin->i18n('info_updated');
+      $success = true;
+      rex_install_webservice::deleteCache('core');
+    }
+    return new rex_api_result($success, $message);
+  }
+}
--- redaxo/src/addons/install/plugins/core/pages/index.inc.php
+++ redaxo/src/addons/install/plugins/core/pages/index.inc.php
@@ -1,3 +1,52 @@
 <?php
 
-echo 'Core';
+$versions = array();
+
+$apiFunc = rex_api_function::factory();
+if($apiFunc && ($result = $apiFunc->getResult()) && $result->isSuccessfull())
+{
+  header('Location: index.php?page=install&subpage=core&info='. urlencode($result->getMessage()));
+  exit;
+}
+
+if($info = rex_get('info', 'string'))
+  echo rex_view::info($info);
+else
+  echo rex_api_function::getMessage();
+
+try
+{
+  $versions = rex_api_install_core_update::getVersions();
+}
+catch(rex_functional_exception $e)
+{
+  echo rex_view::warning($e->getMessage());
+}
+
+echo '
+  <div class="rex-area">
+  	<h2 class="rex-hl2">'. $this->i18n('available_updates', count($versions)) .'</h2>
+  	<table class="rex-table">
+  		<tr>
+  			<th class="rex-icon"></th>
+  			<th>'. $this->i18n('version') .'</th>
+  			<th>'. $this->i18n('description') .'</th>
+  			<th></th>
+  		</tr>';
+
+foreach($versions as $id => $version)
+{
+  $a = '<a%s href="index.php?page=install&amp;subpage=core&amp;version_id='. $id .'">%s</a>';
+  echo '
+    	<tr>
+    		<td class="rex-icon"><span class="rex-i-element rex-i-addon"><span class="rex-i-element-text">'. $version['version'] .'</span></span></td>
+    		<td>'. $version['version'] .'</td>
+    		<td>'. nl2br($version['description']) .'</td>
+    		<td><a href="index.php?page=install&amp;subpage=core&amp;rex-api-call=install_core_update&amp;version_id='. $id .'">'. $this->i18n('update') .'</a></td>
+    	</tr>';
+}
+
+echo '
+  	</table>
+  </div>
+  ';
