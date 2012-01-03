5e9b4ef00f16f97a0cceb29dae6630ed7b967055
Gregor Harlan <gharlan@web.de>
2012-01-03 18:24:22 +0100

    improved performance of creating archives significantly

--- redaxo/src/addons/install/lib/helper.php
+++ redaxo/src/addons/install/lib/helper.php
@@ -4,23 +4,22 @@ class rex_install_helper
 {
   static public function copyDirToArchive($dir, $archive, $basename = null)
   {
-    $basename = $basename ?: basename($dir);
     $archive = new PharData($archive, 0, null, Phar::ZIP);
-    $archive->addEmptyDir($basename);
-    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::SELF_FIRST);
+    $iterator = rex_dir::recursiveIterator($dir);
     $iterator->excludeVersionControl();
     $iterator->excludeTemporaryFiles();
-    foreach($iterator as $path => $file)
+    if($basename)
     {
-      $path = $basename . DIRECTORY_SEPARATOR . str_replace($dir, '', $path);
-      if($file->isDir())
+      $array = array();
+      foreach($iterator as $path => $file)
       {
-        $archive->addEmptyDir($path);
+        $array[$basename .'/'. str_replace($dir, '', $path)] = $path;
       }
-      else
-      {
-        $archive->addFile($file->getRealPath(), $path);
-      }
+      $archive->buildFromIterator(new ArrayIterator($array));
+    }
+    else
+    {
+      $archive->buildFromIterator($iterator, dirname($dir));
     }
     $archive->compressFiles(Phar::GZ);
   }
