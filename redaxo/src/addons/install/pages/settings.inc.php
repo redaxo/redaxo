41ce7d4bd969c73401e0fb16cbf13d6dbe521aec
Gregor Harlan <gharlan@web.de>
2012-01-04 17:02:46 +0100

    fixed indention

--- redaxo/src/addons/install/pages/settings.inc.php
+++ redaxo/src/addons/install/pages/settings.inc.php
@@ -16,29 +16,29 @@ if(!empty($settings))
 
 echo '
   <div class="rex-form">
-  	<h2 class="rex-hl2">'. $this->i18n('settings') .'</h2>
-  	<form action="index.php?page=install&amp;subpage=settings" method="post">
-  		<fieldset class="rex-form-col-1">
-  			<legend>'. $this->i18n('myredaxo_account') .'</legend>
-  			<div class="rex-form-wrapper">
-  				<div class="rex-form-row">
+    <h2 class="rex-hl2">'. $this->i18n('settings') .'</h2>
+    <form action="index.php?page=install&amp;subpage=settings" method="post">
+      <fieldset class="rex-form-col-1">
+        <legend>'. $this->i18n('myredaxo_account') .'</legend>
+        <div class="rex-form-wrapper">
+          <div class="rex-form-row">
             <p class="rex-form-col-a rex-form-text">
-            	<label for="install-packages-settings-api-login">'. $this->i18n('api_login') .'</label>
-          		<input id="install-packages-settings-api-login" class="rex-form-text" type="text" name="settings[api_login]" value="'. $this->getConfig('api_login') .'" />
+              <label for="install-settings-api-login">'. $this->i18n('api_login') .'</label>
+              <input id="install-settings-api-login" class="rex-form-text" type="text" name="settings[api_login]" value="'. $this->getConfig('api_login') .'" />
             </p>
           </div>
-  				<div class="rex-form-row">
+          <div class="rex-form-row">
             <p class="rex-form-col-a rex-form-text">
-            	<label for="install-packages-settings-api-key">'. $this->i18n('api_key') .'</label>
-          		<input id="install-packages-settings-api-key" class="rex-form-text" type="text" name="settings[api_key]" value="'. $this->getConfig('api_key') .'" />
+              <label for="install-settings-api-key">'. $this->i18n('api_key') .'</label>
+              <input id="install-settings-api-key" class="rex-form-text" type="text" name="settings[api_key]" value="'. $this->getConfig('api_key') .'" />
             </p>
           </div>
-  				<div class="rex-form-row">
-     				<p class="rex-form-col-a rex-form-submit rex-form-submit-2">
-     					<input id="install-packages-settings-save" type="submit" name="settings[save]" class="rex-form-submit" value="'. rex_i18n::msg('form_save') .'" />
-						</p>
+          <div class="rex-form-row">
+            <p class="rex-form-col-a rex-form-submit rex-form-submit-2">
+            	<input id="install-settings-save" type="submit" name="settings[save]" class="rex-form-submit" value="'. rex_i18n::msg('form_save') .'" />
+            </p>
           </div>
-  			</div>
-  		</fieldset>
-  	</form>
+        </div>
+      </fieldset>
+    </form>
   </div>';
