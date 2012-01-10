821066042883694fa41b8474759d8be3f8ff25a6
Gregor Harlan <gharlan@web.de>
2012-01-10 12:02:42 +0100

    fixed copyDirToArchive()

--- redaxo/src/addons/install/lib/helper.php
+++ redaxo/src/addons/install/lib/helper.php
@@ -5,7 +5,7 @@ class rex_install_helper
   static public function copyDirToArchive($dir, $archive, $basename = null)
   {
     $archive = new PharData($archive, 0, null, Phar::ZIP);
-    $iterator = rex_dir::recursiveIterator($dir)->excludeVersionControl()->excludeTemporaryFiles();
+    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::LEAVES_ONLY)->excludeVersionControl()->excludeTemporaryFiles();
     if($basename)
     {
       $array = array();
