3a6f7ad00b1efb09bf7aeb9629871e286e537b6b
Gregor Harlan <gharlan@web.de>
2012-01-17 17:17:58 +0100

    fixed problem with empty files in archives

--- redaxo/src/addons/install/lib/helper.php
+++ redaxo/src/addons/install/lib/helper.php
@@ -4,21 +4,25 @@ class rex_install_helper
 {
   static public function copyDirToArchive($dir, $archive, $basename = null)
   {
-    $archive = new PharData($archive, 0, null, Phar::ZIP);
-    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::LEAVES_ONLY)->excludeVersionControl()->excludeTemporaryFiles();
-    if($basename)
+    $dir = rtrim($dir, '/\\');
+    $basename = $basename ?: basename($dir);
+    rex_dir::create(dirname($archive));
+    $phar = new PharData($archive, 0, null, Phar::ZIP);
+    $files = array();
+    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::LEAVES_ONLY, RecursiveDirectoryIterator::CURRENT_AS_PATHNAME);
+    $iterator->excludeVersionControl()->excludeTemporaryFiles();
+    foreach($iterator as $path => $file)
     {
-      $array = array();
-      foreach($iterator as $path => $file)
+      $files[str_replace($dir, $basename, $path)] = $path;
+    }
+    $phar->buildFromIterator(new ArrayIterator($files));
+    $phar->compressFiles(Phar::GZ);
+    foreach($files as $path => $realpath)
+    {
+      if(filesize($realpath) == 0)
       {
-        $array[$basename .'/'. str_replace($dir, '', $path)] = $path;
+        $phar[$path]->decompress();
       }
-      $archive->buildFromIterator(new ArrayIterator($array));
     }
-    else
-    {
-      $archive->buildFromIterator($iterator, dirname($dir));
-    }
-    $archive->compressFiles(Phar::GZ);
   }
 }
