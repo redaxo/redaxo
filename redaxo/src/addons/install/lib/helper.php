d961e53f96823996dc0834195c2b62faacb39393
Gregor Harlan <gharlan@web.de>
2012-01-17 20:29:48 +0100

    removed unused parameter

--- redaxo/src/addons/install/lib/helper.php
+++ redaxo/src/addons/install/lib/helper.php
@@ -9,7 +9,7 @@ class rex_install_helper
     rex_dir::create(dirname($archive));
     $phar = new PharData($archive, 0, null, Phar::ZIP);
     $files = array();
-    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::LEAVES_ONLY, RecursiveDirectoryIterator::CURRENT_AS_PATHNAME);
+    $iterator = rex_dir::recursiveIterator($dir, rex_dir_recursive_iterator::LEAVES_ONLY);
     $iterator->excludeVersionControl()->excludeTemporaryFiles();
     foreach($iterator as $path => $file)
     {
