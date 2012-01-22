32e2c701fb77a62a920d5f9bca2665c7e526599c
Gregor Harlan <gharlan@web.de>
2012-01-22 15:08:27 +0100

    changed default skin

--- redaxo/src/core/master.inc.php
+++ redaxo/src/core/master.inc.php
@@ -55,7 +55,7 @@ foreach($config as $key => $value)
 date_default_timezone_set(rex::getProperty('timezone', 'Europe/Berlin'));
 
 // ----------------- OTHER STUFF
-rex::setProperty('setup_packages', array('be_style', 'be_style/agk_skin'));
+rex::setProperty('setup_packages', array('be_style', 'be_style/redaxo'));
 rex::setProperty('system_packages', array('modules', 'templates', 'mediapool', 'structure', 'structure/content', 'structure/linkmap', 'import_export', 'metainfo', 'be_search', 'be_style', 'be_style/agk_skin', 'media_manager', 'users', 'install', 'install/core', 'install/packages'));
 
 // ----------------- REX PERMS
