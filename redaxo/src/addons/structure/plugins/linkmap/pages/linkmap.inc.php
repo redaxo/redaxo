2c470c8669ac95d7e6254d8655ac75e35be16001
Markus Staab <markus.staab@redaxo.de>
2012-01-29 16:57:24 +0100

    use rex_view::title instead of rex_title

--- redaxo/src/addons/structure/plugins/linkmap/pages/linkmap.inc.php
+++ redaxo/src/addons/structure/plugins/linkmap/pages/linkmap.inc.php
@@ -100,7 +100,7 @@ if ($category)
 $navi_path .= '</ul>';
 
 //rex_title(rex::getProperty('servername'), 'Linkmap');
-rex_title('Linkmap', $navi_path);
+rex_view::title('Linkmap', $navi_path);
 
 ?>
 
