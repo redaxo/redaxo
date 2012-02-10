5b9599b3120e4e2d516960fec7316abd0b55df5a
Gregor Harlan <gharlan@web.de>
2012-02-11 00:05:38 +0100

    use dataProvider annotation

--- redaxo/src/addons/tests/lib/tests/core/util/rex_string_test.php
+++ redaxo/src/addons/tests/lib/tests/core/util/rex_string_test.php
@@ -2,19 +2,21 @@
 
 class rex_string_test extends PHPUnit_Framework_TestCase
 {
-  public function testSplit()
+  public function splitProvider()
   {
-    $s = 'a b c';
-    $a = array('a', 'b', 'c');
-    $this->assertEquals($a, rex_string::split($s), 'splits string by space');
+    return array(
+      array('a b c',                             array('a', 'b', 'c')),
+      array('"a b" cdef \'ghi kl\'',             array('a b', 'cdef', 'ghi kl')),
+      array('a=1 b=xyz c="hu hu" 123=\'he he\'', array('a' => 1, 'b' => 'xyz', 'c' => 'hu hu', '123' => 'he he'))
+    );
+  }
 
-    $s = '"a b" cdef \'ghi kl\'';
-    $a = array('a b', 'cdef', 'ghi kl');
-    $this->assertEquals($a, rex_string::split($s), 'supports quoted strings');
-
-    $s = 'a=1 b=xyz c="hu hu" 123=\'he he\'';
-    $a = array('a' => 1, 'b' => 'xyz', 'c' => 'hu hu', '123' => 'he he');
-    $this->assertEquals($a, rex_string::split($s), 'supports key value pairs');
+  /**
+   * @dataProvider splitProvider
+   */
+  public function testSplit($string, $expectedArray)
+  {
+    $this->assertEquals($expectedArray, rex_string::split($string));
   }
 
   public function testSize()
@@ -22,30 +24,40 @@ class rex_string_test extends PHPUnit_Framework_TestCase
     $this->assertEquals(3, rex_string::size('aä'));
   }
 
-  public function testCompareVersions()
+  public function compareVersionsProvider()
   {
-    $this->assertTrue(rex_string::compareVersions('1', '1', '='), '1 is equal to 1');
-    $this->assertTrue(rex_string::compareVersions('1.0', '1.0', '='), '1.0 is equal to 1.0');
-    $this->assertTrue(rex_string::compareVersions('1', '1.0', '='), '1 is equal to 1.0');
-    $this->assertTrue(rex_string::compareVersions('1.0 a1', '1.0.a1', '='), '1.0 a1 is equal to 1.0.a1');
-    $this->assertTrue(rex_string::compareVersions('1.0a1', '1.0.a1', '='), '1.0a1 is equal to 1.0.a1');
-    $this->assertTrue(rex_string::compareVersions('1.0 alpha 1', '1.0.a1', '='), '1.0 alpha 1 is equal to 1.0.a1');
+    return array(
+      array('1',      '1',      '='),
+      array('1.0',    '1.0',    '='),
+      array('1',      '1.0',    '='),
+      array('1.0 a1', '1.0.a1', '='),
+      array('1.0a1',  '1.0.a1', '='),
+      array('1.0 alpha 1', '1.0.a1', '='),
 
-    $this->assertTrue(rex_string::compareVersions('1', '2', '<'), '1 is less than 2');
-    $this->assertTrue(rex_string::compareVersions('1', '1.1', '<'), '1 is less than 1.1');
-    $this->assertTrue(rex_string::compareVersions('1.0', '1.1', '<'), '1.0 is less than 1.1');
-    $this->assertTrue(rex_string::compareVersions('1.1', '1.2', '<'), '1.1 is less than 1.2');
-    $this->assertTrue(rex_string::compareVersions('1.2', '1.10', '<'), '1.2 is less than 1.10');
-    $this->assertTrue(rex_string::compareVersions('1.a1', '1', '<'), '1.a1 is less than 1');
-    $this->assertTrue(rex_string::compareVersions('1.a1', '1.0', '<'), '1.a1 is less than 1.0');
-    $this->assertTrue(rex_string::compareVersions('1.a1', '1.a2', '<'), '1.a1 is less than 1.a2');
-    $this->assertTrue(rex_string::compareVersions('1.a1', '1.b1', '<'), '1.a1 is less than 1.b1');
-    $this->assertTrue(rex_string::compareVersions('1.0.a1', '1', '<'), '1.0.a1 is less than 1');
-    $this->assertTrue(rex_string::compareVersions('1.0.a1', '1.0.0.0', '<'), '1.0.a1 is less than 1.0.0.0');
-    $this->assertTrue(rex_string::compareVersions('1.0a1', '1.0', '<'), '1.0a1 is less than 1.0');
-    $this->assertTrue(rex_string::compareVersions('1.0a1', '1.0.1', '<'), '1.0a1 is less than 1.0.1');
-    $this->assertTrue(rex_string::compareVersions('1.0a1', '1.0a2', '<'), '1.0a1 is less than 1.0a2');
-    $this->assertTrue(rex_string::compareVersions('1.0', '1.1a1', '<'), '1.0 is less than 1.1a1');
-    $this->assertTrue(rex_string::compareVersions('1.0.1', '1.1a1', '<'), '1.0.1 is less than 1.1a1');
+      array('1',      '2',        '<'),
+      array('1',      '1.1',      '<'),
+      array('1.0',    '1.1',      '<'),
+      array('1.1',    '1.2',      '<'),
+      array('1.2',    '1.10',     '<'),
+      array('1.a1',   '1',        '<'),
+      array('1.a1',   '1.0',      '<'),
+      array('1.a1',   '1.a2',     '<'),
+      array('1.a1',   '1.b1',     '<'),
+      array('1.0.a1', '1',        '<'),
+      array('1.0.a1', '1.0.0.0.', '<'),
+      array('1.0a1',  '1.0',      '<'),
+      array('1.0a1',  '1.0.1',    '<'),
+      array('1.0a1',  '1.0a2',    '<'),
+      array('1.0',    '1.1a1',    '<'),
+      array('1.0.1',  '1.1a1',    '<')
+    );
+  }
+
+  /**
+  * @dataProvider compareVersionsProvider
+  */
+  public function testCompareVersions($version1, $version2, $comparator)
+  {
+    $this->assertTrue(rex_string::compareVersions($version1, $version2, $comparator));
   }
 }
