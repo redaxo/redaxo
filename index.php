63f8c434c15eb48074f5bb34b727f8e775a76557
Markus Staab <markus.staab@redaxo.de>
2012-05-09 22:28:59 +0200

    added fe/be switch to frontend index.php

--- index.php
+++ index.php
@@ -6,3 +6,12 @@ $REX['HTDOCS_PATH'] = './';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 
 include $REX['BACKEND_FOLDER'] .'/src/core/master.inc.php';
+
+if(rex::isBackend())
+{
+  require rex_path::core('index_be.inc.php');
+}
+else
+{
+  require rex_path::core('index_fe.inc.php');
+}
