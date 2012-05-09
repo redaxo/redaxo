917141c689022d8e69517e398d12947144b753d3
Markus Staab <markus.staab@redaxo.de>
2012-05-09 22:39:12 +0200

    simplified index.php files

--- redaxo/index.php
+++ redaxo/index.php
@@ -5,13 +5,6 @@ $REX['REDAXO'] = true;
 $REX['HTDOCS_PATH'] = '../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 
-include 'src/core/master.inc.php';
+require 'src/core/master.inc.php';
 
-if(rex::isBackend())
-{
-  require rex_path::core('index_be.inc.php');
-}
-else
-{
-  require rex_path::core('index_fe.inc.php');
-}
+require rex_path::core('index_be.inc.php');
