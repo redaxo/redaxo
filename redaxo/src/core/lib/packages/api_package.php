48b01763698bfba28a74c58479f7dace030e6736
Gregor Harlan <gharlan@web.de>
2012-05-11 19:18:27 +0200

    avoid errors when recalling api urls on addon page

--- redaxo/src/core/lib/packages/api_package.php
+++ redaxo/src/core/lib/packages/api_package.php
@@ -4,16 +4,23 @@ class rex_api_package extends rex_api_function
 {
   public function execute()
   {
-    $packageId = rex_request('package', 'string');
-    $package = rex_package::get($packageId);
-    if($package instanceof rex_null_package)
-    {
-      throw new rex_api_exception('Package "'.$packageId.'" doesn\'t exists!');
-    }
     $function = rex_request('function', 'string');
     if(!in_array($function, array('install', 'uninstall', 'activate', 'deactivate', 'delete')))
     {
-      throw new rex_api_exception('Unknown package function "'.$function.'"!');
+      throw new rex_api_exception('Unknown package function "'. $function .'"!');
+    }
+    $packageId = rex_request('package', 'string');
+    $package = rex_package::get($packageId);
+    if($function == 'uninstall' && !$package->isInstalled()
+      || $function == 'activate' && $package->isActivated()
+      || $function == 'deactivate' && !$package->isActivated()
+      || $function == 'delete' && !rex_package::exists($packageId))
+    {
+      return null;
+    }
+    if($package instanceof rex_null_package)
+    {
+      throw new rex_api_exception('Package "'. $packageId .'" doesn\'t exists!');
     }
     $manager = rex_package_manager::factory($package);
     $success = $manager->$function();
