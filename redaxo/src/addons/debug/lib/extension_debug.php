09dcddb10d4dc7153835762a9a961890dc5c15e8
jdlx <github@rexdev.de>
2012-05-12 02:27:38 +0200

    stripped old code..

--- redaxo/src/addons/debug/lib/extension_debug.php
+++ redaxo/src/addons/debug/lib/extension_debug.php
@@ -10,8 +10,6 @@ rex_extension::register('OUTPUT_FILTER', array('rex_extension_debug', 'doLog'));
 class rex_extension_debug extends rex_extension
 {
   private static
-    $extensionCalls = array(),
-    $epCalls        = array(),
     $log            = array();
 
   protected static function invokeExtension($function, $params)
@@ -19,7 +17,6 @@ class rex_extension_debug extends rex_extension
     $timer  = new rex_timer();
     $result = parent::invokeExtension($function, $params);
 
-    self::$extensionCalls[] = $timer->getFormattedTime(rex_timer::MILLISEC);
     self::$log[] = array(
                          'type'     =>'EXT',
                          'ep'       =>$params['extension_point'],
@@ -44,8 +41,6 @@ class rex_extension_debug extends rex_extension
 
     $memory = rex_formatter :: format(memory_get_usage(true), 'filesize', array(3));
 
-    self::$epCalls[] = array($extensionPoint, $epDur, self::$extensionCalls, $absDur, $memory);
-    self::$extensionCalls = array();
     self::$log[] = array(
                          'type'      =>'EP',
                          'ep'        =>$extensionPoint,
@@ -65,56 +60,34 @@ class rex_extension_debug extends rex_extension
 
   static public function doLog($params)
   {
-    $firephp     = FirePHP::getInstance(true);
-    $firephp->group(__CLASS__);
-    $ext_table   = array();
-    $log_table[] = array('ExtensionPoint','Started','Duration','Memory','remark');
+    $firephp = FirePHP::getInstance(true);
 
-    foreach(self::$epCalls as $call)
-    {
-
-      // when a extension takes longer than 5ms, send a warning
-      if(strtr($call[1], ',', '.') > 5)
-      {
-        $detail = '';
-        if(!empty($call[2]))
-        {
-          $detail = '; extensions '. json_encode($call[2]);
-        }
-
-        $firephp->warn('EP: '. $call[0]. ' (started ' .$call[3] . 'ms, duration ' .$call[1] . 'ms; '. $call[4] .')'. $detail);
-        $log_table[] = array($call[0],$call[3].'ms',$call[1].'ms',$call[4],'slow');
-      }
-      else
-      {
-        $firephp->log('EP: '. $call[0]. ' (started ' .$call[3] . 'ms, duration ' .$call[1] . 'ms; '. $call[4] .')');
-        $log_table[] = array($call[0],$call[3].'ms',$call[1].'ms',$call[4],'–');
-      }
-    }
-    $firephp->groupEnd();
-    $firephp->table('ExtensionPoints',$log_table);
-
-    // EP LOG ALTERNATE STYLE ALA FIREPHP ADDON..
+    $counter        = array('ep'=>0,'ext'=>0,'timing_err'=>0);
     $registered_eps = $log_table = array();
     $log_table[]    = array('#','Type','Timing','ExtensionPoint','Callable','Started','Duration','Memory','subject','params','result','read_only');
+
     foreach(self::$log as $k=>$v)
     {
       $i = $k+1;
       switch($v['type'])
       {
         case'EP':
+          $counter['ep']++;
           $registered_eps[] = $v['ep'];
-                            //'#','Type'    ,'Timing','ExtensionPoint','Callable','Started'    ,'Duration'    ,'Memory'    ,'subject'    ,'params'    ,'result'    ,'read_only'
           $log_table[] = array($i,$v['type'],'–'     ,$v['ep']        ,'–'       ,$v['started'],$v['duration'],$v['memory'],$v['subject'],$v['params'],$v['result'],$v['read_only']);
           break;
 
         case'EXT':
-          $timing = in_array($v['ep'],$registered_eps) ? 'late' : 'ok';
-                            //'#','Type'    ,'Timing','ExtensionPoint','Callable'    ,'Started','Duration','Memory','subject','params'    ,'result'    ,'read_only'
+          $counter['ext']++;
+          $timing = 'ok';
+          if(in_array($v['ep'],$registered_eps)) {
+            $timing = 'late';
+            $counter['timing_err']++;
+          }
           $log_table[] = array($i,$v['type'],$timing ,$v['ep']        ,$v['callable'],'–'      ,'–'       ,'–'     ,'–'      ,$v['params'],$v['result'],'–');
           break;
       }
     }
-    $firephp->table('EP LOG ('.$i.' calls)',$log_table);
+    $firephp->table('EP LOG ( EPs: '.$counter['ep'].', Extensions: '.$counter['ext'].', Late Extensions: '.$counter['timing_err'].' )',$log_table);
   }
 }
