91d1c819a4c55ab61fae0782b1ae702c3c2dbeec
Gregor Harlan <gharlan@web.de>
2012-07-19 17:59:48 +0200

    show error details during setup
    don't show notices/warnings for non-admins (exception: setup)

--- redaxo/src/core/lib/error_handler.php
+++ redaxo/src/core/lib/error_handler.php
@@ -53,7 +53,7 @@ abstract class rex_error_handler
     }
     rex_response::setStatus($status);
 
-    if (($user = rex_backend_login::createUser()) && $user->isAdmin()) {
+    if (rex::isSetup() || ($user = rex_backend_login::createUser()) && $user->isAdmin()) {
       // TODO add a beautiful error page with usefull debugging info
       $buf = '';
       $buf .= '<pre>';
@@ -96,12 +96,16 @@ abstract class rex_error_handler
   static public function handleError($errno, $errstr, $errfile, $errline)
   {
     if (in_array($errno, array(E_USER_ERROR, E_ERROR, E_COMPILE_ERROR, E_RECOVERABLE_ERROR, E_PARSE))) {
+
       throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+
     } elseif ((error_reporting() & $errno) == $errno) {
-      if (ini_get('display_errors')) {
+
+      if (ini_get('display_errors') && (rex::isSetup() || ($user = rex_backend_login::createUser()) && $user->isAdmin())) {
         echo '<div><b>' . self::getErrorType($errno) . "</b>: $errstr in <b>$errfile</b> on line <b>$errline</b></div>";
       }
       rex_logger::logError($errno, $errstr, $errfile, $errline);
+
     }
   }
 
@@ -112,7 +116,7 @@ abstract class rex_error_handler
   {
     if (self::$registered) {
       $error = error_get_last();
-      if (is_array($error)) {
+      if (is_array($error) && in_array($error['type'], array(E_USER_ERROR, E_ERROR, E_COMPILE_ERROR, E_RECOVERABLE_ERROR, E_PARSE))) {
         self::handleException(new ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line']));
       }
     }
