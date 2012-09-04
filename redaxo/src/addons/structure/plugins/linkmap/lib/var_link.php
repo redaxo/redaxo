ef3dc5010303c603359c161e70c1484498f15446
Gregor Harlan <gharlan@web.de>
2012-09-04 12:19:05 +0200

    avoid php notice

--- redaxo/src/addons/structure/plugins/linkmap/lib/var_link.php
+++ redaxo/src/addons/structure/plugins/linkmap/lib/var_link.php
@@ -49,6 +49,7 @@ class rex_var_link extends rex_var
     $art_name = '';
     $clang = '';
     $art = rex_article::getArticleById($value);
+    $category = 0;
 
     // Falls ein Artikel vorausgewählt ist, dessen Namen anzeigen und beim öffnen der Linkmap dessen Kategorie anzeigen
     if ($art instanceof rex_article) {
