90a11402f16d09d0bebc67d5d6029b93cd8bb658
Markus Staab <markus.staab@redaxo.de>
2012-10-06 15:50:24 +0200

    fixed typo. fixes #141.

--- redaxo/src/addons/structure/functions/function_rex_languages.inc.php
+++ redaxo/src/addons/structure/functions/function_rex_languages.inc.php
@@ -4,38 +4,34 @@
  * Dient zur Ausgabe des Sprachen-blocks
  * @package redaxo5
  */
-
 $num_clang = rex_clang::count();
 
-$stop = false;
 $languages = array();
 if ($num_clang > 1) {
-  $i = 1;
-  foreach (rex_clang::getAll() as $id => $clang) {
+    $i = 1;
+    foreach (rex_clang::getAll() as $id => $_clang) {
 
-     if (rex::getUser()->getComplexPerm('clang')->hasPerm($id)) {
-       $lang = array();
-    $lang['id'] = $id;
-    $lang['title'] = rex_i18n::translate($clang->getName());
+        if (rex::getUser()->getComplexPerm('clang')->hasPerm($id)) {
+            $lang = array();
+            $lang['id'] = $id;
+            $lang['title'] = rex_i18n::translate($_clang->getName());
 
-    $lang['linkClasses'] = array();
-    if ($id == $clang)
-      $lang['linkClasses'][] = 'rex-active';
+            $lang['linkClasses'] = array();
+            if ($id == $clang)
+                $lang['linkClasses'][] = 'rex-active';
 
-    $lang['itemClasses'] = $lang['linkClasses'];
-    $lang['href'] = 'index.php?page=' . rex::getProperty('page') . '&amp;clang=' . $id . $sprachen_add . '&amp;ctype=' . $ctype;
+            $lang['itemClasses'] = $lang['linkClasses'];
+            $lang['href'] = 'index.php?page=' . rex::getProperty('page') . '&amp;clang=' . $id . $sprachen_add . '&amp;ctype=' . $ctype;
 
-    $languages[] = $lang;
+            $languages[] = $lang;
+        }
+        $i++;
+    }
 
-     }
-     $i++;
-  }
+    $langfragment = new rex_fragment();
+    $langfragment->setVar('type', 'switch');
+    $langfragment->setVar('blocks', array(array('headline' => array('title' => rex_i18n::msg('languages')), 'navigation' => $languages)), false);
+    echo $langfragment->parse('navigation.tpl');
 
-  $langfragment = new rex_fragment();
-  $langfragment->setVar('type', 'switch');
-  $langfragment->setVar('blocks', array( array('headline' => array('title' => rex_i18n::msg('languages')), 'navigation' => $languages)), false);
-  echo $langfragment->parse('navigation.tpl');
-
-  unset($langfragment);
-
+    unset($langfragment);
 }
