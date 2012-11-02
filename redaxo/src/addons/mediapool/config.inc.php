83472992e6abcb0f5344259ee13f32c3e4a246ca
Gregor Harlan <gharlan@web.de>
2012-11-02 12:56:30 +0100

    fixed mediapool

--- redaxo/src/addons/mediapool/config.inc.php
+++ redaxo/src/addons/mediapool/config.inc.php
@@ -12,7 +12,7 @@ $mypage = 'mediapool';
 
 rex_complex_perm::register('media', 'rex_media_perm');
 
-if (rex::isBackend()) {
+if (rex::isBackend() && rex::getUser()) {
   $mpool = new rex_be_page_popup('mediapool', rex_i18n::msg('mediapool'), 'openMediaPool(); return false;');
   $mpool->setRequiredPermissions('media/hasMediaPerm');
   $mpool->addSubPage(new rex_be_page('', rex_i18n::msg('pool_file_list')));
@@ -26,8 +26,6 @@ if (rex::isBackend()) {
   $this->setProperty('page', $mainPage);
 
   require_once dirname(__FILE__) . '/functions/function_rex_mediapool.inc.php';
-  // im backend und eingeloggt?
-  if (rex::getUser()) {
-    rex_extension::register('PAGE_HEADER', 'rex_mediapool_add_assets');
-  }
+
+  rex_extension::register('PAGE_HEADER', 'rex_mediapool_add_assets');
 }
