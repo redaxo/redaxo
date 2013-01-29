a50797e5e9f60f501215863ff6ae751e973983fd
tbaddade <thomas@redaxo.de>
2013-01-29 11:24:10 +0100

    Neues Backend Design

---
+++ redaxo/src/addons/be_style/plugins/rex/assets/js.js
@@ -0,0 +1,53 @@
+// ------------------------------------------------------------ Drop Menu
+
+jQuery(function($) {
+  $('.rex-js-drop-button, .rex-js-drop .rex-js-close').click(function(){
+    checkDrop($(this));
+  });
+});
+
+
+function checkDrop($object) {
+  var $menu = $object.closest('.rex-js-drop');
+
+  if ($menu.hasClass('rex-open')) {
+    closeDrop($menu);
+  }
+  else {
+    openDrop($menu);
+  }
+}
+
+function openDrop($object) {
+  $object.addClass('rex-open');
+}
+function closeDrop($object) {
+  $object.removeClass('rex-open');
+}
+
+
+// ------------------------------------------------------------ Context Menu
+
+jQuery(function($) {
+  $('.rex-js-context-menu-button, .rex-js-context-menu .rex-js-close').click(function(){
+    checkContextMenu($(this));
+  });
+});
+
+function checkContextMenu($object) {
+  var $menu = $object.closest('.rex-js-context-menu');
+
+  if ($menu.hasClass('rex-open')) {
+    closeContextMenu($menu);
+  }
+  else {
+    openContextMenu($menu);
+  }
+}
+
+function openContextMenu($object) {
+  $object.addClass('rex-open');
+}
+function closeContextMenu($object) {
+  $object.removeClass('rex-open');
+}
---
+++ redaxo/src/addons/be_style/plugins/rex/help.inc.php
@@ -0,0 +1,1 @@
+REDAXO Default-Theme
