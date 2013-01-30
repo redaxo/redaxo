c1331d4e5b6c9cc0e4e2a15a9370c5db66a43e4d
tbaddade <thomas@redaxo.de>
2013-01-30 21:29:46 +0100

    Fonts via laden PHP laden

---
+++ redaxo/src/addons/be_style/plugins/rex/pages/font.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+$font = rex_request('be_style_' . $mypage . '_font', 'string');
+
+
+$fonts = array(
+  'entypo' => array(
+    'family' => 'entypo',
+    'filename' => 'entypo',
+    'svgID' => 'entypo',
+    'weight' => 'normal',
+    'style' => 'normal',
+  )
+);
+
+
+if ($font != '' && isset($fonts[$font])) {
+
+  $output = '
+@font-face {
+  font-family: "' . $fonts[$font]['family'] . '";
+  font-style: "' . $fonts[$font]['style'] . '";
+  font-weight: "' . $fonts[$font]['weight'] . '";
+  src: url("' . rex_url::pluginAssets('be_style', $mypage, $font) . '/' . $fonts[$font]['filename'] . '.eot");
+  src: url("' . rex_url::pluginAssets('be_style', $mypage, $font) . '/' . $fonts[$font]['filename'] . '.eot?#iefix") format("eot"),
+  url("' . rex_url::pluginAssets('be_style', $mypage, $font) . '/' . $fonts[$font]['filename'] . '.woff") format("woff"),
+  url("' . rex_url::pluginAssets('be_style', $mypage, $font) . '/' . $fonts[$font]['filename'] . '.ttf") format("truetype"),
+  url("' . rex_url::pluginAssets('be_style', $mypage, $font) . '/' . $fonts[$font]['filename'] . '.svg#' . $fonts[$font]['svgID'] . '") format("svg");
+}';
+
+  // Output
+  header('Content-type: text/css');
+
+  print trim($output);
+  exit();
+}
