577fbc3bad3694550aa6b67258231f93c66ab6ef
Gregor Harlan <gharlan@web.de>
2013-02-01 19:53:34 +0100

    fixed path to rex_path class
    refs #189

--- redaxo/src/core/master.inc.php
+++ redaxo/src/core/master.inc.php
@@ -21,7 +21,7 @@ mb_internal_encoding('UTF-8');
 // set arg_separator to get valid html output if session.use_trans_sid is activated
 ini_set('arg_separator.output', '&amp;');
 
-require_once dirname(__FILE__) . '/lib/path.php';
+require_once dirname(__FILE__) . '/lib/util/path.php';
 rex_path::init($REX['HTDOCS_PATH'], $REX['BACKEND_FOLDER']);
 
 require_once rex_path::core('lib/autoload.php');
