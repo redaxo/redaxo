94e6ca1fd380332beb356208235b7d44d6797fed
Markus Staab <markus.staab@redaxo.de>
2013-03-10 23:22:19 +0100

    added typeahead js dependency

--- update_requirements.sh
+++ update_requirements.sh
@@ -19,7 +19,11 @@ curl -# http://code.jquery.com/jquery-latest.min.js > redaxo/src/core/assets/jqu
 echo "\nUpdate redaxo/src/core/assets/jquery-pjax.min.js"
 curl -#d output_info=compiled_code -d compilation_level=SIMPLE_OPTIMIZATIONS -d code_url=https://github.com/defunkt/jquery-pjax/raw/master/jquery.pjax.js http://closure-compiler.appspot.com/compile > redaxo/src/core/assets/jquery-pjax.min.js
 
-echo "redaxo/src/core/assets/jquery.min.js\nredaxo/src/core/assets/jquery-pjax.min.js" | php coding_standards.phar fix
+echo "\nUpdate redaxo/src/core/assets/typeahead.min.js"
+curl -# http://twitter.github.com/typeahead.js/releases/latest/typeahead.min.js > redaxo/src/core/assets/typeahead.min.js
+
+echo "redaxo/src/core/assets/jquery.min.js\nredaxo/src/core/assets/jquery-pjax.min.js\nredaxo/src/core/assets/typeahead.min.js" | php coding_standards.phar fix
 
 cp redaxo/src/core/assets/jquery-pjax.min.js assets
 cp redaxo/src/core/assets/jquery.min.js assets
+cp redaxo/src/core/assets/typeahead.min.js assets
