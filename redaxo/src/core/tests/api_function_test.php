a370d6afd2ae4e44a4a45f5b5355fc10fd2dbdbe
Markus Staab <markus.staab@redaxo.de>
2013-03-17 20:21:38 +0100

    more tests

--- redaxo/src/core/tests/api_function_test.php
+++ redaxo/src/core/tests/api_function_test.php
@@ -22,4 +22,16 @@ class rex_api_result_test extends PHPUnit_Framework_TestCase
         $result->array = [1,2,3];
         $this->assertSame('{"data":"yes","array":{1,2,3}}', $result->toJsonData());
     }
+    
+    public function testEmptyToJson() {
+        $result = new rex_api_result(true, 'a message');
+        $this->assertSame('{"succeeded":1,"message":"a message"}', $result->toJson());
+    }
+    
+    public function testToJson() {
+        $result = new rex_api_result(true, 'a message');
+        $result->data = 'yes';
+        $result->array = [1,2,3];
+        $this->assertSame('{"succeeded":1,"message":"a message","data":"yes","array":{1,2,3}}', $result->toJson());
+    }
 }
