8364a68987dbe8969b919a7ddfe1733e347d063f
Gregor Harlan <mail@gh01.de>
2013-04-05 13:56:43 +0200

    removed obsolete subpage declaration

--- redaxo/src/addons/users/boot.php
+++ redaxo/src/addons/users/boot.php
@@ -8,10 +8,3 @@ rex_perm::register('users[]', rex_i18n::msg('user_management'));
 
 rex_extension::register('COMPLEX_PERM_REMOVE_ITEM', 'rex_user_role::removeOrReplaceItem');
 rex_extension::register('COMPLEX_PERM_REPLACE_ITEM', 'rex_user_role::removeOrReplaceItem');
-
-if (rex::getUser() && rex::getUser()->isAdmin()) {
-    $this->setProperty('pages1', [
-        ['', rex_i18n::msg('users')],
-        ['roles', rex_i18n::msg('roles')]
-    ]);
-}
