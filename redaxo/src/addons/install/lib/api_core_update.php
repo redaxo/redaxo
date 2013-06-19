01c072a746299527d26bff4611e72fd2739b9b8c
Gregor Harlan <mail@gh01.de>
2013-06-19 17:30:01 +0200

    delete cache after core update

--- redaxo/src/addons/install/lib/api_core_update.php
+++ redaxo/src/addons/install/lib/api_core_update.php
@@ -82,6 +82,7 @@ class rex_api_install_core_update extends rex_api_function
         } else {
             $message = $addon->i18n('info_core_updated');
             $success = true;
+            rex_delete_cache();
             rex_install_webservice::deleteCache('core');
         }
         return new rex_api_result($success, $message);
