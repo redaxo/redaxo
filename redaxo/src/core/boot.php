1a606351d7d0cffdbaa75c375b55e83f6a4bf3ac
tbaddade <thomas@redaxo.de>
2013-07-31 15:06:56 +0200

    Helferfunktionen gelöscht

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -63,7 +63,6 @@ rex_fragment::addDirectory(rex_path::core('fragments/'));
 
 // ----------------- FUNCTIONS
 require_once rex_path::core('functions/function_rex_globals.php');
-require_once rex_path::core('functions/function_rex_helpers.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
