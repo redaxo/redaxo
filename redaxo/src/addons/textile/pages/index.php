7f5aef15e256385687f34b4f908d1bd0721b41f0
Gregor Harlan <mail@gh01.de>
2013-10-29 10:52:54 +0100

    textile: fixed example module

--- redaxo/src/addons/textile/pages/index.php
+++ redaxo/src/addons/textile/pages/index.php
@@ -14,7 +14,7 @@ echo rex_view::title('Textile');
 $mdl_input = '<?php
 if (rex_addon::get(\'textile\')->isAvailable())
 {
-    echo \'<textarea name="VALUE[1]" rows="10">REX_VALUE[1]</textarea><br /><br />\';
+    echo \'<textarea name="REX_INPUT_VALUE[1]" rows="10">REX_VALUE[1]</textarea><br /><br />\';
     echo \'<a href="#" onclick="jQuery(\\\'#rex-textile-help\\\').toggle(\\\'fast\\\');">Zeige/verberge Textile Hilfe</a><br />\';
     echo \'<div id="rex-textile-help" style="display:none">\';
     rex_textile::showHelpOverview();
@@ -24,7 +24,7 @@ else
 {
     echo rex_view::warning(\'Dieses Modul benötigt das "textile" Addon!\');
 }
-?>';
+';
 
 echo rex_view::content('block', rex_string::highlight($mdl_input), rex_i18n::msg('textile_code_for_module_input'));
 
@@ -44,6 +44,6 @@ else
 {
     echo rex_view::warning(\'Dieses Modul benötigt das "textile" Addon!\');
 }
-?>';
+';
 
 echo rex_view::content('block', rex_string::highlight($mdl_output), rex_i18n::msg('textile_code_for_module_output'));
