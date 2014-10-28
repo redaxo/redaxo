384c716f036ad971f27a259a278926ca6919eecd
tbaddade <thomas@redaxo.org>
2014-10-28 12:54:18 +0100

    benötigte Dateien kopieren

--- redaxo/src/addons/be_style/plugins/redaxo/boot.php
+++ redaxo/src/addons/be_style/plugins/redaxo/boot.php
@@ -19,25 +19,43 @@ $mypage = 'redaxo';
 
 if (rex::isBackend()) {
 
-    require __DIR__ . '/pages/javascripts.php';
+    $files = [
+        $this->getAddOn()->getPath('vendor/bootstrap/assets/javascripts/bootstrap.js') => $this->getAssetsPath('javascripts/bootstrap.js'),
+        $this->getAddOn()->getPath('vendor/bootstrap/assets/fonts/bootstrap/glyphicons-halflings-regular.eot') => $this->getAssetsPath('fonts/bootstrap/glyphicons-halflings-regular.eot'),
+        $this->getAddOn()->getPath('vendor/bootstrap/assets/fonts/bootstrap/glyphicons-halflings-regular.svg') => $this->getAssetsPath('fonts/bootstrap/glyphicons-halflings-regular.svg'),
+        $this->getAddOn()->getPath('vendor/bootstrap/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf') => $this->getAssetsPath('fonts/bootstrap/glyphicons-halflings-regular.ttf'),
+        $this->getAddOn()->getPath('vendor/bootstrap/assets/fonts/bootstrap/glyphicons-halflings-regular.woff') => $this->getAssetsPath('fonts/bootstrap/glyphicons-halflings-regular.woff'),
+        $this->getAddOn()->getPath('vendor/font-awesome/fonts/fontawesome-webfont.eot') => $this->getAssetsPath('fonts/fontawesome-webfont.eot'),
+        $this->getAddOn()->getPath('vendor/font-awesome/fonts/fontawesome-webfont.svg') => $this->getAssetsPath('fonts/fontawesome-webfont.svg'),
+        $this->getAddOn()->getPath('vendor/font-awesome/fonts/fontawesome-webfont.ttf') => $this->getAssetsPath('fonts/fontawesome-webfont.ttf'),
+        $this->getAddOn()->getPath('vendor/font-awesome/fonts/fontawesome-webfont.woff') => $this->getAssetsPath('fonts/fontawesome-webfont.woff'),
+        $this->getAddOn()->getPath('vendor/font-awesome/fonts/FontAwesome.otf') => $this->getAssetsPath('fonts/FontAwesome.otf'),
+    ];
+
+
+    foreach ($files as $source => $destination) {
+
+        if (! file_exists($destination)) {
+
+            rex_file::copy($source, $destination);
+
+        }
+
+    }
 
 
     $compiler = new rex_scss_compiler();
     $compiler->setScssFile($this->getPath('scss/master.scss'));
 
     // Compile in frontend assets dir
-    $compiler->setCssFile($this->getAssetsPath('styles.css'));
+    $compiler->setCssFile($this->getAssetsPath('css/styles.css'));
 
     // Compile in backend assets dir
     //$compiler->setCssFile($this->getPath('assets/styles.css'));
 
     $compiler->compile();
 
-    rex_view::addCssFile($this->getAssetsUrl('styles.css'));
-    //rex_view::addJsFile($this->getAddon()->getPath('vendor/bootstrap/assets/javascripts/bootstrap.js'));
-    rex_view::addJsFile(rex_url::backendController(['be_style_' . $mypage . '_js_files' => ['vendor/bootstrap/assets/javascripts/bootstrap.js']]));
-
-
-
+    rex_view::addCssFile($this->getAssetsUrl('css/styles.css'));
+    rex_view::addJsFile($this->getAssetsUrl('javascripts/bootstrap.js'));
 
 }
