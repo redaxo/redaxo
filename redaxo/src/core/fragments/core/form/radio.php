ad1c9b77ad172af940c94c94e1e42587fe29ffd1
tbaddade <thomas@redaxo.org>
2014-10-30 10:02:43 +0100

    Radio Fragment überarbeitet

--- redaxo/src/core/fragments/core/form/radio.php
+++ redaxo/src/core/fragments/core/form/radio.php
@@ -1,10 +1,35 @@
 <?php
 
-$group = isset($this->group) ? $this->group : false;
-$flush = isset($this->flush) ? $this->flush : false;
+$out = '';
 
-$fragment = new rex_fragment();
-$fragment->setVar('elements', $this->elements, false);
-$fragment->setVar('group', $group);
-$fragment->setVar('flush', $flush);
-echo $fragment->parse('core/form/checkbox.php');
+foreach ($this->elements as $element) {
+
+    $id         = isset($element['id'])         ? ' id="' . $element['id'] . '"' : '';
+    $label      = isset($element['label'])      ? $element['label'] : '<label></label>';
+    $field      = isset($element['field'])      ? $element['field'] : '';
+    $note       = isset($element['note'])       ? '<span class="rex-form-note">' . $element['note'] . '</span>' : '';
+    $highlight  = isset($element['highlight'])  ? $element['highlight'] : false;
+
+    if ($field != '') {
+        $match = $highlight ? '<em class="rex-highlight">$2</em>' : '$2';
+        $label = preg_replace('@(<label\b[^>]*>)(.*?)(</label>)@', '$1' . $field . $match . '$3', $label);
+    }
+
+    $classes = '';
+
+    $error = '';
+    if (isset($element['error']) && $element['error'] != '') {
+        $classes .= ' rex-form-error';
+        $error  = '<dd class="rex-error">' . $element['error'] . '</dd>';
+    }
+    if (isset($element['required']) && $element['required']) {
+        $classes .= ' rex-required';
+    }
+
+    $out .= '<div class="rex-form-radio' . $classes . '"' . $id . '>';
+    $out .= $label;
+    $out .= $note;
+    $out .= '</div>';
+}
+
+echo $out;
