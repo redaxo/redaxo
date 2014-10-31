31f09acaa90ac5c14a6f3478859a25a626b8bd1f
tbaddade <thomas@redaxo.org>
2014-10-31 17:42:04 +0100

    section Fragment verwendet die Panel Komponente von Bootstrap

--- redaxo/src/core/fragments/core/page/section.php
+++ redaxo/src/core/fragments/core/page/section.php
@@ -1,22 +1,32 @@
 <section class="rex-page-section">
-    <?php
-    $header = '';
-    if (isset($this->heading) && $this->heading != '') {
-        $header .= '<h2>' . $this->heading . '</h2>';
-    }
-    if (isset($this->header) && $this->header != '') {
-        $header .= $this->header;
-    }
-    echo $header != '' ? '<header class="rex-page-section-header">' . $header . '</header>' : '';
-    ?>
-    <?php if (isset($this->content) && $this->content != ''): ?>
-        <div class="rex-page-section-body">
-            <?= $this->content; ?>
-        </div>
-    <?php endif; ?>
-    <?php if (isset($this->footer) && $this->footer != ''): ?>
-        <footer class="rex-page-section-footer">
-            <?= $this->footer; ?>
-        </footer>
-    <?php endif; ?>
+
+    <div class="panel panel-default">
+
+        <?php if (isset($this->before)) echo $this->before; ?>
+
+        <?php
+        $header = '';
+        if (isset($this->heading) && $this->heading != '') {
+            $header .= '<h2>' . $this->heading . '</h2>';
+        }
+        if (isset($this->header) && $this->header != '') {
+            $header .= $this->header;
+        }
+        echo $header != '' ? '<header class="panel-heading">' . $header . '</header>' : '';
+        ?>
+
+        <?php if (isset($this->content) && $this->content != ''): ?>
+            <div class="panel-body">
+                <?= $this->content; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (isset($this->footer) && $this->footer != ''): ?>
+            <footer>
+                <?= $this->footer; ?>
+            </footer>
+        <?php endif; ?>
+
+        <?php if (isset($this->after)) echo $this->after; ?>
+    </div>
 </section>
