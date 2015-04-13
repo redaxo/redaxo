901b2a91a1d224731a21c2d6e25de24ff9793281
tbaddade <thomas@redaxo.org>
2015-04-13 08:45:13 +0200

    Übersichtstabelle verwendet panel

--- redaxo/src/addons/metainfo/pages/field.php
+++ redaxo/src/addons/metainfo/pages/field.php
@@ -7,7 +7,7 @@
  * @package redaxo5
  */
 
-
+$title = '';
 $content = '';
 
 //------------------------------> Parameter
@@ -27,9 +27,9 @@ if ($func == 'delete') {
     $field_id = rex_request('field_id', 'int', 0);
     if ($field_id != 0) {
         if (rex_metainfo_delete_field($field_id)) {
-            echo rex_view::info(rex_i18n::msg('minfo_field_successfull_deleted'));
+            echo rex_view::success(rex_i18n::msg('minfo_field_successfull_deleted'));
         } else {
-            echo rex_view::warning(rex_i18n::msg('minfo_field_error_deleted'));
+            echo rex_view::error(rex_i18n::msg('minfo_field_error_deleted'));
         }
     }
     $func = '';
@@ -37,15 +37,16 @@ if ($func == 'delete') {
 
 //------------------------------> Eintragsliste
 if ($func == '') {
+    $title = rex_i18n::msg('minfo_field_list_caption');
+
     // replace LIKE wildcards
     $likePrefix = str_replace(['_', '%'], ['\_', '\%'], $prefix);
 
     $list = rex_list::factory('SELECT id, name FROM ' . rex::getTablePrefix() . 'metainfo_field WHERE `name` LIKE "' . $likePrefix . '%" ORDER BY priority');
-    $list->addTableAttribute('class', 'rex-table-middle table-striped');
-    $list->setCaption(rex_i18n::msg('minfo_field_list_caption'));
-
-    $tdIcon = '<span class="rex-icon rex-icon-metainfo"></span>';
-    $thIcon = '<a href="' . $list->getUrl(['func' => 'add']) . '"><span class="rex-icon rex-icon-add-metainfo"></span></a>';
+    $list->addTableAttribute('class', 'table-striped');
+    
+    $tdIcon = '<i class="rex-icon rex-icon-metainfo"></i>';
+    $thIcon = '<a href="' . $list->getUrl(['func' => 'add']) . '"><i class="rex-icon rex-icon-add-metainfo"></i></a>';
     $list->addColumn($thIcon, $tdIcon, 0, ['<th class="rex-slim">###VALUE###</th>', '<td class="rex-slim">###VALUE###</td>']);
     $list->setColumnParams($thIcon, ['func' => 'edit', 'field_id' => '###id###']);
 
@@ -59,12 +60,12 @@ if ($func == '') {
     $list->setColumnParams('name', ['func' => 'edit', 'field_id' => '###id###']);
 
 
-    $list->addColumn(rex_i18n::msg('minfo_field_label_functions'), rex_i18n::msg('edit'));
+    $list->addColumn(rex_i18n::msg('minfo_field_label_functions'), '<i class="rex-icon rex-icon-edit"></i> ' . rex_i18n::msg('edit'));
     $list->setColumnLayout(rex_i18n::msg('minfo_field_label_functions'),  ['<th class="rex-function" colspan="2">###VALUE###</th>', '<td class="rex-edit">###VALUE###</td>']);
     $list->setColumnParams(rex_i18n::msg('minfo_field_label_functions'), ['func' => 'edit', 'field_id' => '###id###']);
     $list->addLinkAttribute(rex_i18n::msg('minfo_field_label_functions'), 'class', 'rex-edit');
 
-    $list->addColumn('delete', rex_i18n::msg('delete'));
+    $list->addColumn('delete', '<i class="rex-icon rex-icon-delete"></i> ' . rex_i18n::msg('delete'));
     $list->setColumnLayout('delete',  ['', '<td class="rex-delete">###VALUE###</td>']);
     $list->setColumnParams('delete', ['func' => 'delete', 'field_id' => '###id###']);
     $list->addLinkAttribute('delete', 'data-confirm', rex_i18n::msg('delete') . ' ?');
@@ -75,17 +76,30 @@ if ($func == '') {
     $list->setNoRowsMessage(rex_i18n::msg('minfo_metainfos_not_found'));
 
     $content .= $list->get();
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('title', $title);
+    $fragment->setVar('content', $content, false);
+    $content = $fragment->parse('core/page/section.php');
 }
 //------------------------------> Formular
 elseif ($func == 'edit' || $func == 'add') {
-    $form = new rex_metainfo_table_expander($prefix, $metaTable, rex::getTablePrefix() . 'metainfo_field', rex_i18n::msg('minfo_field_fieldset'), 'id=' . $field_id);
+    $title = rex_i18n::msg('minfo_field_fieldset');
+    $form = new rex_metainfo_table_expander($prefix, $metaTable, rex::getTablePrefix() . 'metainfo_field', '', 'id=' . $field_id);
 
     if ($func == 'edit') {
         $form->addParam('field_id', $field_id);
     }
 
     $content .= $form->get();
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('title', $title);
+    $fragment->setVar('body', $content, false);
+    $content = $fragment->parse('core/page/section.php');
 }
 
 
-echo rex_view::content('block', $content, '', $params = ['flush' => true]);
+
+
+echo $content;
