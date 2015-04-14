0c9a5cc82ef342bc2b17120082ce067c8a7d226b
tbaddade <thomas@redaxo.org>
2015-04-14 15:11:08 +0200

    page-header Fragment angepasst

--- redaxo/src/core/fragments/core/page/header.php
+++ redaxo/src/core/fragments/core/page/header.php
@@ -1,10 +1,14 @@
-<header class="rex-page-header page-header">
-    <h1><?= $this->heading ?>
-        <?php if (isset($this->subheading) && $this->subheading != ''): ?>
-            <small><?= $this->subheading ?></small>
-        <?php endif; ?>
-    </h1>
+<header class="rex-page-header">
+    <div class="page-header">
+        <h1><?= $this->heading ?>
+            <?php if (isset($this->subheading) && $this->subheading != ''): ?>
+                <small><?= $this->subheading ?></small>
+            <?php endif; ?>
+        </h1>
+    </div>
     <?php if (isset($this->subtitle) && $this->subtitle != ''): ?>
-        <?= $this->subtitle ?>
+        <nav class="nav rex-page-nav">
+            <?= $this->subtitle ?>
+        </nav>
     <?php endif; ?>
 </header>
