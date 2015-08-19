6150f2d496b0c4aeb2acbe56c7ca4ce8ea487029
tbaddade <thomas@redaxo.org>
2015-08-19 12:33:00 +0200

    Credits page ist nur erreichbar, wenn man eingeloggt ist.
    Url auf der Loginpage stimmte nicht, verwies ins Backend

--- redaxo/src/core/fragments/core/footer.php
+++ redaxo/src/core/fragments/core/footer.php
@@ -5,7 +5,7 @@
                 <li><a href="http://www.yakamara.de">yakamara.de</a></li>
                 <li><a href="http://www.redaxo.org">redaxo.org</a></li>
                 <li><a href="http://www.redaxo.org/de/forum/"><?php echo rex_i18n::msg('footer_joinforum'); ?></a></li>
-                <li><a href="<?php echo(rex::isSetup()) ? 'http://www.redaxo.org/' : rex_url::backendPage('credits') ?>"><?php echo rex_i18n::msg('footer_credits'); ?></a></li>
+                <li><a href="<?php echo(rex::getUser()) ? rex_url::backendPage('credits') : 'http://www.redaxo.org/' ?>"><?php echo rex_i18n::msg('footer_credits'); ?></a></li>
                 <li><?php echo rex_i18n::msg('footer_datetime', rex_formatter::strftime(time(), 'date')); ?></li>
                 <li><!--DYN--><?php echo rex_i18n::msg('footer_scripttime', $this->time); ?><!--/DYN--></li>
             </ul>
