5a57db21826fe490d09a0a20eb87f5262e585129
tbaddade <thomas@redaxo.org>
2015-11-11 16:46:41 +0100

    Loader angepasst

--- redaxo/src/core/fragments/core/top.php
+++ redaxo/src/core/fragments/core/top.php
@@ -33,10 +33,12 @@
 <body<?php echo $this->bodyAttr; ?>>
 
 <div class="rex-ajax-loader" id="rex-js-ajax-loader">
-    <div class="rex-ajax-loader-element1 rex-ajax-loader-element"></div>
-    <div class="rex-ajax-loader-element2 rex-ajax-loader-element"></div>
-    <div class="rex-ajax-loader-element3 rex-ajax-loader-element"></div>
-    <div class="rex-ajax-loader-element4 rex-ajax-loader-element"></div>
-    <div class="rex-ajax-loader-element5 rex-ajax-loader-element"></div>
+    <div class="rex-ajax-loader-elements">
+        <div class="rex-ajax-loader-element1 rex-ajax-loader-element"></div>
+        <div class="rex-ajax-loader-element2 rex-ajax-loader-element"></div>
+        <div class="rex-ajax-loader-element3 rex-ajax-loader-element"></div>
+        <div class="rex-ajax-loader-element4 rex-ajax-loader-element"></div>
+        <div class="rex-ajax-loader-element5 rex-ajax-loader-element"></div>
+    </div>
 </div>
 <div id="rex-start-of-page" class="rex-page">
