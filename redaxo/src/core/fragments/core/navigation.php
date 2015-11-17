1b39155f328e0d2a919032ff166488bc1660dc7f
tbaddade <thomas@redaxo.org>
2015-11-17 15:06:38 +0100

    id-attribute gesetzt

--- redaxo/src/core/fragments/core/navigation.php
+++ redaxo/src/core/fragments/core/navigation.php
@@ -1,6 +1,6 @@
 
         <?php if ($this->navigation != ''): ?>
-        <nav class="rex-nav-main navbar-default" role="navigation">
+        <nav id="rex-js-nav-main" class="rex-nav-main navbar-default" role="navigation">
             <div class="navbar-collapse collapse">
                 <?= $this->navigation ?>
             </div>
