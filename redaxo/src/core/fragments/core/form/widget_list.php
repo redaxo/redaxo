310b5311697b9327309a3f7babef13e6c7db9d5e
tbaddade <thomas@redaxo.org>
2015-11-18 17:34:18 +0100

    List Widget überarbeitet

--- redaxo/src/core/fragments/core/form/widget_list.php
+++ redaxo/src/core/fragments/core/form/widget_list.php
@@ -3,25 +3,34 @@
 $out = '';
 
 foreach ($this->elements as $element) {
-    $field = isset($element['field'])           ? $element['field']           : '';
-    $moveButtons = isset($element['moveButtons'])     ? $element['moveButtons']     : '';
+    $field = isset($element['field']) ? $element['field'] : '';
+    $moveButtons = isset($element['moveButtons']) ? $element['moveButtons'] : '';
     $functionButtons = isset($element['functionButtons']) ? $element['functionButtons'] : '';
+    $addon = trim($moveButtons . $functionButtons) == '' ? false : true;
 
-    $out .= '<div class="btn-toolbar">';
-
-    if ($field != '') {
-        $out .= '<span class="input-group">' . $field . '</span>';
+    if (isset($element['before']) && $element['before'] != '') {
+        $out .= $element['before'];
     }
 
-    if ($moveButtons != '') {
-        $out .= '<div class="btn-group"><div class="btn-group-vertical">' . $moveButtons . '</div></div>';
-    }
+    $out .= '<div class="input-group">' . $field;
 
-    if ($functionButtons != '') {
-        $out .= '<div class="btn-group">' . $functionButtons . '</div>';
+    if ($addon) {
+        $out .= '<span class="input-group-addon">';
+        if ($moveButtons != '') {
+            $out .= '<div class="btn-group-vertical">' . $moveButtons . '</div>';
+        }
+
+        if ($functionButtons != '') {
+            $out .= '<div class="btn-group-vertical">' . $functionButtons . '</div>';
+        }
+        $out .= '</span>';
     }
 
     $out .= '</div>';
+
+    if (isset($element['after']) && $element['after'] != '') {
+        $out .= $element['after'];
+    }
 }
 
 echo $out;
