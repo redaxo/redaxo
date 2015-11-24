cae6828a49d45779517ee4b237fa89d985a51bfc
tbaddade <thomas@redaxo.org>
2015-11-24 17:38:18 +0100

    Loginscreen angepasst

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -2,12 +2,14 @@
         <nav class="rex-nav-top navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".rex-nav-main > .navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <?php if (rex_be_controller::getCurrentPage() != 'login'): ?>
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".rex-nav-main > .navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    <?php endif; ?>
                     <?php if (rex_be_controller::getCurrentPageObject()->isPopup()): ?>
                         <span class="navbar-brand"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::assets('redaxo-logo.svg') ?>" /></span>
                     <?php else: ?>
