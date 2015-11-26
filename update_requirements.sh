53c5c2bdc33af335b75866282238b1ed1e70509a
Gregor Harlan <mail@gh01.de>
2015-11-26 14:06:02 +0100

    jquery.min.map aufgenommen, closes #466

--- update_requirements.sh
+++ update_requirements.sh
@@ -12,6 +12,7 @@ composer update --no-dev -d redaxo/src/addons/textile/
 
 echo "\nUpdate redaxo/src/core/assets/jquery.min.js"
 curl -# http://cdn.jsdelivr.net/jquery/2/jquery.min.js > redaxo/src/core/assets/jquery.min.js
+curl -# http://cdn.jsdelivr.net/jquery/2/jquery.min.map > redaxo/src/core/assets/jquery.min.map
 
 echo "\nUpdate redaxo/src/core/assets/jquery-pjax.min.js"
 curl -#d output_info=compiled_code -d compilation_level=SIMPLE_OPTIMIZATIONS -d code_url=https://github.com/defunkt/jquery-pjax/raw/master/jquery.pjax.js http://closure-compiler.appspot.com/compile > redaxo/src/core/assets/jquery-pjax.min.js
