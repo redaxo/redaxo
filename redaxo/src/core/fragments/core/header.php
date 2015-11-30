c29d3a3e583f56c11f8c14437a0f5ecb12db4f0b
tbaddade <thomas@redaxo.org>
2015-11-30 20:19:08 +0100

    Hamburger nicht im Popup anzeigen

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -2,7 +2,7 @@
         <nav class="rex-nav-top navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <?php if (rex_be_controller::getCurrentPage() != 'login'): ?>
+                    <?php if (rex_be_controller::getCurrentPage() != 'login' && !rex_be_controller::getCurrentPageObject()->isPopup()): ?>
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".rex-nav-main > .navbar-collapse">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
