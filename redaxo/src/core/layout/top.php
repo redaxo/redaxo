229d2b2f7435d6a8c5e55e54585e7c9bd807a51d
Gregor Harlan <mail@gh01.de>
2015-12-02 12:31:11 +0100

    Accesskey für Logout entfernt

--- redaxo/src/core/layout/top.php
+++ redaxo/src/core/layout/top.php
@@ -63,7 +63,7 @@ if (rex::getUser() && $hasNavigation) {
     $item = [];
     $item['title'] = '<i class="rex-icon rex-icon-sign-out"></i> ' . rex_i18n::msg('logout');
     $item['href'] = rex_url::backendController(['rex_logout' => 1]);
-    $item['attributes'] = 'class="rex-logout"' . rex::getAccesskey(rex_i18n::msg('logout'), 'logout');
+    $item['attributes'] = 'class="rex-logout"';
     $meta_items[] = $item;
     unset($item);
 } elseif ($hasNavigation) {
