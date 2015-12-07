7f1f2fb642f4fc06c41549556924bd733e1e6272
Markus Staab <markus.staab@redaxo.de>
2015-12-07 12:57:32 +0100

    fixed typo

--- redaxo/src/addons/debug/help.php
+++ redaxo/src/addons/debug/help.php
@@ -12,6 +12,6 @@
 <h3>Debug Addon</h3>
 
 <p>
-Dieses erleichtert die Entwicklung im Backend durch zusätzliche Informationen über <a onclick="window.open(this.href); return false;" href="http://www.firephp.org">FirePHP</a>.
+Dieses Addon erleichtert die Entwicklung im Backend durch zusätzliche Informationen über <a onclick="window.open(this.href); return false;" href="http://www.firephp.org">FirePHP</a>.
 Dazu ist es nötig, dass die FireFox Erweiterung FirePHP installiert ist.
 </p>
