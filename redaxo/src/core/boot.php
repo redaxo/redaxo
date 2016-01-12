016695591482108b2de2ea18c8222930f26c342a
Gregor Harlan <mail@gh01.de>
2016-01-12 09:59:17 +0100

    Versionen auf Final gesetzt

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -65,7 +65,7 @@ require_once rex_path::core('functions/function_rex_globals.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
-rex::setProperty('version', '5.0.0-rc');
+rex::setProperty('version', '5.0.0');
 
 $cacheFile = rex_path::coreCache('config.yml.cache');
 $configFile = rex_path::coreData('config.yml');
