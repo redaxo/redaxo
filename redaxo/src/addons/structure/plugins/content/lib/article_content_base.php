1e43b4a908df7371f87a8cdb81f4fbeb528827a3
schuer <ds@decaf.de>
2016-01-27 22:00:59 +0100

    replaceCommonVars vor replaceObjectVars, closes #570

--- redaxo/src/addons/structure/plugins/content/lib/article_content_base.php
+++ redaxo/src/addons/structure/plugins/content/lib/article_content_base.php
@@ -435,8 +435,8 @@ class rex_article_content_base
     // ----- Modulvariablen werden ersetzt
     protected function replaceVars(rex_sql $sql, $content)
     {
-        $content = $this->replaceObjectVars($sql, $content);
         $content = $this->replaceCommonVars($content);
+        $content = $this->replaceObjectVars($sql, $content);
         $content = str_replace(
             [
                 'REX_MODULE_ID',
