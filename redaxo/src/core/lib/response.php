0da82808ad6a99befd1dd00a83e16b9d3b7abbda
Gregor Harlan <mail@gh01.de>
2016-03-11 19:28:21 +0100

    ETag in Chrome nutzen

--- redaxo/src/core/lib/response.php
+++ redaxo/src/core/lib/response.php
@@ -156,7 +156,7 @@ class rex_response
             // Safari incorrectly caches 304s as empty pages, so don't serve it 304s
             // http://tech.vg.no/2013/10/02/ios7-bug-shows-white-page-when-getting-304-not-modified-from-server/
             // https://bugs.webkit.org/show_bug.cgi?id=32829
-            false === strpos($_SERVER['HTTP_USER_AGENT'], 'Safari')
+            (false === strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') || false !== strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome'))
         ) {
             // ----- Last-Modified
             if (!self::$sentLastModified
