05ea06dfa641da1e90716f069fe44cd5048da550
Gregor Harlan <mail@gh01.de>
2016-03-13 14:16:56 +0100

    Weitere Tests für rex_context

--- redaxo/src/core/tests/context_test.php
+++ redaxo/src/core/tests/context_test.php
@@ -2,62 +2,84 @@
 
 class rex_context_test extends PHPUnit_Framework_TestCase
 {
-    private $context;
-
-    public function setUp()
-    {
-        $globalParams = ['int' => '25', 'str' => '<a b$c&?>'];
-        $this->context = new rex_context($globalParams);
-
-        parent::setUp();
-    }
-
-    public function tearDown()
-    {
-        $this->context = null;
-
-        parent::tearDown();
-    }
-
     public function testGetUrl()
     {
-        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E', $this->context->getUrl(), 'parameters get properly encoded');
-        $this->assertEquals('index.php?int=25&amp;str=xyz', $this->context->getUrl(['str' => 'xyz']), 'local params override global params');
-        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;str2=xyz', $this->context->getUrl(['str2' => 'xyz']), 'new params are appended');
-        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;myarr[0]=xyz&amp;myarr[1]=123', $this->context->getUrl(['myarr' => ['xyz', 123]]), 'numeric arrays are handled');
-        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;myarr[a]=xyz&amp;myarr[b]=123', $this->context->getUrl(['myarr' => ['a' => 'xyz', 'b' => 123]]), 'assoc arrays are handled');
+        $globalParams = ['int' => '25', 'str' => '<a b$c&?>'];
+        $context = new rex_context($globalParams);
+
+        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E', $context->getUrl(), 'parameters get properly encoded');
+        $this->assertEquals('index.php?int=25&amp;str=xyz', $context->getUrl(['str' => 'xyz']), 'local params override global params');
+        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;str2=xyz', $context->getUrl(['str2' => 'xyz']), 'new params are appended');
+        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;myarr[0]=xyz&amp;myarr[1]=123', $context->getUrl(['myarr' => ['xyz', 123]]), 'numeric arrays are handled');
+        $this->assertEquals('index.php?int=25&amp;str=%3Ca+b%24c%26%3F%3E&amp;myarr[a]=xyz&amp;myarr[b]=123', $context->getUrl(['myarr' => ['a' => 'xyz', 'b' => 123]]), 'assoc arrays are handled');
     }
 
     public function testGetHiddenInputFields()
     {
+        $globalParams = ['int' => '25', 'str' => '<a b$c&?>'];
+        $context = new rex_context($globalParams);
+
         $this->assertEquals(
             '<input type="hidden" name="int" value="25" /><input type="hidden" name="str" value="&lt;a b$c&amp;?&gt;" />',
-            $this->context->getHiddenInputFields(),
+            $context->getHiddenInputFields(),
             'parameters get properly encoded'
         );
 
         $this->assertEquals(
             '<input type="hidden" name="int" value="25" /><input type="hidden" name="str" value="xyz" />',
-            $this->context->getHiddenInputFields(['str' => 'xyz']),
+            $context->getHiddenInputFields(['str' => 'xyz']),
             'local params override global params'
         );
 
         $this->assertEquals(
             '<input type="hidden" name="int" value="25" /><input type="hidden" name="str" value="&lt;a b$c&amp;?&gt;" /><input type="hidden" name="str2" value="xyz" />',
-            $this->context->getHiddenInputFields(['str2' => 'xyz']),
+            $context->getHiddenInputFields(['str2' => 'xyz']),
             'new params are appended'
         );
 
         $this->assertEquals(
             '<input type="hidden" name="int" value="25" /><input type="hidden" name="str" value="&lt;a b$c&amp;?&gt;" /><input type="hidden" name="myarr[0]" value="xyz" /><input type="hidden" name="myarr[1]" value="123" />',
-            $this->context->getHiddenInputFields(['myarr' => ['xyz', 123]]),
+            $context->getHiddenInputFields(['myarr' => ['xyz', 123]]),
             'numeric arrays are handled'
         );
 
         $this->assertEquals(
             '<input type="hidden" name="int" value="25" /><input type="hidden" name="str" value="&lt;a b$c&amp;?&gt;" /><input type="hidden" name="myarr[a]" value="xyz" /><input type="hidden" name="myarr[b]" value="123" />',
-            $this->context->getHiddenInputFields(['myarr' => ['a' => 'xyz', 'b' => 123]]),
+            $context->getHiddenInputFields(['myarr' => ['a' => 'xyz', 'b' => 123]]),
             'assoc arrays are handled'
         );
+    }
+
+    public function testFromGet()
+    {
+        $key = 'context_test_get';
+        $_GET[$key] = 1;
+
+        $context = rex_context::fromGet();
+
+        $this->assertEquals($_GET[$key], $context->getParam($key));
+    }
+
+    public function testFromPost()
+    {
+        $key = 'context_test_post';
+        $_POST[$key] = 'foo';
+
+        $context = rex_context::fromPost();
+
+        $this->assertEquals($_POST[$key], $context->getParam($key));
+    }
+
+    public function testRestore()
+    {
+        $keyGet = 'context_test_restore_1';
+        $keyPost = 'context_test_restore_2';
+        $_GET[$keyGet] = 'foo';
+        $_POST[$keyPost] = 'bar';
+
+        $context = rex_context::restore();
+
+        $this->assertEquals($_GET[$keyGet], $context->getParam($keyGet));
+        $this->assertEquals($_POST[$keyPost], $context->getParam($keyPost));
     }
 }
