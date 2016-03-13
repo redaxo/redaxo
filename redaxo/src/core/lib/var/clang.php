c7b8a3e28aadad880898412aa3fb7224c35db42d
Gregor Harlan <mail@gh01.de>
2016-03-13 12:31:12 +0100

    Neue Rex-Var: REX_CLANG[]
    closes #645

---
+++ redaxo/src/core/lib/var/clang.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * REX_CLANG[id=x field=xzy].
+ *
+ * Arguments:
+ *   - id
+ *   - field
+ *
+ * @author gharlan
+ *
+ * @package redaxo\core
+ */
+class rex_var_clang extends rex_var
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getOutput()
+    {
+        $field = $this->getParsedArg('field', null, true);
+        if (null === $field) {
+            return false;
+        }
+
+        $id = $this->getParsedArg('id');
+        $getMethod = $id ? 'get(' . $id . ')' : 'getCurrent()';
+
+        return 'htmlspecialchars(rex_clang::' . $getMethod . '->getValue(' . $field . '))';
+    }
+}
