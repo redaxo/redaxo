3223b303ec85af0171aca444878924fed3428991
tbaddade <thomas@redaxo.org>
2016-03-17 17:52:02 +0100

    Bereitstellen der icons angepasst

--- redaxo/src/addons/be_style/plugins/redaxo/boot.php
+++ redaxo/src/addons/be_style/plugins/redaxo/boot.php
@@ -46,27 +46,26 @@ if (rex::isBackend()) {
     rex_view::addJsFile($this->getAssetsUrl('javascripts/redaxo.js'));
 
     rex_extension::register('PAGE_HEADER', function (rex_extension_point $ep) {
-        $ep->setSubject('
-            <link rel="apple-touch-icon-precomposed" sizes="57x57" href="' . $this->getAssetsUrl('images/apple-touch-icon-57x57.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="60x60" href="' . $this->getAssetsUrl('images/apple-touch-icon-60x60.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="76x76" href="' . $this->getAssetsUrl('images/apple-touch-icon-76x76.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="72x72" href="' . $this->getAssetsUrl('images/apple-touch-icon-72x72.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="114x114" href="' . $this->getAssetsUrl('images/apple-touch-icon-114x114.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="120x120" href="' . $this->getAssetsUrl('images/apple-touch-icon-120x120.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . $this->getAssetsUrl('images/apple-touch-icon-144x144.png') . '" />
-            <link rel="apple-touch-icon-precomposed" sizes="152x152" href="' . $this->getAssetsUrl('images/apple-touch-icon-152x152.png') . '" />
-            <link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-16x16.png') . '" sizes="16x16" />
-            <link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-32x32.png') . '" sizes="32x32" />
-            <link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-96x96.png') . '" sizes="96x96" />
-            <link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-128x128.png') . '" sizes="128x128" />
-            <link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-196x196.png') . '" sizes="196x196" />
-            <meta name="msapplication-TileColor" content="#FFFFFF" />
-            <meta name="msapplication-TileImage" content="' . $this->getAssetsUrl('images/mstile-144x144.png') . '" />
-            <meta name="msapplication-square70x70logo" content="' . $this->getAssetsUrl('images/mstile-70x70.png') . '" />
-            <meta name="msapplication-square150x150logo" content="' . $this->getAssetsUrl('images/mstile-150x150.png') . '" />
-            <meta name="msapplication-square310x310logo" content="' . $this->getAssetsUrl('images/mstile-310x310.png') . '" />
-            <meta name="msapplication-wide310x150logo" content="' . $this->getAssetsUrl('images/mstile-310x150.png') . '" />
-            ' . $ep->getSubject()
-        );
-    }, rex_extension::EARLY);
+        $icons = [];
+        foreach (['57', '60', '72', '76', '114', '120', '144', '152'] as $size) {
+            $size = $size . 'x' . $size;
+            $icons[] = '<link rel="apple-touch-icon-precomposed" sizes="' . $size . '" href="' . $this->getAssetsUrl('images/apple-touch-icon-' . $size . '.png') . '" />';
+        }
+        foreach (['16', '32', '96', '128', '196'] as $size) {
+            $size = $size . 'x' . $size;
+            $icons[] = '<link rel="icon" type="image/png" href="' . $this->getAssetsUrl('images/favicon-' . $size . '.png') . '" sizes="' . $size . '" />';
+        }
+
+        $icons[] = '<meta name="msapplication-TileColor" content="#FFFFFF" />';
+        $icons[] = '<meta name="msapplication-TileImage" content="' . $this->getAssetsUrl('images/mstile-144x144.png') . '" />';
+
+        foreach (['70', '150', '310'] as $size) {
+            $size = $size . 'x' . $size;
+            $icons[] = '<meta name="msapplication-square' . $size . 'logo" content="' . $this->getAssetsUrl('images/mstile-' . $size . '.png') . '" />';
+        }
+        $icons[]= '<meta name="msapplication-wide310x150logo" content="' . $this->getAssetsUrl('images/mstile-310x150.png') . '" />';
+
+        $icons = implode("\n    ", $icons);
+        $ep->setSubject($icons . $ep->getSubject());
+    });
 }
