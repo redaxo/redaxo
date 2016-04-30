b2d0f1ed57ff471b31cebd1663dbe5a8adeb9817
Gregor Harlan <mail@gh01.de>
2016-04-30 20:40:01 +0200

    Versionen aktualisiert

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -65,7 +65,7 @@ require_once rex_path::core('functions/function_rex_globals.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
-rex::setProperty('version', '5.1.0');
+rex::setProperty('version', '5.2.0-dev');
 
 $cacheFile = rex_path::coreCache('config.yml.cache');
 $configFile = rex_path::coreData('config.yml');
