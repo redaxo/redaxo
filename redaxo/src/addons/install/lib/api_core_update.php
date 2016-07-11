9996adda88bfa95b30a0bc735858c5c168ded763
Gregor Harlan <mail@gh01.de>
2016-07-11 20:24:26 +0200

    Bei Core-Update auch Packages-Cache leeren

--- redaxo/src/addons/install/lib/api_core_update.php
+++ redaxo/src/addons/install/lib/api_core_update.php
@@ -152,6 +152,7 @@ class rex_api_install_core_update extends rex_api_function
             $success = true;
             rex_delete_cache();
             rex_install_webservice::deleteCache('core');
+            rex_install_packages::deleteCache();
             rex::setConfig('version', $version['version']);
 
             // ---- update package order
