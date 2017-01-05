2aeb1793978356ccb39b6d497544514b8a64a8a6
Gregor Harlan <mail@gh01.de>
2017-01-05 22:37:14 +0100

    package.yml: null-Werte erlauben
    fixes #897

--- redaxo/src/core/lib/util/i18n.php
+++ redaxo/src/core/lib/util/i18n.php
@@ -232,7 +232,7 @@ class rex_i18n
             return $array;
         } elseif (is_string($array)) {
             return self::translate($array, $use_htmlspecialchars, $i18nFunction);
-        } elseif (is_scalar($array)) {
+        } elseif (null === $array || is_scalar($array)) {
             return $array;
         } else {
             throw new InvalidArgumentException('Expecting $text to be a String or Array of Scalar, "' . gettype($array) . '" given!');
