4fd333109da93b7a5329990580913205f3c3c86c
Gregor Harlan <mail@gh01.de>
2017-02-10 18:25:06 +0100

    Tabellenanpassung muss auch geschehen, wenn das Plugin nur installiert, aber nicht aktiviert ist

--- redaxo/src/addons/structure/update.php
+++ redaxo/src/addons/structure/update.php
@@ -2,7 +2,7 @@
 
 /** @var rex_addon $this */
 
-if ($this->getPlugin('history')->isAvailable() && rex_string::versionCompare($this->getVersion(), '2.3.0-dev', '<')) {
+if ($this->getPlugin('history')->isInstalled() && rex_string::versionCompare($this->getVersion(), '2.3.0-dev', '<')) {
     rex_sql_table::get(rex_article_slice_history::getTable())
         ->ensureColumn(new rex_sql_column('history_user', 'varchar(255)'))
         ->alter();
