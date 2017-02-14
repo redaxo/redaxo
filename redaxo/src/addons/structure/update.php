b1d96e87f5eba6ff8fe9ee069b653f6f1af65eed
Gregor Harlan <mail@gh01.de>
2017-02-14 12:09:46 +0100

    Mediumtext-Spalten für Templates und Module (#1041)

--- redaxo/src/addons/structure/update.php
+++ redaxo/src/addons/structure/update.php
@@ -2,6 +2,16 @@
 
 /** @var rex_addon $this */
 
+if ($this->getPlugin('content')->isInstalled() && rex_string::versionCompare($this->getVersion(), '2.3.0', '<')) {
+    rex_sql_table::get(rex::getTable('template'))
+        ->ensureColumn(new rex_sql_column('content', 'mediumtext', true))
+        ->alter();
+    rex_sql_table::get(rex::getTable('module'))
+        ->ensureColumn(new rex_sql_column('input', 'mediumtext', true))
+        ->ensureColumn(new rex_sql_column('output', 'mediumtext', true))
+        ->alter();
+}
+
 if ($this->getPlugin('history')->isInstalled() && rex_string::versionCompare($this->getVersion(), '2.3.0-dev', '<')) {
     rex_sql_table::get(rex_article_slice_history::getTable())
         ->ensureColumn(new rex_sql_column('history_user', 'varchar(255)'))
