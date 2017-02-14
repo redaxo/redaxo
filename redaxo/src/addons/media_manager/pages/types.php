610e090296d4f3881ed0dbf0addf618d849de88e
Gregor Harlan <mail@gh01.de>
2017-02-14 11:54:48 +0100

    rex-icon-duplicate ergänzt
    closes #1028

--- redaxo/src/addons/media_manager/pages/types.php
+++ redaxo/src/addons/media_manager/pages/types.php
@@ -110,7 +110,7 @@ if ($func == '') {
     $list->addColumn('editType', '<i class="rex-icon rex-icon-edit"></i> ' . rex_i18n::msg('media_manager_type_edit'), -1, ['', '<td class="rex-table-action">###VALUE###</td>']);
     $list->setColumnParams('editType', ['func' => 'edit', 'type_id' => '###id###']);
 
-    $list->addColumn('copyType', '<i class="rex-icon rex-icon-copy"></i> ' . rex_i18n::msg('media_manager_type_copy'), -1, ['', '<td class="rex-table-action">###VALUE###</td>']);
+    $list->addColumn('copyType', '<i class="rex-icon rex-icon-duplicate"></i> ' . rex_i18n::msg('media_manager_type_copy'), -1, ['', '<td class="rex-table-action">###VALUE###</td>']);
     $list->setColumnParams('copyType', ['func' => 'copy', 'type_id' => '###id###']);
 
     // remove delete link on internal types (status == 1)
