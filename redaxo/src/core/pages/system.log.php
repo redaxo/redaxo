4bbaa5fd7334cbfb5e4d3e1a180ec7580c27ea4f
Markus Staab <markus.staab@redaxo.de>
2017-02-18 13:45:03 +0100

    added a prototype for PrettyPageHandler rendered syslog-details

--- redaxo/src/core/pages/system.log.php
+++ redaxo/src/core/pages/system.log.php
@@ -23,6 +23,43 @@ if ($func == 'delLog') {
     } else {
         $error = rex_i18n::msg('syslog_delete_error');
     }
+} else if ($func == 'inspect') {
+    ob_clean();
+
+    $exception = new ErrorException(
+        'session_regenerate_id(): Session object destruction failed',
+        0,
+        E_USER_WARNING,
+        realpath(rex_path::base('./'.'redaxo\src\core\lib\login\login.php')),
+        '286'
+    );
+
+    $whoops = new \Whoops\Run();
+    $whoops->writeToOutput(false);
+    $whoops->allowQuit(false);
+
+    $handler = new \Whoops\Handler\PrettyPageHandler();
+    if (ini_get('xdebug.file_link_format')) {
+        $handler->setEditor('xdebug');
+    }
+
+    $whoops->pushHandler($handler);
+
+    $errPage = $whoops->handleException($exception);
+
+    $errPageStyle = '
+        <style>
+            /*disable details, as those show data of the current request, not those at error time. */
+            .details {
+                display: none;
+            }
+        </style>
+    ';
+
+    $errPage = str_replace('</head>', $errPageStyle . '</head>', $errPage);
+
+    echo $errPage;
+    exit();
 }
 $message = '';
 $content = '';
@@ -49,6 +86,7 @@ $content .= '
                 <tbody>';
 
 if ($file = new rex_log_file($logFile)) {
+    $first = true;
     foreach (new LimitIterator($file, 0, 30) as $entry) {
         /* @var rex_log_entry $entry */
         $data = $entry->getData();
@@ -64,6 +102,16 @@ if ($file = new rex_log_file($logFile)) {
                         <td data-title="' . rex_i18n::msg('syslog_file') . '"><div class="rex-word-break">' . (isset($data[2]) ? $data[2] : '') . '</div></td>
                         <td class="rex-table-number" data-title="' . rex_i18n::msg('syslog_line') . '">' . (isset($data[3]) ? $data[3] : '') . '</td>
                     </tr>';
+
+        if ($first) {
+            $first = false;
+
+            $content .= '
+                <tr>
+                    <td colspan="5"><iframe style="width:100%; height: 550px" src="'. rex_url::backendController(['page' => 'system/log', 'func' => 'inspect']) .'"></iframe></td>
+                </tr>
+            ';
+        }
     }
 }
 
