fe6bc4c8eeb50f91d7ac8f67af9624ad2b459d6f
Gregor Harlan <mail@gh01.de>
2017-03-19 18:20:32 +0100

    typo fix

--- redaxo/src/addons/media_manager/lib/managed_media.php
+++ redaxo/src/addons/media_manager/lib/managed_media.php
@@ -177,7 +177,7 @@ class rex_managed_media
 
         ob_start();
         if ($this->format == 'jpg' || $this->format == 'jpeg') {
-            $quality = $this->getImageProperty('jpg_quality', $addon->getConfig('interlace', 85));
+            $quality = $this->getImageProperty('jpg_quality', $addon->getConfig('jpg_quality', 85));
             imagejpeg($this->image['src'], null, $quality);
         } elseif ($this->format == 'png') {
             $compression = $this->getImageProperty('png_compression', $addon->getConfig('png_compression', 6));
