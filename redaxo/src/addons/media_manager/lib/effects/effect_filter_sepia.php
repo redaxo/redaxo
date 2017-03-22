d2d33fce32dce57b55f06f4eae1a31036e4496c0
Hirbod <info@nightstomp.com>
2017-03-22 05:13:45 +0100

    fix cs

--- redaxo/src/addons/media_manager/lib/effects/effect_filter_sepia.php
+++ redaxo/src/addons/media_manager/lib/effects/effect_filter_sepia.php
@@ -9,9 +9,9 @@ class rex_effect_filter_sepia extends rex_effect_abstract
     {
         $this->media->asImage();
         $img = $this->media->getImage();
-        imagefilter($img,IMG_FILTER_GRAYSCALE);
-        imagefilter($img,IMG_FILTER_BRIGHTNESS,-30);
-        imagefilter($img,IMG_FILTER_COLORIZE, 90, 55, 30);
+        imagefilter($img, IMG_FILTER_GRAYSCALE);
+        imagefilter($img, IMG_FILTER_BRIGHTNESS, -30);
+        imagefilter($img, IMG_FILTER_COLORIZE, 90, 55, 30);
         $this->keepTransparent($img);
         $this->media->setImage($img);
     }
