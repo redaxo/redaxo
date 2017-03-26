b770353645da9228d2bfc503a7ec38e75de25580
Hirbod <Hirbod@users.noreply.github.com>
2017-03-26 19:58:45 +0200

    added brightness and contrast filter effect (#1166)

---
+++ redaxo/src/addons/media_manager/lib/effects/effect_filter_brightness.php
@@ -0,0 +1,36 @@
+<?php
+
+class rex_effect_filter_brightness extends rex_effect_abstract
+{
+    public function execute()
+    {
+        $this->params['brightness'] = (int) $this->params['brightness'];
+        if (!$this->params['brightness']) {
+            $this->params['brightness'] = 0;
+        }
+        if ($this->params['brightness'] < -255) {
+            $this->params['brightness'] = -255;
+        }
+        if ($this->params['brightness'] > 255) {
+            $this->params['brightness'] = 255;
+        }
+        $this->media->asImage();
+        $img = $this->media->getImage();
+
+        imagefilter($img, IMG_FILTER_BRIGHTNESS, $this->params['brightness']);
+        $this->media->setImage($img);
+    }
+
+    public function getParams()
+    {
+        return [
+            [
+                'label' => rex_i18n::msg('media_manager_effect_brightness'),
+                'notice' => rex_i18n::msg('media_manager_effect_brightness_notice'),
+                'name' => 'brightness',
+                'type' => 'int',
+                'default' => '',
+            ],
+        ];
+    }
+}
---
+++ redaxo/src/addons/media_manager/lib/effects/effect_filter_contrast.php
@@ -0,0 +1,36 @@
+<?php
+
+class rex_effect_filter_contrast extends rex_effect_abstract
+{
+    public function execute()
+    {
+        $this->params['contrast'] = (int) $this->params['contrast'];
+        if (!$this->params['contrast']) {
+            $this->params['contrast'] = 0;
+        }
+        if ($this->params['contrast'] < -100) {
+            $this->params['contrast'] = -100;
+        }
+        if ($this->params['contrast'] > 100) {
+            $this->params['contrast'] = 100;
+        }
+        $this->media->asImage();
+        $img = $this->media->getImage();
+
+        imagefilter($img, IMG_FILTER_CONTRAST, $this->params['contrast']);
+        $this->media->setImage($img);
+    }
+
+    public function getParams()
+    {
+        return [
+            [
+                'label' => rex_i18n::msg('media_manager_effect_contrast'),
+                'notice' => rex_i18n::msg('media_manager_effect_contrast_notice'),
+                'name' => 'contrast',
+                'type' => 'int',
+                'default' => '',
+            ],
+        ];
+    }
+}
