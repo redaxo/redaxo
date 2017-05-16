8b539936efce1f78b6b5666c50c9797607e7efed
Gregor Harlan <mail@gh01.de>
2017-05-16 19:28:43 +0200

    cs fix

--- redaxo/src/core/lib/rex.php
+++ redaxo/src/core/lib/rex.php
@@ -317,7 +317,7 @@ class rex
             $command = 'which git 2>&1 1>/dev/null && cd '. escapeshellarg($path) .' && git show --oneline -s';
             @exec($command, $output, $exitCode);
             if ($exitCode === 0) {
-                $output = implode("", $output);
+                $output = implode('', $output);
                 if (preg_match('{^[0-9a-f]+}', $output, $matches)) {
                     $gitHash[$path] = $matches[0];
                 }
