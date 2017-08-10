3c3c8695bee1f6d82261a09612ef0c6f6124f5ac
Gregor Harlan <mail@gh01.de>
2017-08-10 22:48:31 +0200

    path-provider: Bessere Vererbung und eigene Methode für bin-Ordner (#1263)

--- redaxo/src/core/lib/util/path_default_provider.php
+++ redaxo/src/core/lib/util/path_default_provider.php
@@ -33,7 +33,7 @@ class rex_path_default_provider
     }
 
     /**
-     * Returns a base path.
+     * Returns the base/root path.
      *
      * @param string $file File
      *
@@ -48,7 +48,7 @@ class rex_path_default_provider
     }
 
     /**
-     * Returns the path to the frontend.
+     * Returns the path to the frontend (the document root).
      *
      * @param string $file File
      *
@@ -70,7 +70,7 @@ class rex_path_default_provider
     }
 
     /**
-     * Returns the path to the backend.
+     * Returns the path to the backend (folder where the backend controller is placed).
      *
      * @param string $file File
      *
@@ -79,7 +79,7 @@ class rex_path_default_provider
     public function backend($file)
     {
         if ($this->provideAbsolutes) {
-            return $this->base($this->backend . '/' . $file);
+            return $this->frontend($this->backend . '/' . $file);
         }
         return $this->backend . $file;
     }
@@ -103,7 +103,7 @@ class rex_path_default_provider
      */
     public function media($file)
     {
-        return $this->base('media/' . $file);
+        return $this->frontend('media/' . $file);
     }
 
     /**
@@ -115,7 +115,7 @@ class rex_path_default_provider
      */
     public function assets($file)
     {
-        return $this->base('assets/' . $file);
+        return $this->frontend('assets/' . $file);
     }
 
     /**
@@ -159,6 +159,18 @@ class rex_path_default_provider
     public function pluginAssets($addon, $plugin, $file)
     {
         return $this->addonAssets($addon, 'plugins/' . $plugin . '/' . $file);
+    }
+
+    /**
+     * Returns the path to the bin folder.
+     *
+     * @param string $file File
+     *
+     * @return string
+     */
+    public function bin($file)
+    {
+        return $this->backend('bin/' . $file);
     }
 
     /**
