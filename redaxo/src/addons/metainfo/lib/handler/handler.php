0747c0ebc5e1eacb9ef5fd6f322fbdd1813a8096
Gregor Harlan <mail@gh01.de>
2017-09-08 14:28:04 +0200

    Metainfos: checked-Status bei Checkboxen mit value 0 richtig setzen
    fixes #1129

--- redaxo/src/addons/metainfo/lib/handler/handler.php
+++ redaxo/src/addons/metainfo/lib/handler/handler.php
@@ -57,7 +57,7 @@ abstract class rex_metainfo_handler
                     $dbvalues = explode('|+|', $activeItem->getValue($name));
                 } else {
                     // Neue Notation mit | als Trenner
-                    $dbvalues = explode('|', $activeItem->getValue($name));
+                    $dbvalues = explode('|', trim($activeItem->getValue($name), '|'));
                 }
             } else {
                 $dbvalues = (array) $sqlFields->getValue('default');
@@ -171,7 +171,7 @@ abstract class rex_metainfo_handler
                         }
 
                         $selected = '';
-                        if (in_array($key, $dbvalues)) {
+                        if (in_array((string) $key, $dbvalues, true)) {
                             $selected = ' checked="checked"';
                         }
 
