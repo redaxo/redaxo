bf82b75f2a8170a6dc97f9d16eaf9d7da29eee31
Gregor Harlan <mail@gh01.de>
2017-09-08 11:15:58 +0200

    htmlspecialchars in Systemlog nutzen

--- redaxo/src/core/pages/system.log.php
+++ redaxo/src/core/pages/system.log.php
@@ -59,10 +59,10 @@ if ($file = new rex_log_file($logFile)) {
         $content .= '
                     <tr class="rex-state-' . $class . '">
                         <td data-title="' . rex_i18n::msg('syslog_timestamp') . '">' . $entry->getTimestamp('%d.%m.%Y %H:%M:%S') . '</td>
-                        <td data-title="' . rex_i18n::msg('syslog_type') . '">' . $data[0] . '</td>
-                        <td data-title="' . rex_i18n::msg('syslog_message') . '">' . $data[1] . '</td>
-                        <td data-title="' . rex_i18n::msg('syslog_file') . '"><div class="rex-word-break">' . (isset($data[2]) ? $data[2] : '') . '</div></td>
-                        <td class="rex-table-number" data-title="' . rex_i18n::msg('syslog_line') . '">' . (isset($data[3]) ? $data[3] : '') . '</td>
+                        <td data-title="' . rex_i18n::msg('syslog_type') . '">' . htmlspecialchars($data[0]) . '</td>
+                        <td data-title="' . rex_i18n::msg('syslog_message') . '">' . htmlspecialchars($data[1]) . '</td>
+                        <td data-title="' . rex_i18n::msg('syslog_file') . '"><div class="rex-word-break">' . (isset($data[2]) ? htmlspecialchars($data[2]) : '') . '</div></td>
+                        <td class="rex-table-number" data-title="' . rex_i18n::msg('syslog_line') . '">' . (isset($data[3]) ? htmlspecialchars($data[3]) : '') . '</td>
                     </tr>';
     }
 }
