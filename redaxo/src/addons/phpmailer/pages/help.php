3ed8e51c64de920b2b60e9adca11f6b97d775dd3
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-08 16:27:57 +0200

    rex_markdown::factory()->parse()
    anstelle von parsedown

--- redaxo/src/addons/phpmailer/pages/help.php
+++ redaxo/src/addons/phpmailer/pages/help.php
@@ -10,12 +10,8 @@
  * @var rex_addon $this
  */
 $file = rex_file::get(rex_path::addon('phpmailer','README.md'));
-$Parsedown = new Parsedown();
-$body =  '<div id="phpmailer">'.$Parsedown->text($file);
-
+$body = rex_markdown::factory()->parse($file);
 $fragment = new rex_fragment();
-
 $fragment->setVar('body', $body, false);
 $content = $fragment->parse('core/page/section.php');
-
 echo $content;
