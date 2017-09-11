538d442e7fac9d486f655dd7ef2ac2709ab1445a
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-11 10:15:38 +0200

    phpmailer::checkmail + help (#1281)

--- redaxo/src/addons/phpmailer/install.php
+++ redaxo/src/addons/phpmailer/install.php
@@ -11,7 +11,8 @@
  */
 
 if (!$this->hasConfig()) {
-    $this->setConfig('from', 'from@example.com');
+    $this->setConfig('from', '');
+    $this->setConfig('test_address', '');
     $this->setConfig('fromname', 'Mailer');
     $this->setConfig('confirmto', '');
     $this->setConfig('bcc', '');
