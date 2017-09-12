cbff9553c0e2d225ef75caed5b68b1f3e0ba8505
Gregor Harlan <mail@gh01.de>
2017-09-12 18:16:28 +0200

    rex_socket: Bessere Fehlermeldungen

--- redaxo/src/core/lib/util/socket/socket.php
+++ redaxo/src/core/lib/util/socket/socket.php
@@ -255,11 +255,31 @@ class rex_socket
     protected function openConnection()
     {
         $host = ($this->ssl ? 'ssl://' : '') . $this->host;
-        if (!($this->stream = @fsockopen($host, $this->port, $errno, $errstr))) {
+
+        $prevError = null;
+        set_error_handler(function ($errno, $errstr) use (&$prevError) {
+            if (null === $prevError) {
+                $prevError = $errstr;
+            }
+        });
+
+        if ($this->stream = @fsockopen($host, $this->port, $errno, $errstr)) {
+            stream_set_timeout($this->stream, $this->timeout);
+
+            return;
+        }
+
+        restore_error_handler();
+
+        if ($errstr) {
             throw new rex_socket_exception($errstr . ' (' . $errno . ')');
         }
 
-        stream_set_timeout($this->stream, $this->timeout);
+        if ($prevError) {
+            throw new rex_socket_exception($prevError);
+        }
+
+        throw new rex_socket_exception('Unknown error.');
     }
 
     /**
