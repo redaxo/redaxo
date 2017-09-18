113d54ea7addd813bbca4e5314399717af556a64
Gregor Harlan <mail@gh01.de>
2017-09-18 12:49:53 +0200

    cs fix

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -12,59 +12,54 @@
 $content = '';
 $emptymail = '1';
 
-if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '' ) {
-	$emptymail ='';
+if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '') {
+    $emptymail = '';
 }
 
-if ($emptymail!='') {
-  // PHPMailer-Instanz 
-  $mail = new rex_mailer();
- 
-  //Absenderadresse überschreiben
- // $mail->From = "absender@domain.tld";
-  
-  //Absendername überschreiben
-  // $mail->FromName = "Vorname Nachname";
-  
-  // Antwortadresse festlegen 
-  // $mail->AddReplyTo("username@domain.com", "Software Simian");
-  
-  // Empfänger 
-  $mail->AddAddress($this->getConfig('test_address'));
-  
-  // Empfänger als CC hinzufügen - Weitere anlegen wenn mehrere erwünscht
-  // $mail->AddCC("empfaenger2@domain.tld);
-  
-  // Empfänger als BCC hinzufügen - Weitere anlegen wenn mehrere erwünscht
-  // $mail->AddBCC("empfaenger3@domain.tld");
- 
-  //Betreff der E-Mail 
-  $mail->Subject = "PHPMailer-Test";
- 
-  //Text der EMail setzen
-  $mail->Body = "Hi \n\n this mail was sent by PHPMailer!";
-  
-  //Überprüfen ob E-Mail gesendet wurde
-  if(!$mail->Send()) {
-     $content .= '<div class="alert alert-danger">';
-     $content .=  '<h2>'.$this->i18n('checkmail_error_headline') . '</h2><hr>';
-     $content .=  $this->i18n('checkmail_error') . ': ' . $mail->ErrorInfo;
-     $content .=  '</div>';
-  }
-  else {
-     $content .=  '<div class="alert alert-success">';
-     $content .=  '<strong>'.$this->i18n('checkmail_send') . '</strong> ' . $this->getConfig('test_address') . '<br>' . $this->i18n('checkmail_info');
-     $content .=  '<br><br><strong>'.$this->i18n('checkmail_info_subject').'</strong>';
-	 $content .=  '</div>';
-  }
-  
-}
+if ($emptymail != '') {
+    // PHPMailer-Instanz
+    $mail = new rex_mailer();
 
-else {
-    $content .=  '<div class="alert alert-warning">';
-	$content .=  $this->i18n('checkmail_noadress');
-	$content .=  '</div>';
-	
+    //Absenderadresse überschreiben
+    // $mail->From = "absender@domain.tld";
+
+    //Absendername überschreiben
+    // $mail->FromName = "Vorname Nachname";
+
+    // Antwortadresse festlegen
+    // $mail->AddReplyTo("username@domain.com", "Software Simian");
+
+    // Empfänger
+    $mail->AddAddress($this->getConfig('test_address'));
+
+    // Empfänger als CC hinzufügen - Weitere anlegen wenn mehrere erwünscht
+    // $mail->AddCC("empfaenger2@domain.tld);
+
+    // Empfänger als BCC hinzufügen - Weitere anlegen wenn mehrere erwünscht
+    // $mail->AddBCC("empfaenger3@domain.tld");
+
+    //Betreff der E-Mail
+    $mail->Subject = 'PHPMailer-Test';
+
+    //Text der EMail setzen
+    $mail->Body = "Hi \n\n this mail was sent by PHPMailer!";
+
+    //Überprüfen ob E-Mail gesendet wurde
+    if (!$mail->Send()) {
+        $content .= '<div class="alert alert-danger">';
+        $content .= '<h2>'.$this->i18n('checkmail_error_headline') . '</h2><hr>';
+        $content .= $this->i18n('checkmail_error') . ': ' . $mail->ErrorInfo;
+        $content .= '</div>';
+    } else {
+        $content .= '<div class="alert alert-success">';
+        $content .= '<strong>'.$this->i18n('checkmail_send') . '</strong> ' . $this->getConfig('test_address') . '<br>' . $this->i18n('checkmail_info');
+        $content .= '<br><br><strong>'.$this->i18n('checkmail_info_subject').'</strong>';
+        $content .= '</div>';
+    }
+} else {
+    $content .= '<div class="alert alert-warning">';
+    $content .= $this->i18n('checkmail_noadress');
+    $content .= '</div>';
 }
 
 $fragment = new rex_fragment();
@@ -72,5 +67,3 @@ $fragment->setVar('title', $this->i18n('checkmail_headline'));
 $fragment->setVar('body', $content, false);
 $out = $fragment->parse('core/page/section.php');
 echo $out;
-
-
