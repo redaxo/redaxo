e9f5f2586673343401d93ff84432a11f2b34026e
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-18 12:04:57 +0200

    $content gesetzt

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -9,15 +9,14 @@
  *
  * @var rex_addon $this
  */
+$content = '';
 $emptymail = '1';
 
-if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '' )
-{
+if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '' ) {
 	$emptymail ='';
 }
 
-if ($emptymail!='')
-{
+if ($emptymail!='') {
   // PHPMailer-Instanz 
   $mail = new rex_mailer();
  
@@ -46,16 +45,13 @@ if ($emptymail!='')
   $mail->Body = "Hi \n\n this mail was sent by PHPMailer!";
   
   //Überprüfen ob E-Mail gesendet wurde
-  if(!$mail->Send())
-  {
+  if(!$mail->Send()) {
      $content .= '<div class="alert alert-danger">';
      $content .=  '<h2>'.$this->i18n('checkmail_error_headline') . '</h2><hr>';
      $content .=  $this->i18n('checkmail_error') . ': ' . $mail->ErrorInfo;
      $content .=  '</div>';
   }
-  else
-  {
-  	
+  else {
      $content .=  '<div class="alert alert-success">';
      $content .=  '<strong>'.$this->i18n('checkmail_send') . '</strong> ' . $this->getConfig('from') . '<br>' . $this->i18n('checkmail_info');
      $content .=  '<br><br><strong>'.$this->i18n('checkmail_info_subject').'</strong>';
