3d6585be94917a75f5b5cf2451c2c050a6c6a8c5
Daniel Weitenauer <github@studio-ahoi.de>
2017-09-24 19:52:42 +0200

    There were to api functions to move a category, keep one as comment, fixes #1

--- redaxo/src/addons/structure/lib/buttons/category_move/api_category_move.php
+++ redaxo/src/addons/structure/lib/buttons/category_move/api_category_move.php
@@ -29,3 +29,39 @@ class rex_api_category_move extends rex_api_function
         throw new rex_api_exception(rex_i18n::msg('no_rights_to_this_function'));
     }
 }
+
+// It seems, this class was not used anywhere, instead the code before this was used
+
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+/*
+class rex_api_category_move extends rex_api_function
+{
+    public function execute()
+    {
+        // the category to move
+        $catId = rex_request('category-id', 'int');
+        // the destination category in which the given category will be moved
+        $newCatId = rex_request('new-category-id', 'int');
+        // a optional priority for the moved category
+        $newPriority = rex_request('new-priority', 'int', 0);
+
+        $user = rex::getUser();
+
+        // check permissions
+        if ($user->getComplexPerm('structure')->hasCategoryPerm($catId) && $user->getComplexPerm('structure')->hasCategoryPerm($newCatId)) {
+            rex_category_service::moveCategory($catId, $newCatId);
+
+            // doesnt matter which clang id
+            $data['catpriority'] = $newPriority;
+            rex_category_service::editCategory($catId, 0, $data);
+
+            $result = new rex_api_result(true, rex_i18n::msg('category_status_updated'));
+            return $result;
+        }
+        throw new rex_api_exception('user has no permission for this category!');
+    }
+}*/
