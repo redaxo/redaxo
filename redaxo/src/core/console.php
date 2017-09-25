25e45d3588356a66fcaa32af3365392d2edb48c5
Gregor Harlan <mail@gh01.de>
2017-09-25 15:25:42 +0200

    Console: Alle Packages immer listen, aber ggf. nur ydeploy booten

--- redaxo/src/core/console.php
+++ redaxo/src/core/console.php
@@ -7,11 +7,15 @@ require __DIR__.'/boot.php';
 // force debug mode to enable output of notices/warnings and dump() function
 rex::setProperty('debug', true);
 
+rex_addon::initialize();
+
+foreach (rex::getConfig('package-order') as $packageId) {
+    rex_package::get($packageId)->enlist();
+}
+
 $application = new rex_console_application();
 
 rex::setProperty('console', $application);
-
-rex_addon::initialize();
 
 $application->setCommandLoader(new rex_console_command_loader());
 
