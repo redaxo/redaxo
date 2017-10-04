742be83b241101aac0961f2a7cd650083ec55d61
Gregor Harlan <mail@gh01.de>
2017-10-04 16:18:30 +0200

    Mediapool security fixes (#1384)

--- redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
+++ redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
@@ -18,6 +18,11 @@ function rex_mediapool_filename($FILENAME, $doSubindexing = true)
 {
     // ----- neuer filename und extension holen
     $NFILENAME = rex_string::normalize($FILENAME, '_', '.-');
+
+    if ('.' === $NFILENAME[0]) {
+        $NFILENAME[0] = '_';
+    }
+
     if (strrpos($NFILENAME, '.') != '') {
         $NFILE_NAME = substr($NFILENAME, 0, strlen($NFILENAME) - (strlen($NFILENAME) - strrpos($NFILENAME, '.')));
         $NFILE_EXT = substr($NFILENAME, strrpos($NFILENAME, '.'), strlen($NFILENAME) - strrpos($NFILENAME, '.'));
