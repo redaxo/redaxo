d494da36a7dfcc6a20898d3e2d791efe68e0a675
Thomas Skerbis <thomas.skerbis@mac.com>
2017-10-08 19:06:16 +0200

    Scheckmail unnötige Kommentare entfernt

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -15,36 +15,11 @@ $emptymail = '1';
 if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '') {
     $emptymail = '';
 }
-
 if ($emptymail != '') {
-    // PHPMailer-Instanz
     $mail = new rex_mailer();
-
-    //Absenderadresse überschreiben
-    // $mail->From = "absender@domain.tld";
-
-    //Absendername überschreiben
-    // $mail->FromName = "Vorname Nachname";
-
-    // Antwortadresse festlegen
-    // $mail->AddReplyTo("username@domain.com", "Software Simian");
-
-    // Empfänger
     $mail->AddAddress($this->getConfig('test_address'));
-
-    // Empfänger als CC hinzufügen - Weitere anlegen wenn mehrere erwünscht
-    // $mail->AddCC("empfaenger2@domain.tld);
-
-    // Empfänger als BCC hinzufügen - Weitere anlegen wenn mehrere erwünscht
-    // $mail->AddBCC("empfaenger3@domain.tld");
-
-    //Betreff der E-Mail
     $mail->Subject = 'PHPMailer-Test';
-
-    //Text der EMail setzen
     $mail->Body = "Hi \n\n this mail was sent by PHPMailer!";
-
-    //Überprüfen ob E-Mail gesendet wurde
     if (!$mail->Send()) {
         $content .= '<div class="alert alert-danger">';
         $content .= '<h2>'.$this->i18n('checkmail_error_headline') . '</h2><hr>';
@@ -61,7 +36,6 @@ if ($emptymail != '') {
     $content .= $this->i18n('checkmail_noadress');
     $content .= '</div>';
 }
-
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('checkmail_headline'));
 $fragment->setVar('body', $content, false);
