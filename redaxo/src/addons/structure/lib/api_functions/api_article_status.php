27a5e25bd44902462ec6a2ded475474f977648d0
Daniel Weitenauer <github@studio-ahoi.de>
2017-10-11 10:59:44 +0200

    Restore correct file content

--- redaxo/src/addons/structure/lib/api_functions/api_article_status.php
+++ redaxo/src/addons/structure/lib/api_functions/api_article_status.php
@@ -4,29 +4,24 @@
  *
  * @internal
  */
-class rex_button_article_status extends rex_structure_button
+class rex_api_article_status extends rex_api_function
 {
-    public function get()
+    public function execute()
     {
-        $article = rex_article::get($this->edit_id);
-        $category_id = $article->getCategoryId();
+        $category_id = rex_request('category_id', 'int');
+        $article_id = rex_request('article_id', 'int');
+        $clang = rex_request('clang', 'int');
         $user = rex::getUser();
 
-        if ($article->isStartArticle() || !$user->hasPerm('publishArticle[]') || !$user->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            return '';
+        // check permissions
+        if ($user->getComplexPerm('structure')->hasCategoryPerm($category_id) && $user->hasPerm('publishArticle[]')) {
+            rex_article_service::articleStatus($article_id, $clang);
+
+            $result = new rex_api_result(true, rex_i18n::msg('article_status_updated'));
+
+            return $result;
         }
 
-        $status_index = (int) $article->isOnline();
-        $states = rex_article_service::statusTypes();
-        $status = $states[$status_index][0];
-        $status_class = $states[$status_index][1];
-        $status_icon = $states[$status_index][2];
-
-        $params = array_merge($this->params, [
-            'rex-api-call' => 'article_status',
-            'article_id' => $this->edit_id,
-        ]);
-
-        return '<a class="btn btn-default '.$status_class.'" href="'.$this->context->getUrl($params).'" title="'.$status.'"><i class="rex-icon '.$status_icon.'"></i></a>';
+        throw new rex_api_exception('user has no permission for this article!');
     }
 }
