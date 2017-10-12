redaxo/redaxo

commit 4dcb6e1cf874011cea72738453ba65626bd1308e

Die Dateien api_article und api_category aufteilen (#1372)

redaxo/src/addons/structure/lib/api_functions/api_article_status.php
@@ -1,0 +1,32 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_button_article_status extends rex_structure_button
+{
+    public function get()
+    {
+        $article = rex_article::get($this->edit_id);
+        $category_id = $article->getCategoryId();
+        $user = rex::getUser();
+
+        if ($article->isStartArticle() || !$user->hasPerm('publishArticle[]') || !$user->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
+            return '';
+        }
+
+        $status_index = (int) $article->isOnline();
+        $states = rex_article_service::statusTypes();
+        $status = $states[$status_index][0];
+        $status_class = $states[$status_index][1];
+        $status_icon = $states[$status_index][2];
+
+        $params = array_merge($this->params, [
+            'rex-api-call' => 'article_status',
+            'article_id' => $this->edit_id,
+        ]);
+
+        return '<a class="btn btn-default '.$status_class.'" href="'.$this->context->getUrl($params).'" title="'.$status.'"><i class="rex-icon '.$status_icon.'"></i></a>';
+    }
+}

redaxo/src/addons/structure/lib/api_article.php
@@ -1,207 +1,0 @@
-<?php
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article_add extends rex_api_function
-{
-    public function execute()
-    {
-        $category_id = rex_request('category_id', 'int');
-
-        // check permissions
-        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        $data = [];
-        $data['name'] = rex_post('article-name', 'string');
-        $data['priority'] = rex_post('article-position', 'int');
-        $data['template_id'] = rex_post('template_id', 'int');
-        $data['category_id'] = $category_id;
-
-        $result = new rex_api_result(true, rex_article_service::addArticle($data));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article_edit extends rex_api_function
-{
-    public function execute()
-    {
-        $category_id = rex_request('category_id', 'int');
-        $article_id = rex_request('article_id', 'int');
-        $clang = rex_request('clang', 'int');
-
-        // check permissions
-        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        // --------------------- ARTIKEL EDIT
-        $data = [];
-        $data['priority'] = rex_post('article-position', 'int');
-        $data['name'] = rex_post('article-name', 'string');
-        $data['template_id'] = rex_post('template_id', 'int');
-
-        $result = new rex_api_result(true, rex_article_service::editArticle($article_id, $clang, $data));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article_delete extends rex_api_function
-{
-    public function execute()
-    {
-        $category_id = rex_request('category_id', 'int');
-        $article_id = rex_request('article_id', 'int');
-
-        // check permissions
-        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        $result = new rex_api_result(true, rex_article_service::deleteArticle($article_id));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article_status extends rex_api_function
-{
-    public function execute()
-    {
-        $catId = rex_request('category_id', 'int');
-        $article_id = rex_request('article_id', 'int');
-        $clang = rex_request('clang', 'int');
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // check permissions
-        if ($user->getComplexPerm('structure')->hasCategoryPerm($catId) && $user->hasPerm('publishArticle[]')) {
-            rex_article_service::articleStatus($article_id, $clang);
-
-            $result = new rex_api_result(true, rex_i18n::msg('article_status_updated'));
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this article!');
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article2category extends rex_api_function
-{
-    public function execute()
-    {
-        $article_id = rex_request('article_id', 'int');
-
-        $ooArticle = rex_article::get($article_id);
-        $category_id = $ooArticle->getCategoryId();
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // check permissions
-        if ($user->hasPerm('article2category[]') && rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            if (rex_article_service::article2category($article_id)) {
-                $result = new rex_api_result(true, rex_i18n::msg('content_tocategory_ok'));
-            } else {
-                $result = new rex_api_result(false, rex_i18n::msg('content_tocategory_failed'));
-            }
-
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this article!');
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category2article extends rex_api_function
-{
-    public function execute()
-    {
-        $article_id = rex_request('article_id', 'int');
-
-        $ooArticle = rex_article::get($article_id);
-        $category_id = $ooArticle->getCategoryId();
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // article2category und category2article verwenden das gleiche Recht: article2category
-        if ($user->hasPerm('article2category[]') && rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            if (rex_article_service::category2article($article_id)) {
-                $result = new rex_api_result(true, rex_i18n::msg('content_toarticle_ok'));
-            } else {
-                $result = new rex_api_result(false, rex_i18n::msg('content_toarticle_failed'));
-            }
-
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this article!');
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_article2startarticle extends rex_api_function
-{
-    public function execute()
-    {
-        $article_id = rex_request('article_id', 'int');
-
-        $ooArticle = rex_article::get($article_id);
-        $category_id = $ooArticle->getCategoryId();
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // article2category und category2article verwenden das gleiche Recht: article2category
-        if ($user->hasPerm('article2startarticle[]') && rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            if (rex_article_service::article2startarticle($article_id)) {
-                $result = new rex_api_result(true, rex_i18n::msg('content_tostartarticle_ok'));
-            } else {
-                $result = new rex_api_result(false, rex_i18n::msg('content_tostartarticle_failed'));
-            }
-
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this article!');
-    }
-}

redaxo/src/addons/structure/lib/api_category.php
@@ -1,144 +1,0 @@
-<?php
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category_add extends rex_api_function
-{
-    public function execute()
-    {
-        $parentId = rex_request('parent-category-id', 'int');
-
-        // check permissions
-        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($parentId)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        // prepare and validate parameters
-        $data = [];
-        $data['catpriority'] = rex_post('category-position', 'int');
-        $data['catname'] = rex_post('category-name', 'string');
-
-        $result = new rex_api_result(true, rex_category_service::addCategory($parentId, $data));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category_edit extends rex_api_function
-{
-    public function execute()
-    {
-        $catId = rex_request('category-id', 'int');
-        $clangId = rex_request('clang', 'int');
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // check permissions
-        if (!$user->getComplexPerm('structure')->hasCategoryPerm($catId)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        // prepare and validate parameters
-        $data = [];
-        $data['catpriority'] = rex_post('category-position', 'int');
-        $data['catname'] = rex_post('category-name', 'string');
-
-        $result = new rex_api_result(true, rex_category_service::editCategory($catId, $clangId, $data));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category_delete extends rex_api_function
-{
-    public function execute()
-    {
-        $catId = rex_request('category-id', 'int');
-
-        // check permissions
-        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($catId)) {
-            throw new rex_api_exception('user has no permission for this category!');
-        }
-
-        $result = new rex_api_result(true, rex_category_service::deleteCategory($catId));
-        return $result;
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category_status extends rex_api_function
-{
-    public function execute()
-    {
-        $catId = rex_request('category-id', 'int');
-        $clangId = rex_request('clang', 'int');
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // check permissions
-        if ($user->getComplexPerm('structure')->hasCategoryPerm($catId) && $user->hasPerm('publishArticle[]')) {
-            rex_category_service::categoryStatus($catId, $clangId);
-
-            $result = new rex_api_result(true, rex_i18n::msg('category_status_updated'));
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this category!');
-    }
-}
-
-/**
- * @package redaxo\structure
- *
- * @internal
- */
-class rex_api_category_move extends rex_api_function
-{
-    public function execute()
-    {
-        // the category to move
-        $catId = rex_request('category-id', 'int');
-        // the destination category in which the given category will be moved
-        $newCatId = rex_request('new-category-id', 'int');
-        // a optional priority for the moved category
-        $newPriority = rex_request('new-priority', 'int', 0);
-
-        /**
-         * @var rex_user
-         */
-        $user = rex::getUser();
-
-        // check permissions
-        if ($user->getComplexPerm('structure')->hasCategoryPerm($catId) && $user->getComplexPerm('structure')->hasCategoryPerm($newCatId)) {
-            rex_category_service::moveCategory($catId, $newCatId);
-
-            // doesnt matter which clang id
-            $data['catpriority'] = $newPriority;
-            rex_category_service::editCategory($catId, 0, $data);
-
-            $result = new rex_api_result(true, rex_i18n::msg('category_status_updated'));
-            return $result;
-        }
-        throw new rex_api_exception('user has no permission for this category!');
-    }
-}

redaxo/src/addons/structure/lib/api_functions/api_article2category.php
@@ -1,0 +1,27 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_api_article2category extends rex_api_function
+{
+    public function execute()
+    {
+        $article_id = rex_request('article_id', 'int');
+        $category_id = rex_article::get($article_id)->getCategoryId();
+        $user = rex::getUser();
+
+        // Check permissions
+        if ($user->hasPerm('article2category[]') && $user->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
+            if (rex_article_service::article2category($article_id)) {
+                $result = new rex_api_result(true, rex_i18n::msg('content_tocategory_ok'));
+            } else {
+                $result = new rex_api_result(false, rex_i18n::msg('content_tocategory_failed'));
+            }
+
+            return $result;
+        }
+        throw new rex_api_exception('User has no permission for this article!');
+    }
+}

redaxo/src/addons/structure/lib/api_functions/api_article2startarticle.php
@@ -1,0 +1,28 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_api_article2startarticle extends rex_api_function
+{
+    public function execute()
+    {
+        $article_id = rex_request('article_id', 'int');
+        $category_id = rex_article::get($article_id)->getCategoryId();
+        $user = rex::getUser();
+
+        // Check permissions
+        if ($user->hasPerm('article2startarticle[]') && $user->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
+            if (rex_article_service::article2startarticle($article_id)) {
+                $result = new rex_api_result(true, rex_i18n::msg('content_tostartarticle_ok'));
+            } else {
+                $result = new rex_api_result(false, rex_i18n::msg('content_tostartarticle_failed'));
+            }
+
+            return $result;
+        }
+
+        throw new rex_api_exception('user has no permission for this article!');
+    }
+}

redaxo/src/addons/structure/lib/api_functions/api_article_add.php
@@ -1,0 +1,27 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_api_article_add extends rex_api_function
+{
+    public function execute()
+    {
+        $category_id = rex_request('category_id', 'int');
+
+        // check permissions
+        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
+            throw new rex_api_exception('user has no permission for this category!');
+        }
+
+        $data = [];
+        $data['name'] = rex_post('article-name', 'string');
+        $data['priority'] = rex_post('article-position', 'int');
+        $data['template_id'] = rex_post('template_id', 'int');
+        $data['category_id'] = $category_id;
+
+        $result = new rex_api_result(true, rex_article_service::addArticle($data));
+        return $result;
+    }
+}

redaxo/src/addons/structure/lib/api_functions/api_article_copy.php
@@ -1,0 +1,37 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_api_article_copy extends rex_api_function
+{
+    public function execute()
+    {
+        $article_id = rex_request('article_id', 'int');
+        $clang = rex_request('clang', 'int', 1);
+        $category_copy_id_new = rex_request('category_copy_id_new', 'int');
+        $user = rex::getUser();
+
+        $context = new rex_context([
+            'page' => rex_be_controller::getCurrentPage(),
+            'clang' => $clang,
+        ]);
+
+        if ($user->hasPerm('copyArticle[]') && $user->getComplexPerm('structure')->hasCategoryPerm($category_copy_id_new)) {
+            if (($new_id = rex_article_service::copyArticle($article_id, $category_copy_id_new)) !== false) {
+                $result = new rex_api_result(true, rex_i18n::msg('content_articlecopied'));
+                rex_response::sendRedirect($context->getUrl([
+                    'article_id' => $new_id,
+                    'info' => $result->getMessage(),
+                ], false));
+            } else {
+                $result = new rex_api_result(false, rex_i18n::msg('content_errorcopyarticle'));
+            }
+
+            return $result;
+        }
+
+        throw new rex_api_exception(rex_i18n::msg('no_rights_to_this_function'));
+    }
+}

redaxo/src/addons/structure/lib/api_functions/api_article_delete.php
@@ -1,0 +1,22 @@
+<?php
+/**
+ * @package redaxo\structure
+ *
+ * @internal
+ */
+class rex_api_article_delete extends rex_api_function
+{
+    public function execute()
+    {
+        $category_id = rex_request('category_id', 'int');
+        $article_id = rex_request('article_id', 'int');
+
+        // Check permissions
+        if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
+            throw new rex_api_exception('user has no permission for this category!');
+        }
+
+        $result = new rex_api_result(true, rex_article_service::deleteArticle($article_id));
+        return $result;
+    }
+}

commit 55b32a5f626fd3c2dc53508b74e982351a159969

Datei-Inhalt wiederherstellen (#1401).
Fixes #1400 which was caused by https://github.com/redaxo/redaxo/commit/4dcb6e1cf874011cea72738453ba65626bd1308e

redaxo/src/addons/structure/lib/api_functions/api_article_status.php
@@ -4,29 +4,24 @@
  *
  * @internal
  */
-class rex_button_article_status extends rex_structure_button
+class rex_api_article_status extends rex_api_function
 {
-    public function get()
+    public function execute()
     {
-        $article = rex_article::get($this->edit_id);
-        $category_id = $article->getCategoryId();
+        $category_id = rex_request('category_id', 'int');
+        $article_id = rex_request('article_id', 'int');
+        $clang = rex_request('clang', 'int');
         $user = rex::getUser();
 
-        if ($article->isStartArticle() || !$user->hasPerm('publishArticle[]') || !$user->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
-            return '';
+        // check permissions
+        if ($user->getComplexPerm('structure')->hasCategoryPerm($category_id) && $user->hasPerm('publishArticle[]')) {
+            rex_article_service::articleStatus($article_id, $clang);
+
+            $result = new rex_api_result(true, rex_i18n::msg('article_status_updated'));
+
+            return $result;
         }
 
-        $status_index = (int) $article->isOnline();
-        $states = rex_article_service::statusTypes();
-        $status = $states[$status_index][0];
-        $status_class = $states[$status_index][1];
-        $status_icon = $states[$status_index][2];
-
-        $params = array_merge($this->params, [
-            'rex-api-call' => 'article_status',
-            'article_id' => $this->edit_id,
-        ]);
-
-        return '<a class="btn btn-default '.$status_class.'" href="'.$this->context->getUrl($params).'" title="'.$status.'"><i class="rex-icon '.$status_icon.'"></i></a>';
+        throw new rex_api_exception('user has no permission for this article!');
     }
 }