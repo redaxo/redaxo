874405a6196971c5c95d4cd83a691edc8e8756a7
Gregor Harlan <mail@gh01.de>
2017-11-01 15:15:22 +0100

    Version hochgesetzt, Cache löschen bei Update

--- redaxo/src/addons/media_manager/update.php
+++ redaxo/src/addons/media_manager/update.php
@@ -2,7 +2,7 @@
 
 /** @var rex_addon $this */
 
-if (rex_string::versionCompare($this->getVersion(), '2.3.0-dev', '<')) {
+if (rex_string::versionCompare($this->getVersion(), '2.4.1-dev', '<')) {
     rex_media_manager::deleteCache();
 }
 
