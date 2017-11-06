2ee85aaedd83d372290a3a8ce2c48ef6f0b53e0b
Gregor Harlan <mail@gh01.de>
2017-11-06 11:05:06 +0100

    Header-Effekt: Expires kann gesetzt werden

--- redaxo/src/addons/media_manager/lib/effects/effect_header.php
+++ redaxo/src/addons/media_manager/lib/effects/effect_header.php
@@ -10,6 +10,8 @@ class rex_effect_header extends rex_effect_abstract
         if ($this->params['cache'] == 'no_cache') {
             $this->media->setHeader('Cache-Control', 'must-revalidate, proxy-revalidate, private, no-cache, max-age=0');
             $this->media->setHeader('Expires', 'Sat, 26 Jul 1997 05:00:00 GMT'); // in the past
+        } elseif (isset($this->params['expires']) && 'no' !== $this->params['expires']) {
+            $this->media->setHeader('Expires', gmdate('D, d M Y H:i:s T', strtotime('+'.$this->params['expires'])));
         }
 
         if ($this->params['download'] == 'download') {
@@ -45,6 +47,29 @@ class rex_effect_header extends rex_effect_abstract
                 'type' => 'select',
                 'options' => ['no_cache', 'cache'],
                 'default' => 'no_cache',
+            ],
+            [
+                'label' => rex_i18n::msg('media_manager_effect_header_expires'),
+                'name' => 'expires',
+                'type' => 'select',
+                'options' => ['no', '1 min', '1 hour', '1 day', '1 week', '1 month', '1 year'],
+                'default' => 'no',
+                'suffix' => '
+<script type="text/javascript">
+<!--
+$(function() {
+    var $cache = $("#media-manager-rex-effect-header-cache-select");
+    var $expires = $("#media-manager-rex-effect-header-expires-select");
+
+    $cache.change(function() {
+        if ("no_cache" === $cache.val()) {
+            $expires.prop("disabled", true).val("no");
+        } else {
+            $expires.prop("disabled", false);
+        }
+    }).change();
+});
+//--></script>',
             ],
         ];
     }
