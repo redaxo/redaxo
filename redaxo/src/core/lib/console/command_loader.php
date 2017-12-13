2bbe60396abe47ff752712d6c28ea11d6372e2cc
bloep <der_marcell@hotmail.com>
2017-12-13 22:02:26 +0100

    Added Console Command package:uninstall

--- redaxo/src/core/lib/console/command_loader.php
+++ redaxo/src/core/lib/console/command_loader.php
@@ -19,6 +19,7 @@ class rex_console_command_loader implements CommandLoaderInterface
             'package:activate' => rex_command_package_activate::class,
             'package:deactivate' => rex_command_package_deactivate::class,
             'package:install' => rex_command_package_install::class,
+            'package:uninstall' => rex_command_package_uninstall::class,
         ];
         foreach ($commands as $command => $class) {
             $this->commands[$command] = ['class' => $class];
