4f70298b005a4f5544a4b853dbc5f143d09b8489
Markus Staab <maggus.staab@googlemail.com>
2017-12-15 16:39:16 +0100

    Phpdocs (#1528)

--- redaxo/src/addons/media_manager/lib/media_manager.php
+++ redaxo/src/addons/media_manager/lib/media_manager.php
@@ -20,6 +20,9 @@ class rex_media_manager
     }
 
     /**
+     * Creates a rex_managed_media object for the given file and mediatype.
+     * This object might be used to determine the dimension of a image or similar.
+     *
      * @param string $type Media type
      * @param string $file Media file
      *
