6bb9062f48b4e9bd6e3be0e8e869d2a024f65e76
Markus Staab <maggus.staab@googlemail.com>
2017-12-16 13:33:05 +0100

    Update boot.php

--- redaxo/src/addons/cronjob/boot.php
+++ redaxo/src/addons/cronjob/boot.php
@@ -13,6 +13,11 @@
 define('REX_CRONJOB_LOG_FOLDER', $this->getDataPath());
 define('REX_CRONJOB_TABLE', rex::getTable('cronjob'));
 
+if (rex::getConsole()) {
+    // don't run cronjobs while running console commands
+    return;
+}
+
 rex_extension::register('PACKAGES_INCLUDED', function () {
     foreach ($this->getAvailablePlugins() as $plugin) {
         if (($type = $plugin->getProperty('cronjob_type')) != '') {
@@ -21,16 +26,14 @@ rex_extension::register('PACKAGES_INCLUDED', function () {
     }
 });
 
-if (PHP_SAPI != "cli") {
-    $nexttime = $this->getConfig('nexttime', 0);
+$nexttime = $this->getConfig('nexttime', 0);
 
-    if ($nexttime != 0 && time() >= $nexttime) {
-        $env = rex_cronjob_manager::getCurrentEnvironment();
-        $EP = 'backend' === $env ? 'PAGE_CHECKED' : 'PACKAGES_INCLUDED';
-        rex_extension::register($EP, function () use ($env) {
-            if ('backend' !== $env || !in_array(rex_be_controller::getCurrentPagePart(1), ['setup', 'login', 'cronjob'])) {
-                rex_cronjob_manager_sql::factory()->check();
-            }
-        });
-    }
+if ($nexttime != 0 && time() >= $nexttime) {
+    $env = rex_cronjob_manager::getCurrentEnvironment();
+    $EP = 'backend' === $env ? 'PAGE_CHECKED' : 'PACKAGES_INCLUDED';
+    rex_extension::register($EP, function () use ($env) {
+        if ('backend' !== $env || !in_array(rex_be_controller::getCurrentPagePart(1), ['setup', 'login', 'cronjob'])) {
+            rex_cronjob_manager_sql::factory()->check();
+        }
+    });
 }
