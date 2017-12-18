6431cf54c47c2a9f2d296d7e85b798ebc94bcfcf
Gregor Harlan <mail@gh01.de>
2017-12-18 11:34:00 +0100

    Schema-Dumper (rex_sql_table) (#1427)

--- redaxo/src/core/lib/console/command_loader.php
+++ redaxo/src/core/lib/console/command_loader.php
@@ -16,6 +16,7 @@ class rex_console_command_loader implements CommandLoaderInterface
     {
         $commands = [
             'cache:clear' => rex_command_cache_clear::class,
+            'db:dump-schema' => rex_command_db_dump_schema::class,
             'package:activate' => rex_command_package_activate::class,
             'package:deactivate' => rex_command_package_deactivate::class,
             'package:install' => rex_command_package_install::class,
