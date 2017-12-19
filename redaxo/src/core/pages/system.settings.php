e80dad2af77f75ea43b54bf32c479627f7042209
Gregor Harlan <mail@gh01.de>
2017-12-19 13:21:31 +0100

    CSRF-Schutz für Core (#1551)

--- redaxo/src/core/pages/system.settings.php
+++ redaxo/src/core/pages/system.settings.php
@@ -10,7 +10,11 @@ $success = '';
 
 $func = rex_request('func', 'string');
 
-if ($func == 'setup') {
+$csrfToken = rex_csrf_token::factory('system');
+
+if ($func && !$csrfToken->isValid()) {
+    $error[] = rex_i18n::msg('csrf_token_invalid');
+} elseif ($func == 'setup') {
     // REACTIVATE SETUP
 
     $configFile = rex_path::coreData('config.yml');
@@ -108,11 +112,11 @@ $content = [];
 $content[] = '
                         <h3>' . rex_i18n::msg('delete_cache') . '</h3>
                         <p>' . rex_i18n::msg('delete_cache_description') . '</p>
-                        <p><a class="btn btn-delete" href="' . rex_url::currentBackendPage(['func' => 'generate']) . '">' . rex_i18n::msg('delete_cache') . '</a></p>
+                        <p><a class="btn btn-delete" href="' . rex_url::currentBackendPage(['func' => 'generate'] + $csrfToken->getUrlParams()) . '">' . rex_i18n::msg('delete_cache') . '</a></p>
 
                         <h3>' . rex_i18n::msg('setup') . '</h3>
                         <p>' . rex_i18n::msg('setup_text') . '</p>
-                        <p><a class="btn btn-setup" href="' . rex_url::currentBackendPage(['func' => 'setup']) . '" data-confirm="' . rex_i18n::msg('setup_restart') . '?" data-pjax="false">' . rex_i18n::msg('setup') . '</a></p>';
+                        <p><a class="btn btn-setup" href="' . rex_url::currentBackendPage(['func' => 'setup'] + $csrfToken->getUrlParams()) . '" data-confirm="' . rex_i18n::msg('setup_restart') . '?" data-pjax="false">' . rex_i18n::msg('setup') . '</a></p>';
 
 $content[] = '
                         <h3>' . rex_i18n::msg('version') . '</h3>
@@ -219,6 +223,7 @@ $content = $fragment->parse('core/page/section.php');
 $content = '
 <form id="rex-form-system-setup" action="' . rex_url::currentBackendPage() . '" method="post">
     <input type="hidden" name="func" value="updateinfos" />
+    ' . $csrfToken->getHiddenField() . '
     ' . $content . '
 </form>';
 
