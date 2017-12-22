242638bfd877a9f4709421d016ce0f34aa8a4d03
Gregor Harlan <mail@gh01.de>
2017-12-22 11:06:29 +0100

    Cache-Path bei Nutzung von rex_media_manager::create() korrigiert

--- redaxo/src/addons/media_manager/tests/media_manager_test.php
+++ redaxo/src/addons/media_manager/tests/media_manager_test.php
@@ -4,17 +4,18 @@ class rex_media_manager_test extends PHPUnit_Framework_TestCase
 {
     public function testGetCacheFilename()
     {
-        $manager = new rex_media_manager(new rex_managed_media(__DIR__.'/foo.jpg'));
+        $media = new rex_managed_media(__DIR__.'/foo.jpg');
+        $manager = new rex_media_manager($media);
 
         $cachePath = rex_path::addonCache('media_manager');
         $manager->setCachePath($cachePath);
+
+        $media->setMediaPath(__DIR__.'/bar.gif');
 
         $property = new ReflectionProperty(rex_media_manager::class, 'type');
         $property->setAccessible(true);
         $property->setValue($manager, 'test');
 
-        $_GET['rex_media_file'] = 'bar.gif';
-
-        $this->assertSame($cachePath.'test/bar.gif', $manager->getCacheFilename());
+        $this->assertSame($cachePath.'test/foo.jpg', $manager->getCacheFilename());
     }
 }
