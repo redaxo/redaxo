fe5c26d3b366d7dd6fc1fccc1f8897d55210727c
Gregor Harlan <mail@gh01.de>
2018-02-11 20:03:30 +0100

    Kommentare

--- redaxo/src/addons/backup/lib/backup.php
+++ redaxo/src/addons/backup/lib/backup.php
@@ -212,6 +212,7 @@ class rex_backup
 
         // generated neu erstellen, wenn kein Fehler aufgetreten ist
         if ($error == '') {
+            // delete cache before EP to avoid obsolete caches while running extensions
             rex_delete_cache();
 
             // ----- EXTENSION POINT
@@ -224,6 +225,7 @@ class rex_backup
             // require import skript to do some userside-magic
             self::importScript(str_replace('.sql', '.php', $filename), self::IMPORT_DB, self::IMPORT_EVENT_POST);
 
+            // delete cache again because the extensions and the php script could have changed data again
             $msg .= rex_delete_cache();
             $return['state'] = true;
         }
