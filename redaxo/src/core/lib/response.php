a134f0993f8ca7f039f00c1187b9702dafce50c1
Gregor Harlan <mail@gh01.de>
2018-05-11 11:06:20 +0200

    cs fix

--- redaxo/src/core/lib/response.php
+++ redaxo/src/core/lib/response.php
@@ -15,7 +15,7 @@ class rex_response
     const HTTP_UNAUTHORIZED = '401 Unauthorized';
     const HTTP_INTERNAL_ERROR = '500 Internal Server Error';
     const HTTP_SERVICE_UNAVAILABLE = '503 Service Unavailable';
-    
+
     private static $httpStatus = self::HTTP_OK;
     private static $sentLastModified = false;
     private static $sentEtag = false;
