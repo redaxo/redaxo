d0db233091d1362135bcb0cffc5e5545e69762f0
tbaddade <thomas@redaxo.org>
2018-05-14 21:30:23 +0200

    Anderer Begriff und Icon für Homepage, #1617

--- redaxo/src/addons/structure/functions/function_rex_category.php
+++ redaxo/src/addons/structure/functions/function_rex_category.php
@@ -32,7 +32,7 @@ if ($object) {
     }
 }
 
-$title = '<a href="' . rex_url::backendPage('structure', ['category_id' => 0, 'clang' => $clang]) . '"><i class="rex-icon rex-icon-sitestartarticle"></i> ' . rex_i18n::msg('homepage') . '</a>';
+$title = '<a href="' . rex_url::backendPage('structure', ['category_id' => 0, 'clang' => $clang]) . '"><i class="rex-icon rex-icon-structure-root-level"></i> ' . rex_i18n::msg('website') . '</a>';
 
 $fragment = new rex_fragment();
 $fragment->setVar('id', 'rex-js-structure-breadcrumb', false);
