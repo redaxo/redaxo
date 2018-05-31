567335f2d65bd683e7c827dbc045b5f649b37827
Gregor Harlan <mail@gh01.de>
2018-05-31 21:42:06 +0200

    Console: Neues Passwort kann gesetzt werden (#1778)

--- redaxo/src/core/lib/console/command_loader.php
+++ redaxo/src/core/lib/console/command_loader.php
@@ -24,6 +24,7 @@ class rex_console_command_loader implements CommandLoaderInterface
             'package:install' => rex_command_package_install::class,
             'package:uninstall' => rex_command_package_uninstall::class,
             'setup:check' => rex_command_setup_check::class,
+            'user:set-password' => rex_command_user_set_password::class,
         ];
         foreach ($commands as $command => $class) {
             $this->commands[$command] = ['class' => $class];
