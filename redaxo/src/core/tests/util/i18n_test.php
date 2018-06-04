4ae356026e01db2bbfc9c7b20dce9dcd87445f5f
Gregor Harlan <mail@gh01.de>
2018-06-04 19:16:31 +0200

    tests

--- redaxo/src/core/tests/util/i18n_test.php
+++ redaxo/src/core/tests/util/i18n_test.php
@@ -40,10 +40,28 @@ LANG;
         rex_i18n::addDirectory($this->getPath());
 
         $this->assertSame('abc def', rex_i18n::msg('rex_i18n_test_foo'));
-        $this->assertSame('', rex_i18n::msg('rex_i18n_test_bar'));
+        $this->assertSame('[translate:rex_i18n_test_bar]', rex_i18n::msg('rex_i18n_test_bar'));
         $this->assertSame('ghi', rex_i18n::msg('rex_i18n_test_baz'));
         $this->assertSame('abc=def', rex_i18n::msg('rex_i18n_test_4'));
         $this->assertSame('abc def', rex_i18n::msg('rex_i18n_test_5'));
+    }
+
+    public function testHasMsg()
+    {
+        rex_i18n::addDirectory($this->getPath());
+
+        $this->assertTrue(rex_i18n::hasMsg('rex_i18n_test_foo'));
+        $this->assertFalse(rex_i18n::hasMsg('rex_i18n_test_bar'));
+        $this->assertFalse(rex_i18n::hasMsg('rex_i18n_test_6'));
+    }
+
+    public function testHasMsgOrFallback()
+    {
+        rex_i18n::addDirectory($this->getPath());
+
+        $this->assertTrue(rex_i18n::hasMsgOrFallback('rex_i18n_test_foo'));
+        $this->assertFalse(rex_i18n::hasMsgOrFallback('rex_i18n_test_bar'));
+        $this->assertTrue(rex_i18n::hasMsgOrFallback('rex_i18n_test_6'));
     }
 
     public function testGetMsgFallback()
