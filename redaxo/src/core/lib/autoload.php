cc048fc7d7841188ea192d2a845b98c5a1f53e42
Markus Staab <maggus.staab@googlemail.com>
2018-06-14 12:37:42 +0200

    Update autoload.php

--- redaxo/src/core/lib/autoload.php
+++ redaxo/src/core/lib/autoload.php
@@ -242,7 +242,7 @@ class rex_autoload
         $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dirPath, RecursiveDirectoryIterator::SKIP_DOTS));
         foreach ($iterator as $path => $file) {
             /** @var SplFileInfo $file */
-            if (!$file->isFile() || !isset($extWhitelist[$file->getExtension()])) {
+            if (!$file->isFile() || !in_array($file->getExtension(), ['php', 'inc'])) {
                 continue;
             }
 
