ca3879caf88d636fd895a1326b1066a0c66f2539
Marcel Kuhmann <der_marcell@hotmail.com>
2018-07-09 20:32:58 +0200

    Added be_style:compile Command

---
+++ redaxo/src/addons/be_style/lib/command/compile.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @package redaxo\be_style
+ */
+class rex_be_style_command_compile extends rex_console_command
+{
+    protected function configure()
+    {
+        $this->setName('be_style:compile')
+            ->setDescription('Coverts SCSS files to CSS');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = $this->getStyle($input, $output);
+        $io->title('be_style scss compiler');
+
+        $compiler = new rex_scss_compiler();
+        $scss_files = rex_extension::registerPoint(new rex_extension_point('BE_STYLE_SCSS_FILES', [$this->getPackage()->getPath('scss/master.scss')]));
+        $compiler->setScssFile($scss_files);
+
+        // Compile in backend assets dir
+        $compiler->setCssFile($this->getPackage()->getPath('assets/css/styles.css'));
+        $compiler->compile();
+
+        // Compiled file to copy in frontend assets dir
+        rex_file::copy($this->getPackage()->getPath('assets/css/styles.css'), $this->getPackage()->getAssetsPath('css/styles.css'));
+
+        $io->success('Styles successful compiled');
+    }
+}
