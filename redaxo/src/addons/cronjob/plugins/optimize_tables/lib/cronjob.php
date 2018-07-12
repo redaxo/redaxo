4ca254a1ff99ae5b48635e627eee29514f9e5d6b
Gregor Harlan <mail@gh01.de>
2018-07-12 23:29:18 +0200

    rex_sql: getTables statt showTables nutzen

--- redaxo/src/addons/cronjob/plugins/optimize_tables/lib/cronjob.php
+++ redaxo/src/addons/cronjob/plugins/optimize_tables/lib/cronjob.php
@@ -12,7 +12,7 @@ class rex_cronjob_optimize_tables extends rex_cronjob
 {
     public function execute()
     {
-        $tables = rex_sql::showTables(1, rex::getTablePrefix());
+        $tables = rex_sql::factory()->getTables(rex::getTablePrefix());
         if (is_array($tables) && !empty($tables)) {
             $sql = rex_sql::factory();
             // $sql->setDebug();
