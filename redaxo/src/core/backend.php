d4f13340ebe231ea3191c0f50cb426958ce083b4
Markus Staab <markus.staab@redaxo.de>
2018-07-15 10:26:25 +0200

    disable cache revalidation headers, so the actual long living cache headers will be taken into account

--- redaxo/src/core/backend.php
+++ redaxo/src/core/backend.php
@@ -19,7 +19,11 @@ if (rex_get('asset') && rex_get('buster')) {
         throw new Exception('Assets can only be streamed from within the assets folder');
     }
 
-    rex_response::setHeader('Cache-Control', 'max-age=31536000, immutable');
+    // disable cache revalidation headers, so the actual long living cache headers will be taken into account
+    rex::setProperty('use_last_modified', false);
+    rex::setProperty('use_etag', false);
+
+    rex_response::sendCacheControl('max-age=31536000, immutable');
     $ext = rex_file::extension($assetFile);
     if ('js' === $ext) {
         rex_response::sendFile($assetFile, 'application/javascript');
