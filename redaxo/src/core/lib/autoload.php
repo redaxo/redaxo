01b35e08e00d52bf0ae5d2fefb91a79733e02630
Markus Staab <markus.staab@redaxo.de>
2018-07-15 20:37:07 +0200

    relative pfade in classmaps generieren

--- redaxo/src/core/lib/autoload.php
+++ redaxo/src/core/lib/autoload.php
@@ -158,8 +158,8 @@ class rex_autoload
             }
         }
 
-        $content = "<?php return ". var_export([self::$classes, self::$dirs], true). ";";
-        // $content = str_replace(rex_path::base(), '__DIR__', $content);
+        $content = "<?php\n\$vendorDir = dirname(__FILE__, 4);\nreturn ". var_export([self::$classes, self::$dirs], true). ";";
+        $content = str_replace(rtrim(var_export(rex_path::base(), true), "'"), "\$vendorDir .'". DIRECTORY_SEPARATOR, $content);
         if (!rex_file::put(self::$cacheFile, $content)) {
             throw new Exception("Unable to write autoload cachefile '" . self::$cacheFile . "'!");
         }
