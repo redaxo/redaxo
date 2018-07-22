5fb72617d84f46a5fa8e5ce298faa20557df7ff7
Gregor Harlan <mail@gh01.de>
2018-07-22 19:10:35 +0200

    table

--- redaxo/src/core/pages/system.report.php
+++ redaxo/src/core/pages/system.report.php
@@ -1,11 +1,17 @@
 <?php
 
-foreach (rex_system_report::factory()->get() as $title => $group) {
+$report = rex_system_report::factory()->get();
+
+echo '<div class="row"><div class="col-sm-6">';
+
+foreach ($report as $title => $group) {
+    if ('Packages' === $title) {
+        echo '</div><div class="col-sm-6">';
+    }
+
     $content = '';
 
     foreach ($group as $label => $value) {
-        $content .= '<dt>'.rex_escape($label).'</dt>';
-
         if (is_bool($value)) {
             $class = $value ? 'fa-check text-success' : 'fa-times text-danger';
             $value = '<i class="rex-icon '.$class.'"></i>';
@@ -13,13 +19,20 @@ foreach (rex_system_report::factory()->get() as $title => $group) {
             $value = rex_escape($value);
         }
 
-        $content .= '<dd>'.$value.'</dd>';
+        $content .= '
+            <tr>
+                <th width="120">'.rex_escape($label).'</th>
+                <td>'.$value.'</td>
+            </tr>
+        ';
     }
 
-    $content = '<dl class="dl-horizontal">'.$content.'</dl>';
+    $content = '<table class="table table-hover table-bordered">'.$content.'</table>';
 
     $fragment = new rex_fragment();
     $fragment->setVar('title', $title);
     $fragment->setVar('body', $content, false);
     echo $fragment->parse('core/page/section.php');
 }
+
+echo '</div></div>';
