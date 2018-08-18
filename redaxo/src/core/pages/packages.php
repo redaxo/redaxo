2826880b69a71b9098a52577ca6eb84d3bde074a
Markus Staab <markus.staab@redaxo.de>
2018-08-18 19:36:19 +0200

    locale basierten Übersetzungen für readme Dateien unterstützen
    Dateinamen nach dem Muster README.<locale>.md werden abhängig von der aktuell im backend eingestellten Sprache bevorzugt

--- redaxo/src/core/pages/packages.php
+++ redaxo/src/core/pages/packages.php
@@ -26,6 +26,10 @@ if ($subpage == 'help') {
         ob_start();
         $package->includeFile('help.php');
         $content .= ob_get_clean();
+    } elseif (is_readable($package->getPath('README.'. rex_i18n::getLocale() .'.md'))) {
+        $fragment = new rex_fragment();
+        $fragment->setVar('content', rex_markdown::factory()->parse(rex_file::get($package->getPath('README.'. rex_i18n::getLocale() .'.md'))), false);
+        $content .= $fragment->parse('core/page/docs.php');
     } elseif (is_readable($package->getPath('README.md'))) {
         $fragment = new rex_fragment();
         $fragment->setVar('content', rex_markdown::factory()->parse(rex_file::get($package->getPath('README.md'))), false);
