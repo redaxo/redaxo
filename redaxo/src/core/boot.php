a24d20cc993c3001dc1d9429d290cfd9aa6f6490
tbaddade <thomas@redaxo.org>
2018-08-27 09:32:08 +0200

    Scripttime aus altem Footer als eigenes Element

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -132,7 +132,8 @@ if ('cli' !== PHP_SAPI && !rex::isSetup()) {
 }
 
 // ----------------- Minibar
-rex_minibar::getInstance()->addElement(new rex_minibar_system());
+rex_minibar::getInstance()->addElement(new rex_minibar_element_system());
+rex_minibar::getInstance()->addElement(new rex_minibar_element_time());
 
 if (!rex::isBackend()) {
     rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
