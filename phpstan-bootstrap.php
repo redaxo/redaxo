8cc55322cc71831f31fc615adea20c794b42bf94
Markus Staab <markus.staab@redaxo.de>
2018-08-29 21:45:38 +0200

    Create phpstan-bootstrap.php

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+unset($REX);
+$REX['REDAXO'] = false;
+$REX['HTDOCS_PATH'] = './';
+$REX['BACKEND_FOLDER'] = 'redaxo';
+$REX['LOAD_PAGE'] = false;
+
+require $REX['BACKEND_FOLDER'] . '/src/core/boot.php';
