4d46b4fc31f64984f0b43cec8d9083ba0e1ce8da
Markus Staab <markus.staab@redaxo.de>
2018-09-09 13:03:07 +0200

    fix CS

--- redaxo/src/addons/phpmailer/lib/system_setting_phpmailer_errormail.php
+++ redaxo/src/addons/phpmailer/lib/system_setting_phpmailer_errormail.php
@@ -10,7 +10,6 @@
  */
 class rex_system_setting_phpmailer_errormail extends rex_system_setting
 {
-    
     public function getKey()
     {
         return 'phpmailer_errormail';
@@ -22,19 +21,18 @@ class rex_system_setting_phpmailer_errormail extends rex_system_setting
         $field->setAttribute('class', 'form-control selectpicker');
         $field->setLabel(rex_i18n::msg('system_setting_errormail'));
         $select = $field->getSelect();
-        $select->addOption(rex_i18n::msg('phpmailer_errormail_disabled') , 0);
-        $select->addOption(rex_i18n::msg('phpmailer_errormail_15min') , 900);
-        $select->addOption(rex_i18n::msg('phpmailer_errormail_30min') , 1800);
-        $select->addOption(rex_i18n::msg('phpmailer_errormail_60min') , 3600);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_disabled'), 0);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_15min'), 900);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_30min'), 1800);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_60min'), 3600);
         $select->setSelected(rex_config::get('phpmailer', 'errormail', 1));
         return $field;
     }
 
     public function setValue($value)
     {
-        $value = (int)$value;
+        $value = (int) $value;
         rex_config::set('phpmailer', 'errormail', $value);
         return true;
     }
 }
-
