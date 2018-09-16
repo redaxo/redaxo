a4be97177805bb4cfe7695e90063eef2753590bc
Thomas Skerbis <thomas.skerbis@mac.com>
2018-09-16 23:58:14 +0200

    _DIR_ entfernt und durch  $this->getPath ersetzt
    Funktionen über 
    `require $this->getPath('functions/function_rex_category.php');`
    Dadurch kann kann die index.php z.B. in das Projekt-AddOn kopiert werden und individualisiert werden.

--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -74,7 +74,7 @@ echo rex_view::title(rex_i18n::msg('title_structure'));
 echo rex_view::clangSwitchAsButtons($context);
 
 // --------------------------------------------- Path
-require __DIR__ . '/../functions/function_rex_category.php';
+require $this->getPath('functions/function_rex_category.php');
 
 // -------------- STATUS_TYPE Map
 $catStatusTypes = rex_category_service::statusTypes();
