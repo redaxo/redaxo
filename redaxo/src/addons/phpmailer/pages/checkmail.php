1121523fd79cf6d424e27d2e17f62663882c40b0
Thomas Skerbis <thomas.skerbis@mac.com>
2018-09-19 17:41:58 +0200

    Checkmail mit Infos

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * PHPMailer Addon.
  *
@@ -11,27 +10,61 @@
  */
 $content = '';
 $emptymail = '1';
-
-if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '') {
+$date = new DateTime();
+if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '')
+{
     $emptymail = '';
 }
-if ($emptymail != '') {
+if ($emptymail != '')
+{
     $mail = new rex_mailer();
     $mail->addAddress($this->getConfig('test_address'));
-    $mail->Subject = 'PHPMailer-Test';
-    $mail->Body = "Hi \n\n this mail was sent by PHPMailer!";
-    if (!$mail->send()) {
+    $mail->Subject = 'PHPMailer-Test | ' . rex_escape(rex::getServerName()) . ' | ' . date_format($date, 'Y-m-d H:i:s');
+
+    $devider = "\n--------------------------------------------------";
+    $security_mode = $this->getConfig('security_mode');
+
+    if ($this->getConfig('mailer') == 'smtp')
+    {
+        $smtpinfo = "";
+        $smtpinfo .= "\nHost: " . rex_escape($this->getConfig('host'));
+        $smtpinfo .= "\nPort: " . rex_escape($this->getConfig('port'));
+        $smtpinfo .= $devider;
+
+        if ($security_mode == false)
+        {
+            $security_mode = "manual configured,  " . $this->getConfig('smtpsecure');
+            $security_mode = "\n".$this->i18n('security_mode')."\n" . $security_mode . $devider;
+        }
+
+        else
+        {
+            $security_mode = "Auto";
+            $security_mode = "\n".$this->i18n('security_mode')."\n" . $security_mode . $devider;
+        }
+
+    }
+
+    $mail->Body = $this->i18n('checkmail_greeting') ."\n\n" .  $this->i18n('checkmail_text') ." ". rex::getServerName();
+    $mail->Body .= " \n\n Mailer: \n" . $this->getConfig('mailer') . $devider . $smtpinfo . $security_mode;
+
+    if (!$mail->send())
+    {
         $content .= '<div class="alert alert-danger">';
-        $content .= '<h2>'.$this->i18n('checkmail_error_headline') . '</h2><hr>';
+        $content .= '<h2>' . $this->i18n('checkmail_error_headline') . '</h2><hr>';
         $content .= $this->i18n('checkmail_error') . ': ' . $mail->ErrorInfo;
         $content .= '</div>';
-    } else {
+    }
+    else
+    {
         $content .= '<div class="alert alert-success">';
-        $content .= '<strong>'.$this->i18n('checkmail_send') . '</strong> ' . $this->getConfig('test_address') . '<br>' . $this->i18n('checkmail_info');
-        $content .= '<br><br><strong>'.$this->i18n('checkmail_info_subject').'</strong>';
+        $content .= '<strong>' . $this->i18n('checkmail_send') . '</strong> ' . rex_escape($this->getConfig('test_address')) . '<br>' . $this->i18n('checkmail_info');
+        $content .= '<br><br><strong>' . $this->i18n('checkmail_info_subject') . '</strong>';
         $content .= '</div>';
     }
-} else {
+}
+else
+{
     $content .= '<div class="alert alert-warning">';
     $content .= $this->i18n('checkmail_noadress');
     $content .= '</div>';
