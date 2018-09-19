8b4c1599b8f86858387616c8f5a0a55278f49a7e
tbaddade <thomas@redaxo.org>
2018-09-19 22:05:58 +0200

    Visibility Url für Frontend angepasst, man landete u.U. auf der 404 Seite

--- redaxo/src/core/fragments/core/minibar/minibar_frontend.php
+++ redaxo/src/core/fragments/core/minibar/minibar_frontend.php
@@ -1,8 +1,8 @@
 <div class="rex-minibar" data-minibar="<?= rex_minibar::getInstance()->isVisible() ? 'true' : 'false' ?>">
-    <a class="rex-minibar-opener" href="<?= rex_context::fromGet()->getUrl(['visibility' => true] + rex_api_minibar::getUrlParams()) ?>">
+    <a class="rex-minibar-opener" href="<?= rex_getUrl('', '', ['visibility' => true] + rex_api_minibar::getUrlParams()) ?>">
         <i class="rex-icon rex-icon-minibar-open"></i>
     </a>
-    <a class="rex-minibar-close" href="<?= rex_context::fromGet()->getUrl(['visibility' => false] + rex_api_minibar::getUrlParams()) ?>">
+    <a class="rex-minibar-close" href="<?= rex_getUrl('', '', ['visibility' => false] + rex_api_minibar::getUrlParams()) ?>">
         <i class="rex-icon rex-icon-minibar-close"></i>
     </a>
     <div class="rex-minibar-elements">
