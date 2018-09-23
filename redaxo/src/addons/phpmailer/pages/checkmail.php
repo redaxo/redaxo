7fd34de01fc3e4bdb9fd824531d6e7f554b5b151
Markus Staab <markus.staab@redaxo.de>
2018-09-23 12:00:40 +0200

    fix CS

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -11,12 +11,10 @@
 $content = $smtpinfo = '';
 $emptymail = true;
 $date = new DateTime();
-if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '')
-{
+if ($this->getConfig('from') == '' || $this->getConfig('test_address') == '') {
     $emptymail = false;
 }
-if ($emptymail = true)
-{
+if ($emptymail = true) {
     $mail = new rex_mailer();
     $mail->addAddress($this->getConfig('test_address'));
     $mail->Subject = 'PHPMailer-Test | ' . rex_escape(rex::getServerName()) . ' | ' . date_format($date, 'Y-m-d H:i:s');
@@ -24,54 +22,41 @@ if ($emptymail = true)
     $devider = "\n--------------------------------------------------";
     $security_mode = '';
 
-    if ($this->getConfig('mailer') == 'smtp')
-    {
-
+    if ($this->getConfig('mailer') == 'smtp') {
         $security_mode = $this->getConfig('security_mode');
 
-        $smtpinfo = "";
+        $smtpinfo = '';
         $smtpinfo .= "\nHost: " . rex_escape($this->getConfig('host'));
         $smtpinfo .= "\nPort: " . rex_escape($this->getConfig('port'));
         $smtpinfo .= $devider;
 
-        if ($security_mode == false)
-        {
-            $security_mode = "manual configured,  " . $this->getConfig('smtpsecure');
+        if ($security_mode == false) {
+            $security_mode = 'manual configured,  ' . $this->getConfig('smtpsecure');
             $security_mode = "\n".$this->i18n('security_mode')."\n" . $security_mode . $devider;
-        }
-
-        else
-        {
-            $security_mode = "Auto";
+        } else {
+            $security_mode = 'Auto';
             $security_mode = "\n".$this->i18n('security_mode').": \n" . $security_mode . $devider;
         }
-
     }
 
-    $mail->Body = $this->i18n('checkmail_greeting') ."\n\n" .  $this->i18n('checkmail_text') ." ". rex::getServerName();
+    $mail->Body = $this->i18n('checkmail_greeting') ."\n\n" .  $this->i18n('checkmail_text') .' '. rex::getServerName();
     $mail->Body .= "\n\nDomain: ".  $_SERVER['HTTP_HOST'];
-
 
     $mail->Body .= "\nMailer: " . $this->getConfig('mailer') . $devider.$security_mode;
     $mail->Body .= "\n". $this->i18n('checkmail_domain_note'). "\n". $devider;
 
-    if (!$mail->send())
-    {
+    if (!$mail->send()) {
         $content .= '<div class="alert alert-danger">';
         $content .= '<h2>' . $this->i18n('checkmail_error_headline') . '</h2><hr>';
         $content .= $this->i18n('checkmail_error') . ': ' . $mail->ErrorInfo;
         $content .= '</div>';
-    }
-    else
-    {
+    } else {
         $content .= '<div class="alert alert-success">';
         $content .= '<strong>' . $this->i18n('checkmail_send') . '</strong> ' . rex_escape($this->getConfig('test_address')) . '<br>' . $this->i18n('checkmail_info');
         $content .= '<br><br><strong>' . $this->i18n('checkmail_info_subject') . '</strong>';
         $content .= '</div>';
     }
-}
-else
-{
+} else {
     $content .= '<div class="alert alert-warning">';
     $content .= $this->i18n('checkmail_noadress');
     $content .= '</div>';
