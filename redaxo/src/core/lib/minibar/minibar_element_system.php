6ed100127e29b54704ff19d1d4833a92351a3722
Thomas Skerbis <thomas.skerbis@mac.com>
2018-10-20 14:48:51 +0200

    Github link to help section

--- redaxo/src/core/lib/minibar/minibar_element_system.php
+++ redaxo/src/core/lib/minibar/minibar_element_system.php
@@ -64,13 +64,8 @@ class rex_minibar_element_system extends rex_minibar_element
                     <b>'.rex_i18n::msg('minibar_help').'</b>
                     <span>
                         <a href="https://redaxo.org/forum/" target="_blank" rel="help noreferrer noopener">'.rex_i18n::msg('minibar_board_link_label').'</a><br />
-                        <a href="https://redaxo.org/slack/" target="_blank" rel="help noreferrer noopener">'.rex_i18n::msg('minibar_slack_link_label').'</a>
-                    </span>
-                </div>
-                <div class="rex-minibar-info-piece">
-                    <b>'.rex_i18n::msg('minibar_errorreport').'</b>
-                    <span>
-                        <a href="https://github.com/redaxo/redaxo/issues" target="_blank" rel="help noreferrer noopener">GitHub</a>
+                        <a href="https://redaxo.org/slack/" target="_blank" rel="help noreferrer noopener">'.rex_i18n::msg('minibar_slack_link_label').'</a><br />
+                        <a href="https://github.com/redaxo/redaxo/issues" target="_blank" rel="help noreferrer noopener">'.rex_i18n::msg('minibar_errorreport').'</a>
                     </span>
                 </div>
             </div>
