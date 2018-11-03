6abd6c642565021445d2c1d1a6316c73ada618d9
Marcel Kuhmann <kuhmann@mynetworx.de>
2018-11-03 14:10:44 +0100

    add comment style output

--- redaxo/src/core/console.php
+++ redaxo/src/core/console.php
@@ -24,6 +24,7 @@ rex_i18n::setLocale('en_gb');
 
 $application->setCommandLoader(new rex_console_command_loader());
 
+// Override default list command to display information, that more commands are available after setup.
 $command = new rex_command_list();
 $application->add($command);
 $application->setDefaultCommand($command->getName());
--- redaxo/src/core/lib/console/list_command.php
+++ redaxo/src/core/lib/console/list_command.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Command\ListCommand;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -13,8 +14,10 @@ class rex_command_list extends ListCommand {
 
         if (rex::isSetup()) {
             $io = new SymfonyStyle($input, $output);
+            $outputFormatter = new OutputFormatterStyle('cyan');
+            $io->getFormatter()->setStyle('info', $outputFormatter);
 
-            $io->text('These commands are available during the setup. After setup completed more commands will be available.');
+            $io->text('<info>These commands are available during the setup. After setup completed more commands will be available.</info>');
         }
     }
 }
