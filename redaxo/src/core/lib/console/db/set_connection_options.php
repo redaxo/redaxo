0a3f3f181d95dae50af4be4d7c2cbc7788ffe9e5
Markus Staab <maggus.staab@googlemail.com>
2018-11-03 19:39:49 +0100

    Update set_connection_options.php

--- redaxo/src/core/lib/console/db/set_connection_options.php
+++ redaxo/src/core/lib/console/db/set_connection_options.php
@@ -5,7 +5,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-
 /**
  * @package redaxo\core
  *
