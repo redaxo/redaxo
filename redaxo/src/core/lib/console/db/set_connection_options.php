d352d60765fd248de5510691eb5f92765fe2c51a
Markus Staab <maggus.staab@googlemail.com>
2018-11-03 19:38:36 +0100

    fix CS

--- redaxo/src/core/lib/console/db/set_connection_options.php
+++ redaxo/src/core/lib/console/db/set_connection_options.php
@@ -11,7 +11,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @internal
  */
-class rex_command_db_set_connection_options extends rex_console_command {
+class rex_command_db_set_connection_options extends rex_console_command
+{
     protected function configure()
     {
         $this
