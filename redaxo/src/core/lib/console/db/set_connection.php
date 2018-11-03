5f61b4fd81813643f313a66e8b3e52bf450d0a46
Markus Staab <maggus.staab@googlemail.com>
2018-11-03 16:10:04 +0100

    Update redaxo/src/core/lib/console/db/set_connection.php
    Co-Authored-By: bloep <der_marcell@hotmail.com>

--- redaxo/src/core/lib/console/db/set_connection.php
+++ redaxo/src/core/lib/console/db/set_connection.php
@@ -17,7 +17,7 @@ class rex_command_db_set_connection extends rex_console_command {
         $this
             ->setDescription('Sets database connection credentials')
             ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'database host')
-            ->addOption('login', null, InputOption::VALUE_OPTIONAL, 'Sets database user')
+            ->addOption('login', null, InputOption::VALUE_OPTIONAL, 'database user')
             ->addOption('password', null, InputOption::VALUE_OPTIONAL, 'Sets database password')
             ->addOption('database', null, InputOption::VALUE_OPTIONAL, 'Sets database name');
     }
