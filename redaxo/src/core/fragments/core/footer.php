a1da3bcab0e61174d6d11304713d130f23721e86
tbaddade <thomas@redaxo.org>
2018-11-05 09:39:26 +0100

    Forum Links gelöscht, close #2172
    auch im ehemaligen Footer

--- redaxo/src/core/fragments/core/footer.php
+++ redaxo/src/core/fragments/core/footer.php
@@ -4,7 +4,6 @@
                 <li><a href="#rex-start-of-page"><i class="fa fa-arrow-up"></i></a></li>
                 <li><a href="https://www.yakamara.de" target="_blank" rel="noreferrer noopener">yakamara.de</a></li>
                 <li><a href="https://www.redaxo.org" target="_blank" rel="noreferrer noopener">redaxo.org</a></li>
-                <li><a href="https://www.redaxo.org/de/forum/" target="_blank" rel="noreferrer noopener"><?php echo rex_i18n::msg('footer_joinforum'); ?></a></li>
                 <?php if (rex::getUser() && rex::getUser()->isAdmin()): ?>
                     <li><a href="https://www.redaxo.org/doku/master" target="_blank" rel="noreferrer noopener' ?>"><?php echo rex_i18n::msg('footer_doku'); ?></a></li>
                 <?php endif; ?>
