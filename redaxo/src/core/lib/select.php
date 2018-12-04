818d569a272e1a1346685020f2e28d7f8ec0a10a
Markus Staab <maggus.staab@googlemail.com>
2018-12-04 20:20:39 +0100

    Rückwärtskompatibilität in rex_select wiederhergestellt (#2243)

--- redaxo/src/core/lib/select.php
+++ redaxo/src/core/lib/select.php
@@ -121,7 +121,7 @@ class rex_select
                 $this->setSelected($sectvalue);
             }
         } else {
-            $this->option_selected[] = $selected;
+            $this->option_selected[] = (string) rex_escape($selected, 'html_attr');
         }
     }
 
@@ -290,6 +290,11 @@ class rex_select
 
     protected function outOption($name, $value, $level = 0, array $attributes = [])
     {
+        $name = rex_escape($name);
+        // for BC reasons, we always expect value to be a string.
+        // this also makes sure that the strict in_array() check below works.
+        $value = (string) rex_escape($value, 'html_attr');
+
         $bsps = '';
         if ($level > 0) {
             $bsps = str_repeat('&nbsp;&nbsp;&nbsp;', $level);
@@ -304,8 +309,6 @@ class rex_select
             $attr .= ' ' . $n . '="' . $v . '"';
         }
 
-        $name = rex_escape($name);
-        $value = rex_escape($value, 'html_attr');
         return '        <option value="' . $value . '"' . $attr . '>' . $bsps . $name . '</option>' . "\n";
     }
 
