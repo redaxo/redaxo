e78c0391a81d2f3b75aac1fdaadc7cbdd3eef464
Marcel Kuhmann <der_marcell@hotmail.com>
2018-12-04 20:56:49 +0100

    Profil: Reload nach Speichern (#2244)

--- redaxo/src/core/pages/profile.php
+++ redaxo/src/core/pages/profile.php
@@ -54,6 +54,12 @@ if ($update) {
     }
 }
 
+// Restore success message after redirect
+// is necessary to show the whole page in the selected language
+if (rex_request('rex_user_updated', 'bool', false)) {
+    $success = rex_i18n::msg('user_data_updated');
+}
+
 if ($update && !$error) {
     $userminibar = rex_request('minibar', 'bool');
 
@@ -70,12 +76,14 @@ if ($update && !$error) {
 
     try {
         $updateuser->update();
-        $success = rex_i18n::msg('user_data_updated');
 
         rex_extension::registerPoint(new rex_extension_point('PROFILE_UPDATED', '', [
             'user_id' => $user_id,
             'user' => new rex_user($updateuser->setQuery('SELECT * FROM '.rex::getTable('user').' WHERE id = ?', [$user_id])),
         ], true));
+
+        // trigger a fullpage-reload which immediately reflects a possible changed language
+        rex_response::sendRedirect(rex_url::currentBackendPage(['rex_user_updated' => true], false));
     } catch (rex_sql_exception $e) {
         $error = $e->getMessage();
     }
@@ -193,7 +201,7 @@ $fragment->setVar('buttons', $buttons, false);
 $content = $fragment->parse('core/page/section.php');
 
 $content = '
-    <form action="' . rex_url::currentBackendPage() . '" method="post">
+    <form action="' . rex_url::currentBackendPage() . '" method="post" data-pjax="false">
         ' . $csrfToken->getHiddenField() . '
         ' . $content . '
     </form>';
