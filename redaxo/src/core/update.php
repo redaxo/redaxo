4298e1a56b079253f73efe12e2fbf34c7a8a0e6f
Gregor Harlan <mail@gh01.de>
2018-12-18 00:12:59 +0100

    Zwischenupdate erfordern bei <5.4

--- redaxo/src/core/update.php
+++ redaxo/src/core/update.php
@@ -18,6 +18,11 @@ if (rex_string::versionCompare($mysqlVersion, $minMysqlVersion, '<')) {
     throw new rex_functional_exception($message);
 }
 
+// REDAXO 5.4 required because of some `rex_sql_table` usages in core addons
+if (rex_string::versionCompare(rex::getVersion(), '5.4', '<')) {
+    throw new rex_functional_exception(sprintf('The REDAXO version "%s" is too old for this update, please update to 5.6.5 before.', rex::getVersion()));
+}
+
 // Installer >= 2.1.2 required because of https://github.com/redaxo/redaxo/issues/1018
 // (Installer < 2.0.3 also works, because it does not contain the bug)
 $installerVersion = rex_addon::get('install')->getVersion();
