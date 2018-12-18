bc089410fd7bcbea993c3c36eeb449609f4354f8
Markus Staab <maggus.staab@googlemail.com>
2018-12-18 10:22:17 +0100

    Update footer.php

--- redaxo/src/core/fragments/core/footer.php
+++ redaxo/src/core/fragments/core/footer.php
@@ -1,5 +1,5 @@
 <?php
-@trigger_error("The core/footer.php is deprecated and will be removed in a future version.", E_USER_DEPRECATED);
+@trigger_error("The core/footer.php fragment is deprecated and will be removed in a future version.", E_USER_DEPRECATED);
 ?>
     <footer class="rex-global-footer">
         <nav class="rex-nav-footer">
