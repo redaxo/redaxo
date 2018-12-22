303577eea4f0c4c548e3ea856dc3da42612159ff
Gregor Harlan <mail@gh01.de>
2018-12-22 07:26:35 +0100

    db:set-connection statt db:set-connection-options (#2320)

--- redaxo/src/core/lib/console/command_loader.php
+++ redaxo/src/core/lib/console/command_loader.php
@@ -18,7 +18,7 @@ class rex_console_command_loader implements CommandLoaderInterface
             'cache:clear' => rex_command_cache_clear::class,
             'config:get' => rex_command_config_get::class,
             'db:connection-options' => rex_command_db_connection_options::class,
-            'db:set-connection-options' => rex_command_db_set_connection_options::class,
+            'db:set-connection' => rex_command_db_set_connection::class,
             'setup:check' => rex_command_setup_check::class,
         ];
 
