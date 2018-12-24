4919c6732df7fcfcc5747767cc35146b6f58d5eb
Daniel Weitenauer <github@studio-ahoi.de>
2018-12-24 14:51:54 +0100

    Move column to fragment

--- redaxo/src/addons/structure/fragments/structure/table_row_categories_form.php
+++ redaxo/src/addons/structure/fragments/structure/table_row_categories_form.php
@@ -17,7 +17,7 @@
             <button class="btn btn-save" type="submit" name="category-add-button"<?=rex::getAccesskey(rex_i18n::msg('add_category'), 'save');?>><?=rex_i18n::msg('add_category');?></button>
         </td>
     <?php elseif ($this->structure_context->getFunction() == 'edit_cat' && $this->structure_context->getEditId() == $this->i_category_id):?>
-        <?=$this->kat_icon_td;?>
+        <td class="rex-table-icon"><a href="<?=$this->kat_link;?>" title="<?=htmlspecialchars($this->KAT->getValue('catname'));?>"><i class="rex-icon rex-icon-category"></i></a></td>
         <td class="rex-table-id" data-title="<?=rex_i18n::msg('header_id');?>"><?=$this->i_category_id;?></td>
         <td data-title="<?=rex_i18n::msg('header_category');?>">
             <input class="form-control rex-js-autofocus" type="text" name="category-name" value="<?=htmlspecialchars($this->KAT->getValue('catname'));?>" autofocus />
