480d0f563ae28a19c337017ac8c4cabb712d5976
Markus Staab <markus.staab@redaxo.de>
2018-12-28 20:07:09 +0100

    docs

--- redaxo/src/core/lib/minibar/minibar_lazy_element.php
+++ redaxo/src/core/lib/minibar/minibar_lazy_element.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * minibar element base class which provides lazy loading abilities for resource extensive use-cases
+ *
+ * @package redaxo\core
+ */
 abstract class rex_minibar_lazy_element extends rex_minibar_element
 {
     public function render()
@@ -16,7 +21,18 @@ abstract class rex_minibar_lazy_element extends rex_minibar_element
         return !($apiFn instanceof rex_api_minibar);
     }
 
+    /**
+     * Returns the initial/light-weight html representation of this element
+     *
+     * @return string
+     */
     abstract protected function renderFirstView();
 
+    /**
+     * Returns the full html for this element.
+     * This method will be called asynchronously after user starts interacting with the initial element.
+     *
+     * @return string
+     */
     abstract protected function renderComplete();
 }
