61f66db8d1dc3b947e46c07e3e0489de2a1510f6
Markus Staab <markus.staab@redaxo.de>
2018-12-28 19:55:59 +0100

    fix CS

--- redaxo/src/core/lib/minibar/api_minibar.php
+++ redaxo/src/core/lib/minibar/api_minibar.php
@@ -9,7 +9,6 @@ class rex_api_minibar extends rex_api_function
 
     public function execute()
     {
-
         $visibility = rex_get('visibility', 'bool', null);
         if ($visibility !== null) {
             rex_minibar::getInstance()->setVisibility($visibility);
