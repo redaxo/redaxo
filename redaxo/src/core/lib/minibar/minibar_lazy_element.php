189d73ef83e0b4aeea9d70949892a3ad68bcf64f
Markus Staab <markus.staab@redaxo.de>
2018-12-28 20:22:08 +0100

    fix CS

--- redaxo/src/core/lib/minibar/minibar_lazy_element.php
+++ redaxo/src/core/lib/minibar/minibar_lazy_element.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * minibar element base class which provides lazy loading abilities for resource extensive use-cases
+ * minibar element base class which provides lazy loading abilities for resource extensive use-cases.
  *
  * @package redaxo\core
  */
@@ -22,7 +22,7 @@ abstract class rex_minibar_lazy_element extends rex_minibar_element
     }
 
     /**
-     * Returns the initial/light-weight html representation of this element
+     * Returns the initial/light-weight html representation of this element.
      *
      * @return string
      */
