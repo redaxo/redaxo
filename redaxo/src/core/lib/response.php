3cc0f4ea85dcc372138fc7e0b42e5d91235ca574
Gregor Harlan <mail@gh01.de>
2018-12-31 14:56:51 +0100

    update

--- redaxo/src/core/lib/response.php
+++ redaxo/src/core/lib/response.php
@@ -97,7 +97,7 @@ class rex_response
     private static function sendServerTimingHeaders()
     {
         // see https://w3c.github.io/server-timing/#the-server-timing-header-field
-        foreach (rex_timer::$timers as $label => $durationMs) {
+        foreach (rex_timer::$serverTimings as $label => $durationMs) {
             $label = preg_replace('{[^!#$%&\'*+-\.\^_`|~\w]}i', '_', $label);
             header('Server-Timing: '. $label .';dur='. number_format($durationMs, 3, '.', ''), false);
         }
