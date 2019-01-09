9c1e2d1eeeb7e60723ae71fa2c1d332ad64548c9
schuer <ds@decaf.de>
2019-01-09 12:35:09 +0100

    Minibar im Medienpool ausblenden, wenn als Popup geöffnet
    Prüft, ob die Seite ein Popup ist, und blendet nur dann die Minibar aus. Provisorisch für den Fall, dass der Medienpool mal in anderem Kontext als normale Seite geöffnet werden sollte.

--- redaxo/src/addons/mediapool/pages/index.php
+++ redaxo/src/addons/mediapool/pages/index.php
@@ -101,5 +101,15 @@ if (!rex_request::isXmlHttpRequest()) {
     <?php
 }
 
+// -------------- Minibar
+if (class_exists('rex_minibar')) {
+    $page = rex_be_controller::getCurrentPageObject();
+
+    if ($page && $page->isPopup()) {
+        // Hide minibar if media pool is popup
+        rex_minibar::getInstance()->setActive(false);
+    }
+}
+
 // -------------- Include Page
 rex_be_controller::includeCurrentPageSubPath(compact('opener_input_field', 'opener_link', 'arg_url', 'args', 'arg_fields', 'rex_file_category', 'rex_file_category_name', 'PERMALL', 'file_id', 'error', 'success'));
