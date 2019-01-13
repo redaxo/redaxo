9b11ffd5f9d6f080229cfb34ac383a15ef040b04
Markus Staab <markus.staab@redaxo.de>
2019-01-13 13:44:55 +0100

    include all functions, which might otherwise only be conditionally be included

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -7,3 +7,16 @@ $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = false;
 
 require $REX['BACKEND_FOLDER'] . '/src/core/boot.php';
+
+// include all functions, which might otherwise only be conditionally be included
+$finder = rex_finder::factory('redaxo/src/')
+    ->recursive()
+    // ignore files, which dont contain declarations execute logic
+    ->ignoreFiles('function_rex_category.php')
+    ->filesOnly();
+/** @var SplFileInfo $file */
+foreach ($finder as $path => $file) {
+    if (strpos($file->getPath(), 'functions') !== false) {
+        require_once $file->getRealPath();
+    }
+}
