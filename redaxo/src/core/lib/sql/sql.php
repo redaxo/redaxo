417297dc3950dac1e0dc8edc522f44130d7e2ec4
Markus Staab <markus.staab@redaxo.de>
2019-01-14 20:34:01 +0100

    rex_sql: console als global update/create field unterstützen

--- redaxo/src/core/lib/sql/sql.php
+++ redaxo/src/core/lib/sql/sql.php
@@ -1251,7 +1251,11 @@ class rex_sql implements Iterator
     public function addGlobalUpdateFields($user = null)
     {
         if (!$user) {
-            $user = rex::getUser()->getValue('login');
+            if (rex::getConsole()) {
+                $user = 'console';
+            } else {
+                $user = rex::getUser()->getValue('login');
+            }
         }
 
         $this->setDateTimeValue('updatedate', time());
@@ -1268,7 +1272,11 @@ class rex_sql implements Iterator
     public function addGlobalCreateFields($user = null)
     {
         if (!$user) {
-            $user = rex::getUser()->getValue('login');
+            if (rex::getConsole()) {
+                $user = 'console';
+            } else {
+                $user = rex::getUser()->getValue('login');
+            }
         }
 
         $this->setDateTimeValue('createdate', time());
