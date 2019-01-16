706e4c381c7ae002eb54ce7d624098be8def3b36
Markus Staab <markus.staab@redaxo.de>
2019-01-16 14:33:58 +0100

    Console: Fixed 'PHP Notice: Undefined index: REQUEST_URI'

--- redaxo/src/addons/debug/boot.php
+++ redaxo/src/addons/debug/boot.php
@@ -1,10 +1,11 @@
 <?php
 
+// all these debugging capabilities require ChromePhp, which only works in the browser
 if (rex_server('REQUEST_URI')) {
-    require 'vendor/chromephp/ChromePhp.php';
-}
+    require_once 'vendor/chromephp/ChromePhp.php';
 
-rex_sql::setFactoryClass('rex_sql_debug');
-rex_extension::setFactoryClass('rex_extension_debug');
-rex_logger::setFactoryClass('rex_logger_debug');
-rex_api_function::setFactoryClass('rex_api_function_debug');
+    rex_sql::setFactoryClass('rex_sql_debug');
+    rex_extension::setFactoryClass('rex_extension_debug');
+    rex_logger::setFactoryClass('rex_logger_debug');
+    rex_api_function::setFactoryClass('rex_api_function_debug');
+}
