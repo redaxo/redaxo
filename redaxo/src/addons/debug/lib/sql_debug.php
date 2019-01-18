d2dddddc7273325961d92629970ca15ed55b480c
Markus Staab <maggus.staab@googlemail.com>
2019-01-18 20:14:14 +0100

    Fixed Warnings
    Variable $file might not be defined.  
    Variable $line might not be defined.

--- redaxo/src/addons/debug/lib/sql_debug.php
+++ redaxo/src/addons/debug/lib/sql_debug.php
@@ -23,7 +23,10 @@ class rex_sql_debug extends rex_sql
             parent::setQuery($qry, $params, $options);
         } catch (rex_exception $e) {
             $trace = debug_backtrace();
-            for ($i = 0; $trace && $i < count($trace); ++$i) {
+
+            $file = $trace[0]['file'];
+	        $line = $trace[0]['line'];
+            for ($i = 1; $i < count($trace); ++$i) {
                 if (isset($trace[$i]['file']) && strpos($trace[$i]['file'], 'sql.php') === false) {
                     $file = $trace[$i]['file'];
                     $line = $trace[$i]['line'];
