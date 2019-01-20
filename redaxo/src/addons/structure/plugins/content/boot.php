ea7ab2d4e6fbf0d97b4c41aeee0dc0ce1463d9d3
Markus Staab <markus.staab@redaxo.de>
2019-01-20 19:09:57 +0100

    Update boot.php

--- redaxo/src/addons/structure/plugins/content/boot.php
+++ redaxo/src/addons/structure/plugins/content/boot.php
@@ -108,5 +108,4 @@ rex_extension::register('EDITOR_URL', function (rex_extension_point $ep) {
 
 if (!rex::isBackend() || (rex::isBackend() && (rex_be_controller::getCurrentPagePart(1) === 'content' || rex_be_controller::getCurrentPagePart(1) === 'structure'))) {
     rex_minibar::getInstance()->addElement(new rex_minibar_element_structure_article());
-    rex_minibar::getInstance()->addElement(new rex_minibar_element_structure_clang());
 }
