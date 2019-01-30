ec6ceec363f9ef2580b4d19412bf1bf5a7295530
Markus Staab <markus.staab@redaxo.de>
2019-01-30 20:09:39 +0100

    use $myaddon

--- redaxo/src/addons/cronjob/boot.php
+++ redaxo/src/addons/cronjob/boot.php
@@ -6,11 +6,11 @@
  * @author gharlan[at]web[dot]de Gregor Harlan
  *
  * @package redaxo5
- *
- * @var rex_addon $this
  */
 
-define('REX_CRONJOB_LOG_FOLDER', $this->getDataPath());
+$myaddon = rex_addon::get('cronjob');
+
+define('REX_CRONJOB_LOG_FOLDER', $myaddon->getDataPath());
 define('REX_CRONJOB_TABLE', rex::getTable('cronjob'));
 
 if (rex::getConsole()) {
@@ -18,15 +18,15 @@ if (rex::getConsole()) {
     return;
 }
 
-rex_extension::register('PACKAGES_INCLUDED', function () {
-    foreach ($this->getAvailablePlugins() as $plugin) {
+rex_extension::register('PACKAGES_INCLUDED', function () use ($myaddon) {
+    foreach ($myaddon->getAvailablePlugins() as $plugin) {
         if (($type = $plugin->getProperty('cronjob_type')) != '') {
             rex_cronjob_manager::registerType($type);
         }
     }
 });
 
-$nexttime = $this->getConfig('nexttime', 0);
+$nexttime = $myaddon->getConfig('nexttime', 0);
 
 if ($nexttime != 0 && time() >= $nexttime) {
     $env = rex_cronjob_manager::getCurrentEnvironment();
