c3e3bd29b3d9aaa2a61774427e376c0065593ab4
Markus Staab <markus.staab@redaxo.de>
2019-02-27 19:09:36 +0100

    Update sql.php

--- redaxo/src/core/lib/sql/sql.php
+++ redaxo/src/core/lib/sql/sql.php
@@ -108,7 +108,7 @@ class rex_sql implements Iterator
 
         $port = null;
         if (strpos($host, ':') !== false) {
-            list($host, $port) = explode(':', $host);
+            list($host, $port) = explode(':', $host, 2);
         }
 
         $dsn = 'mysql:host=' . $host;
