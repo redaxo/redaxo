e8741e33816b2b11db8b749a00f941e2eaf08d63
Gregor Harlan <mail@gh01.de>
2019-02-28 22:46:38 +0100

    Installer: Requirements-Checks korrigiert

--- redaxo/src/addons/install/lib/api_core_update.php
+++ redaxo/src/addons/install/lib/api_core_update.php
@@ -205,31 +205,27 @@ class rex_api_install_core_update extends rex_api_function
             $addon = rex_addon::get($addonkey);
             $addonPath = $temppath . 'addons/' . $addonkey . '/';
 
-            if (isset($config['requires'])) {
-                $requirements[$addon] = $addon->getProperty('requires');
-                $addon->setProperty('requires', $config['requires']);
-            }
-            if (isset($config['conflicts'])) {
-                $conflicts[$addon] = $addon->getProperty('conflicts');
-                $addon->setProperty('conflicts', $config['conflicts']);
-            }
+            $requirements[$addon] = $addon->getProperty('requires', []);
+            $addon->setProperty('requires', isset($config['requires']) ? $config['requires'] : []);
+
+            $conflicts[$addon] = $addon->getProperty('conflicts', []);
+            $addon->setProperty('conflicts', isset($config['conflicts']) ? $config['conflicts'] : []);
+
             $versions[$addon] = $addon->getVersion();
             $addon->setProperty('version', $config['version']);
+
             foreach ($addon->getAvailablePlugins() as $plugin) {
                 if (is_dir($addonPath . 'plugins/' . $plugin->getName())) {
                     $config = rex_file::getConfig($addonPath . 'plugins/' . $plugin->getName() . '/' . rex_package::FILE_PACKAGE);
-                    if (isset($config['requires'])) {
-                        $requirements[$plugin] = $plugin->getProperty('requires');
-                        $plugin->setProperty('requires', $config['requires']);
-                    }
-                    if (isset($config['conflicts'])) {
-                        $conflicts[$plugin] = $plugin->getProperty('conflicts');
-                        $plugin->setProperty('conflicts', $config['conflicts']);
-                    }
-                    if (isset($config['version'])) {
-                        $versions[$plugin] = $plugin->getProperty('version');
-                        $plugin->setProperty('version', $config['version']);
-                    }
+
+                    $requirements[$plugin] = $plugin->getProperty('requires', []);
+                    $plugin->setProperty('requires', isset($config['requires']) ? $config['requires'] : []);
+
+                    $conflicts[$plugin] = $plugin->getProperty('conflicts', []);
+                    $plugin->setProperty('conflicts', isset($config['conflicts']) ? $config['conflicts'] : []);
+                
+                    $versions[$plugin] = $plugin->getProperty('version');
+                    $plugin->setProperty('version', isset($config['version']) ? $config['version'] : null);
                 }
             }
         }
