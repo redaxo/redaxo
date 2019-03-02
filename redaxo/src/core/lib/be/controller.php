59a7a20a0280c503058972aa7c79c6803847642a
Gregor Harlan <mail@gh01.de>
2019-03-02 11:26:50 +0100

    Markdown-Pages mit Sprungankernavi (#2520)

--- redaxo/src/core/lib/be/controller.php
+++ redaxo/src/core/lib/be/controller.php
@@ -435,8 +435,10 @@ class rex_be_controller
             $path = $languagePath;
         }
 
+        list($toc, $content) = rex_markdown::factory()->parseWithToc(rex_file::get($path));
         $fragment = new rex_fragment();
-        $fragment->setVar('content', rex_markdown::factory()->parse(rex_file::get($path)), false);
+        $fragment->setVar('content', $content, false);
+        $fragment->setVar('toc', $toc, false);
         $content = $fragment->parse('core/page/docs.php');
 
         $fragment = new rex_fragment();
--- redaxo/src/core/tests/util/markdown_test.php
+++ redaxo/src/core/tests/util/markdown_test.php
@@ -18,4 +18,75 @@ class rex_markdown_test extends PHPUnit_Framework_TestCase
             ["<p>foo<br />\nbar</p>\n<p>baz</p>", "foo\nbar\n\nbaz"],
         ];
     }
+
+    public function testParseWithToc()
+    {
+        $input = <<<'MARKDOWN'
+Test
+====
+
+Foo bar
+
+Sub 1
+-----
+
+Foo bar
+
+### Sub 1.1
+
+### Sub 1.2
+
+#### Sub Sub 1.2.1
+
+##### Sub Sub 1.2.1.1
+
+## Sub 2
+
+### Sub 2.1
+
+## Sub 3
+
+### Sub 3.1
+MARKDOWN;
+
+        list($toc, $content) = rex_markdown::factory()->parseWithToc($input, 2, 4);
+
+        $expected = <<<'HTML'
+<ul>
+<li>
+<a href="#header-sub-1">Sub 1</a>
+<ul>
+<li>
+<a href="#header-sub-1-1">Sub 1.1</a>
+</li>
+<li>
+<a href="#header-sub-1-2">Sub 1.2</a>
+<ul>
+<li>
+<a href="#header-sub-sub-1-2-1">Sub Sub 1.2.1</a>
+</li>
+</ul>
+</li>
+</ul>
+<li>
+<a href="#header-sub-2">Sub 2</a>
+<ul>
+<li>
+<a href="#header-sub-2-1">Sub 2.1</a>
+</li>
+</ul>
+<li>
+<a href="#header-sub-3">Sub 3</a>
+<ul>
+<li>
+<a href="#header-sub-3-1">Sub 3.1</a>
+</li>
+</ul>
+</li>
+</ul>
+
+HTML;
+
+        $this->assertSame($expected, $toc);
+    }
 }
