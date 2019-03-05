c927b4efb2c2da94135548ab1f24330c12e8ae37
Gregor Harlan <mail@gh01.de>
2019-03-05 10:22:36 +0100

    MP: "Hinzufügen und übernehmen" korrigiert (#2535)

--- redaxo/src/addons/mediapool/pages/index.php
+++ redaxo/src/addons/mediapool/pages/index.php
@@ -39,7 +39,7 @@ if ($opener_input_field != '') {
     }
 
     $arg_url['opener_input_field'] = $opener_input_field;
-    $arg_fields .= '<input type="hidden" id="opener_input_field" name="opener_input_field" value="' . rex_escape($opener_input_field) . '" data-opener-id="'. $opener_id .'"/>' . "\n";
+    $arg_fields .= '<input type="hidden" name="opener_input_field" value="' . rex_escape($opener_input_field) . '"/>' . "\n";
 }
 
 // -------------- CatId in Session speichern
@@ -97,6 +97,7 @@ if (!rex_request::isXmlHttpRequest()) {
     ?>
     <script type="text/javascript">
         rex_retain_popup_event_handlers("rex:selectMedia");
+        <?= $opener_input_field ? 'rex.mediapoolOpenerInputField = "'.rex_escape($opener_input_field, 'js').'";' : '' ?>
     </script>
     <?php
 }
