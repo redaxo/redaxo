84b9103fdb0d0a99fb57c71cf33d893506bf8cf7
Gregor Harlan <mail@gh01.de>
2019-03-06 11:33:36 +0100

    phpdoc

--- redaxo/src/addons/media_manager/lib/media_manager.php
+++ redaxo/src/addons/media_manager/lib/media_manager.php
@@ -382,6 +382,8 @@ class rex_media_manager
      * @param null|int         $timestamp Last change timestamp of given file, for cache buster parameter
      *                                    (not nessary when the file is given by a `rex_media` object)
      * @param bool             $escape
+     *
+     * @return string
      */
     public static function url($type, $file, $timestamp = null, $escape = true)
     {
