85196e73feeb82c698969331686a67b55e7055b9
Dirk Schürjohann <ds@decaf.de>
2019-03-08 11:07:46 +0100

    Leere Ctypes in Grau darstellen (#2559)
    Sind innerhalb eines Ctypes keine Inhaltsblöcke angelegt, wird der Link in Grau ausgegeben statt im normalen Link-Blau. Siehe im Screenshot »Seitenspalte links« und »Fußzeile«.
    
    ![screenshot 2019-03-08 at 00 43 59](https://user-images.githubusercontent.com/1297466/53997136-59624880-413b-11e9-801a-bad31057177f.png)
    
    closes #838

--- redaxo/src/addons/structure/plugins/content/pages/content.php
+++ redaxo/src/addons/structure/plugins/content/pages/content.php
@@ -333,9 +333,14 @@ if ($article->getRows() == 1) {
         $editPage = rex_be_controller::getPageObject('content/edit');
 
         foreach ($ctypes as $key => $val) {
+            $hasSlice = true;
+            if ($ctype != $key) {
+                $hasSlice = rex_article_slice::getFirstSliceForCtype($key, $article_id, $clang) !== null;
+            }
             $editPage->addSubpage((new rex_be_page('ctype' . $key, rex_i18n::translate($val)))
                 ->setHref(['page' => 'content/edit', 'article_id' => $article_id, 'clang' => $clang, 'ctype' => $key], false)
                 ->setIsActive($ctype == $key)
+                ->setItemAttr('class', $hasSlice ? '' : 'rex-empty')
             );
         }
 
