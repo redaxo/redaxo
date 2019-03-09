5b1d1beba098117726804555b2053a1a8c2bc264
Gregor Harlan <mail@gh01.de>
2019-03-09 17:31:19 +0100

    Debug-Mode-Marker: Fehler auf Loginseite vermeiden (#2569)

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -7,7 +7,7 @@
                     <?php else: ?>
                         <a class="navbar-brand" href="<?= rex_url::backendController() ?>"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></a>
                     <?php endif; ?>
-                    <?php if (rex::getUser()->isAdmin() && rex::isDebugMode()): ?>
+                    <?php if (rex::getUser() && rex::getUser()->isAdmin() && rex::isDebugMode()): ?>
                         <a class="rex-marker-debugmode" href="<?= rex_url::backendPage('system/settings') ?>" title="<?= rex_i18n::msg('debug_mode_marker') ?>">
                             <i class="rex-icon fa-heartbeat"></i>
                         </a>
