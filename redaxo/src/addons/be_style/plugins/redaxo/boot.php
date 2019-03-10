037ba2f72efbcd7688db07f1d96fde22f1e2ccd2
schuer <ds@decaf.de>
2019-03-10 11:06:39 +0100

    Customizer: Erkennungsfarbe für mask-icon verwenden

--- redaxo/src/addons/be_style/plugins/redaxo/boot.php
+++ redaxo/src/addons/be_style/plugins/redaxo/boot.php
@@ -41,13 +41,15 @@ if (rex::isBackend()) {
     rex_view::addJsFile($plugin->getAssetsUrl('javascripts/redaxo.js'));
 
     rex_extension::register('PAGE_HEADER', function (rex_extension_point $ep) use ($plugin) {
+        $labelColor = rex_plugin::get('be_style', 'customizer')->getConfig('labelcolor') ?: '#404040';
+
         $icons = [];
 
         $icons[] = '<link rel="apple-touch-icon" sizes="180x180" href="' . $plugin->getAssetsUrl('icons/apple-touch-icon.png') . '" />';
         $icons[] = '<link rel="icon" type="image/png" sizes="32x32" href="' . $plugin->getAssetsUrl('icons/favicon-32x32.png') . '" />';
         $icons[] = '<link rel="icon" type="image/png" sizes="16x16" href="' . $plugin->getAssetsUrl('icons/favicon-16x16.png') . '" />';
         $icons[] = '<link rel="manifest" href="' . $plugin->getAssetsUrl('icons/manifest.json') . '">';
-        $icons[] = '<link rel="mask-icon" href="' . $plugin->getAssetsUrl('icons/safari-pinned-tab.svg') . '" color="#404040">';
+        $icons[] = '<link rel="mask-icon" href="' . $plugin->getAssetsUrl('icons/safari-pinned-tab.svg') . '" color="' . $labelColor . '">';
         $icons[] = '<link rel="shortcut icon" href="' . $plugin->getAssetsUrl('icons/favicon.ico') . '">';
         $icons[] = '<meta name="msapplication-TileColor" content="#ffffff">';
         $icons[] = '<meta name="msapplication-TileImage" content="' . $plugin->getAssetsUrl('icons/mstile-144x144.png') . '">';
