900c9cb3d39ae04eec69bc0aa313a1de9b5e2a6d
Markus Staab <maggus.staab@googlemail.com>
2019-03-12 22:01:20 +0100

    Serverseitige daten nur löschen, wenn login-geschützte pages aufgeruf… (#2606)

--- redaxo/src/core/backend.php
+++ redaxo/src/core/backend.php
@@ -119,14 +119,16 @@ if (rex::isSetup()) {
         $page = 'login';
         rex_be_controller::setCurrentPage('login');
 
-        // clear in-browser data of a previous session with the same browser for security reasons.
-        // a possible attacker should not be able to access cached data of a previous valid session on the same computer.
-        // clearing "executionContext" or "cookies" would result in a endless loop.
-        rex_response::setHeader('Clear-Site-Data', '"cache", "storage"');
+        if ('login' !== rex_request('page', 'string', 'login')) {
+            // clear in-browser data of a previous session with the same browser for security reasons.
+            // a possible attacker should not be able to access cached data of a previous valid session on the same computer.
+            // clearing "executionContext" or "cookies" would result in a endless loop.
+            rex_response::setHeader('Clear-Site-Data', '"cache", "storage"');
 
-        // Currently browsers like Safari do not support the header Clear-Site-Data.
-        // we dont kill/regenerate the session so e.g. the frontend will not get logged out
-        rex_request::clearSession();
+            // Currently browsers like Safari do not support the header Clear-Site-Data.
+            // we dont kill/regenerate the session so e.g. the frontend will not get logged out
+            rex_request::clearSession();
+        }
     } else {
         // Userspezifische Sprache einstellen
         $user = $login->getUser();
