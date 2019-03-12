c2a0f8b9270eaf90c20b7590fa9854fa95335ab4
Dirk Schürjohann <ds@decaf.de>
2019-03-12 21:49:07 +0100

    Debug-Mode Bugfixes (#2577)

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -7,9 +7,9 @@
                     <?php else: ?>
                         <a class="navbar-brand" href="<?= rex_url::backendController() ?>"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></a>
                     <?php endif; ?>
-                    <?php if (rex::getUser() && rex::getUser()->isAdmin() && rex::isDebugMode()): ?>
+                    <?php if (!rex_be_controller::getCurrentPageObject()->isPopup() && rex::getUser() && rex::getUser()->isAdmin() && rex::isDebugMode()): ?>
                         <a class="rex-marker-debugmode" href="<?= rex_url::backendPage('system/settings') ?>" title="<?= rex_i18n::msg('debug_mode_marker') ?>">
-                            <i class="rex-icon fa-heartbeat"></i>
+                            <i class="rex-icon rex-icon-heartbeat"></i>
                         </a>
                     <?php endif; ?>
                 </div>
