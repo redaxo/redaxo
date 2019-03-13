fa0a8ea74fcbb7fa76a11c5a073de1144037b51b
Markus Staab <maggus.staab@googlemail.com>
2019-03-13 21:05:47 +0100

    Fix date

--- redaxo/src/core/pages/setup.php
+++ redaxo/src/core/pages/setup.php
@@ -140,7 +140,7 @@ if ($step == 3) {
         $security .= rex_view::warning(rex_i18n::msg('setup_security_warn_mod_security'));
     }
 
-    if (version_compare(PHP_VERSION, '7.2', '<') == 1 && time() > strtotime('1 Jan 2019')) {
+    if (version_compare(PHP_VERSION, '7.2', '<') == 1 && time() > strtotime('1 Dec 2019')) {
         $security .= rex_view::warning(rex_i18n::msg('setup_security_deprecated_php', PHP_VERSION));
     } elseif (version_compare(PHP_VERSION, '7.3', '<') == 1 && time() > strtotime('30 Nov 2020')) {
         $security .= rex_view::warning(rex_i18n::msg('setup_security_deprecated_php', PHP_VERSION));
