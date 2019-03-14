81d7dbc4a502f1ac9ae0ff6b431097e68185543e
Gregor Harlan <mail@gh01.de>
2019-03-15 00:53:33 +0100

    ordered_imports

--- redaxo/src/core/lib/console/user/create.php
+++ redaxo/src/core/lib/console/user/create.php
@@ -2,8 +2,8 @@
 
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
