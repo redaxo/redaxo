a79ec40dd4eaa8188ca84fbac806a54748dc3bf1
Gregor Harlan <mail@gh01.de>
2019-03-15 00:19:11 +0100

    Vendor updates (Symfony 4.2) (#2628)

--- redaxo/src/core/vendor/symfony/console/Helper/TableCell.php
+++ redaxo/src/core/vendor/symfony/console/Helper/TableCell.php
@@ -24,16 +24,8 @@ class TableCell
         'colspan' => 1,
     ];
 
-    /**
-     * @param string $value
-     * @param array  $options
-     */
-    public function __construct($value = '', array $options = [])
+    public function __construct(string $value = '', array $options = [])
     {
-        if (is_numeric($value) && !\is_string($value)) {
-            $value = (string) $value;
-        }
-
         $this->value = $value;
 
         // check option names
