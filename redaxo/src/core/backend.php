6f90eabe28364a90f9f8b2c0b4f94d6fe3df6993
Markus Staab <markus.staab@redaxo.de>
2019-03-28 22:16:56 +0100

    asset-streaming: /assets/ rooted pfade untersützen

--- redaxo/src/core/backend.php
+++ redaxo/src/core/backend.php
@@ -13,9 +13,14 @@ header("Content-Security-Policy: frame-ancestors 'self'");
 if (rex_get('asset') && rex_get('buster')) {
     $assetFile = rex_get('asset');
 
-    $fullPath = realpath($assetFile);
+    if (strpos($assetFile, '/assets/') === 0) {
+        // relative to the assets-root
+        $fullPath = realpath('..'. $assetFile);
+    } else {
+        $fullPath = realpath($assetFile);
+    }
+    
     $assetDir = rex_path::assets();
-
     if (strpos($fullPath, $assetDir) !== 0) {
         throw new Exception('Assets can only be streamed from within the assets folder. "'. $fullPath .'" is not within "'. $assetDir .'"');
     }
