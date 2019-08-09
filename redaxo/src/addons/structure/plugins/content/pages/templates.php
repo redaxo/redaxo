d301978c61187c684ac4beb74d2a1a2cbc0272bb
tbaddade <thomas@redaxo.org>
2019-08-09 16:41:16 +0200

    Hinweis das Feldnamen übersetzbar sind
    close #1189

--- redaxo/src/addons/structure/plugins/content/pages/templates.php
+++ redaxo/src/addons/structure/plugins/content/pages/templates.php
@@ -346,6 +346,7 @@ if ('add' == $function || 'edit' == $function) {
         $n = [];
         $n['label'] = '<label for="rex-id-templatename">' . rex_i18n::msg('template_name') . '</label>';
         $n['field'] = '<input class="form-control" id="rex-id-templatename" type="text" name="templatename" value="' . rex_escape($templatename) . '" />';
+        $n['note'] = rex_i18n::msg('translatable');
         $formElements[] = $n;
 
         $fragment = new rex_fragment();
