ccd1f07cc8360a88e67c6979e6de07647a033698
Gregor Harlan <mail@gh01.de>
2019-08-11 16:51:50 +0200

    Cache korrekt löschen nach Prio-Setzung

--- redaxo/src/addons/structure/lib/service_article.php
+++ redaxo/src/addons/structure/lib/service_article.php
@@ -440,6 +440,12 @@ class rex_article_service
             );
 
             rex_article_cache::deleteLists($parent_id);
+            rex_article_cache::deleteMeta($parent_id);
+
+            $ids = rex_sql::factory()->getArray('SELECT id FROM '.rex::getTable('article').' WHERE parent_id = ?', [$parent_id]);
+            foreach ($ids as $id) {
+                rex_article_cache::deleteMeta($id['id']);
+            }
         }
     }
 
