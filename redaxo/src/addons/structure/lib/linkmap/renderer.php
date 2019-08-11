d7e2ca21fcb5a835ec211ed92063f273ef7d4421
Thomas Blum <thomas@redaxo.org>
2019-08-11 12:29:26 +0200

    Ids in der Linkmap und Sitemap optisch zurückgenommen (#2815)
    close #2519

--- redaxo/src/addons/structure/lib/linkmap/renderer.php
+++ redaxo/src/addons/structure/lib/linkmap/renderer.php
@@ -91,8 +91,6 @@ abstract class rex_linkmap_tree_renderer
             $label = '&nbsp;';
         }
 
-        $label .= ' [' . $OOobject->getId() . ']';
-
         if ($OOobject instanceof rex_article && !$OOobject->hasTemplate()) {
             $label .= ' [' . rex_i18n::msg('linkmap_has_no_template') . ']';
         }
@@ -112,7 +110,7 @@ abstract class rex_linkmap_tree_renderer
 
         $icon = '<i class="rex-icon rex-icon-' . ($OOobject->isSiteStartArticle() ? 'sitestartarticle' : ($OOobject->isStartArticle() ? 'startarticle' : 'article')) . '"></i>';
 
-        return '<li' . $liAttr . '><a' . $linkAttr . '>' . $icon . ' ' . rex_escape($label) . '</a>';
+        return '<li' . $liAttr . '><a' . $linkAttr . '>' . $icon . ' ' . rex_escape($label) . '<span class="list-item-suffix">'.$OOobject->getId().'</span></a>';
     }
 }
 
