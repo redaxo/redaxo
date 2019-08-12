2bac008c017c71b21d64dc60998e2f8451d68010
Gregor Harlan <mail@gh01.de>
2019-08-12 19:29:58 +0200

    Revert "be_style: rex_copy() return wert während installation auswerten (#2658)"
    This reverts commit 76f67851f3e79e698382083465526d19bc375778.

--- redaxo/src/addons/be_style/install.php
+++ redaxo/src/addons/be_style/install.php
@@ -16,7 +16,5 @@ $addon = rex_addon::get('be_style');
 $files = require __DIR__.'/vendor_files.php';
 
 foreach ($files as $source => $destination) {
-    if (false === rex_file::copy($addon->getPath($source), $addon->getAssetsPath($destination))) {
-        throw new rex_functional_exception('Unable to copy file from "'. $addon->getPath($source) .'" to "'. $addon->getAssetsPath($destination) .'"');
-    }
+    rex_file::copy($addon->getPath($source), $addon->getAssetsPath($destination));
 }
