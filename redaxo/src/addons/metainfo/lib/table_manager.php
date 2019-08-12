5534c856cd75ccab7535f0725c68fd98e0dd89eb
Markus Staab <maggus.staab@googlemail.com>
2019-08-12 20:27:17 +0200

    rex_metainfo_table_manager konstanten definieren
    statt globaler konstanten

--- redaxo/src/addons/metainfo/lib/table_manager.php
+++ redaxo/src/addons/metainfo/lib/table_manager.php
@@ -12,6 +12,21 @@
 
 class rex_metainfo_table_manager
 {
+    public const FIELD_TEXT = 1;
+    public const FIELD_TEXTAREA = 2;
+    public const FIELD_SELECT = 3;
+    public const FIELD_RADIO = 4;
+    public const FIELD_CHECKBOX = 5;
+    public const FIELD_REX_MEDIA_WIDGET = 6;
+    public const FIELD_REX_MEDIALIST_WIDGET = 7;
+    public const FIELD_REX_LINK_WIDGET = 8;
+    public const FIELD_REX_LINKLIST_WIDGET = 9;
+    public const FIELD_DATE = 10;
+    public const FIELD_DATETIME = 11;
+    public const FIELD_LEGEND = 12;
+    public const FIELD_TIME = 13;
+    public const FIELD_COUNT = 13;
+    
     private $tableName;
     private $DBID;
 
