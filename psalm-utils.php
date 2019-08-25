55845e69d2485cd8641806d93fdd5e01208f6d4d
Markus Staab <maggus.staab@googlemail.com>
2019-08-25 20:50:08 +0200

    Update psalm-utils.php

--- psalm-utils.php
+++ psalm-utils.php
@@ -36,7 +36,7 @@ class RexTypeReturnProvider implements \Psalm\Plugin\Hook\MethodReturnTypeProvid
             && isset($call_args[1]->value->inferredType)
             && $call_args[1]->value->inferredType->isSingleStringLiteral()
         ) {
-             $vartype = (string) $call_args[1]->value->inferredType;
+             $vartype = (string) $call_args[1]->value->inferredType->getSingleStringLiteral();
              
              switch ($vartype) {
                 case 'bool':
