47eecf9044f993fdaf43cf776f96fcb0bc3ed1f4
DanielWeitenauer <github@studio-ahoi.de>
2019-09-01 20:37:51 +0200

    Cleanup

--- redaxo/src/addons/structure/fragments/structure/table_categories.php
+++ redaxo/src/addons/structure/fragments/structure/table_categories.php
@@ -1,4 +1,14 @@
-<?php if ($this->structure_context->getFunction() == 'add_cat' || $this->structure_context->getFunction() == 'edit_cat'): ?>
+<?php
+/**
+ * Category table and header
+ *
+ * Variables:
+ * $this->structure_context  Context data
+ * $this->content            Table rows
+ */
+?>
+
+<?php if (in_array($this->structure_context->getFunction(), ['add_cat', 'edit_cat'])): ?>
     <form action="<?=$this->structure_context->getContext()->getUrl(['catstart' => $this->structure_context->getCatStart()]);?>" method="post">
         <fieldset>
             <input type="hidden" name="edit_id" value="<?=$this->structure_context->getEditId();?>" />
@@ -8,7 +18,9 @@
     <thead>
         <tr>
             <th class="rex-table-icon">
-                <?=$this->structure_context->hasCategoryPermission() ? '<a href="'.$this->structure_context->getContext()->getUrl(['function' => 'add_cat', 'catstart' => $this->structure_context->getCatStart()]).'" '.rex::getAccesskey(rex_i18n::msg('add_category'), 'add').'><i class="rex-icon rex-icon-add-category"></i></a>' : '';?>
+                <?php if ($this->structure_context->hasCategoryPermission()):?>
+                    <a href="<?=$this->structure_context->getContext()->getUrl(['function' => 'add_cat', 'catstart' => $this->structure_context->getCatStart()]);?>" <?=rex::getAccesskey(rex_i18n::msg('add_category'), 'add');?>><i class="rex-icon rex-icon-add-category"></i></a>
+                <?php endif;?>
             </th>
             <th class="rex-table-id"><?=rex_i18n::msg('header_id');?></th>
             <th><?=rex_i18n::msg('header_category');?></th>
@@ -17,11 +29,11 @@
         </tr>
     </thead>
     <tbody>
-        <?php if ($this->structure_context->getCategoryId() != 0 && ($category = rex_category::get($this->structure_context->getCategoryId()))): ?>
+        <?php if (rex_category::get($this->structure_context->getCategoryId()) instanceof rex_category): ?>
             <tr>
                 <td class="rex-table-icon"><i class="rex-icon rex-icon-open-category"></i></td>
                 <td class="rex-table-id">-</td>
-                <td data-title="<?=rex_i18n::msg('header_category');?>"><a href="<?=$this->structure_context->getContext()->getUrl(['category_id' => $category->getParentId()]);?>">..</a></td>
+                <td data-title="<?=rex_i18n::msg('header_category');?>"><a href="<?=$this->structure_context->getContext()->getUrl(['category_id' => rex_category::get($this->structure_context->getCategoryId())->getParentId()]);?>">..</a></td>
                 <td class="rex-table-priority" data-title="<?=rex_i18n::msg('header_priority');?>">&nbsp;</td>
                 <td class="rex-table-action" colspan="3">&nbsp;</td>
             </tr>
@@ -30,7 +42,7 @@
     </tbody>
 </table>
 
-<?php if ($this->structure_context->getFunction() == 'add_cat' || $this->structure_context->getFunction() == 'edit_cat'): ?>
+            <?php if (in_array($this->structure_context->getFunction(), ['add_cat', 'edit_cat'])): ?>
         </fieldset>
     </form>
 <?php endif;?>
