82d512366b483a429f25ed8b1e91784e30115a71
DanielWeitenauer <github@studio-ahoi.de>
2019-10-05 14:20:19 +0200

    Add template select class
    Extract template selection from page and move it to a dedicated class.

---
+++ redaxo/src/addons/structure/lib/select_template.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @package redaxo\structure
+ */
+class rex_template_select extends rex_select
+{
+    /**
+     * @var bool
+     */
+    protected $loaded = false;
+    /**
+     * @var int
+     */
+    protected $category_id;
+    /**
+     * @var array
+     */
+    protected $templates;
+    /**
+     * @var int
+     */
+    protected $clang_id;
+
+    /**
+     * @param int $category_id
+     * @param int $clang_id
+     */
+    public function __construct($category_id = 0, $clang_id = 1)
+    {
+        $this->category_id = (int) $category_id;
+        $this->clang_id = (int) $clang_id;
+
+        parent::__construct();
+    }
+
+    /**
+     * @return string
+     */
+    public function get()
+    {
+        if (!$this->loaded) {
+            $templates = $this->getTemplates();
+
+            if (count($templates) > 0) {
+                foreach ($templates as $t_id => $t_name) {
+                    $this->addOption($t_name, $t_id);
+                }
+            } else {
+                $this->addOption(rex_i18n::msg('option_no_template'), '0');
+            }
+
+            $this->loaded = true;
+        }
+
+        return parent::get();
+    }
+
+    /**
+     * @param int $selected
+     *
+     * @throws rex_sql_exception
+     */
+    public function setSelected($selected = -1)
+    {
+        if ($selected > 0) {
+            parent::setSelected($selected);
+        } else {
+            // Inherit template_id from start article
+            if ($this->category_id > 0) {
+                $sql = rex_sql::factory();
+                $sql->setQuery('SELECT template_id FROM '.rex::getTable('article').' WHERE id = ? AND clang_id = ? AND startarticle = 1', [
+                    $this->category_id,
+                    $this->clang_id,
+                ]);
+                if ($sql->getRows() == 1) {
+                    $selected = $sql->getValue('template_id');
+                }
+            }
+
+            $templates = $this->getTemplates();
+            if (!$selected || !isset($templates[$selected])) {
+                $selected = rex_template::getDefaultId();
+            }
+
+            if ($selected && isset($templates[$selected])) {
+                parent::setSelected($selected);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemplates()
+    {
+        if (!isset($this->templates)) {
+            $this->templates = [];
+
+            $templates = rex_template::getTemplatesForCategory($this->category_id);
+
+            if (count($templates) > 0) {
+                foreach ($templates as $t_id => $t_name) {
+                    $this->templates[$t_id] = rex_i18n::translate($t_name, false);
+                }
+            }
+        }
+
+        return $this->templates;
+    }
+}
--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -329,7 +329,7 @@ $echo = '';
 
 // --------------------- READ TEMPLATES
 
-$template_select = new rex_select();
+$template_select = new rex_template_select($category_id, $clang);
 
 if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('structure')->hasMountpoints())) {
     $withTemplates = $addon->getPlugin('content')->isAvailable();
@@ -339,16 +339,9 @@ if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('s
         $template_select->setSize(1);
         $template_select->setStyle('class="form-control selectpicker"');
 
-        $templates = rex_template::getTemplatesForCategory($category_id);
-        if (count($templates) > 0) {
-            foreach ($templates as $t_id => $t_name) {
-                $template_select->addOption(rex_i18n::translate($t_name, false), $t_id);
-                $TEMPLATE_NAME[$t_id] = rex_i18n::translate($t_name);
-            }
-        } else {
-            $template_select->addOption(rex_i18n::msg('option_no_template'), '0');
-        }
+        $TEMPLATE_NAME = $template_select->getTemplates();
         $TEMPLATE_NAME[0] = rex_i18n::msg('template_default_name');
+
         $tmpl_head = '<th>' . rex_i18n::msg('header_template') . '</th>';
     }
 
@@ -423,22 +416,7 @@ if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('s
     // --------------------- ARTIKEL ADD FORM
     if ('add_art' == $function && $KATPERM) {
         $tmpl_td = '';
-        if ($withTemplates) {
-            $selectedTemplate = 0;
-            if ($category_id) {
-                // template_id vom Startartikel erben
-                $sql2 = rex_sql::factory();
-                $sql2->setQuery('SELECT template_id FROM ' . rex::getTablePrefix() . 'article WHERE id=' . $category_id . ' AND clang_id=' . $clang . ' AND startarticle=1');
-                if (1 == $sql2->getRows()) {
-                    $selectedTemplate = $sql2->getValue('template_id');
-                }
-            }
-            if (!$selectedTemplate || !isset($TEMPLATE_NAME[$selectedTemplate])) {
-                $selectedTemplate = rex_template::getDefaultId();
-            }
-            if ($selectedTemplate && isset($TEMPLATE_NAME[$selectedTemplate])) {
-                $template_select->setSelected($selectedTemplate);
-            }
+            $template_select->setSelected();
 
             $tmpl_td = '<td data-title="' . rex_i18n::msg('header_template') . '">' . $template_select->get() . '</td>';
         }
@@ -475,7 +453,7 @@ if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('s
 
         if ('edit_art' == $function && $sql->getValue('id') == $article_id && $KATPERM) {
             $tmpl_td = '';
-            if ($withTemplates) {
+            if ($structure_context->hasTemplates()) {
                 $template_select->setSelected($sql->getValue('template_id'));
                 $tmpl_td = '<td data-title="' . rex_i18n::msg('header_template') . '">' . $template_select->get() . '</td>';
             }
@@ -515,7 +493,7 @@ if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('s
             $editModeUrl = $context->getUrl(['page' => 'content/edit', 'article_id' => $sql->getValue('id'), 'mode' => 'edit']);
 
             $tmpl_td = '';
-            if ($withTemplates) {
+            if ($structure_context->hasTemplates()) {
                 $tmpl = isset($TEMPLATE_NAME[$sql->getValue('template_id')]) ? $TEMPLATE_NAME[$sql->getValue('template_id')] : '';
                 $tmpl_td = '<td data-title="' . rex_i18n::msg('header_template') . '">' . $tmpl . '</td>';
             }
@@ -539,7 +517,7 @@ if ($category_id > 0 || (0 == $category_id && !rex::getUser()->getComplexPerm('s
             $art_status_icon = $artStatusTypes[$sql->getValue('status')][2];
 
             $tmpl_td = '';
-            if ($withTemplates) {
+            if ($structure_context->hasTemplates()) {
                 $tmpl = isset($TEMPLATE_NAME[$sql->getValue('template_id')]) ? $TEMPLATE_NAME[$sql->getValue('template_id')] : '';
                 $tmpl_td = '<td data-title="' . rex_i18n::msg('header_template') . '">' . $tmpl . '</td>';
             }
