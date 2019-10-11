9973b05ee4eb1560ebc98b1c44c38eb40b390905
Thomas Skerbis <thomas.skerbis@mac.com>
2019-10-11 16:59:20 +0200

    Weitere Intervalle für Errormail

--- redaxo/src/addons/phpmailer/lib/system_setting_phpmailer_errormail.php
+++ redaxo/src/addons/phpmailer/lib/system_setting_phpmailer_errormail.php
@@ -22,9 +22,12 @@ class rex_system_setting_phpmailer_errormail extends rex_system_setting
         $field->setLabel(rex_i18n::msg('system_setting_errormail'));
         $select = $field->getSelect();
         $select->addOption(rex_i18n::msg('phpmailer_errormail_disabled'), 0);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_60min'), 3600);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_6hrs'), 21600);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_12hrs'), 43200);
+        $select->addOption(rex_i18n::msg('phpmailer_errormail_24hrs'), 86400);
         $select->addOption(rex_i18n::msg('phpmailer_errormail_15min'), 900);
         $select->addOption(rex_i18n::msg('phpmailer_errormail_30min'), 1800);
-        $select->addOption(rex_i18n::msg('phpmailer_errormail_60min'), 3600);
         $select->setSelected(rex_config::get('phpmailer', 'errormail', 1));
         return $field;
     }
