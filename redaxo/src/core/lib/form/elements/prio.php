58ac1bded9a1f3d4b2c9d02c264ff9dba2e728f2
Markus Staab <maggus.staab@googlemail.com>
2019-10-12 09:06:48 +0200

    added phpdocs

--- redaxo/src/core/lib/form/elements/prio.php
+++ redaxo/src/core/lib/form/elements/prio.php
@@ -5,18 +5,28 @@
  */
 class rex_form_prio_element extends rex_form_select_element
 {
+    /** @var string */
     private $labelField;
+    /** @var callable */
     private $labelCallback;
+    /** @var string */
     private $whereCondition;
+    /** @var string */
     private $primaryKey;
+    /** @var string */
     private $firstOptionMsg;
+    /** @var string */
     private $optionMsg;
+    
+    /** @var rex_form */
+    protected $table;
 
     // 1. Parameter nicht genutzt, muss aber hier stehen,
     // wg einheitlicher Konstrukturparameter
     public function __construct($tag = '', rex_form $table = null, array $attributes = [])
     {
         parent::__construct('', $table, $attributes);
+        $this->table = $table;
 
         $this->labelField = '';
         $this->whereCondition = '';
