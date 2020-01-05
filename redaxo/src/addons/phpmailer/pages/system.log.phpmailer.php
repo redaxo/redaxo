0e9bb15426de5a29d115e56a9e9aeff830b7af50
Thomas Skerbis <skerbis@klxm.de>
2020-01-05 23:31:07 +0100

    Formatierte Log-Ausgabe
    /Users/thomas/Desktop/Bildschirmfoto 2020-01-05 um 23.30.45.png

--- redaxo/src/addons/phpmailer/pages/system.log.phpmailer.php
+++ redaxo/src/addons/phpmailer/pages/system.log.phpmailer.php
@@ -1,5 +1,120 @@
 <?php
 
-$logFile = rex_mailer::logFile();
+/**
+ * PHPMailer Addon.
+ *
+ * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ *
+ * @package redaxo\phpmailer
+ */
 
-require rex_path::core('pages/system.log.external.php');
+use PHPMailer\PHPMailer\PHPMailer;
+
+class rex_mailer extends PHPMailer
+{
+    private $log;
+
+    public function __construct($exceptions = false)
+    {
+        $addon = rex_addon::get('phpmailer');
+        $this->Timeout = 10;
+        $this->setLanguage(rex_i18n::getLanguage(), $addon->getPath('vendor/phpmailer/phpmailer/language/'));
+        $this->XMailer = 'REXMailer';
+        $this->From = $addon->getConfig('from');
+        $this->FromName = $addon->getConfig('fromname');
+        $this->ConfirmReadingTo = $addon->getConfig('confirmto');
+        $this->Mailer = $addon->getConfig('mailer');
+        $this->Host = $addon->getConfig('host');
+        $this->Port = $addon->getConfig('port');
+        $this->CharSet = $addon->getConfig('charset');
+        $this->WordWrap = $addon->getConfig('wordwrap');
+        $this->Encoding = $addon->getConfig('encoding');
+        if (0 == $addon->getConfig('priority')) {
+            $this->Priority = null;
+        } else {
+            $this->Priority = $addon->getConfig('priority');
+        }
+        $this->SMTPDebug = $addon->getConfig('smtp_debug');
+        $this->SMTPSecure = $addon->getConfig('smtpsecure');
+        $this->SMTPAuth = $addon->getConfig('smtpauth');
+        $this->SMTPAutoTLS = $addon->getConfig('security_mode');
+        $this->Username = $addon->getConfig('username');
+        $this->Password = $addon->getConfig('password');
+
+        if ($bcc = $addon->getConfig('bcc')) {
+            $this->addBCC($bcc);
+        }
+
+        $this->log = $addon->getConfig('log');
+
+        parent::__construct($exceptions);
+    }
+
+    public function send()
+    {
+        return rex_timer::measure(__METHOD__, function () {
+            if ($this->log) {
+                $this->log();
+            }
+     parent::send();
+     if(!parent::send())
+         {
+           $this->toErrorLog();
+         return false;
+     }
+     return true;
+        });
+    }
+
+    public function toErrorLog()
+    {
+        $log = new rex_log_file(rex_path::addonData('phpmailer', 'mail.log'), 2000000);
+        $data = [
+            ($this->From.PHP_EOL),
+            (implode(', ', array_column($this->getToAddresses(), 0)).PHP_EOL),
+            ($this->Subject.PHP_EOL),
+            strip_tags($this->ErrorInfo),
+        ];
+        $log->add($data);
+    }    
+
+    /**
+     * @param bool $status
+     */
+    public function setLog($status)
+    {
+        $this->log = $status;
+    }
+
+    private function log()
+    {
+        $content = '<!-- '.PHP_EOL.date('d.m.Y H:i:s').PHP_EOL;
+        $content .= 'From : '.$this->From.PHP_EOL;
+        $content .= 'To : '.implode(', ', array_column($this->getToAddresses(), 0)).PHP_EOL;
+        $content .= 'Subject : '.$this->Subject.PHP_EOL;
+        $content .= ' -->'.PHP_EOL;
+        $content .= $this->Body;
+
+        $dir = self::logFolder().'/'.date('Y').'/'.date('m');
+
+        $count = 1;
+        $logFile = $dir.'/'.date('Y-m-d_H_i_s').'.html';
+        while (file_exists($logFile)) {
+            $logFile = $dir.'/'.date('Y-m-d_H_i_s').'_'.(++$count).'.html';
+        }
+
+        rex_file::put($logFile, $content);
+    }
+
+    public static function logFolder()
+    {
+        return rex_path::addonData('phpmailer', 'mail_log');
+    }
+    
+       public static function logFile()
+    {
+        return rex_path::addonData('phpmailer', 'mail.log');
+    }
+
+}
+
