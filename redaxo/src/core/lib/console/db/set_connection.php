76be23b62ef89ff8135f80366ac7877c16a82a18
bloep <bloep@users.noreply.github.com>
2020-01-05 19:09:15 +0000

    Apply php-cs-fixer changes

--- redaxo/src/core/lib/console/db/set_connection.php
+++ redaxo/src/core/lib/console/db/set_connection.php
@@ -15,7 +15,7 @@ class rex_command_db_set_connection extends rex_console_command
     protected function configure()
     {
         $this
-            ->setDescription("Sets database connection credentials.")
+            ->setDescription('Sets database connection credentials.')
             ->setHelp('Checks by default if a database connection can be established with the new settings.')
             ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'database host')
             ->addOption('login', null, InputOption::VALUE_OPTIONAL, 'database user')
