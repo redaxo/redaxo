4975132c2c9aa91fc826e89c1e1ffc1add3f494f
schuer <ds@decaf.de>
2020-01-09 13:43:32 +0100

    fix: remove legacy class breaking our styles

--- redaxo/src/addons/be_style/assets/javascripts/main.js
+++ redaxo/src/addons/be_style/assets/javascripts/main.js
@@ -1,3 +1,6 @@
 $(document).on('rex:ready', function (event, container) {
-    container.find('.selectpicker').selectpicker();
+    container.find('.selectpicker').selectpicker().on('loaded.bs.select', function () {
+        // remove legacy class `.bs3-has-addon` since it brings in broken styles
+        $(this).parent().removeClass('bs3-has-addon');
+    });
 });
