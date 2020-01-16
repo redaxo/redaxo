8a8aefcc9ccf1b1de870374e133765d23799c0dc
Jan Kristinus <jan.kristinus@yakamara.de>
2020-01-16 12:55:59 +0100

    fragment für toolbar erweitert, damit arbeits/Liveversionsbar bündiger läuft

--- redaxo/src/core/fragments/core/toolbar.php
+++ redaxo/src/core/fragments/core/toolbar.php
@@ -4,7 +4,7 @@ $target = 'rex-js-collapse-' . rand(100, 999) . rand(100, 999);
 
 ?>
 
-<nav class="navbar navbar-<?= (isset($this->inverse) && $this->inverse) ? 'inverse' : 'default'; ?><?= (isset($this->cssClass) && $this->cssClass != '') ? ' ' . $this->cssClass : ''; ?>">
+<nav class="navbar navbar-<?= (isset($this->inverse) && $this->inverse) ? 'inverse' : 'default'; ?><?= (!isset($this->brand) || !$this->brand || $this->brand === '') ? ' rex-navbar-without-brand' : ' rex-navbar-without-brand' ?><?= (isset($this->cssClass) && $this->cssClass != '') ? ' ' . $this->cssClass : ''; ?>">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#<?= $target; ?>">
@@ -15,7 +15,7 @@ $target = 'rex-js-collapse-' . rand(100, 999) . rand(100, 999);
             </button>
             <?php if (isset($this->brand) && $this->brand != ''): ?>
                 <span class="navbar-brand">
-                    <?= $this->brand; ?>
+                    <?= $this->brand; ?>ddd
                 </span>
             <?php endif; ?>
         </div>
