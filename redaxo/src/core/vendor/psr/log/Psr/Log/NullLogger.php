83c525b6e4c022007e676c1437a90271091b34d3
Markus Staab <maggus.staab@googlemail.com>
2020-01-26 13:49:23 +0100

    Revert NullLogger.php

--- redaxo/src/core/vendor/psr/log/Psr/Log/NullLogger.php
+++ redaxo/src/core/vendor/psr/log/Psr/Log/NullLogger.php
@@ -19,7 +19,7 @@ class NullLogger extends AbstractLogger
      * @param string $message
      * @param array  $context
      *
-     *
+     * @return void
      *
      * @throws \Psr\Log\InvalidArgumentException
      */
