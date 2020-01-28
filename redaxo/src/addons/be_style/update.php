56aaa427309e0316b0f14a3ff319d11c7d52c084
Gregor Harlan <mail@gh01.de>
2020-01-28 23:09:10 +0100

    comment

--- redaxo/src/addons/be_style/update.php
+++ redaxo/src/addons/be_style/update.php
@@ -5,6 +5,7 @@ $addon = rex_addon::get('be_style');
 // use path relative to __DIR__ to get correct path in update temp dir
 $addon->includeFile(__DIR__.'/install.php');
 
+// update.php only exist in addons and is also responsible for their plugins
 foreach ($addon->getInstalledPlugins() as $plugin) {
     $file = __DIR__.'/plugins/'.$plugin->getName().'/install.php';
 
