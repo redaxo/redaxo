3964abd676bd696e06e6aa91092732e122f06fac
Gregor Harlan <mail@gh01.de>
2020-01-28 19:49:33 +0100

    be_style: update.php für customizer reinstall

---
+++ redaxo/src/addons/be_style/update.php
@@ -0,0 +1,14 @@
+<?php
+
+$addon = rex_addon::get('be_style');
+
+// use path relative to __DIR__ to get correct path in update temp dir
+$addon->includeFile(__DIR__.'/install.php');
+
+foreach ($addon->getInstalledPlugins() as $plugin) {
+    $file = __DIR__.'/plugins/'.$plugin->getName().'/install.php';
+
+    if (file_exists($file)) {
+        $plugin->includeFile($file);
+    }
+}
