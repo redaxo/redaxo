78d71ac63d3042ff8a036891189df482909580a1
Markus Staab <maggus.staab@googlemail.com>
2020-02-05 14:33:34 +0100

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 unset($REX);
 $REX['REDAXO'] = false;
 $REX['HTDOCS_PATH'] = './';
-$REX['BACKEND_FOLDER'] = 'redaxo';
+$REX['BACKEND_FOLDER'] = 'redaxowewer';
 $REX['LOAD_PAGE'] = true;
 
 require $REX['BACKEND_FOLDER'] . '/src/core/boot.php';
