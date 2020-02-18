e5d834a7806b2be6d3f410258f0700958827889f
Marcel Kuhmann <kuhmann@koders.nrw>
2020-02-18 18:05:33 +0100

    create real testcase

--- redaxo/src/core/tests/console/config/get_test.php
+++ redaxo/src/core/tests/console/config/get_test.php
@@ -4,9 +4,9 @@ class rex_command_config_get_test extends \PHPUnit\Framework\TestCase {
 
     public function testGetConfig() {
         $commandTester = new \Symfony\Component\Console\Tester\CommandTester(new rex_command_config_get());
-        $website = $commandTester->execute([
-            'website'
+        $configValue = $commandTester->execute([
+            'config-key' => 'setup'
         ]);
-        $this->assertEquals('website', $website);
+        $this->assertTrue($configValue);
     }
 }
