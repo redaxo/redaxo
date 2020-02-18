e9c68574691e03c332d8f8f4c81c3622c870b7dc
Marcel Kuhmann <kuhmann@koders.nrw>
2020-02-18 18:39:42 +0100

    base tester class more config::get tests

--- redaxo/src/core/tests/console/config/get_test.php
+++ redaxo/src/core/tests/console/config/get_test.php
@@ -1,16 +1,37 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * @internal
  */
-class rex_command_config_get_test extends \PHPUnit\Framework\TestCase
+class rex_command_config_get_test extends TestCase
 {
-    public function testGetConfig()
+    /**
+     * @dataProvider dataKeyFound
+     */
+    public function testKeyFound($expectedValue, $key)
     {
-        $commandTester = new \Symfony\Component\Console\Tester\CommandTester(new rex_command_config_get());
+        $commandTester = new rex_console_command_tester(new rex_command_config_get());
         $configValue = $commandTester->execute([
-            'config-key' => 'setup',
+            'config-key' => $key,
         ]);
-        static::assertTrue($configValue);
+        static::assertEquals($expectedValue, $configValue);
+    }
+
+    public function dataKeyFound() {
+        return [
+            [0, 'setup'],
+            [null, 'session.cookie.backend.lifetime'],
+            ['root', 'db.1.login'],
+        ];
+    }
+
+    public function testKeyNotFound() {
+        $commandTester = new rex_console_command_tester(new rex_command_config_get());
+        $commandTester->execute([
+            'config-key' => 'foo.bar'
+        ]);
+        $this->assertEquals(1, $commandTester->getStatusCode());
     }
 }
---
+++ redaxo/src/core/tests/console/rex_console_command_tester.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @package redaxo/core
+ * @internal
+ */
+class rex_console_command_tester extends CommandTester
+{
+
+}
