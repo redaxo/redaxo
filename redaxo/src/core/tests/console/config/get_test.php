194b7792037f10c389a3b2f03df0e3cda68f5547
bloep <bloep@users.noreply.github.com>
2020-02-18 17:02:06 +0000

    Apply php-cs-fixer changes

--- redaxo/src/core/tests/console/config/get_test.php
+++ redaxo/src/core/tests/console/config/get_test.php
@@ -1,12 +1,16 @@
 <?php
 
-class rex_command_config_get_test extends \PHPUnit\Framework\TestCase {
-
-    public function testGetConfig() {
+/**
+ * @internal
+ */
+class rex_command_config_get_test extends \PHPUnit\Framework\TestCase
+{
+    public function testGetConfig()
+    {
         $commandTester = new \Symfony\Component\Console\Tester\CommandTester(new rex_command_config_get());
         $website = $commandTester->execute([
-            'website'
+            'website',
         ]);
-        $this->assertEquals('website', $website);
+        static::assertEquals('website', $website);
     }
 }
