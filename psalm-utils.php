969f27b9ec1d56bfb9be5771caf1d41310162e93
Gregor Harlan <mail@gh01.de>
2020-02-19 09:50:18 +0100

    CS in Fragmenten und psalm-utils fixen (#3302)

--- psalm-utils.php
+++ psalm-utils.php
@@ -7,14 +7,13 @@ use Psalm\Type;
 
 class RexTypeReturnProvider implements \Psalm\Plugin\Hook\MethodReturnTypeProviderInterface
 {
-
-    public static function getClassLikeNames() : array
+    public static function getClassLikeNames(): array
     {
         return ['rex_type'];
     }
 
     /**
-     * @param  array<PhpParser\Node\Arg>    $call_args
+     * @param array<PhpParser\Node\Arg> $call_args
      *
      * @return ?Type\Union
      */
@@ -29,13 +28,13 @@ class RexTypeReturnProvider implements \Psalm\Plugin\Hook\MethodReturnTypeProvid
         string $called_fq_classlike_name = null,
         string $called_method_name_lowercase = null
     ) {
-        if ($method_name_lowercase === 'cast'
+        if ('cast' === $method_name_lowercase
             && isset($call_args[1]->value->inferredType)
             && $call_args[1]->value->inferredType->isSingleStringLiteral()
         ) {
-             $vartype = (string) $call_args[1]->value->inferredType->getSingleStringLiteral()->value;
+            $vartype = (string) $call_args[1]->value->inferredType->getSingleStringLiteral()->value;
 
-             switch ($vartype) {
+            switch ($vartype) {
                 case 'bool':
                 case 'boolean':
                 case 'int':
--- redaxo/src/core/fragments/core/navigation.php
+++ redaxo/src/core/fragments/core/navigation.php
@@ -1,5 +1,5 @@
 
-        <?php if ($this->navigation != ''): ?>
+        <?php if ('' != $this->navigation): ?>
         <nav id="rex-js-nav-main" class="rex-nav-main navbar-default" role="navigation">
             <div class="navbar-collapse collapse">
                 <?= $this->navigation ?>
