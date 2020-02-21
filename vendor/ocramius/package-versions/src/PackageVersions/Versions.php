5cd15516ce623c4df272b0d75cfdccb728cdb92c
staabm <staabm@users.noreply.github.com>
2020-02-21 21:31:35 +0000

    Apply php-cs-fixer changes

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -60,7 +60,7 @@ final class Versions
   'webmozart/assert' => '1.7.0@aed98a490f9a8f78468232db345ab9cf606cf598',
   'webmozart/glob' => '4.1.0@3cbf63d4973cf9d780b93d2da8eec7e4a9e63bbe',
   'webmozart/path-util' => '2.3.0@d939f7edc24c9a1bb9c0dee5cb05d8e859490725',
-  '__root__' => 'dev-121b9286bea770b65baa9fd0c608fb41a6901de2@121b9286bea770b65baa9fd0c608fb41a6901de2',
+  '__root__' => 'dev-ff39e5a3edf98468ff14da58f97e20f84882b871@ff39e5a3edf98468ff14da58f97e20f84882b871',
 );
 
     private function __construct()
