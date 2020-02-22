427d7f4d2667fa4a3fd3baf935566148105b523e
Markus Staab <maggus.staab@googlemail.com>
2020-02-23 00:40:44 +0100

    implement rex_file::move() (#3347)

--- redaxo/src/core/lib/util/file.php
+++ redaxo/src/core/lib/util/file.php
@@ -136,6 +136,19 @@ class rex_file
     }
 
     /**
+     * Renames a file.
+     *
+     * @param string $srcfile Path of the source file
+     * @param string $dstfile Path of the destination file or directory
+     *
+     * @return bool TRUE on success, FALSE on failure
+     */
+    public static function move(string $srcfile, string $dstfile): bool
+    {
+        return rename($srcfile, $dstfile);
+    }
+
+    /**
      * Deletes a file.
      *
      * @param string $file Path of the file
