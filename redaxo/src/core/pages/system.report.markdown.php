b0422f55c1f800887bdc486b38944c0f18f0538f
Markus Staab <markus.staab@redaxo.de>
2020-02-23 00:27:31 +0100

    added clipboard-copy button for markdown report
    because copy paste via select+copy doesnt work

--- redaxo/src/core/pages/system.report.markdown.php
+++ redaxo/src/core/pages/system.report.markdown.php
@@ -6,16 +6,18 @@ echo '
     <div id="rex-system-report-markdown" contenteditable="true" spellcheck="false">
         <pre>'.rex_escape($report).'</pre>
     </div>
-    
+
+    <clipboard-copy for="rex-system-report-markdown">Copy</clipboard-copy>
+
     <script>
         $("#rex-system-report-markdown")
-            .on("cut paste", function (event) { 
-                event.preventDefault(); 
+            .on("cut paste", function (event) {
+                event.preventDefault();
             })
-            .on("keydown", function (event) { 
+            .on("keydown", function (event) {
                 if (!event.metaKey) {
                     event.preventDefault();
-                } 
+                }
             });
     </script>
 ';
