58047386097d4f08a44fea1230289b0d038a78a4
Marcel Kuhmann <kuhmann@koders.nrw>
2020-02-23 15:17:00 +0100

    added command package:delete

---
+++ redaxo/src/core/lib/console/package/delete.php
@@ -0,0 +1,44 @@
+<?php
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @package redaxo\core
+ *
+ * @internal
+ */
+class rex_command_package_delete extends rex_console_command
+{
+    protected function configure()
+    {
+        $this
+            ->setDescription('Deletes the selected package')
+            ->addArgument('package-id', InputArgument::REQUIRED, 'The id of the package (addon or plugin); e.g. "cronjob" or "structure/content"');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = $this->getStyle($input, $output);
+
+        $packageId = $input->getArgument('package-id');
+        $package = rex_package::get($packageId);
+        if ($package instanceof rex_null_package) {
+            $io->error('Package "'.$packageId.'" doesn\'t exists!');
+            return 1;
+        }
+
+        $manager = rex_package_manager::factory($package);
+        $success = $manager->delete();
+        $message = $this->decodeMessage($manager->getMessage());
+
+        if ($success) {
+            $io->success($message);
+            return 0;
+        }
+
+        $io->error($message);
+        return 1;
+    }
+}
