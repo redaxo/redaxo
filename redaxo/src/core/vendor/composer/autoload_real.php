e41bdc49895a855d63c7dcc4628d3e0ca35242e4
Gregor Harlan <mail@gh01.de>
2020-02-27 18:12:19 +0100

    classmap-authoritative rückgängig

--- redaxo/src/core/vendor/composer/autoload_real.php
+++ redaxo/src/core/vendor/composer/autoload_real.php
@@ -29,13 +29,22 @@ class ComposerAutoloaderInitRedaxoCore
 
             call_user_func(\Composer\Autoload\ComposerStaticInitRedaxoCore::getInitializer($loader));
         } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         if ($useStaticLoader) {
