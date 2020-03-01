68ee648c49b1d576695511ac5a55bd39f7ee1ec1
Gregor Harlan <mail@gh01.de>
2020-03-01 11:35:12 +0100

    regex

--- redaxo/src/core/lib/util/version.php
+++ redaxo/src/core/lib/util/version.php
@@ -15,11 +15,6 @@ class rex_version
     public static function isUnstable(?string $version): bool
     {
         // see https://www.php.net/manual/en/function.version-compare.php
-        foreach (['dev', 'alpha', 'a', 'beta', 'b', 'rc', 'pl'] as $unstable) {
-            if (false !== stripos($version, $unstable)) {
-                return true;
-            }
-        }
-        return false;
+        return (bool) preg_match('/(?<![a-z])(?:dev|alpha|a|beta|b|rc|pl)(?![a-z])/i', $version);
     }
 }
