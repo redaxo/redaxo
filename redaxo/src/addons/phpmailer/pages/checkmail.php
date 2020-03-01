9a849afcaa25324f7ecf95f015a6295b07228660
Thomas Skerbis <thomas.skerbis@mac.com>
2020-03-01 10:29:16 +0100

    Update redaxo/src/addons/phpmailer/pages/checkmail.php
    Co-Authored-By: Markus Staab <maggus.staab@googlemail.com>

--- redaxo/src/addons/phpmailer/pages/checkmail.php
+++ redaxo/src/addons/phpmailer/pages/checkmail.php
@@ -46,7 +46,7 @@ if ($emptymail = true) {
     $mail->Body .= "\nMailer: " . $addon->getConfig('mailer') . $devider.$security_mode;
     $mail->Body .= "\n". $addon->i18n('checkmail_domain_note'). "\n". $devider;
     $GLOBALS['mailer_debug'] = '';
-    $mail->Debugoutput = static function ($str, $level) {
+    $mail->Debugoutput = static function ($str, $level) use (&$mailerDebug) {
         $GLOBALS['mailer_debug'] .= date('m.d.Y h:i:s a', time()).' '.nl2br($str);
     };
 
