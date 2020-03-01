342557363aa818b0e1824f6647d6aef28b9efaf5
Gregor Harlan <mail@gh01.de>
2020-03-01 11:40:02 +0100

    nicht nullable

--- redaxo/src/core/lib/util/version.php
+++ redaxo/src/core/lib/util/version.php
@@ -12,7 +12,7 @@ class rex_version
         // noop
     }
 
-    public static function isUnstable(?string $version): bool
+    public static function isUnstable(string $version): bool
     {
         // see https://www.php.net/manual/en/function.version-compare.php
         return (bool) preg_match('/(?<![a-z])(?:dev|alpha|a|beta|b|rc|pl)(?![a-z])/i', $version);
