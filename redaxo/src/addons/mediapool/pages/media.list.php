d62e35feefa5c3bf9132c16488d96d363182370c
Gregor Harlan <mail@gh01.de>
2020-03-01 13:59:33 +0100

    MP: Fallback für alte MM-Versionen entfernt

--- redaxo/src/addons/mediapool/pages/media.list.php
+++ redaxo/src/addons/mediapool/pages/media.list.php
@@ -251,12 +251,8 @@ $panel = '
 
                 if (!rex_addon::get('media_manager')->isAvailable()) {
                     $media_manager_url = null;
-                } elseif (method_exists(rex_media_manager::class, 'getUrl')) {
-                    $media_manager_url = [rex_media_manager::class, 'getUrl'];
                 } else {
-                    $media_manager_url = static function ($type, $file, $timestamp) {
-                        return rex_url::backendController(['rex_media_type' => $type, 'rex_media_file' => $file]);
-                    };
+                    $media_manager_url = [rex_media_manager::class, 'getUrl'];
                 }
 
                 $panel .= '<tbody>';
