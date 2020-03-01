ded67bc940630c2a6a38f0d660f22e422474ef90
Markus Staab <maggus.staab@googlemail.com>
2020-03-01 22:25:59 +0100

    Update redaxo/src/addons/install/lib/api/api_core_update.php
    Co-Authored-By: Gregor Harlan <mail@gh01.de>

--- redaxo/src/addons/install/lib/api/api_core_update.php
+++ redaxo/src/addons/install/lib/api/api_core_update.php
@@ -8,7 +8,7 @@
 class rex_api_install_core_update extends rex_api_function
 {
     /**
-     * @psalm-return array<int, array{version: string, path: string, description: string}>
+     * @psalm-return array<int, array{version: string, description: string, path: string, checksum: string}>
      *
      * @return array
      */
