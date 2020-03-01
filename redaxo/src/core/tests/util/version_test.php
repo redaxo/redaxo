2306cbf2ee8fbb684402164a51089c3b8ea354d0
staabm <staabm@users.noreply.github.com>
2020-03-01 09:12:36 +0000

    Apply php-cs-fixer changes

--- redaxo/src/core/tests/util/version_test.php
+++ redaxo/src/core/tests/util/version_test.php
@@ -9,16 +9,16 @@ class rex_version_test extends TestCase
 {
     public function testIsUnstable()
     {
-        $this->assertTrue(rex_version::isUnstable('1.0-dev'));
-        $this->assertTrue(rex_version::isUnstable('2.1.0beta'));
-        $this->assertTrue(rex_version::isUnstable('1.0b1'));
-        $this->assertTrue(rex_version::isUnstable('3.5.1-alpha'));
-        $this->assertTrue(rex_version::isUnstable('99.99RC5'));
-        $this->assertTrue(rex_version::isUnstable('199.199 RC3'));
-        
-        $this->assertFalse(rex_version::isUnstable('1.0'));
-        $this->assertFalse(rex_version::isUnstable('1.0-final'));
-        $this->assertFalse(rex_version::isUnstable('2.45 stable'));
-        $this->assertFalse(rex_version::isUnstable('1.0 codename starship'));
+        static::assertTrue(rex_version::isUnstable('1.0-dev'));
+        static::assertTrue(rex_version::isUnstable('2.1.0beta'));
+        static::assertTrue(rex_version::isUnstable('1.0b1'));
+        static::assertTrue(rex_version::isUnstable('3.5.1-alpha'));
+        static::assertTrue(rex_version::isUnstable('99.99RC5'));
+        static::assertTrue(rex_version::isUnstable('199.199 RC3'));
+
+        static::assertFalse(rex_version::isUnstable('1.0'));
+        static::assertFalse(rex_version::isUnstable('1.0-final'));
+        static::assertFalse(rex_version::isUnstable('2.45 stable'));
+        static::assertFalse(rex_version::isUnstable('1.0 codename starship'));
     }
-    }
+}
