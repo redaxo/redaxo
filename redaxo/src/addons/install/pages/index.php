cb962d78c834907ac66acbb2cf146701115021c7
Gregor Harlan <mail@gh01.de>
2020-03-03 10:13:23 +0100

    Installer: Absicherung in R<5.10 korrigiert

--- redaxo/src/addons/install/pages/index.php
+++ redaxo/src/addons/install/pages/index.php
@@ -12,18 +12,17 @@ $markdown = static function (string $content): string {
     static $markdown;
 
     if (!$markdown) {
-        $parser = rex_markdown::factory();
+        if (rex_string::versionCompare(rex::getVersion(), '5.10.0-dev', '<')) {
+            // prior to 5.10 rex_markdown did not prevent xss
+            // so we use Parsedown directly with enabled safe mode
+            $parser = new ParsedownExtra();
+            $parser->setSafeMode(true);
+            $parser->setBreaksEnabled(true);
 
-        // prior to 5.10 rex_markdown did not prevent xss
-        $stripTags = rex_string::versionCompare(rex::getVersion(), '5.10.0-dev', '<');
-
-        $markdown = static function (string $content) use ($parser, $stripTags): string {
-            if ($stripTags) {
-                $content = strip_tags($content);
-            }
-
-            return $parser->parse($content);
-        };
+            $markdown = [$parser, 'text'];
+        } else {
+            $markdown = [rex_markdown::factory(), 'parse'];
+        }
     }
 
     $fragment = new rex_fragment();
