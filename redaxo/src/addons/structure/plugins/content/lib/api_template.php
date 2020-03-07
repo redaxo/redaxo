8bf5d5b514e3de95a6de1f0f09b2e82460fe502a
staabm <staabm@users.noreply.github.com>
2020-03-07 18:38:45 +0000

    Apply php-cs-fixer changes

--- redaxo/src/addons/structure/plugins/content/lib/api_template.php
+++ redaxo/src/addons/structure/plugins/content/lib/api_template.php
@@ -57,7 +57,6 @@ class rex_template
     public function getKey(): ?string
     {
         if ('' === $this->key) {
-
             $sql = rex_sql::factory()->setQuery(
                 'SELECT `key` FROM '.rex::getTable('template').' WHERE `id` = :id',
                 ['id' => $this->id]
