745d5cddc345705d9943c8a06aeccde19ed4a95a
Marcel Kuhmann <der_marcell@hotmail.com>
2020-03-09 10:17:26 +0100

    console: sync packages with filesystem (#3444)

--- redaxo/src/core/lib/console/package/install.php
+++ redaxo/src/core/lib/console/package/install.php
@@ -25,6 +25,11 @@ class rex_command_package_install extends rex_console_command
         $io = $this->getStyle($input, $output);
 
         $packageId = $input->getArgument('package-id');
+
+        // the package manager don't know new packages in the addon folder
+        // so we need to make them available
+        rex_package_manager::synchronizeWithFileSystem();
+
         $package = rex_package::get($packageId);
         if (!$package instanceof rex_package) {
             $io->error('Package "'.$packageId.'" doesn\'t exists!');
