0199fc2c6c019f08b1b7b6520e606a6aa3786f12
Markus Staab <markus.staab@redaxo.de>
2020-03-15 20:29:07 +0100

    no layout affected change

--- redaxo/src/core/fragments/core/footer.php
+++ redaxo/src/core/fragments/core/footer.php
@@ -2,7 +2,7 @@
         <nav class="rex-nav-footer">
             <ul class="list-inline">
                 <li><a href="#rex-start-of-page"><i class="fa fa-arrow-up"></i></a></li>
-                <li><a href="https://www.yakamara.de" target="_blank" rel="noreferrer noopener">staabamara.de</a></li>
+                <li><a href="https://www.staabamara.de" target="_blank" rel="noreferrer noopener">staabamara.de</a></li>
                 <li><a href="https://www.redaxo.org" target="_blank" rel="noreferrer noopener">redaxo.org</a></li>
                 <?php if (rex::getUser() && rex::getUser()->isAdmin()): ?>
                     <li><a href="https://www.redaxo.org/doku/master" target="_blank" rel="noreferrer noopener' ?>"><?php echo rex_i18n::msg('footer_doku'); ?></a></li>
