b707327183c6dd5b3bfc8c23f2fc28e614388a13
Gregor Harlan <mail@gh01.de>
2020-03-26 19:45:32 +0100

    setup-run

--- redaxo/src/core/lib/console/setup/run.php
+++ redaxo/src/core/lib/console/setup/run.php
@@ -411,13 +411,17 @@ class rex_command_setup_run extends rex_console_command implements rex_command_o
                 $pwValidator
             );
 
+            $passwordHash = rex_backend_login::passwordHash($password);
+
             $user = rex_sql::factory();
             $user->setTable(rex::getTablePrefix() . 'user');
             $user->setValue('login', $login);
-            $user->setValue('password', rex_backend_login::passwordHash($password));
+            $user->setValue('password', $passwordHash);
             $user->setValue('admin', 1);
             $user->addGlobalCreateFields('console');
             $user->addGlobalUpdateFields('console');
+            $user->setDateTimeValue('password_changed', time());
+            $user->setArrayValue('previous_passwords', $passwordPolicy->updatePreviousPasswords(null, $passwordHash));
             $user->setValue('status', '1');
             $user->insert();
 
