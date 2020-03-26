886ebb720e025f903a7d8ac723607be319df8abf
Marcel Kuhmann <der_marcell@hotmail.com>
2020-03-26 19:51:07 +0100

    try to break it.

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -2,7 +2,8 @@
         <nav class="rex-nav-top navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <?php if (rex_be_controller::getCurrentPageObject()->isPopup()): ?>
+                    break it
+                    <?php if (rex_be_controller::getCurrentPageObject()->isPopup()                ): ?>
                         <span class="navbar-brand"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></span>
                     <?php else: ?>
                         <a class="navbar-brand" href="<?= rex_url::backendController() ?>"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></a>
