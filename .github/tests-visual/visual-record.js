9c897f3b16c66903a5d17844cdf914b1a4d98bde
Marcel Kuhmann <der_marcell@hotmail.com>
2020-03-27 15:35:28 +0100

    try to make it more reliable

--- .github/tests-visual/visual-record.js
+++ .github/tests-visual/visual-record.js
@@ -78,7 +78,7 @@ async function createScreenshot(page, screenshotName) {
 }
 
 async function main() {
-    const options = { args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+    const options = { args: ['--no-sandbox', '--disable-setuid-sandbox', '--font-render-hinting=none'] };
 
     if (DEBUGGING) {
         // see https://developers.google.com/web/tools/puppeteer/debugging
