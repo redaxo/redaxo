7c3d8c513aa8beb312dc24e7295422377331f097
Markus Staab <markus.staab@redaxo.de>
2020-04-19 14:36:38 +0200

    fixed types

--- redaxo/src/core/lib/list.php
+++ redaxo/src/core/lib/list.php
@@ -56,7 +56,7 @@ class rex_list implements rex_url_provider_interface
     private $db;
     /** @var string */
     private $query;
-    /** @var string */
+    /** @var rex_sql */
     private $sql;
     /** @var bool */
     private $debug;
@@ -102,7 +102,7 @@ class rex_list implements rex_url_provider_interface
     private $caption;
     /** @psalm-var array<string, string|int|bool> */
     private $tableAttributes;
-    /** @var list<array<string, string|int|bool>> */
+    /** @var list<array<string, string|int|bool|array>> */
     private $tableColumnGroups;
 
     // --------- Link Attributes
