dc491bf2764599e778af8fc0f266e326e75fd6e0
Markus Staab <maggus.staab@googlemail.com>
2020-04-19 20:17:45 +0200

    Update phpstan-console.php

--- phpstan-console.php
+++ phpstan-console.php
@@ -5,5 +5,5 @@ $REX['REDAXO'] = true;
 $REX['HTDOCS_PATH'] = './';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 
-$application = require __DIR__.'/../src/core/console.php';
+$application = require __DIR__.'/redaxo/src/core/console.php';
 return $application;
