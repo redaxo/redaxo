8733a8bdf79028647db463d2012cb516982b106d
Markus Staab <maggus.staab@googlemail.com>
2020-05-13 18:37:15 +0200

    Update api_function.php

--- redaxo/src/core/lib/api_function.php
+++ redaxo/src/core/lib/api_function.php
@@ -101,7 +101,7 @@ abstract class rex_api_function
      * The method must be called on sub classes.
      *
      * @return array
-     * @psalm-return {rex-api-call: string, _csrf_token: string}
+     * @psalm-return array{rex-api-call: string, _csrf_token: string}
      */
     public static function getUrlParams()
     {
