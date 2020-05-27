22b0bee559c3fb0d70f54272c5d63a59ed6ecf30
Thomas Skerbis <thomas.skerbis@mac.com>
2020-05-27 10:46:10 +0200

    Update der Rollen.
    @gharlan ich habe prüfe hier auf eine Version 2.4.3-beta1, welche Version soll ich prüfen?

--- redaxo/src/addons/mediapool/update.php
+++ redaxo/src/addons/mediapool/update.php
@@ -2,4 +2,24 @@
 
 $addon = rex_addon::get('mediapool');
 
-$addon->includeFile(__DIR__.'/install.php');
+$addon->includeFile(__DIR__ . '/install.php');
+
+if (rex_addon::get('users')->isInstalled() && rex_addon::get('mediapool')->isInstalled()  && rex_string::versionCompare($addon->getVersion(), '2.4.3-beta1', '<')) {
+    $sql = rex_sql::factory();
+    $sql->transactional(static function () use ($sql) {
+        $roles = rex_sql::factory()->setQuery('SELECT * FROM ' . rex::getTable('user_role'));
+        /** @var rex_sql $role */
+        foreach ($roles as $role) {
+            $perms = $role->getArrayValue('perms');
+            if ($perms['media'] == 'all') {
+                $perms['general'] = $perms['general'] ?? '|';
+                $perms['general'] .= 'media[sync]|';
+                $sql
+                    ->setTable(rex::getTable('user_role'))
+                    ->setWhere(['id' => $role->getValue('id')])
+                    ->setArrayValue('perms', $perms)
+                    ->update();
+            }
+        }
+    });
+}
