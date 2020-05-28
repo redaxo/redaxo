ff6867204b2b39efe453420743788634441af524
Joachim Doerr <mail@joachim-doerr.com>
2020-05-28 12:46:28 +0200

    add permission to boot and package, add translations

--- redaxo/src/addons/mediapool/boot.php
+++ redaxo/src/addons/mediapool/boot.php
@@ -13,10 +13,13 @@ $addon = rex_addon::get('mediapool');
 $mypage = 'mediapool';
 
 rex_complex_perm::register('media', rex_media_perm::class);
+rex_complex_perm::register('media_read', rex_media_read_perm::class);
 
 require_once __DIR__ . '/functions/function_rex_mediapool.php';
 
 if (rex::isBackend() && rex::getUser()) {
+    rex_perm::register('media_pool[categories_editor]', null, rex_perm::EXTRAS);
+
     rex_view::addJsFile($addon->getAssetsUrl('lazysizes.min.js'), [rex_view::JS_IMMUTABLE => true, rex_view::JS_ASYNC => true]);
     rex_view::addJsFile($addon->getAssetsUrl('mediapool.js'), [rex_view::JS_IMMUTABLE]);
     rex_view::setJsProperty('imageExtensions', $addon->getProperty('image_extensions'));
