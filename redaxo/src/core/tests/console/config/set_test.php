62f7ed24cbc06dba438c1f96aad178fdcfaf246c
Marcel Kuhmann <der_marcell@hotmail.com>
2020-06-01 20:45:42 +0200

    Update set_test.php

--- redaxo/src/core/tests/console/config/set_test.php
+++ redaxo/src/core/tests/console/config/set_test.php
@@ -8,12 +8,17 @@ use Symfony\Component\Console\Tester\CommandTester;
  */
 class rex_command_config_set_test extends TestCase
 {
+    private $initialConfig;
+    
+    protected function setUp() {
+        $configPath = rex_path::coreData('config.yml');
+        $this->initialConfig = file_get_contents($configPath);
+    }
+    
     protected function tearDown()
     {
         $configPath = rex_path::coreData('config.yml');
-        $config = rex_file::getConfig($configPath);
-        unset($config['test']);
-        rex_file::putConfig($configPath, $config);
+        file_put_contents($configPath, $this->initialConfig);
     }
 
     /**
