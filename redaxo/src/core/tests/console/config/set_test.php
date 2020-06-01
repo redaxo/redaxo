5098fcceb6973455bc42b5d867cf4a7433522d16
Marcel Kuhmann <kuhmann@koders.nrw>
2020-06-01 14:22:46 +0200

    add tests

---
+++ redaxo/src/core/tests/console/config/set_test.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @internal
+ */
+class rex_command_config_set_test extends TestCase
+{
+    /**
+     * @dataProvider dataSetBoolean
+     */
+    public function testSetBoolean($expectedValue, $value)
+    {
+        $commandTester = new CommandTester(new rex_command_config_set());
+        $commandTester->execute([
+            'type' => 'bool',
+            'config-key' => 'test',
+            'value' => $value,
+        ]);
+        $config = rex_file::getConfig(rex_path::coreData('config.yml'));
+        static::assertIsBool($config['test']);
+        static::assertEquals($expectedValue, $config['test']);
+        static::assertEquals(0, $commandTester->getStatusCode());
+    }
+
+    public function dataSetBoolean()
+    {
+        return [
+            [true, '1'],
+            [false, '0'],
+            [true, 'true'],
+            [false, 'false'],
+        ];
+    }
+}
