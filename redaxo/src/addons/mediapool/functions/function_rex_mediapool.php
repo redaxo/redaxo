904a34069398802776a37cb29234488cd27aab58
Gregor Harlan <mail@gh01.de>
2020-06-05 16:52:37 +0200

    etwas vereinfacht

--- redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
+++ redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
@@ -91,7 +91,6 @@ function rex_mediapool_saveMedia($FILE, $rex_file_category, $FILEINFOS, $userlog
     $dstFile = rex_path::media($NFILENAME);
 
     $success = true;
-    $isFileSync = !$isFileUpload;
     $FILETYPE = $isFileUpload ? rex_file::mimeType($FILE['tmp_name']) : rex_file::mimeType($srcFile);
 
     // Bevor die Datei engueltig in den Medienpool uebernommen wird, koennen
@@ -116,20 +115,17 @@ function rex_mediapool_saveMedia($FILE, $rex_file_category, $FILEINFOS, $userlog
         // ein Addon hat die Fehlermeldung gesetzt, dem Upload also faktisch widersprochen
         $success = false;
         $message[] = $upload_error_msg;
-    }
-
-    // kein Addon hat dem Upload widersprochen, die Datei kann kopiert werden
-    if ($isFileUpload && !$upload_error_msg) { // Fileupload?
+    } elseif ($isFileUpload) { // Fileupload?
         if (!@move_uploaded_file($FILE['tmp_name'], $dstFile)) {
             $message[] = rex_i18n::msg('pool_file_movefailed');
             $success = false;
         }
-    } elseif ($isFileSync && !$upload_error_msg) { // Filesync?
+    } else { // Filesync?
         if (!@rename($srcFile, $dstFile)) {
             $message[] = rex_i18n::msg('pool_file_movefailed');
             $success = false;
         }
-    } // else-Fall entfaellt da Upload im EP widersprochen wurde
+    }
 
     if ($success) {
         @chmod($dstFile, rex::getFilePerm());
