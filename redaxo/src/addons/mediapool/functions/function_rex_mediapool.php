15aae9b17d9dc1dd812f6f6f6e96c48aa6ad621a
Markus Staab <markus.staab@redaxo.de>
2020-06-05 20:37:01 +0200

    revert unrelated changes

--- redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
+++ redaxo/src/addons/mediapool/functions/function_rex_mediapool.php
@@ -579,7 +579,6 @@ function rex_mediapool_Syncform($rex_file_category)
  * check if mediatpye(extension) is allowed for upload.
  *
  * @param string $filename
- * @param array  $args
  *
  * @return bool
  */
