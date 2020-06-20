cb9c35d0d97a4b802880651bc111f52f9d83a534
Gregor Harlan <mail@gh01.de>
2020-06-20 15:00:48 +0200

    Setup: rex_backup-Klasse verfügbar machen

--- redaxo/src/core/lib/console/setup/run.php
+++ redaxo/src/core/lib/console/setup/run.php
@@ -266,6 +266,9 @@ class rex_command_setup_run extends rex_console_command implements rex_command_o
             $io->block('Database version: '.$sql->getDbType(). ' '.$sql->getDbVersion());
         }
 
+        // force loading rex_backup class, even if backup addon is not installed
+        require_once rex_path::addon('backup', 'lib/backup.php');
+
         // Search for exports
         $backups = [];
         foreach (rex_backup::getBackupFiles('') as $file) {
