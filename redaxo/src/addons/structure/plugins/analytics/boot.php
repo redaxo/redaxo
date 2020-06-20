25288a5359878ab3f0e6b555dd19a8a1d8172a26
Markus Staab <markus.staab@redaxo.de>
2020-06-20 21:30:40 +0200

    refactor extension code out of boot.php

--- redaxo/src/addons/structure/plugins/analytics/boot.php
+++ redaxo/src/addons/structure/plugins/analytics/boot.php
@@ -42,33 +42,7 @@ if (rex::isFrontend()) {
     }
 
     rex_extension::register('OUTPUT_FILTER', function (\rex_extension_point $ep) use ($plugin) {
-        $response = $ep->getSubject();
-
-        $analyticsUrl = $_SERVER['REQUEST_URI'];
-        if (strpos($analyticsUrl, '?') === false) {
-            $analyticsUrl .= '?rex_analytics=1';
-        } else {
-            $analyticsUrl .= '&rex_analytics=1';
-        }
-
-        $js = '<script defer src="' . $plugin->getAssetsUrl('web-vitals.min.js') .'"></script>';
-        $js .= '
-        <script>
-            addEventListener(\'DOMContentLoaded\', function() {
-                function sendToAnalytics(metric) {
-                  const body = JSON.stringify(metric);
-                  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-                  (navigator.sendBeacon && navigator.sendBeacon(\''. $analyticsUrl .'\', body)) ||
-                      fetch(\''. $analyticsUrl .'\', {body, method: \'POST\', keepalive: true});
-                }
-
-                webVitals.getCLS(sendToAnalytics);
-                webVitals.getFID(sendToAnalytics);
-                webVitals.getLCP(sendToAnalytics);
-            });
-        </script>';
-        $response = str_ireplace('</body>', $js. '</body>', $response);
-        $ep->setSubject($response);
+        rex_analytics_extensions::injectIntoFrontend($ep, $plugin);
     });
 }
 
---
+++ redaxo/src/addons/structure/plugins/analytics/lib/extensions.php
@@ -0,0 +1,33 @@
+<?php
+
+final class rex_analytics_extensions {
+    static public function injectIntoFrontend(\rex_extension_point $ep, rex_plugin $plugin) {
+        $response = $ep->getSubject();
+
+        $analyticsUrl = $_SERVER['REQUEST_URI'];
+        if (strpos($analyticsUrl, '?') === false) {
+            $analyticsUrl .= '?rex_analytics=1';
+        } else {
+            $analyticsUrl .= '&rex_analytics=1';
+        }
+
+        $js = '<script defer src="' . $plugin->getAssetsUrl('web-vitals.min.js') .'"></script>';
+        $js .= '
+                <script>
+                    addEventListener(\'DOMContentLoaded\', function() {
+                        function sendToAnalytics(metric) {
+                          const body = JSON.stringify(metric);
+                          // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+                          (navigator.sendBeacon && navigator.sendBeacon(\''. $analyticsUrl .'\', body)) ||
+                              fetch(\''. $analyticsUrl .'\', {body, method: \'POST\', keepalive: true});
+                        }
+
+                        webVitals.getCLS(sendToAnalytics);
+                        webVitals.getFID(sendToAnalytics);
+                        webVitals.getLCP(sendToAnalytics);
+                    });
+                </script>';
+        $response = str_ireplace('</body>', $js. '</body>', $response);
+        $ep->setSubject($response);
+    }
+}
