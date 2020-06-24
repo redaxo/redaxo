67756c8fdaaa28dd38013391c055b90a12996c16
tbaddade <thomas@redaxo.org>
2020-06-24 10:51:39 +0200

    Leerzeile wieder hinzugefügt

--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -398,6 +398,7 @@ if ($structureContext->getCategoryId() > 0 || (0 == $structureContext->getCatego
 
             $tmpl_td = '<td class="rex-table-template" data-title="' . rex_i18n::msg('header_template') . '">' . $template_select->get() . '</td>';
         }
+        
         $echo .= '<tr class="mark">
                     <td class="rex-table-icon"><i class="rex-icon rex-icon-article"></i></td>
                     <td class="rex-table-id" data-title="' . rex_i18n::msg('header_id') . '">-</td>
