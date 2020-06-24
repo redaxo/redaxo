8f70b73271e71c60bd7d4df5166659cc33d301ca
tbaddade <thomas@redaxo.org>
2020-06-24 10:57:55 +0200

    CS

--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -385,7 +385,6 @@ if ($structureContext->getCategoryId() > 0 || (0 == $structureContext->getCatego
                 <tbody>
                 ';
 
-
     $canEdit = rex::getUser()->hasPerm('editArticle[]');
     $canDelete = rex::getUser()->hasPerm('deleteArticle[]');
     $colspan = (int) $canEdit + (int) $canDelete + 1;
@@ -398,7 +397,7 @@ if ($structureContext->getCategoryId() > 0 || (0 == $structureContext->getCatego
 
             $tmpl_td = '<td class="rex-table-template" data-title="' . rex_i18n::msg('header_template') . '">' . $template_select->get() . '</td>';
         }
-        
+
         $echo .= '<tr class="mark">
                     <td class="rex-table-icon"><i class="rex-icon rex-icon-article"></i></td>
                     <td class="rex-table-id" data-title="' . rex_i18n::msg('header_id') . '">-</td>
