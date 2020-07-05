f685d3f6e73fd9561f6bb73d7074da4b1bc32fa8
Gregor Harlan <mail@gh01.de>
2020-07-05 17:24:42 +0200

    Versuch

--- phpstan-console.php
+++ phpstan-console.php
@@ -1,9 +1,16 @@
 <?php
 
-unset($REX);
-$REX['REDAXO'] = true;
-$REX['HTDOCS_PATH'] = './';
-$REX['BACKEND_FOLDER'] = 'redaxo';
+if (!defined('REX_MIN_PHP_VERSION')) {
+    unset($REX);
+    $REX['REDAXO'] = true;
+    $REX['HTDOCS_PATH'] = './';
+    $REX['BACKEND_FOLDER'] = 'redaxo';
+    $REX['LOAD_PAGE'] = false;
 
-require __DIR__.'/redaxo/src/core/console.php';
-return rex::getConsole();
+    require __DIR__.'/redaxo/src/core/boot.php';
+}
+
+$application = new rex_console_application();
+$application->setCommandLoader(new rex_console_command_loader());
+
+return $application;
