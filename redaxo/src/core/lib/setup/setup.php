412b3dc1b87f3c2fef6dee46f3cefa2f6fe823c4
Gregor Harlan <mail@gh01.de>
2020-07-22 12:01:31 +0200

    comment

--- redaxo/src/core/lib/setup/setup.php
+++ redaxo/src/core/lib/setup/setup.php
@@ -7,6 +7,7 @@
  */
 class rex_setup
 {
+    // These values must be synchronized with the values in redaxo/src/core/update.php
     public const MIN_PHP_VERSION = REX_MIN_PHP_VERSION;
     public const MIN_MYSQL_VERSION = '5.6';
     public const MIN_MARIADB_VERSION = '10.1';
