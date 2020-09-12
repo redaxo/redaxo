52e693d185605eb3e6ae2d4f95f4b2468a5876bd
Markus Staab <markus.staab@redaxo.de>
2020-09-12 09:42:32 +0200

    fix test

--- redaxo/src/core/tests/console/config/get_test.php
+++ redaxo/src/core/tests/console/config/get_test.php
@@ -45,7 +45,7 @@ class rex_command_config_get_test extends TestCase
             'config-key' => 'author',
             '--package' => 'backup', ]
         );
-        static::assertEquals('Jan Kristinus, Markus Staab', $commandTester->getDisplay());
+        static::assertEquals("\"Jan Kristinus, Markus Staab\"\n", $commandTester->getDisplay());
         static::assertEquals(0, $commandTester->getStatusCode());
     }
 }
