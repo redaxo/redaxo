bbbd19ae26a9ea0a290219ecd34382de54213d91
Joachim Doerr <mail@joachim-doerr.com>
2020-09-22 11:17:04 +0200

    renameing media categories permission
    # Conflicts:
    #	redaxo/src/addons/mediapool/package.yml

--- redaxo/src/addons/mediapool/boot.php
+++ redaxo/src/addons/mediapool/boot.php
@@ -18,7 +18,7 @@ rex_complex_perm::register('media_read', rex_media_read_perm::class);
 require_once __DIR__ . '/functions/function_rex_mediapool.php';
 
 if (rex::isBackend() && rex::getUser()) {
-    rex_perm::register('media_pool[categories_editor]', null, rex_perm::EXTRAS);
+    rex_perm::register('media[categories]', null, rex_perm::GENERAL);
 
     rex_view::addJsFile($addon->getAssetsUrl('lazysizes.min.js'), [rex_view::JS_IMMUTABLE => true, rex_view::JS_ASYNC => true]);
     rex_view::addJsFile($addon->getAssetsUrl('mediapool.js'), [rex_view::JS_IMMUTABLE]);
