76a5d1b024f45e38ae12be4093b9ce7b12236054
schuer <ds@decaf.de>
2020-09-24 23:19:14 +0200

    add login fragments

---
+++ redaxo/src/core/fragments/core/login_background.php
@@ -0,0 +1,17 @@
+<picture class="rex-background">
+    <source
+        srcset="
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/peter-olexa-RYtiT3b7XW4-unsplash_2100.webp') ?> 2100w,
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/peter-olexa-RYtiT3b7XW4-unsplash_3000.webp') ?> 3000w"
+        sizes="100vw"
+        type="image/webp"
+    />
+    <img
+        alt=""
+        src="<?= rex_url::pluginAssets('be_style', 'redaxo', 'images/peter-olexa-RYtiT3b7XW4-unsplash_2100.jpg') ?>"
+        srcset="
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/peter-olexa-RYtiT3b7XW4-unsplash_2100.jpg') ?> 2100w,
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/peter-olexa-RYtiT3b7XW4-unsplash_3000.jpg') ?> 3000w"
+        sizes="100vw"
+    />
+</picture>
