f9ce30dbdc2b9a86eadcb1524e2003611e5c498b
schuer <ds@decaf.de>
2020-09-25 09:02:53 +0200

    add class for background-credits

--- redaxo/src/core/fragments/core/login_background_credits.php
+++ redaxo/src/core/fragments/core/login_background_credits.php
@@ -1,1 +1,1 @@
-<li><a href="https://unsplash.com/@deeezyfree" target="_blank" rel="noreferrer noopener">Photo by Peter Olexa on Unsplash</a></li>
+<li class="rex-background-credits"><a href="https://unsplash.com/@deeezyfree" target="_blank" rel="noreferrer noopener">Photo by Peter Olexa on Unsplash</a></li>
