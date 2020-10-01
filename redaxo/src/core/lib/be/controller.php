80a42b6ff1e7ed141fc8c10375c0af2a846dd480
christophboecker <christoph_boecker@t-online.de>
2020-10-01 08:07:17 +0200

    Leerzeichen in Zeile 440 eingefügt

--- redaxo/src/core/lib/be/controller.php
+++ redaxo/src/core/lib/be/controller.php
@@ -437,7 +437,7 @@ class rex_be_controller
             $path = $languagePath;
         }
 
-        [$toc, $content] = rex_markdown::factory()->parseWithToc(rex_file::get($path),2,3,false);
+        [$toc, $content] = rex_markdown::factory()->parseWithToc(rex_file::get($path), 2, 3, false);
         $fragment = new rex_fragment();
         $fragment->setVar('content', $content, false);
         $fragment->setVar('toc', $toc, false);
