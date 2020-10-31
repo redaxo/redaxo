f5552655e0a12bba3b5d416c92814a87b24febef
staabm <staabm@users.noreply.github.com>
2020-10-31 22:12:23 +0000

    Apply php-cs-fixer changes

--- redaxo/src/core/lib/util/socket/socket.php
+++ redaxo/src/core/lib/util/socket/socket.php
@@ -189,9 +189,10 @@ class rex_socket
     /**
      * Makes a POST request.
      *
-     * @param string|array|callable $data  Body data as string or array (POST parameters) or a callback for writing the body
+     * @param string|array|callable $data Body data as string or array (POST parameters) or a callback for writing the body
      * @psalm-param string|callable(resource): void $data
-     * @param array                 $files Files array, e.g. `array('myfile' => array('path' => $path, 'type' => 'image/png'))`
+     *
+     * @param array $files Files array, e.g. `array('myfile' => array('path' => $path, 'type' => 'image/png'))`
      *
      * @throws rex_socket_exception
      *
