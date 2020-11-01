52dc711cb8747f0343de7fa7bdd7d73f94bb25e6
Markus Staab <maggus.staab@googlemail.com>
2020-11-01 19:54:15 +0100

    Update api_user_impersonate.php

--- redaxo/src/core/lib/login/api_user_impersonate.php
+++ redaxo/src/core/lib/login/api_user_impersonate.php
@@ -7,6 +7,9 @@
  */
 class rex_api_user_impersonate extends rex_api_function
 {
+    /**
+     * @psalm-return never
+     */
     public function execute()
     {
         $impersonate = rex_get('_impersonate');
