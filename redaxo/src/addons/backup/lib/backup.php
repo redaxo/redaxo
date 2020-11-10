154366c5a31e844abc2d1715d98ac57122d56515
Gregor Harlan <mail@gh01.de>
2020-11-10 23:13:42 +0100

    nl2br

--- redaxo/src/addons/backup/lib/backup.php
+++ redaxo/src/addons/backup/lib/backup.php
@@ -137,7 +137,7 @@ class rex_backup
             try {
                 $sql->setQuery($line['query']);
             } catch (rex_sql_exception $e) {
-                $error .= "\n" . rex_escape($e->getMessage());
+                $error .= "\n" . nl2br(rex_escape($e->getMessage()));
             }
         }
 
