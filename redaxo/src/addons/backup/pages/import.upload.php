e5f29e15c326f4f80eaa8b995b91040fbf6f3688
Markus Staab <maggus.staab@googlemail.com>
2020-11-19 18:31:32 +0100

    Update import.upload.php

--- redaxo/src/addons/backup/pages/import.upload.php
+++ redaxo/src/addons/backup/pages/import.upload.php
@@ -16,9 +16,7 @@ $EXPDIR = rex_post('EXPDIR', 'array');
 
 @set_time_limit(0);
 
-/** @psalm-taint-escape text */
 $impname = rex_path::basename($impname);
-
 if ('dbimport' == $function && '.sql' != substr($impname, -4, 4)) {
     $impname = '';
 } elseif ('fileimport' == $function && '.tar.gz' != substr($impname, -7, 7)) {
