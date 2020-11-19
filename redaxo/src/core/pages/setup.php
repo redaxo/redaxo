c4f2b5d43016d91706fcede29a723e36ed058327
Markus Staab <markus.staab@redaxo.de>
2020-11-19 10:06:37 +0100

    setup: sanitize import filename to prevent path-traversal attacks

--- redaxo/src/core/pages/setup.php
+++ redaxo/src/core/pages/setup.php
@@ -195,6 +195,11 @@ if ($step > 5 && $createdb > -1) {
         }
     } elseif (3 == $createdb) {
         $import_name = rex_post('import_name', 'string');
+        /**
+         * @psalm-taint-escape text
+         */
+        $import_name = basename($import_name);
+
         $error = rex_setup_importer::loadExistingImport($import_name);
         if ('' != $error) {
             $errors[] = rex_view::error($error);
