25542ee0bd64d3581863e6010c0e4171c201ddcc
Markus Staab <markus.staab@redaxo.de>
2020-11-21 20:20:33 +0100

    backup: added splitbrain/php-archive vendor

---
+++ redaxo/src/addons/backup/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9d4c4797eaa093f2f3f169ef0d3f2f4f01d147ac',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d4c4797eaa093f2f3f169ef0d3f2f4f01d147ac',
+    ),
+    'splitbrain/php-archive' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd4cf2d9a2d82548b7e4f1dc04802c526eba68b65',
+    ),
+  ),
+);
