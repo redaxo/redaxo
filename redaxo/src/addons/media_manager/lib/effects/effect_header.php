578552af2f234909e344792d2187d1d850bbc404
Markus Staab <maggus.staab@googlemail.com>
2020-11-22 10:06:10 +0100

    php-src basename() als deprecated behandeln (#4096)

--- redaxo/src/addons/media_manager/lib/effects/effect_header.php
+++ redaxo/src/addons/media_manager/lib/effects/effect_header.php
@@ -45,7 +45,7 @@ class rex_effect_header extends rex_effect_abstract
         }
 
         $disposition = 'download' == $this->params['download'] ? 'attachment' : 'inline';
-        $disposition .= '; filename="' . basename($this->media->getMediaFilename()) . '"';
+        $disposition .= '; filename="' . rex_path::basename($this->media->getMediaFilename()) . '"';
 
         if ('originalname' == $this->params['filename']) {
             $disposition .= "; filename*=utf-8''" . rawurldecode(rex_media::get($this->media->getMediaFilename())->getOriginalFileName());
