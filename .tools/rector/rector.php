bafeb6e6d8ebd502681b562c036ba50eaae25327
Markus Staab <maggus.staab@googlemail.com>
2020-11-28 11:46:02 +0100

    rector: prevent 'Use of undefined constant REX_MIN_PHP_VERSION' warnings (#4124)

--- .tools/rector/rector.php
+++ .tools/rector/rector.php
@@ -16,6 +16,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         // SetList::DEAD_CODE,
     ]);
 
+    $parameters->set(OPTION::OPTION_AUTOLOAD_FILE, [
+        __DIR__.'/../constants.php',
+    ]);
+
     // this list will grow over time.
     // to make sure we can review every transformation and not introduce unseen bugs
     $parameters->set(Option::PATHS, [
