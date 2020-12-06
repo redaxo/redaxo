7296da947df330d236d1895bef77afe0178f26dc
Markus Staab <markus.staab@redaxo.de>
2020-12-06 11:32:25 +0100

    structure: add separate error message when a clang with id=0 exists
    this indicates a problem in the clang table. this is likely caused by a broken update from REDAXO4, in which clangs started with 0.

--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -18,8 +18,13 @@ $structureContext = new rex_structure_context([
 ]);
 
 if (0 == $structureContext->getClangId()) {
-    echo rex_view::error('You have no permission to access this area');
-    exit;
+    if (rex_clang::exists(0)) {
+        echo rex_view::error('A invalid clang with id=0 exists. Looks like a broken update from a previous REDAXO version. Please make sure the clang-ids start with 1.');
+        exit;
+    } else {
+        echo rex_view::error('You have no permission to access this area');
+        exit;
+    }
 }
 
 // --------------------- Extension Point
