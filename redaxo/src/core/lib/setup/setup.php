7702f2c20f35300bc677a4ca2d3f13929b403571
Markus Staab <maggus.staab@googlemail.com>
2020-12-06 20:54:09 +0100

    added php 8.0 eol handling

--- redaxo/src/core/lib/setup/setup.php
+++ redaxo/src/core/lib/setup/setup.php
@@ -191,6 +191,8 @@ class rex_setup
             $security[] = rex_i18n::msg('setup_security_deprecated_php', PHP_VERSION);
         } elseif (1 == version_compare(PHP_VERSION, '8.0', '<') && time() > strtotime('28 Nov 2022')) {
             $security[] = rex_i18n::msg('setup_security_deprecated_php', PHP_VERSION);
+        } elseif (1 == version_compare(PHP_VERSION, '8.1', '<') && time() > strtotime('26 Nov 2023')) {
+            $security[] = rex_i18n::msg('setup_security_deprecated_php', PHP_VERSION);
         }
 
         return $security;
