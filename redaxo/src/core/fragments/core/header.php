d4f93b710cb48c8974f51168e680cc3dde33a335
schuer <ds@decaf.de>
2020-12-11 23:40:41 +0100

    use REDAXO logo SVG instead of img
    ref #4138

--- redaxo/src/core/fragments/core/header.php
+++ redaxo/src/core/fragments/core/header.php
@@ -9,9 +9,9 @@
             <div class="container-fluid">
                 <div class="navbar-header">
                     <?php if (rex_be_controller::getCurrentPageObject()->isPopup()): ?>
-                        <span class="navbar-brand"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></span>
+                        <span class="navbar-brand"><?= rex_file::get(rex_url::coreAssets('redaxo-logo.svg')) ?></span>
                     <?php else: ?>
-                        <a class="navbar-brand" href="<?= rex_url::backendController() ?>"><img class="rex-js-svg rex-redaxo-logo" src="<?= rex_url::coreAssets('redaxo-logo.svg') ?>" /></a>
+                        <a class="navbar-brand" href="<?= rex_url::backendController() ?>"><?= rex_file::get(rex_url::coreAssets('redaxo-logo.svg')) ?></a>
                     <?php endif; ?>
                     <?php if (!rex_be_controller::getCurrentPageObject()->isPopup() && rex::getUser() && rex::getUser()->isAdmin() && rex::isDebugMode()): ?>
                         <a class="rex-marker-debugmode" href="<?= rex_url::backendPage('system/settings') ?>" title="<?= rex_i18n::msg('debug_mode_marker') ?>">
