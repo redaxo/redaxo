d8d7d8d8ee50db2c8225b722a77c61ab26983bf0
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-05 22:11:43 +0100

    Update searchfield.php

--- redaxo/src/core/fragments/core/form/searchfield.php
+++ redaxo/src/core/fragments/core/form/searchfield.php
@@ -3,20 +3,17 @@
 $id = $clear = $placeholder = '';
 $class = 'input-group input-group-xs has-feedback form-clear-button';
 $clear = rex_i18n::msg('search_clear');
-$placeholder = ' placeholder="' . rex_i18n::msg('search_placeholder') . '"';
+$placeholder = ' placeholder="'.$this->getVar('placeholder', rex_i18n::msg('search_placeholder')).'"';
 
 if ($this->id) {
     $id = ' id="' . $this->id .'"';
-}
-if ($this->placeholder) {
-    $placeholder = ' placeholder="' . $this->placeholder . '"';
 }
 if ($this->class) {
     $class = ' class="' . $this->class . '"';
 }
 
 echo '<div class="'. $class . '"' . $id . '>
-      <span class="input-group-addon clear-button"><i class="fa fa-search "></i></span>
+      <span class="input-group-addon clear-button"><i class="rex-icon rex-icon-search"></i></span>
       <input class="form-control" type="text" autofocus ' . $placeholder . '>
       <span title="' . $clear . '" class="form-control-clear fa fa-times-circle form-control-feedback hidden"></span>
 </div>';
