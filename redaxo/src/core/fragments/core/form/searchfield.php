eb9ab52ec83ef3ed800cbff7aa88b00ad1b1f21b
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-05 23:22:08 +0100

    code optimized
    thx to @staabm  @tbaddade

--- redaxo/src/core/fragments/core/form/searchfield.php
+++ redaxo/src/core/fragments/core/form/searchfield.php
@@ -1,15 +1,12 @@
 <?php
 
-$id = $clear = $placeholder = '';
-$class = 'input-group input-group-xs has-feedback form-clear-button';
+$id = $clear = $class = $placeholder = '';
+$class = $this->getVar('class','input-group input-group-xs has-feedback form-clear-button');
 $clear = rex_i18n::msg('search_clear');
 $placeholder = ' placeholder="'.$this->getVar('placeholder', rex_i18n::msg('search_placeholder')).'"';
 
 if ($this->id) {
     $id = ' id="' . $this->id .'"';
-}
-if ($this->class) {
-    $class = $this->class;
 }
 
 echo '<div class="'. $class . '"' . $id . '>
