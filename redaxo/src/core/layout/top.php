824c3507886be7507b667e49963ae9c94c60eea9
schuer <ds@decaf.de>
2021-01-24 15:27:03 +0100

    Revert "Switch pjax library (#4212)"
    This reverts commit c4d678a812687b8c128ba1c76b30a7bc785f8a14.
    
    # Conflicts:
    #	redaxo/src/core/assets/standard.js
    #	redaxo/src/core/fragments/core/header.php

--- redaxo/src/core/layout/top.php
+++ redaxo/src/core/layout/top.php
@@ -8,7 +8,16 @@
 
 $curPage = rex_be_controller::getCurrentPageObject();
 
+if (rex_request::isPJAXRequest()) {
+    // add title to the page, so pjax can update it. see gh#136
+    echo '<title>' . rex_escape(rex_be_controller::getPageTitle()) . '</title>';
+}
+
 if (!$curPage->hasLayout()) {
+    if (rex_request::isPJAXRequest()) {
+        echo '<section class="rex-page-main-inner" id="rex-js-page-main" data-pjax-container="#rex-js-page-main">';
+    }
+
     return;
 }
 
@@ -109,11 +118,6 @@ if (rex::getUser() && $hasNavigation) {
             if (!$pageObj->getHref()) {
                 $pageObj->setHref(rex_url::backendPage($p, [], false));
             }
-
-            if (!$pageObj->allowsPjax()) {
-                $pageObj->setLinkAttr('data-pjax', 'false');
-            }
-
             /*
              if(isset ($REX['ACKEY']['ADDON'][$page]))
                 $item['extra'] = rex_accesskey($name, $REX['ACKEY']['ADDON'][$page]);
@@ -184,25 +188,26 @@ if ('setup' == rex_be_controller::getCurrentPagePart(1)) {
 }
 
 /* PJAX Footer Header ***********************************************************/
+if (!rex_request::isPJAXContainer('#rex-js-page-container')) {
+    $fragment = new rex_fragment();
+    $fragment->setVar('pageTitle', rex_be_controller::getPageTitle());
+    $fragment->setVar('cssFiles', rex_view::getCssFiles());
+    $fragment->setVar('jsFiles', rex_view::getJsFilesWithOptions());
+    $fragment->setVar('jsProperties', json_encode(rex_view::getJsProperties()), false);
+    $fragment->setVar('favicon', rex_view::getFavicon());
+    $fragment->setVar('pageHeader', rex_extension::registerPoint(new rex_extension_point('PAGE_HEADER', '')), false);
+    $fragment->setVar('bodyAttr', $body, false);
+    echo $fragment->parse('core/top.php');
 
-$fragment = new rex_fragment();
-$fragment->setVar('pageTitle', rex_be_controller::getPageTitle());
-$fragment->setVar('cssFiles', rex_view::getCssFiles());
-$fragment->setVar('jsFiles', rex_view::getJsFilesWithOptions());
-$fragment->setVar('jsProperties', json_encode(rex_view::getJsProperties()), false);
-$fragment->setVar('favicon', rex_view::getFavicon());
-$fragment->setVar('pageHeader', rex_extension::registerPoint(new rex_extension_point('PAGE_HEADER', '')), false);
-$fragment->setVar('bodyAttr', $body, false);
-echo $fragment->parse('core/top.php');
+    $fragment = new rex_fragment();
+    $fragment->setVar('items', $meta_items, false);
+    $meta_navigation = $fragment->parse('core/navigations/meta.php');
 
-$fragment = new rex_fragment();
-$fragment->setVar('items', $meta_items, false);
-$meta_navigation = $fragment->parse('core/navigations/meta.php');
-
-$fragment = new rex_fragment();
-// $fragment->setVar('pageHeader', rex_extension::registerPoint(new rex_extension_point('PAGE_HEADER', '')), false);
-$fragment->setVar('meta_navigation', $meta_navigation, false);
-echo $fragment->parse('core/header.php');
+    $fragment = new rex_fragment();
+    // $fragment->setVar('pageHeader', rex_extension::registerPoint(new rex_extension_point('PAGE_HEADER', '')), false);
+    $fragment->setVar('meta_navigation', $meta_navigation, false);
+    echo $fragment->parse('core/header.php');
+}
 
 echo '<div id="rex-js-page-container" class="rex-page-container">';
 
