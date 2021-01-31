f84c8ba64065a261f75a0bc945bca90f516c15d4
Gregor Harlan <mail@gh01.de>
2021-01-31 11:16:39 +0100

    comment

--- .tools/phpstan/console.php
+++ .tools/phpstan/console.php
@@ -9,6 +9,8 @@ if (!defined('REX_MIN_PHP_VERSION')) {
 
     require dirname(__DIR__, 2).'/redaxo/src/core/boot.php';
 
+    // initialize autoloader before phpstan autoload wrapper is active
+    // https://github.com/redaxo/redaxo/pull/4369#issuecomment-770195916
     class_exists(rex_backend_login::class);
 }
 
