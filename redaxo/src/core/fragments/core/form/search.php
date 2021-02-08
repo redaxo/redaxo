09328d3b29f95bb453cd693159a9cff3b31d4b56
Thomas Skerbis <thomas.skerbis@mac.com>
2021-02-08 10:03:30 +0100

    fragment search autofocus (#4415)
    Co-authored-by: rex-bot <78283150+rex-bot@users.noreply.github.com>
    Co-authored-by: Gregor Harlan <330436+gharlan@users.noreply.github.com>

--- redaxo/src/core/fragments/core/form/search.php
+++ redaxo/src/core/fragments/core/form/search.php
@@ -9,6 +9,10 @@ $id = '';
 $class = $this->getVar('class', 'input-group input-group-xs has-feedback form-clear-button');
 $clear = rex_i18n::msg('search_clear');
 $placeholder = ' placeholder="'.$this->getVar('placeholder', rex_i18n::msg('search_placeholder')).'"';
+$autofocus = '';
+if (isset($this->autofocus) && $this->autofocus) {
+    $autofocus = ' autofocus ';
+}
 
 if ($this->id) {
     $id = ' id="' . $this->id .'"';
@@ -16,6 +20,6 @@ if ($this->id) {
 
 echo '<div class="'. $class . '"' . $id . '>
       <span class="input-group-addon clear-button"><i class="rex-icon rex-icon-search"></i></span>
-      <input class="form-control" type="text" autofocus ' . $placeholder . '>
+      <input class="form-control" type="text"' . $autofocus . $placeholder . '>
       <span title="' . $clear . '" class="form-control-clear rex-icon rex-icon-clear form-control-feedback hidden"></span>
 </div>';
