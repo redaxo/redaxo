1e83ed8cb2babf2aa697dbc4cd80467ecb010035
Thomas Skerbis <thomas.skerbis@mac.com>
2021-02-09 15:35:05 +0100

    AutoTLS per default deaktiviert
    Hier gibt es wohl Probleme mit eigen hostern

--- redaxo/src/addons/phpmailer/install.php
+++ redaxo/src/addons/phpmailer/install.php
@@ -19,6 +19,10 @@ if ($addon->hasConfig('log')) {
 if (!$addon->hasConfig('errormail')) {
     $addon->setConfig('errormail', 0);
 }
+if (!$addon->hasConfig('security_mode')) {
+    $addon->setConfig('security_mode', false); // AutoTLS per default deaktiviert
+}
+
 $oldBackUpFolder = rex_path::addonData('phpmailer', 'mail_backup');
 $logFolder = rex_path::addonData('phpmailer', 'mail_log');
 if (is_dir($oldBackUpFolder) && !is_dir($logFolder)) {
