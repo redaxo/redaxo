d461c7bd999fe2dc13a436d2f45e42b9b511a839
Gregor Harlan <mail@gh01.de>
2021-06-03 11:57:11 +0200

    Update vendors

--- redaxo/src/core/vendor/symfony/console/Descriptor/TextDescriptor.php
+++ redaxo/src/core/vendor/symfony/console/Descriptor/TextDescriptor.php
@@ -117,7 +117,7 @@ class TextDescriptor extends Descriptor
 
             $this->writeText('<comment>Options:</comment>', $options);
             foreach ($definition->getOptions() as $option) {
-                if (\strlen($option->getShortcut()) > 1) {
+                if (\strlen($option->getShortcut() ?? '') > 1) {
                     $laterOptions[] = $option;
                     continue;
                 }
@@ -296,7 +296,7 @@ class TextDescriptor extends Descriptor
     }
 
     /**
-     * @param (Command|string)[] $commands
+     * @param array<Command|string> $commands
      */
     private function getColumnWidth(array $commands): int
     {
