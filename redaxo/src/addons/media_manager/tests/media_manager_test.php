8f57b236ffc91404c443d8e4f8c7a0e58362d4c7
schuer <ds@decaf.de>
2021-08-16 15:34:04 +0200

    fix test

--- redaxo/src/addons/media_manager/tests/media_manager_test.php
+++ redaxo/src/addons/media_manager/tests/media_manager_test.php
@@ -41,7 +41,7 @@ class rex_media_manager_test extends TestCase
         rex_file::put($path, base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII='));
 
         try {
-            $manager = rex_media_manager::create('rex_mediapool_preview', $filename);
+            $manager = rex_media_manager::create('rex_media_small', $filename);
 
             static::assertFileExists($manager->getCacheFilename());
             static::assertFileExists($manager->getHeaderCacheFilename());
