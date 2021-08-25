36fc3d46798a1bf0ae50896ca149c80727a5c5c4
Thomas Skerbis <thomas.skerbis@mac.com>
2021-08-25 17:30:44 +0200

    Create cronjob.php

--- redaxo/src/addons/phpmailer/lib/cronjob.php
+++ redaxo/src/addons/phpmailer/lib/cronjob.php
@@ -23,7 +23,7 @@ class rex_cronjob_mailer_purge extends rex_cronjob
             foreach ($files as $file) {
                 if (is_string($file) && is_dir($file)) {
                     $log = $log + self::purgeMailarchive($days, $file);
-                } elseif (is_int($days) && (time() - filemtime($file)) > (60 * 60 * 24 * $days)) {
+                } elseif (is_string($file) && (time() - filemtime($file)) > (60 * 60 * 24 * $days)) {
                     if (rex_file::delete($file)) {
                         ++$log;
                     }
@@ -40,7 +40,7 @@ class rex_cronjob_mailer_purge extends rex_cronjob
 
     public function execute()
     {
-        $logfolder = rex_mailer::logFolder();
+		$logfolder = rex_mailer::logFolder();
         if (is_string($logfolder) && is_dir($logfolder)) {
             $purgeLog = 0;
             $purgeLog = self::purgeMailarchive($this->getParam('days'), rex_mailer::logFolder());
@@ -79,3 +79,4 @@ class rex_cronjob_mailer_purge extends rex_cronjob
         return $fields;
     }
 }
+
