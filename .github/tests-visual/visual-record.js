41bcc92c47ced9864fb1577042b599f6290d9a74
schuer <ds@decaf.de>
2021-09-13 09:31:34 +0200

    e2e: wait for UI update

--- .github/tests-visual/visual-record.js
+++ .github/tests-visual/visual-record.js
@@ -169,11 +169,13 @@ async function createScreenshots(page, screenshotName) {
 
 async function createLightScreenshot(page, screenshotName) {
     await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'light' }]);
+    await page.waitForTimeout(100); // wait for UI update
     await processScreenshot(page, screenshotName);
 }
 
 async function createDarkScreenshot(page, screenshotName) {
     await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'dark' }]);
+    await page.waitForTimeout(100); // wait for UI update
     await processScreenshot(page, screenshotName.replace('.png', '--dark.png'));
 }
 
