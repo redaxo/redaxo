15aad022e1c017b3fea75baaafbd3b222b78fdeb
Gregor Harlan <mail@gh01.de>
2021-10-24 13:55:38 +0200

    tests

--- redaxo/src/addons/structure/plugins/content/tests/article_content_base_test.php
+++ redaxo/src/addons/structure/plugins/content/tests/article_content_base_test.php
@@ -34,9 +34,25 @@ class rex_article_content_base_test extends TestCase
 
         static::assertEquals('teststring', $instance->getValue('foo'));
         static::assertEquals('teststring', $instance->getValue('art_foo'));
+    }
 
-        static::assertEquals('[bar not found]', $instance->getValue('bar'));
-        static::assertEquals('[art_bar not found]', $instance->getValue('art_bar'));
+    /** @dataProvider dataGetValueNonExisting */
+    public function testGetValueNonExisting(string $value): void
+    {
+        $instance = $this->createArticleContentBaseWithoutConstructor();
+
+        $this->expectException(rex_exception::class);
+
+        $instance->getValue($value);
+    }
+
+    /** @return list<array{string}> */
+    public function dataGetValueNonExisting(): array
+    {
+        return [
+            ['bar'],
+            ['art_bar'],
+        ];
     }
 
     private function createArticleContentBaseWithoutConstructor(): rex_article_content_base
