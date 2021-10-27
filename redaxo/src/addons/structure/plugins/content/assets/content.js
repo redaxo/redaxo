f892f851369ffe2e67df142920e9f8b3190946bd
Gregor Harlan <mail@gh01.de>
2021-10-27 22:05:15 +0200

    revert files

--- redaxo/src/addons/structure/plugins/content/assets/content.js
+++ redaxo/src/addons/structure/plugins/content/assets/content.js
@@ -3,42 +3,41 @@ $(document).on('rex:ready', function (event, viewRoot) {
     var $sidebar = viewRoot.is('#rex-page-content-edit') && viewRoot.find('#rex-js-main-sidebar'),
         sidebar_status,
         $sections;
-
-    function store_sidebar_status() {
+    function store_sidebar_status () {
         sidebar_status = [];
         $sections.each(function (k, v) {
             sidebar_status.push($(v).find('div.collapse').hasClass('in').toString());
         });
         localStorage.setItem('structure_content_sidebar_status', JSON.stringify(sidebar_status));
     }
-
     if ($sidebar.length) {
         sidebar_status = localStorage.getItem('structure_content_sidebar_status');
         $sections = $sidebar.children('section');
         if (!sidebar_status) {
             store_sidebar_status();
-        } else {
+        }
+        else {
             sidebar_status = JSON.parse(sidebar_status);
             if (sidebar_status.length !== $sections.length) {
                 store_sidebar_status();
-            } else {
+            }
+            else {
                 $sections.each(function (k, v) {
                     var $panel = $(v).find('div.collapse'),
                         panel_is_visible = $panel.hasClass('in');
                     if (panel_is_visible.toString() !== sidebar_status[k]) {
                         if (panel_is_visible) {
                             $panel.collapse('hide');
-                        } else {
+                        }
+                        else {
                             $panel.collapse('show');
                         }
                     }
                 });
             }
         }
-        $sidebar.on('shown.bs.collapse hidden.bs.collapse', function () {
-            store_sidebar_status();
+        $sidebar.on('shown.bs.collapse hidden.bs.collapse', function() {
+            store_sidebar_status ();
         });
     }
-
 });
-// End on rex::ready
