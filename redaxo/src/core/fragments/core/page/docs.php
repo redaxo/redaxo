c77f1cbcab0a836e99e6313905d5fdb371da0196
tbaddade <thomas@redaxo.org>
2021-10-28 13:37:20 +0200

    Docs - Anpassungen

--- redaxo/src/core/fragments/core/page/docs.php
+++ redaxo/src/core/fragments/core/page/docs.php
@@ -3,6 +3,18 @@
  * @var rex_fragment $this
  * @psalm-scope-this rex_fragment
  */
+
+$content = $this->getVar('content');
+preg_match_all('@<pre(?: [^>]*)?><code(?: [^>]*)?>(.*?)</code></pre>@s', $content, $matches, PREG_SET_ORDER);
+if (count($matches)) {
+    $search = [];
+    $replace = [];
+    foreach ($matches as $match) {
+        $search[] = $match[0];
+        $replace[] = str_replace("\n", '', '<pre>'.highlight_string(html_entity_decode($match[1]), true).'</pre>');
+    }
+    $content = str_replace($search, $replace, $content);
+}
 ?>
 <div class="rex-docs">
     <?php if ($this->getVar('sidebar') || $this->getVar('toc')): ?>
@@ -13,5 +25,5 @@
             <?= $this->getVar('sidebar') ?>
         </div>
     <?php endif ?>
-    <article class="rex-docs-content"><?= $this->getVar('content') ?></article>
+    <article class="rex-docs-content"><?= $content ?></article>
 </div>
