e99b97c61599a3834b3d1b16472998d5e079861b
Gregor Harlan <mail@gh01.de>
2021-12-05 11:09:36 +0100

    Version 5.13.2-dev (#4957)

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -84,7 +84,7 @@ require_once rex_path::core('functions/function_rex_globals.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
-rex::setProperty('version', '5.13.1');
+rex::setProperty('version', '5.13.2-dev');
 
 $cacheFile = rex_path::coreCache('config.yml.cache');
 $configFile = rex_path::coreData('config.yml');
