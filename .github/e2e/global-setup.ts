1ef84ed106295bbd5f739ef63d0fbac0f461d204
schuer <ds@decaf.de>
2021-12-28 11:12:54 +0100

    update global-setup flow

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -1,16 +1,19 @@
 import {chromium, FullConfig} from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
-    const { baseURL, storageState } = config.projects[0].use;
+    const {baseURL, storageState} = config.projects[0].use;
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto(baseURL!);
-    await page.fill('#rex-id-login-user', 'myusername');
-    await page.fill('#rex-id-login-password', 'mypassword');
-    await page.click('#rex-id-login-stay-logged-in');
-    await page.click('button:has-text("Login")');
-    // Save signed-in state to 'storageState.json'
-    await page.context().storageState({ path: storageState as string });
+    await page.goto(baseURL);
+    // fill out login form if available
+    if (await page.locator('#rex-id-login-user').count() > 0) {
+        await page.fill('#rex-id-login-user', 'admin');
+        await page.fill('#rex-id-login-password', 'admin123');
+        await page.click('#rex-id-login-stay-logged-in');
+        await page.click('button:has-text("Login")');
+        // Save signed-in state to 'storageState.json'
+        await page.context().storageState({path: storageState as string});
+    }
     await browser.close();
 }
 
