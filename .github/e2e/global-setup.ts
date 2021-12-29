9e40e1dc0f7ca0fc00a2242142bac3d25f7645e9
schuer <ds@decaf.de>
2021-12-29 13:30:05 +0100

    update global setup flow

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -9,7 +9,7 @@ async function globalSetup(config: FullConfig) {
     if (await page.locator('#rex-id-login-user').count() > 0) {
         await page.fill('#rex-id-login-user', 'admin');
         await page.fill('#rex-id-login-password', 'admin123');
-        await page.click('#rex-id-login-stay-logged-in');
+        // await page.click('#rex-id-login-stay-logged-in');
         await page.click('button:has-text("Login")');
         // Save signed-in state to 'storageState.json'
         await page.context().storageState({path: storageState as string});
