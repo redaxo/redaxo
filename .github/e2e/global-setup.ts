b3f64005b7a795172e8bc90976d51aa095ad6827
schuer <ds@decaf.de>
2021-12-29 23:07:24 +0100

    remove debug

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -7,19 +7,14 @@ async function globalSetup(config: FullConfig) {
     await page.goto(baseURL, {waitUntil: 'networkidle'});
     // fill out login form if available
     if (await page.locator('#rex-form-login').count() > 0) {
-        console.log('login form!'); // TODO: remove
-        console.log(await page.innerText('body'));
         await page.fill('#rex-id-login-user', 'admin');
         await page.fill('#rex-id-login-password', 'admin123');
         await page.check('#rex-id-login-stay-logged-in');
         await page.click('button[type=submit]');
         await page.waitForLoadState();
-        console.log('structure!'); // TODO: remove
-        console.log(await page.innerText('body'));
     }
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({path: storageState as string});
-    console.log(await page.context().storageState()); // TODO: remove
     await browser.close();
 }
 
