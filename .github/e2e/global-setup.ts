e8967f9ffa1b2e41a39d113972ac855ef11c4427
schuer <ds@decaf.de>
2021-12-29 18:58:27 +0100

    more debug

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -2,11 +2,13 @@ import {chromium, FullConfig} from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
     const {baseURL, storageState} = config.projects[0].use;
+    console.log(baseURL, storageState); // TODO: remove
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto(baseURL, {waitUntil: 'networkidle'});
     // fill out login form if available
     if (await page.locator('#rex-id-login-user').count() > 0) {
+        console.log('login form!'); // TODO: remove
         await page.fill('#rex-id-login-user', 'admin');
         await page.fill('#rex-id-login-password', 'admin123');
         await page.click('#rex-id-login-stay-logged-in');
@@ -14,7 +16,7 @@ async function globalSetup(config: FullConfig) {
     }
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({path: storageState as string});
-    console.log(await page.context().storageState());
+    console.log(await page.context().storageState()); // TODO: remove
     await browser.close();
 }
 
