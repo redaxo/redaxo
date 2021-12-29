ff5e9070fb4ceaaffcfe98f430744aefe1b9830f
schuer <ds@decaf.de>
2021-12-29 18:41:07 +0100

    debug

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -4,7 +4,7 @@ async function globalSetup(config: FullConfig) {
     const {baseURL, storageState} = config.projects[0].use;
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto(baseURL);
+    await page.goto(baseURL, {waitUntil: 'networkidle'});
     // fill out login form if available
     if (await page.locator('#rex-id-login-user').count() > 0) {
         await page.fill('#rex-id-login-user', 'admin');
@@ -14,6 +14,7 @@ async function globalSetup(config: FullConfig) {
     }
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({path: storageState as string});
+    console.log(await page.context().storageState());
     await browser.close();
 }
 
