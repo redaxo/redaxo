32a1024022500fe830040eb60c23fcf11932a42c
schuer <ds@decaf.de>
2021-12-29 22:40:08 +0100

    debug

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -12,8 +12,8 @@ async function globalSetup(config: FullConfig) {
         await page.fill('#rex-id-login-password', 'admin123');
         await page.click('#rex-id-login-stay-logged-in');
         await page.click('button:has-text("Login")');
+        await page.waitForURL(`**/*?page=structure`);
     }
-    await page.waitForTimeout(2000);
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({path: storageState as string});
     console.log(await page.context().storageState()); // TODO: remove
