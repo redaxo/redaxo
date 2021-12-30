b8803ea42776b8470b4a44325fe92bb29c8c3de2
Markus Staab <maggus.staab@googlemail.com>
2021-12-30 21:54:20 +0100

    Update bootstrap.php

--- .tools/phpstan/bootstrap.php
+++ .tools/phpstan/bootstrap.php
@@ -6,5 +6,5 @@ use staabm\PHPStanDba\QueryReflection\QueryReflection;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 QueryReflection::setupReflector(
-    new MysqliQueryReflector(new mysqli('127.0.0.1', 'root', '', 'redaxo5'))
+    new MysqliQueryReflector(new mysqli('127.0.0.1', 'root', 'root', 'redaxo5'))
 );
