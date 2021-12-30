da61a73939a8c9ca27ef5a6b413094b85f8213ac
schuer <ds@decaf.de>
2021-12-30 02:16:35 +0100

    check response again

--- .github/e2e/lib/navigator.ts
+++ .github/e2e/lib/navigator.ts
@@ -6,9 +6,9 @@ export const gotoPage = async (page: Page, browserName: string, slug: string, op
     const response = await page.goto(`${slug}`, {
         ...options
     });
-    // if (!response.ok() && response.status() != 304) {
-    //     throw new Error(`Failed to load ${slug}: the server responded with a status of ${response.status()} (${response.statusText()})`);
-    // }
+    if (!response.ok() && response.status() != 304) {
+        throw new Error(`Failed to load ${slug}: the server responded with a status of ${response.status()} (${response.statusText()})`);
+    }
     await stopAnimations(page);
     await maskContent(page);
     await waitForImagesLazyloaded(page);
