ead5fef4fcb7ac8b4ee53ef601cc55cebef4d5b6
schuer <ds@decaf.de>
2021-12-30 01:24:07 +0100

    activate more tests for debugging

--- .github/e2e/tests/parallel/users.spec.ts
+++ .github/e2e/tests/parallel/users.spec.ts
@@ -24,8 +24,8 @@ test.describe.parallel('All', () => {
     for (const item of testItems) {
 
         test(`${item.name}`, async ({page, browserName}, testInfo) => {
-            // await gotoPage(page, browserName, `${item.url}`);
-            // await matchPageSnapshot(page, `${testInfo.title}`);
+            await gotoPage(page, browserName, `${item.url}`);
+            await matchPageSnapshot(page, `${testInfo.title}`);
         });
     }
 });
