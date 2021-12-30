d5dd7c1cf3b15b1e9fa065ffee40f06e7bbed60f
Markus Staab <maggus.staab@googlemail.com>
2021-12-30 21:46:13 +0100

    added phpstan-dba analysis extension

---
+++ .tools/phpstan/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+use staabm\PHPStanDba\QueryReflection\MysqliQueryReflector;
+use staabm\PHPStanDba\QueryReflection\QueryReflection;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+QueryReflection::setupReflector(
+    new MysqliQueryReflector(new mysqli('127.0.0.1', 'root', '', 'redaxo5'))
+);
