3251372738c33f6d9c6bd310b5f89945b3c4f6e8
schuer <ds@decaf.de>
2021-12-31 22:03:05 +0100

    add htaccess check cookie before loading the login page

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -4,6 +4,14 @@ async function globalSetup(config: FullConfig) {
     const {baseURL, storageState} = config.projects[0].use;
     const browser = await chromium.launch();
     const page = await browser.newPage();
+    // add htaccess check cookie before loading the login page
+    await page.context().addCookies([{
+        "name": "rex_htaccess_check",
+        "value": "1",
+        "domain": "localhost",
+        "path": "/",
+        "expires": -1,
+    }]);
     await page.goto(baseURL, {waitUntil: 'networkidle'});
     // fill out login form if available
     if (await page.locator('#rex-form-login').count() > 0) {
@@ -15,7 +23,6 @@ async function globalSetup(config: FullConfig) {
     }
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({path: storageState as string});
-    console.log(await page.context().cookies());
     await browser.close();
 }
 
