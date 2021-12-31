b24e99920676b6a156d107ef8b7c591acccb6be1
Markus Staab <maggus.staab@googlemail.com>
2021-12-31 10:24:45 +0100

    fix extension class loading

--- .tools/phpstan/bootstrap.php
+++ .tools/phpstan/bootstrap.php
@@ -4,7 +4,6 @@ use staabm\PHPStanDba\QueryReflection\MysqliQueryReflector;
 use staabm\PHPStanDba\QueryReflection\QueryReflection;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__.'/RexClassDynamicReturnTypeExtension.php';
 
 QueryReflection::setupReflector(
     new MysqliQueryReflector(new mysqli('127.0.0.1', 'root', 'root', 'redaxo5'))
