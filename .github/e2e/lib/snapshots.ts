038e2cd8f3e768f20a5caf1e2cccf93560fbbe20
schuer <ds@decaf.de>
2022-01-01 14:37:40 +0100

    adjust threshold

--- .github/e2e/lib/snapshots.ts
+++ .github/e2e/lib/snapshots.ts
@@ -2,5 +2,5 @@ import {expect, Page} from '@playwright/test';
 
 export const matchPageSnapshot = async (page: Page, snapshotTitle: string, options = {}) => {
     const image = await page.screenshot({fullPage: true, ...options});
-    return expect(image).toMatchSnapshot(`${snapshotTitle}.png`, {threshold: 0.2});
+    return expect(image).toMatchSnapshot(`${snapshotTitle}.png`, {threshold: 0.25});
 };
