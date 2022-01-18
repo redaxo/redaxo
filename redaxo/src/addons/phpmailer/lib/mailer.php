a0e7d09a53426753e97733a1b084499d8077978c
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-18 18:02:39 +0100

    $this geht an dieser stelle nicht

--- redaxo/src/addons/phpmailer/lib/mailer.php
+++ redaxo/src/addons/phpmailer/lib/mailer.php
@@ -290,12 +290,12 @@ class rex_mailer extends PHPMailer
         $mailBody .= '    </tbody>';
         $mailBody .= '</table>';
         //End - generate mailbody
-
+        $addon = rex_addon::get('phpmailer');
         $mail = new self();
         $mail->Subject = rex::getServerName() . ' - error report ';
         $mail->Body = $mailBody;
         $mail->AltBody = strip_tags($mailBody);
-        $mail->setFrom($this->From, 'REDAXO error report');
+        $mail->setFrom($addon->getConfig('from'), 'REDAXO error report');
         $mail->addAddress(rex::getErrorEmail());
 
         $addon->setConfig('last_log_file_send_time', time());
