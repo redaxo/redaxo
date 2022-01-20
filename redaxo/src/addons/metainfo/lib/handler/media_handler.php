1a45eb3a7e414b9de4c7d49a3a7830b23f28b2f9
staabm <staabm@users.noreply.github.com>
2022-01-20 21:06:19 +0000

    Apply php-cs-fixer changes

--- redaxo/src/addons/metainfo/lib/handler/media_handler.php
+++ redaxo/src/addons/metainfo/lib/handler/media_handler.php
@@ -187,7 +187,7 @@ class rex_metainfo_media_handler extends rex_metainfo_handler
             unset($params['media']);
         } elseif ('MEDIA_ADDED' == $ep->getName()) {
             $sql = rex_sql::factory();
-            
+
             $qry = 'SELECT id FROM ' . rex::getTablePrefix() . 'media WHERE filename=:filename';
             $sql->setQuery($qry, [':filename' => $params['filename']]);
             if (1 == $sql->getRows()) {
