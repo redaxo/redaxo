aea6d0b557bb83ed1c5c63683bba630035e34540
Tobias Krais <tk@design-to-use.de>
2022-01-22 11:27:24 +0100

    Methode umgezogen

--- redaxo/src/addons/media_manager/boot.php
+++ redaxo/src/addons/media_manager/boot.php
@@ -15,34 +15,5 @@ if (rex::isBackend()) {
     // delete thumbnails on mediapool changes
     rex_extension::register('MEDIA_UPDATED', [rex_media_manager::class, 'mediaUpdated']);
     rex_extension::register('MEDIA_DELETED', [rex_media_manager::class, 'mediaUpdated']);
-   	rex_extension::register('MEDIA_IS_IN_USE', 'rex_media_manager_media_is_in_use');
-}
-
-/**
- * Checks if media is used by this addon
- * @param rex_extension_point $ep Redaxo extension point
- * @return string[] Warning message as array
- */
-function rex_media_manager_media_is_in_use(rex_extension_point $ep) {
-	$warning = $ep->getSubject();
-	$params = $ep->getParams();
-	$filename = $params['filename'];
-
-	if($filename) {
-		$sql = \rex_sql::factory();
-		$query = 'SELECT DISTINCT effect.id AS effect_id, effect.type_id, type.id, type.name FROM `' . rex::getTablePrefix() . 'media_manager_type_effect` AS effect '
-			.'LEFT JOIN `' . rex::getTablePrefix() . 'media_manager_type`AS type ON effect.type_id = type.id '
-			.'WHERE parameters REGEXP ' . $sql->escape('(^|[^[:alnum:]+_-])'. $filename);
-		$sql->setQuery($query);
-
-		// Prepare warnings
-		for($i = 0; $i < $sql->getRows(); $i++) {
-			$message = '<a href="javascript:openPage(\''. rex_url::backendPage('media_manager/types', ['effects' => 1, 'type_id' => $sql->getValue('type_id'), 'effect_id' => $sql->getValue('effect_id'), 'func' => 'edit']) .'\')">'. rex_i18n::msg('media_manager') .' '. rex_i18n::msg('media_manager_effect_name') .': '. $sql->getValue('name') .'</a>';
-			if(!in_array($message, $warning)) {
-				$warning[] = $message;
-			}
-		}
-	}
-
-	return $warning;
+   	rex_extension::register('MEDIA_IS_IN_USE', [rex_media_manager::class, 'mediaIsInUse']);
 }
