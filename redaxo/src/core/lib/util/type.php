e47bdcd820f643de6272b2068b386b0c4c72f9c7
Gregor Harlan <mail@gh01.de>
2022-05-02 16:33:19 +0200

    rex_type::cast: Keine Notice bei array->string (#5133)

--- redaxo/src/core/lib/util/type.php
+++ redaxo/src/core/lib/util/type.php
@@ -59,7 +59,11 @@ class rex_type
                     $var = (float) $var;
                     break;
                 case 'string':
-                    $var = (string) $var;
+                    if (is_array($var)) { // https://github.com/redaxo/redaxo/issues/2900
+                        $var = '';
+                    } else {
+                        $var = (string) $var;
+                    }
                     break;
                 case 'object':
                     $var = (object) $var;
