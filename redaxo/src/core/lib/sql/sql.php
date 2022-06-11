08b0563036325dd4f1d05dff5ba59e482f01afba
Markus Staab <maggus.staab@googlemail.com>
2022-06-11 13:23:19 +0200

    use conditional return-type

--- redaxo/src/core/lib/sql/sql.php
+++ redaxo/src/core/lib/sql/sql.php
@@ -1431,6 +1431,7 @@ class rex_sql implements Iterator
      * @param string $value den zu escapenden Wert
      *
      * @return string
+     * @psalm-return ($value is numeric-string ? numeric-string : ($value is non-empty-string ? non-empty-string : string))
      */
     public function escape($value)
     {
@@ -1453,6 +1454,8 @@ class rex_sql implements Iterator
 
     /**
      * Escapes the `LIKE` wildcard chars "%" and "_" in given value.
+     *
+     * @psalm-return ($value is numeric-string ? numeric-string : ($value is non-empty-string ? non-empty-string : string))
      */
     public function escapeLikeWildcards(string $value): string
     {
