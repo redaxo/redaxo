d87ddae19b46ebbc4d30f99d7f65507b59d58cd3
Markus Staab <maggus.staab@googlemail.com>
2022-07-11 16:28:13 +0200

    Update backend_login.php

--- redaxo/src/core/lib/login/backend_login.php
+++ redaxo/src/core/lib/login/backend_login.php
@@ -180,7 +180,9 @@ class rex_backend_login extends rex_login
     /**
      * @param null|string $passwordHash Passing `null` or ommitting this param is DEPRECATED
      */
-    public function changedPassword(#[\SensitiveParameter] ?string $passwordHash = null): void
+    public function changedPassword(
+    #[\SensitiveParameter] ?string $passwordHash = null
+    ): void
     {
         $this->setSessionVar(self::SESSION_PASSWORD_CHANGE_REQUIRED, false);
 
