f4148692c1fd4b834be829eecf490675aa2922f5
Markus Staab <maggus.staab@googlemail.com>
2022-07-14 15:38:49 +0200

    psalm ignore error

--- redaxo/src/core/lib/login/backend_login.php
+++ redaxo/src/core/lib/login/backend_login.php
@@ -280,6 +280,6 @@ class rex_backend_login extends rex_login
     {
         $loginPolicy = (array) rex::getProperty('backend_login_policy', []);
 
-        return new rex_login_policy($loginPolicy);
+        return new rex_login_policy($loginPolicy); // @psalm-ignore-line
     }
 }
