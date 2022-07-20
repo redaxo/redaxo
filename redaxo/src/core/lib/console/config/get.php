d49cfd82c42bda9c5a6f2cbebb19af32af24c7df
Markus Staab <maggus.staab@googlemail.com>
2022-07-20 12:17:47 +0200

    Restore get.php

--- redaxo/src/core/lib/console/config/get.php
+++ redaxo/src/core/lib/console/config/get.php
@@ -48,7 +48,7 @@ class rex_command_config_get extends rex_console_command
             return 1;
         }
         foreach ($path as $pathPart) {
-            if (!is_array($config[$pathPart]) || !array_key_exists($pathPart, $config)) {
+            if (!isset($config[$pathPart])) {
                 $io->getErrorStyle()->error('Config key not found');
                 return 1;
             }
