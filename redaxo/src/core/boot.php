6d787a3c53781f8df93f25b718e48250ab9175c5
Gregor Harlan <mail@gh01.de>
2022-08-02 18:43:23 +0200

    Release 5.14.1

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -84,7 +84,7 @@ require_once rex_path::core('functions/function_rex_globals.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
-rex::setProperty('version', '5.14.0');
+rex::setProperty('version', '5.14.1');
 
 $cacheFile = rex_path::coreCache('config.yml.cache');
 $configFile = rex_path::coreData('config.yml');
