1d4e255a230f8bf4c7c22e653964398dda7d359e
Markus Staab <maggus.staab@googlemail.com>
2022-08-11 09:55:19 +0200

    Update boot.php

--- redaxo/src/core/boot.php
+++ redaxo/src/core/boot.php
@@ -84,7 +84,7 @@ require_once rex_path::core('functions/function_rex_globals.php');
 require_once rex_path::core('functions/function_rex_other.php');
 
 // ----------------- VERSION
-rex::setProperty('version', '5.14.1');
+rex::setProperty('version', '5.15-dev');
 
 $cacheFile = rex_path::coreCache('config.yml.cache');
 $configFile = rex_path::coreData('config.yml');
