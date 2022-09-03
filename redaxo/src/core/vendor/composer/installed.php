120f5660e52db5de5bd4de149b780ac3b444cf29
Markus Staab <maggus.staab@googlemail.com>
2022-09-03 16:27:33 +0200

    Update installed.php

--- redaxo/src/core/vendor/composer/installed.php
+++ redaxo/src/core/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '12a99723b53b7a1287f52c71039061735f5dc3c9',
+        'reference' => '3ca919fe82340d5c4067b4ea21f0e49860d48a64',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '12a99723b53b7a1287f52c71039061735f5dc3c9',
+            'reference' => '3ca919fe82340d5c4067b4ea21f0e49860d48a64',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
