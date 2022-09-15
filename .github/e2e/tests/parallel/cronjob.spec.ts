07e9bf48912124912300165b6b2d960da4563bae
schuer <ds@decaf.de>
2022-09-15 17:59:17 +0200

    test(cronjob): wait for networkidle

--- .github/e2e/tests/parallel/cronjob.spec.ts
+++ .github/e2e/tests/parallel/cronjob.spec.ts
@@ -16,7 +16,7 @@ test.describe.parallel('All', () => {
     for (const item of testItems) {
 
         test(`${item.name}`, async ({ page, browserName }, testInfo) => {
-            await gotoPage(page, browserName, `${item.url}`);
+            await gotoPage(page, browserName, `${item.url}`, { waitUntil: 'networkidle' }); // wait for networkidle here due to jquery tab shizzle
             await matchPageSnapshot(page, `${testInfo.title}`);
         });
     }
