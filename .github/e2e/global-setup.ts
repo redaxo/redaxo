42da9b1dd2e042dfc36caceead8690d9c7096f7c
schuer <ds@decaf.de>
2022-09-15 17:22:14 +0200

    style

--- .github/e2e/global-setup.ts
+++ .github/e2e/global-setup.ts
@@ -4,6 +4,7 @@ async function globalSetup(config: FullConfig) {
     const { baseURL, storageState } = config.projects[0].use;
     const browser = await chromium.launch();
     const page = await browser.newPage();
+
     // add htaccess check cookie before loading the login page
     await page.context().addCookies([{
         "name": "rex_htaccess_check",
@@ -13,6 +14,7 @@ async function globalSetup(config: FullConfig) {
         "expires": -1,
     }]);
     await page.goto(baseURL, { waitUntil: 'networkidle' });
+
     // fill out login form if available
     if (await page.locator('#rex-form-login').count() > 0) {
         await page.fill('#rex-id-login-user', 'admin');
@@ -21,6 +23,7 @@ async function globalSetup(config: FullConfig) {
         await page.click('button[type=submit]');
         await page.waitForLoadState();
     }
+
     // Save signed-in state to 'storageState.json'
     await page.context().storageState({ path: storageState as string });
     await browser.close();
