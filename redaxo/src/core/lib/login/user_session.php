1911271c9eda5b96aad0313df23821efa6b8dbcb
Marcel Kuhmann <marcel@dermarcel.eu>
2022-09-24 00:31:01 +0200

    handle impersonate

--- redaxo/src/core/lib/login/user_session.php
+++ redaxo/src/core/lib/login/user_session.php
@@ -12,7 +12,7 @@ class rex_user_session
         rex_sql::factory()
             ->setTable(rex::getTable('user_session'))
             ->setValue('session_id', session_id())
-            ->setValue('user_id', $login->getSessionVar(rex_backend_login::SESSION_USER_ID))
+            ->setValue('user_id', $login->getSessionVar(rex_backend_login::SESSION_IMPERSONATOR, null) ?? $login->getSessionVar(rex_backend_login::SESSION_USER_ID))
             ->setValue('ip', rex_request::server('REMOTE_ADDR', 'string'))
             ->setValue('useragent', rex_request::server('HTTP_USER_AGENT', 'string'))
             ->setValue('starttime', rex_sql::datetime($login->getSessionVar(rex_backend_login::SESSION_START_TIME, time())))
