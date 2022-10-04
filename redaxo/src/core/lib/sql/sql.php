78eeea1805193a257af60f7b46ed43fd1bb1f653
Markus Staab <maggus.staab@googlemail.com>
2022-10-04 20:12:21 +0200

    support `non-falsy-string` in `rex_sql::escape()` (#5350)

--- redaxo/src/core/lib/sql/sql.php
+++ redaxo/src/core/lib/sql/sql.php
@@ -1405,7 +1405,10 @@ class rex_sql implements Iterator
      * @param string $value den zu escapenden Wert
      *
      * @return string
-     * @psalm-return ($value is numeric-string ? numeric-string : ($value is non-empty-string ? non-empty-string : string))
+     * @psalm-return ($value is numeric-string ? numeric-string :
+     *   ($value is non-falsy-string ? non-falsy-string :
+     *   ($value is non-empty-string ? non-empty-string : string
+     * )))
      */
     public function escape($value)
     {
@@ -1429,7 +1432,10 @@ class rex_sql implements Iterator
     /**
      * Escapes the `LIKE` wildcard chars "%" and "_" in given value.
      *
-     * @psalm-return ($value is numeric-string ? numeric-string : ($value is non-empty-string ? non-empty-string : string))
+     * @psalm-return ($value is numeric-string ? numeric-string :
+     *   ($value is non-falsy-string ? non-falsy-string :
+     *   ($value is non-empty-string ? non-empty-string : string
+     * )))
      */
     public function escapeLikeWildcards(string $value): string
     {
