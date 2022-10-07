1714e9a3aecf5ca63ada7a0830ed11e53d1be777
staabm <staabm@users.noreply.github.com>
2022-10-07 07:27:52 +0000

    Apply php-cs-fixer changes

--- redaxo/src/addons/metainfo/lib/default_type.php
+++ redaxo/src/addons/metainfo/lib/default_type.php
@@ -3,19 +3,20 @@
 /**
  * @api
  */
-final class rex_metainfo_default_type {
+final class rex_metainfo_default_type
+{
     // keep in sync with install.sql
-    const TEXT = 1;
-    const TEXTAREA = 2;
-    const SELECT = 3;
-    const RADIO = 4;
-    const CHECKBOX = 5;
-    const REX_MEDIA_WIDGET = 6;
-    const REX_MEDIALIST_WIDGET = 7;
-    const REX_LINK_WIDGET = 8;
-    const REX_LINKLIST_WIDGET = 9;
-    const DATE = 10;
-    const DATETIME = 11;
-    const LEGEND = 12;
-    const TIME = 13;
+    public const TEXT = 1;
+    public const TEXTAREA = 2;
+    public const SELECT = 3;
+    public const RADIO = 4;
+    public const CHECKBOX = 5;
+    public const REX_MEDIA_WIDGET = 6;
+    public const REX_MEDIALIST_WIDGET = 7;
+    public const REX_LINK_WIDGET = 8;
+    public const REX_LINKLIST_WIDGET = 9;
+    public const DATE = 10;
+    public const DATETIME = 11;
+    public const LEGEND = 12;
+    public const TIME = 13;
 }
