3f1a8807b8b7e1be71c0e5ed78a453be68a6aaf9
Marcel Kuhmann <kuhmann@koders.nrw>
2022-10-13 10:40:29 +0200

    use baseclass

--- redaxo/src/core/lib/login/user_session.php
+++ redaxo/src/core/lib/login/user_session.php
@@ -25,11 +25,11 @@ class rex_user_session
         rex_sql::factory()
             ->setTable(rex::getTable('user_session'))
             ->setValue('session_id', session_id())
-            ->setValue('user_id', $login->getSessionVar(rex_backend_login::SESSION_IMPERSONATOR, null) ?? $login->getSessionVar(rex_backend_login::SESSION_USER_ID))
+            ->setValue('user_id', $login->getSessionVar(rex_login::SESSION_IMPERSONATOR, null) ?? $login->getSessionVar(rex_login::SESSION_USER_ID))
             ->setValue('ip', rex_request::server('REMOTE_ADDR', 'string'))
             ->setValue('useragent', rex_request::server('HTTP_USER_AGENT', 'string'))
-            ->setValue('starttime', rex_sql::datetime($login->getSessionVar(rex_backend_login::SESSION_START_TIME, time())))
-            ->setValue('last_activity', rex_sql::datetime($login->getSessionVar(rex_backend_login::SESSION_LAST_ACTIVITY)))
+            ->setValue('starttime', rex_sql::datetime($login->getSessionVar(rex_login::SESSION_START_TIME, time())))
+            ->setValue('last_activity', rex_sql::datetime($login->getSessionVar(rex_login::SESSION_LAST_ACTIVITY)))
             ->insertOrUpdate();
 
         $login->setSessionVar(self::SESSION_VAR_LAST_DB_UPDATE, time());
