3eb25b2d39ec6384230a22030a1147af313cdc81
Gregor Harlan <mail@gh01.de>
2022-10-14 09:44:57 +0200

    Session-Liste: intlDateTime nutzen (#5375)

--- redaxo/src/core/pages/profile.sessions.php
+++ redaxo/src/core/pages/profile.sessions.php
@@ -23,10 +23,10 @@ $list->setColumnFormat('last_activity', 'custom', static function () use ($list)
     if (session_id() === $list->getValue('session_id')) {
         return rex_i18n::msg('active_session');
     }
-    return rex_formatter::date((string) $list->getValue('last_activity'), 'd.m.Y H:i');
+    return rex_formatter::intlDateTime((string) $list->getValue('last_activity'), IntlDateFormatter::SHORT);
 });
 $list->setColumnFormat('starttime', 'custom', static function () use ($list) {
-    return rex_formatter::date((string) $list->getValue('starttime'), 'd.m.Y H:i');
+    return rex_formatter::intlDateTime((string) $list->getValue('starttime'), IntlDateFormatter::SHORT);
 });
 $content = $list->get();
 
