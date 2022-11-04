38b170554527b8870268f45b1fedd8e533d34dcf
Markus Staab <maggus.staab@googlemail.com>
2022-11-04 12:40:07 +0100

    fix

--- redaxo/src/core/tests/var/var_test.php
+++ redaxo/src/core/tests/var/var_test.php
@@ -206,13 +206,13 @@ c', "a\nb\nc"],
         $firstVar = $it->current();
         $this->assertInstanceOf(rex_var_test_var::class, $firstVar);
         $this->assertSame("'ab'", $firstVar->getOutput());
-        $this->assertSame("'ef'", $firstVar->getParsedArg('suffix'));
+        $this->assertSame("'ef'", $firstVar->getArg('suffix'));
 
         $it->next();
         $secondVar = $it->current();
         $this->assertInstanceOf(rex_var_test_var::class, $secondVar);
         $this->assertSame("'cd'", $secondVar->getOutput());
-        $this->assertSame("'gh'", $secondVar->getParsedArg('suffix'));
+        $this->assertSame("'gh'", $secondVar->getArg('suffix'));
 
         $it->next();
         $this->assertNull($it->current());
