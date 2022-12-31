3134d3c2067e8ea92d92b8dced9442a192af4066
christophboecker <christoph_boecker@t-online.de>
2022-12-31 11:23:11 +0100

    Code angepasst wg Psalm-Error MixedArrayAccess

--- redaxo/src/core/fragments/core/form/form.php
+++ redaxo/src/core/fragments/core/form/form.php
@@ -19,7 +19,8 @@ foreach ($this->elements as $element) {
 
     $note = isset($element['note']) && '' != $element['note'] ? '<p class="help-block rex-note">' . $element['note'] . '</p>' : '';
 
-    $classes = 'rex-form-group'.(($element['vertical'] ?? false) ? '-vertical' : '').' form-group';
+    $vertical = isset($element['vertical']) && true === $element['vertical'] ? '-vertical' : '';
+    $classes = 'rex-form-group' . $vertical . ' form-group';
 
     $error = '';
     if (isset($element['error']) && '' != $element['error']) {
