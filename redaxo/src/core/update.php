f3ecd09f0f687dc08b7616f0ee61645372a16982
Gregor Harlan <mail@gh01.de>
2023-01-05 00:10:21 +0100

    update

--- redaxo/src/core/update.php
+++ redaxo/src/core/update.php
@@ -4,7 +4,7 @@
 // while updating the core, the constants contain the old min versions from previous core version
 
 if (PHP_VERSION_ID < 80100) {
-    throw new rex_functional_exception(rex_i18n::msg(rex_string::versionCompare(rex::getVersion(), '5.14.0-dev', '<') ? 'setup_301' : 'setup_201', PHP_VERSION, '7.3'));
+    throw new rex_functional_exception(rex_i18n::msg(rex_string::versionCompare(rex::getVersion(), '5.14.0-dev', '<') ? 'setup_301' : 'setup_201', PHP_VERSION, '8.1'));
 }
 
 $minExtensions = ['ctype', 'fileinfo', 'filter', 'iconv', 'intl', 'mbstring', 'pcre', 'pdo', 'pdo_mysql', 'session', 'tokenizer'];
