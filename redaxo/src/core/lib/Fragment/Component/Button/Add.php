f84aead6a72fa160f3a8a60765dc0552b183dc84
tbaddade <thomas@redaxo.org>
2023-01-11 19:43:40 +0100

    allgemeiner Add Button

---
+++ redaxo/src/core/lib/Fragment/Component/Button/Add.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Redaxo\Core\Fragment\Component\Button;
+
+use Redaxo\Core\Fragment\Component\Button;
+use Redaxo\Core\Fragment\Component\ButtonVariant;
+use Redaxo\Core\Fragment\Component\Icon;
+use Redaxo\Core\Fragment\Component\IconLibrary;
+use rex_i18n;
+
+class Add
+{
+    public function __construct(
+        public ?string $href = null,
+        public ?string $slot = null,
+        /** @var array<string, string>|null */
+        public ?array $attributes = null,
+    ) {
+    }
+
+    public function parse(): string
+    {
+        $icon = new Icon(
+            name: IconLibrary::Add,
+            slot: 'prefix'
+        );
+
+        $button = new Button(
+            label: rex_i18n::msg('add'),
+            href: $this->href,
+            prefix: $icon->parse(),
+            variant: ButtonVariant::Primary,
+            attributes: $this->attributes,
+        );
+
+        return $button->parse();
+    }
+}
