a957f02465323ebed719513c2a2fe45312be895d
Gregor Harlan <mail@gh01.de>
2023-01-31 00:05:34 +0100

    psalm

--- redaxo/src/addons/media_manager/lib/effects/effect_workspace.php
+++ redaxo/src/addons/media_manager/lib/effects/effect_workspace.php
@@ -70,7 +70,7 @@ class rex_effect_workspace extends rex_effect_abstract
 
         // Bild als Hintergrund ------------------------------
         if ('image' == $this->params['set_transparent']) {
-            $bgimage = rex_path::media($this->params['bgimage']);
+            $bgimage = rex_path::media((string) $this->params['bgimage']);
             if (!is_file($bgimage)) {
                 return;
             }
