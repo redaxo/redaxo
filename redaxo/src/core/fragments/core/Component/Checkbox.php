4c973062062b3f4cddf1cf0d33d85b950a26dc7a
tbaddade <thomas@redaxo.org>
2023-02-03 21:59:30 +0100

    Checkbox Komponente

---
+++ redaxo/src/core/fragments/core/Component/Checkbox.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @var Checkbox $this
+ * @psalm-scope-this Checkbox
+ */
+
+use Redaxo\Core\Fragment\Component\Checkbox;
+?>
+<sl-checkbox
+    <?= $this->name ? 'name="'.rex_escape($this->name).'"' : '' ?>
+    <?= $this->value ? 'value="'.rex_escape($this->value).'"' : '' ?>
+    <?= $this->label ? 'label="'.rex_escape($this->label).'"' : '' ?>
+    <?= $this->disabled ? 'disabled' : '' ?>
+    <?= $this->checked ? 'checked' : '' ?>
+    <?= $this->indeterminate ? 'indeterminate' : '' ?>
+    <?= $this->required ? 'required' : '' ?>
+    <?= $this->attributes ? rex_string::buildAttributes($this->attributes) : '' ?>
+>
+    <?= $this->slotLabel ? $this->slotLabel->prepare('label')->get() : '' ?>
+</sl-checkbox>
---
+++ redaxo/src/core/lib/Fragment/Component/Checkbox.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Redaxo\Core\Fragment\Component;
+
+use Redaxo\Core\Fragment\Slot;
+use rex_fragment;
+use rex_functional_exception;
+
+class Checkbox extends rex_fragment
+{
+    private string $fileName = 'core/Component/Checkbox.php';
+
+    public function __construct(
+        /**
+         * The checkbox's label. Alternatively, you can use the
+         * label attribute.
+         */
+        public ?Slot $slotLabel = null,
+
+        /**
+         * The checkbox's label. If you need to display HTML,
+         * use the label slot instead.
+         */
+        public ?string $label = null,
+
+        /**
+         * The name of the checkbox, submitted as a name/value
+         * pair with form data.
+         */
+        public ?string $name = null,
+
+        /**
+         * The current value of the input, submitted as a
+         * name/value pair with form data.
+         */
+        public ?string $value = null,
+
+        /**
+         * Draws the checkbox in a checked state.
+         */
+        public bool $checked = false,
+
+        /**
+         * Draws the checkbox in an indeterminate state.
+         * This is usually applied to checkboxes that
+         * represents a "select all/none" behavior when
+         * associated checkboxes have a mix of checked and
+         * unchecked states.
+         */
+        public bool $indeterminate = false,
+
+        /**
+         * Disables the checkbox.
+         */
+        public bool $disabled = false,
+
+        /**
+         * Makes the checkbox a required field.
+         */
+        public bool $required = false,
+
+        /** @var array<string, string>|null */
+        public ?array $attributes = null,
+    ) {
+        parent::__construct([]);
+    }
+
+    public function render(): string
+    {
+        return parent::parse($this->fileName);
+    }
+}
