f93a2d3d8dc213d9b9e8056d91d966f0792ff0e2
Gregor Harlan <mail@gh01.de>
2023-03-13 00:02:32 +0100

    Icon

--- redaxo/src/core/fragments/core/Component/Icon.php
+++ redaxo/src/core/fragments/core/Component/Icon.php
@@ -1,25 +1,25 @@
 <?php
 
+use Redaxo\Core\Fragment\Attributes;
 use Redaxo\Core\Fragment\Component\Icon;
 use Redaxo\Core\Fragment\Component\IconLibrary;
 
 /** @var Icon $this */
-
-$name = match ($this->name) {
-    IconLibrary::Add => 'plus-lg',
-    IconLibrary::AlertError => 'exclamation-octagon',
-    IconLibrary::AlertInfo,
-    IconLibrary::PhpInfo => 'info-circle',
-    IconLibrary::AlertNeutral => 'x-diamond',
-    IconLibrary::AlertSuccess => 'check-circle',
-    IconLibrary::AlertWarning => 'exclamation-triangle',
-    IconLibrary::Debug => 'heart-pulse',
-    IconLibrary::VersionUnstable => 'egg-fried',
-    IconLibrary::Save => 'database-up',
-};
 ?>
-<sl-icon name="<?= $name ?>"
-    <?= $this->label ? 'label="'.rex_escape($this->label).'"' : '' ?>
-    <?= $this->src ? 'src="'.rex_escape($this->src).'"' : '' ?>
->
+<sl-icon <?= (new Attributes([
+    'name' => match ($this->name) {
+        IconLibrary::Add => 'plus-lg',
+        IconLibrary::AlertError => 'exclamation-octagon',
+        IconLibrary::AlertInfo,
+        IconLibrary::PhpInfo => 'info-circle',
+        IconLibrary::AlertNeutral => 'x-diamond',
+        IconLibrary::AlertSuccess => 'check-circle',
+        IconLibrary::AlertWarning => 'exclamation-triangle',
+        IconLibrary::Debug => 'heart-pulse',
+        IconLibrary::VersionUnstable => 'egg-fried',
+        IconLibrary::Save => 'database-up',
+    },
+    'label' => $this->label,
+    'src' => $this->src,
+]))->toString() ?>>
 </sl-icon>
