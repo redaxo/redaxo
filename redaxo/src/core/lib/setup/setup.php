5647675d4e761a2ac7808e26dd4e49bc0051038f
madiko <github@madiko.com>
2023-03-14 14:32:02 +0100

    Korrektur MariaDB-EOL-Daten
    fixes issue #5647
    
    Danke für den Hinweis, dass es MariaDB 10.7 nicht übernommen hatte @staabm
    Auf Wunsch von @gharlan zudem die Daten jeweils auf den Ersten des Monats gesetzt.
    Auf Wunsch von @gharlan ferner den Commit besser benamt (übernehme ich gleich auch noch im PR)

--- redaxo/src/core/lib/setup/setup.php
+++ redaxo/src/core/lib/setup/setup.php
@@ -222,15 +222,15 @@ class rex_setup
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
             } elseif (1 == version_compare($dbVersion, '10.6', '<') && time() > strtotime('1 Jun 2025')) {
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
-            } elseif (1 == version_compare($dbVersion, '10.7', '<') && time() > strtotime('1 Jul 2026')) { // LTS
+            } elseif (1 == version_compare($dbVersion, '10.7', '<') && time() > strtotime('1 Feb 2023')) { // LTS
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
-            } elseif (1 == version_compare($dbVersion, '10.8', '<') && time() > strtotime('20 May 2023')) {
+            } elseif (1 == version_compare($dbVersion, '10.8', '<') && time() > strtotime('1 May 2023')) {
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
-            } elseif (1 == version_compare($dbVersion, '10.9', '<') && time() > strtotime('22 Aug 2023')) {
+            } elseif (1 == version_compare($dbVersion, '10.9', '<') && time() > strtotime('1 Aug 2023')) {
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
-            } elseif (1 == version_compare($dbVersion, '10.10', '<') && time() > strtotime('17 Nov 2023')) {
+            } elseif (1 == version_compare($dbVersion, '10.10', '<') && time() > strtotime('1 Nov 2023')) {
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
-            } elseif (1 == version_compare($dbVersion, '10.11', '<') && time() > strtotime('16 Feb 2028')) {
+            } elseif (1 == version_compare($dbVersion, '10.11', '<') && time() > strtotime('1 Feb 2028')) {
                 $security[] = rex_i18n::msg('setup_security_deprecated_mariadb', $dbVersion);
             }
         } elseif (rex_sql::MYSQL === $dbType) {
