7a54339a7ddd3e4e8d38b10aaa7f96ed6b754af9
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-05-11 12:25:05 +0200

    Optionally add LDAP users if they do not yet exist.

--- redaxo/src/core/lib/login/backend_login_ldap.php
+++ redaxo/src/core/lib/login/backend_login_ldap.php
@@ -2,6 +2,7 @@
 
 class rex_backend_login_ldap extends rex_backend_login
 {
+    private $bindDn = null;
     private $clearTextPassword;
     private static $ignorePassword = false;
 
@@ -28,6 +29,14 @@ class rex_backend_login_ldap extends rex_backend_login
         }
 
         if ($this->doAuthLdap()) {
+            $config = rex::getProperty('backend_login_ldap', []);
+            if ($config['create_users']) {
+                $user = rex_sql::factory();
+                $user->setQuery('SELECT * FROM ' . rex::getTablePrefix() . 'user WHERE login = ?', [$this->userLogin]);
+                if ($user->getRows() === 0) {
+                    $this->addLdapUser();
+                }
+            }
             // we have overridden the passwordVerify method
             self::$ignorePassword = true;
             $result = parent::checkLogin();
@@ -47,27 +56,97 @@ class rex_backend_login_ldap extends rex_backend_login
      */
     private function doAuthLdap()
     {
-        $config = rex::getProperty('backend_login_ldap', []);
-        $uri = $config['ldap_uri'] ?? '';
         $bound = false;
-        $ds = ldap_connect($uri);
+        $this->bindDn = null;
+        $ds = $this->openLdapConnection();
         if ($ds) {
-            if (ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3)) {
-                if (ldap_set_option($ds, LDAP_OPT_REFERRALS, 0)) {
-                    if (empty($config['starttls']) || ldap_start_tls($ds)) {
-                        foreach (($config['bind_dns'] ?? []) as $bind_dn) {
-                            $bind_dn = preg_replace('/%USER%/', $this->userLogin, $bind_dn);
-                            $bound = ldap_bind($ds, $bind_dn, $this->clearTextPassword);
-                            if ($bound) {
-                                break;
-                            }
-                        }
-                    }
+            $config = rex::getProperty('backend_login_ldap', []);
+            foreach (($config['bind_dns'] ?? []) as $bindDn) {
+                $bindDn = preg_replace('/%USER%/', $this->userLogin, $bindDn);
+                $bound = ldap_bind($ds, $bindDn, $this->clearTextPassword);
+                if ($bound) {
+                    $this->bindDn = $bindDn;
+                    break;
                 }
             }
             ldap_close($ds);
         }
         return $bound;
+    }
+
+    private function openLdapConnection()
+    {
+        $config = rex::getProperty('backend_login_ldap', []);
+        $uri = $config['ldap_uri'] ?? '';
+        $ds = ldap_connect($uri);
+        if ($ds) {
+            if (ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3)) {
+                if (ldap_set_option($ds, LDAP_OPT_REFERRALS, 0)) {
+                    if (empty($config['starttls']) || ldap_start_tls($ds)) {
+                        return $ds;
+                    }
+                }
+            }
+            ldap_close($ds);
+        }
+        return null;
+    }
+
+    private function addLdapUser()
+    {
+        $ldapValues = [
+            'name' => $this->userLogin,
+            'email' => null,
+            'description' => null,
+        ];
+
+        $ds = $this->openLdapConnection();
+        if ($ds && ldap_bind($ds, $this->bindDn, $this->clearTextPassword)) {
+            $config = rex::getProperty('backend_login_ldap', []);
+            $ldapFilter = '(objectClass=*)'; // ldap command requires some filter
+            $attributes = array_values($config['attributes']);
+            $searchResult = ldap_read($ds, $this->bindDn, $ldapFilter, $attributes);
+            if ($searchResult) {
+                $ldapEntry = ldap_get_entries($ds, $searchResult);
+                foreach (array_keys($ldapValues) as $key) {
+                    $ldapAttribute = strtolower($config['attributes'][$key] ?? '');
+                    $ldapValue = $ldapEntry[0][$ldapAttribute][0] ?? null;
+                    if (!empty($ldapValue)) {
+                        $ldapValues[$key] = $ldapValue;
+                    }
+                }
+            }
+            ldap_close($ds);
+        }
+
+        $userStatus = 1;
+        $userPswHash = rex_login::passwordHash($this->clearTextPassword);
+
+        $addUser = rex_sql::factory();
+        $addUser->setTable(rex::getTablePrefix() . 'user');
+        $addUser->setValue('name', $ldapValues['name']);
+        $addUser->setValue('password', $userPswHash);
+        $addUser->setValue('login', $this->userLogin);
+        $addUser->setValue('description', $ldapValues['description']);
+        $addUser->setValue('email', $ldapValues['email']);
+        $addUser->setValue('admin', 0);
+        $addUser->setValue('language', '');
+        $addUser->setValue('startpage', '');
+        $addUser->setValue('role', null);
+        $addUser->addGlobalCreateFields();
+        $addUser->addGlobalUpdateFields();
+        $addUser->setDateTimeValue('password_changed', time());
+        $addUser->setArrayValue('previous_passwords', []);
+        $addUser->setValue('password_change_required', 0);
+        $addUser->setValue('status', 1);
+
+        $addUser->insert();
+
+        rex_extension::registerPoint(new rex_extension_point('USER_ADDED', '', [
+            'id' => $addUser->getLastId(),
+            'user' => rex_user::require((int) $addUser->getLastId()),
+            'password' => $this->clearTextPassword,
+        ], true));
     }
 
     /** {@inheritdoc} */
