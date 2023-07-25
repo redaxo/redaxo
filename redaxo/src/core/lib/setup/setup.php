6180422bc9a46b7d1502771a17b2f34643397696
Koala <330686+Koala@users.noreply.github.com>
2023-07-25 12:39:16 +0200

    Setup / add extension check 'gd'
    Die Prüfung auf die Extension 'gd' fehlt.
    Wird wärend des Setup zwingend benötigt für den media_manager.

--- redaxo/src/core/lib/setup/setup.php
+++ redaxo/src/core/lib/setup/setup.php
@@ -9,7 +9,7 @@ class rex_setup
 {
     // These values must be synchronized with the values in redaxo/src/core/update.php
     public const MIN_PHP_VERSION = REX_MIN_PHP_VERSION;
-    public const MIN_PHP_EXTENSIONS = ['ctype', 'fileinfo', 'filter', 'iconv', 'intl', 'mbstring', 'pcre', 'pdo', 'pdo_mysql', 'session', 'tokenizer'];
+    public const MIN_PHP_EXTENSIONS = ['ctype', 'fileinfo', 'filter', 'iconv', 'intl', 'mbstring', 'pcre', 'pdo', 'pdo_mysql', 'session', 'tokenizer', 'gd'];
     public const MIN_MYSQL_VERSION = '5.6';
     public const MIN_MARIADB_VERSION = '10.1';
 
