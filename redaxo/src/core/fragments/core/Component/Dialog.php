b729bdfb60ca339ad6d52240dd6a1a8a7512dfd0
tbaddade <thomas@redaxo.org>
2023-09-04 08:39:14 +0200

    Dialog label kann auch als Fragment genutzt werden

--- redaxo/src/core/fragments/core/Component/Dialog.php
+++ redaxo/src/core/fragments/core/Component/Dialog.php
@@ -6,8 +6,9 @@ use Redaxo\Core\Fragment\Fragment;
 /** @var Dialog $this */
 ?>
 <sl-dialog <?= $this->attributes->with([
-    'label' => $this->label,
+    'label' => is_string($this->label) ? $this->label : null,
 ])->toString() ?>>
+    <?= $this->label instanceof Fragment ? Fragment::slot($this->label, 'label') : '' ?>
     <?= Fragment::slot($this->body) ?>
 </sl-dialog>
 <?php if ($this->button): ?>
