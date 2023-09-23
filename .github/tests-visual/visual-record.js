53249916a58e15a62f41d354438eed5686c6e99a
Marcel Kuhmann <kuhmann@koders.nrw>
2023-09-23 21:57:30 +0200

    comment

--- .github/tests-visual/visual-record.js
+++ .github/tests-visual/visual-record.js
@@ -126,6 +126,8 @@ async function processScreenshot(page, screenshotName) {
     mkdirp.sync(WORKING_DIR);
 
     // hide blinking cursor/icon
+    // disable animations / transitions
+    // disable box-shadow on navbar to prevent visual noise by https://github.com/redaxo/redaxo/blob/97a08682b965cfd3f94e2a5ffa219501544cf71c/redaxo/src/addons/be_style/plugins/redaxo/assets/javascripts/redaxo.js#L211-L222
     await page.evaluate(() => {
         document.body.insertAdjacentHTML('beforeend', `<style type="text/css">input { caret-color: transparent !important; } * { animation: initial !important; transition: none !important;} .navbar {box-shadow: none !important; }</style>`);
     });
