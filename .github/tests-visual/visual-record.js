cbac75eda81360b56d4ed895784494a735901a89
Marcel Kuhmann <marcel@dermarcel.eu>
2023-09-23 22:30:37 +0200

    visual-tests: prevent visual noise by scroll-events/transitions (#5821)

--- .github/tests-visual/visual-record.js
+++ .github/tests-visual/visual-record.js
@@ -126,7 +126,11 @@ async function processScreenshot(page, screenshotName) {
     mkdirp.sync(WORKING_DIR);
 
     // hide blinking cursor/icon
-    await page.addStyleTag({ content: 'input { caret-color: transparent !important; } * { animation: initial !important}' });
+    // disable animations / transitions
+    // disable box-shadow on navbar to prevent visual noise by https://github.com/redaxo/redaxo/blob/97a08682b965cfd3f94e2a5ffa219501544cf71c/redaxo/src/addons/be_style/plugins/redaxo/assets/javascripts/redaxo.js#L211-L222
+    await page.evaluate(() => {
+        document.body.insertAdjacentHTML('beforeend', `<style type="text/css">input { caret-color: transparent !important; } * { animation: initial !important; transition: none !important;} .navbar {box-shadow: none !important; }</style>`);
+    });
 
     // mask dynamic content, to make it not appear like change (visual noise)
     await page.evaluate(function() {
