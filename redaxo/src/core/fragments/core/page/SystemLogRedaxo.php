4d6e43eceb2345c6ce202621e67463ba7bd13361
Thomas Blum <thomas@redaxo.de>
2023-09-30 00:24:40 +0200

    System Log Page als Fragment (#5823)
    Co-authored-by: Gregor Harlan <mail@gh01.de>

---
+++ redaxo/src/core/fragments/core/page/SystemLogRedaxo.php
@@ -0,0 +1,114 @@
+<?php
+
+use Redaxo\Core\Fragment\Component\Alert\Error;
+use Redaxo\Core\Fragment\Component\Alert\Success;
+use Redaxo\Core\Fragment\Component\Button;
+use Redaxo\Core\Fragment\Component\ButtonType;
+use Redaxo\Core\Fragment\Component\ButtonVariant;
+use Redaxo\Core\Fragment\Component\Card;
+use Redaxo\Core\Fragment\Html;
+use Redaxo\Core\Fragment\HtmlAttributes;
+use Redaxo\Core\Fragment\Page\SystemLogRedaxo;
+
+/**
+ * @var SystemLogRedaxo $this
+ * @psalm-scope-this SystemLogRedaxo
+ */
+?>
+
+<?php if ($this->error): ?>
+    <?= (new Error(
+        body: new Html($this->error),
+    ))->render() ?>
+<?php endif ?>
+
+<?php if ($this->success): ?>
+    <?= (new Success(
+        body: new Html($this->success),
+    ))->render() ?>
+<?php endif ?>
+
+<form action="<?= rex_url::currentBackendPage() ?>" method="post">
+    <input type="hidden" name="func" value="delLog" />
+    <?= $this->csrfToken->getHiddenField() ?>
+
+    <?= (new Card(
+        header: rex_i18n::rawMsg('syslog_title', $this->logFilePath),
+        body: new Html(function () { ?>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th><?= rex_i18n::msg('syslog_timestamp') ?></th>
+                        <th><?= rex_i18n::msg('syslog_message') ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($this->getEntries() as $entry): ?>
+                    <tr>
+                        <td class="rex-table-tabular-nums rex-table-date">
+                            <small><?= rex_formatter::intlDateTime($entry->timestamp, [IntlDateFormatter::SHORT, IntlDateFormatter::MEDIUM]) ?></small><br>
+                            <span class="label label-<?= match (strtolower($entry->type)) {
+                                'success' => 'success',
+                                'debug' => 'default',
+                                'info', 'notice', 'deprecated' => 'info',
+                                'warning' => 'warning',
+                                default => 'danger',
+                            } ?>"><?= rex_escape($entry->type) ?></span>
+                        </td>
+                        <td>
+                            <div class="rex-word-break"><b style="font-weight: 500"><?= nl2br(rex_escape($entry->message)) ?></b></div>
+                            <?php if ($entry->file): ?>
+                                <?php $path = rex_escape($entry->file . (null === $entry->line ? '' : ':' . $entry->line)) ?>
+                                <small class="rex-word-break">
+                                    <span class="label label-default"><?= rex_i18n::msg('syslog_file') ?>:</span>
+                                    <?php if ($entry->editorUrl): ?>
+                                        <a href="<?= $entry->editorUrl ?>"><?= $path ?></a>
+                                    <?php else: ?>
+                                        <?= $path ?>
+                                    <?php endif ?>
+                                </small><br>
+                            <?php endif ?>
+                            <?php if ($entry->url): ?>
+                                <small class="rex-word-break">
+                                    <span class="label label-default"><?= rex_i18n::msg('syslog_url') ?>:</span>
+                                    <a href="<?= $url = rex_escape($entry->url) ?>"><?= $url ?></a>
+                                </small>
+                            <?php endif ?>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+                </tbody>
+            </table>
+        <?php }),
+        footer: new Html(function () { ?>
+            <div>
+                <?= (new Button(
+                    label: rex_i18n::msg('syslog_delete'),
+                    name: 'del_btn',
+                    variant: ButtonVariant::Danger,
+                    type: ButtonType::Submit,
+                    attributes: new HtmlAttributes([
+                        'data-confirm' => rex_i18n::msg('delete') . '?',
+                    ]),
+                ))->render() ?>
+                <?php if ($url = $this->editor->getUrl($this->logFilePath, 0)): ?>
+                    <?= (new Button(
+                        label: rex_i18n::rawMsg('system_editor_open_file', rex_path::basename($this->logFilePath)),
+                        variant: ButtonVariant::Success,
+                        href: $url,
+                    ))->render() ?>
+                <?php endif ?>
+                <?php if (is_file($this->logFilePath)): ?>
+                    <?= (new Button(
+                        label: rex_i18n::rawMsg('syslog_download', rex_path::basename($this->logFilePath)),
+                        variant: ButtonVariant::Success,
+                        href: rex_url::currentBackendPage(['func' => 'download'] + $this->csrfToken->getUrlParams()),
+                        attributes: new HtmlAttributes([
+                            'download' => true,
+                        ]),
+                    ))->render() ?>
+                <?php endif ?>
+            </div>
+        <?php }),
+    ))->render() ?>
+</form>
---
+++ redaxo/src/core/lib/Fragment/Page/SystemLogRedaxo.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Redaxo\Core\Fragment\Page;
+
+use LimitIterator;
+use Redaxo\Core\Fragment\Page;
+use rex_csrf_token;
+use rex_editor;
+use rex_i18n;
+use rex_log_entry;
+use rex_log_file;
+use rex_logger;
+use rex_path;
+use rex_response;
+use rex_type;
+use stdClass;
+
+/**
+ * @see redaxo/src/core/fragments/core/page/SystemLogRedaxo.php
+ */
+final class SystemLogRedaxo extends Page
+{
+    public readonly rex_csrf_token $csrfToken;
+    public readonly rex_editor $editor;
+    public readonly string $logFilePath;
+    public ?string $success = null;
+    public ?string $error = null;
+
+    public function __construct()
+    {
+        $this->csrfToken = rex_csrf_token::factory('system-log-redaxo');
+        $this->editor = rex_editor::factory();
+        $this->logFilePath = rex_logger::getPath();
+
+        $func = rex_request('func', 'string');
+
+        if ($func && !$this->csrfToken->isValid()) {
+            $this->error = rex_i18n::msg('csrf_token_invalid');
+        } elseif ('delLog' === $func) {
+            // close logger, to free remaining file-handles to syslog
+            // so we can safely delete the file
+            rex_logger::close();
+
+            if (rex_log_file::delete($this->logFilePath)) {
+                $this->success = rex_i18n::msg('syslog_deleted');
+            } else {
+                $this->error = rex_i18n::msg('syslog_delete_error');
+            }
+        } elseif ('download' === $func && is_file($this->logFilePath)) {
+            rex_response::sendFile($this->logFilePath, 'application/octet-stream', 'attachment');
+            exit;
+        }
+    }
+
+    protected function getPath(): string
+    {
+        return 'core/page/SystemLogRedaxo.php';
+    }
+
+    /** @return iterable<int, object{timestamp: int, type: string, message: string, file: ?string, line: ?int, editorUrl: ?string, url: ?string}> */
+    public function getEntries(): iterable
+    {
+        /** @var rex_log_entry $entry */
+        foreach (new LimitIterator(new rex_log_file($this->logFilePath), 0, 100) as $entry) {
+            $data = $entry->getData();
+
+            $element = new stdClass();
+            $element->timestamp = $entry->getTimestamp();
+            $element->type = rex_type::string($data[0]);
+            $element->message = rex_type::string($data[1]);
+            $element->file = $data[2] ?? null;
+            $element->line = $data[3] ?? null;
+            $element->editorUrl = null;
+            $element->url = $data[4] ?? null;
+
+            if ($element->file) {
+                $fullPath = str_starts_with($element->file, 'rex://') ? $element->file : rex_path::base($element->file);
+                $element->editorUrl = $this->editor->getUrl($fullPath, (int) ($element->line ?? 1));
+            }
+
+            yield $element;
+        }
+    }
+}
