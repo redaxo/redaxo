3221596acf488a3f7c3f2c335bdcf27add096a57
Thomas Blum <thomas@redaxo.de>
2024-02-02 14:38:32 +0100

    Structure remove plugins (#5875)
    Co-authored-by: Gregor Harlan <mail@gh01.de>

--- redaxo/src/addons/structure/plugins/content/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-rex_sql_table::get(rex::getTable('action'))->drop();
-rex_sql_table::get(rex::getTable('article_slice'))->drop();
-rex_sql_table::get(rex::getTable('module'))->drop();
-rex_sql_table::get(rex::getTable('module_action'))->drop();
-rex_sql_table::get(rex::getTable('template'))->drop();
