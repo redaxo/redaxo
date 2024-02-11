4d235b6aa47b5467b42314bd5304b53bfeb6d209
Gregor Harlan <mail@gh01.de>
2024-02-11 19:46:03 +0100

    Remove old update scripts

--- redaxo/src/addons/install/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$addon = rex_addon::get('install');
-
-if (rex_string::versionCompare($addon->getVersion(), '2.0.1', '<') && rex_config::has('install')) {
-    rex_file::putCache($addon->getDataPath('config.json'), rex_config::get('install'));
-    rex_config::removeNamespace('install');
-}
