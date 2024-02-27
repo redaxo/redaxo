243b1d1a9d6ac30f57b8b5c0955682c8b0fce909
skerbis <skerbis@users.noreply.github.com>
2024-02-27 16:48:21 +0000

    Apply php-cs-fixer changes

--- redaxo/src/addons/phpmailer/lib/mailer.php
+++ redaxo/src/addons/phpmailer/lib/mailer.php
@@ -104,7 +104,7 @@ class rex_mailer extends PHPMailer
         return rex_timer::measure(__METHOD__, function () {
             $addon = rex_addon::get('phpmailer');
             $logging = $addon->getConfig('logging');
-            $detourModeActive = $addon->getConfig('detour_mode') && $addon->getConfig('test_address') !== '';
+            $detourModeActive = $addon->getConfig('detour_mode') && '' !== $addon->getConfig('test_address');
 
             rex_extension::registerPoint(new rex_extension_point('PHPMAILER_PRE_SEND', $this));
 
@@ -126,7 +126,7 @@ class rex_mailer extends PHPMailer
                 $this->archive($this->getSentMIMEMessage());
             }
 
-            if ($logging === self::LOG_ALL) {
+            if (self::LOG_ALL === $logging) {
                 $this->log('OK');
             }
 
