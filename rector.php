d5a08bf970026918c2f93e1a616e674862e05a27
Gregor Harlan <mail@gh01.de>
2024-03-01 20:09:05 +0100

    Rector-Ruleset beginnen für Upgrade zu R6

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\Arguments\Rector\MethodCall\RemoveMethodCallParamRector;
 use Rector\CodeQuality\Rector\Assign\CombinedAssignRector;
 use Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector;
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
@@ -22,6 +23,11 @@ use Rector\Php80\Rector\Identical\StrStartsWithRector;
 use Rector\Php80\Rector\NotIdentical\StrContainsRector;
 use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Removing\Rector\ClassMethod\ArgumentRemoverRector;
+use Rector\Removing\Rector\FuncCall\RemoveFuncCallArgRector;
+use Rector\Removing\ValueObject\ArgumentRemover;
+use Rector\Removing\ValueObject\RemoveFuncCallArg;
+use Rector\ValueObject\MethodName;
 use Rector\ValueObject\PhpVersion;
 use Redaxo\Rector\Rule\UnderscoreToCamelCasePropertyNameRector;
 use Redaxo\Rector\Rule\UnderscoreToCamelCaseVariableNameRector;
@@ -84,4 +90,22 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(UnderscoreToCamelCasePropertyNameRector::class);
     $rectorConfig->rule(UnderscoreToCamelCaseVariableNameRector::class);
     $rectorConfig->rule(UnnecessaryTernaryExpressionRector::class);
+
+    // Upgrade REDAXO 5 to 6
+    $rectorConfig->ruleWithConfiguration(RemoveFuncCallArgRector::class, [
+        new RemoveFuncCallArg('rex_getUrl', 3),
+    ]);
+    $rectorConfig->ruleWithConfiguration(ArgumentRemoverRector::class, [
+        new ArgumentRemover(rex_string::class, 'buildQuery', 1, null),
+        new ArgumentRemover(rex_url_provider_interface::class, 'getUrl', 1, null),
+        new ArgumentRemover(rex_url::class, 'frontendController', 1, null),
+        new ArgumentRemover(rex_url::class, 'backendController', 1, null),
+        new ArgumentRemover(rex_url::class, 'backendPage', 2, null),
+        new ArgumentRemover(rex_url::class, 'currentBackendPage', 1, null),
+        new ArgumentRemover(rex_form_base::class, 'getUrl', 1, null),
+        new ArgumentRemover(rex_list::class, 'getUrl', 1, null),
+        new ArgumentRemover(rex_list::class, 'getParsedUrl', 1, null),
+        new ArgumentRemover(rex_structure_element::class, 'getUrl', 1, null),
+        new ArgumentRemover(rex_media_manager::class, 'getUrl', 3, null),
+    ]);
 };
