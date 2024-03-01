a19276a14140743850be26bc4e352aaaca4aa673
Gregor Harlan <mail@gh01.de>
2024-03-01 19:10:56 +0100

    Code-Verschiebung

--- redaxo/src/core/backend.php
+++ redaxo/src/core/backend.php
@@ -499,11 +499,6 @@ if (rex::getConfig('article_work_version', false)) {
     });
 }
 
-if ('content' == rex_be_controller::getCurrentPagePart(1)) {
-    rex_view::addCssFile(rex_url::coreAssets('css/metainfo.css'));
-    rex_view::addJsFile(rex_url::coreAssets('js/metainfo.js'));
-}
-
 // add theme-information to js-variable rex as rex.theme
 // (1) System-Settings (2) no systemforced mode: user-mode (3) fallback: "auto"
 $user = rex::getUser();
@@ -520,6 +515,10 @@ if ('system' == rex_be_controller::getCurrentPagePart(1)) {
     rex_system_setting::register(new rex_system_setting_structure_package_status('article_history'));
     rex_system_setting::register(new rex_system_setting_structure_package_status('article_work_version'));
     rex_system_setting::register(new rex_system_setting_phpmailer_errormail());
+}
+if ('content' == rex_be_controller::getCurrentPagePart(1)) {
+    rex_view::addCssFile(rex_url::coreAssets('css/metainfo.css'));
+    rex_view::addJsFile(rex_url::coreAssets('js/metainfo.js'));
 }
 
 rex_perm::register('users[]');
@@ -564,31 +563,6 @@ rex_extension::register('STRUCTURE_CONTENT_SIDEBAR', function ($ep) {
     $subject = $ep->getSubject();
     $metaSidebar = include rex_path::core('pages/metainfo.content.php');
     return $metaSidebar . $subject;
-});
-
-rex_extension::register('EDITOR_URL', static function (rex_extension_point $ep) {
-    if (!preg_match('@^rex:///metainfo/(\d+)@', $ep->getParam('file'), $match)) {
-        return null;
-    }
-
-    $id = $match[1];
-    $sql = rex_sql::factory();
-    $sql->setQuery('SELECT `name` FROM ' . rex::getTable('metainfo_field') . ' WHERE id = ? LIMIT 1', [$id]);
-
-    if (!$sql->getRows()) {
-        return null;
-    }
-
-    $prefix = rex_metainfo_meta_prefix((string) $sql->getValue('name'));
-    $page = match ($prefix) {
-        'art_' => 'articles',
-        'cat_' => 'categories',
-        'med_' => 'media',
-        'clang_' => 'clangs',
-        default => throw new LogicException('Unknown metainfo prefix "' . $prefix . '"'),
-    };
-
-    return rex_url::backendPage('metainfo/' . $page, ['func' => 'edit', 'field_id' => $id]);
 });
 
 // ----- INCLUDE ADDONS
