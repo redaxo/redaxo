bce9955abf76dba99a17ccae6fd5ba502941984a
Gregor Harlan <mail@gh01.de>
2024-03-11 13:01:38 +0100

    Setup-Commands: Message decoden (#6027)

--- redaxo/src/core/lib/console/setup/run.php
+++ redaxo/src/core/lib/console/setup/run.php
@@ -541,7 +541,7 @@ class rex_command_setup_run extends rex_console_command implements rex_command_o
                     $this->io->warning($warning);
                 }
             } else {
-                $this->io->success(rex_i18n::msg('setup_208', PHP_VERSION));
+                $this->io->success($this->decodeMessage(rex_i18n::msg('setup_208', PHP_VERSION)));
             }
         } else {
             $errors = array_map($this->decodeMessage(...), $errors);
