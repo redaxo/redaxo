e33aef3358bda0fba99005bc4045d31d626a83cd
Gregor Harlan <mail@gh01.de>
2024-03-12 14:22:35 +0100

    Loginseite: neues Hintergrundbild (#6029)

--- redaxo/src/core/fragments/core/login_background.php
+++ redaxo/src/core/fragments/core/login_background.php
@@ -7,27 +7,27 @@
 <picture class="rex-background">
     <source
         srcset="
-            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/richard-horvath-cPccYbPrF-A-unsplash-2400.avif') ?> 2400w,
-            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/richard-horvath-cPccYbPrF-A-unsplash-3500.avif') ?> 3500w"
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/davide-cantelli-ajisKc2uuFk-unsplash-2400.avif') ?> 2400w,
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/davide-cantelli-ajisKc2uuFk-unsplash-3500.avif') ?> 3500w"
         sizes="100vw"
         type="image/avif"
     />
     <img
         alt=""
-        src="<?= rex_url::pluginAssets('be_style', 'redaxo', 'images/richard-horvath-cPccYbPrF-A-unsplash-2400.webp') ?>"
+        src="<?= rex_url::pluginAssets('be_style', 'redaxo', 'images/davide-cantelli-ajisKc2uuFk-unsplash-2400.webp') ?>"
         srcset="
-            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/richard-horvath-cPccYbPrF-A-unsplash-2400.webp') ?> 2400w,
-            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/richard-horvath-cPccYbPrF-A-unsplash-3500.webp') ?> 3500w"
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/davide-cantelli-ajisKc2uuFk-unsplash-2400.webp') ?> 2400w,
+            <?= rex_url::pluginAssets('be_style', 'redaxo', 'images/davide-cantelli-ajisKc2uuFk-unsplash-3500.webp') ?> 3500w"
         sizes="100vw"
     />
 </picture>
 
 <style nonce="<?= rex_response::getNonce() ?>">
     #rex-page-login {
-        background-color: #004eaa;
+        background-color: #6b6e73;
     }
     #rex-page-login .panel-default {
-        background-color: rgba(24, 28, 30, 0.97);
+        background-color: rgba(33, 38, 41, 0.9);
     }
 
     #rex-page-login .btn-primary {
@@ -36,7 +36,7 @@
     }
     #rex-page-login .btn-primary:hover,
     #rex-page-login .btn-primary:focus {
-        background-color: #0387cc;
+        background-color: #8cc600;
     }
 
     #rex-page-login .rex-global-footer {
@@ -62,7 +62,7 @@
         <ul class="list-inline">
             <li><a href="https://www.yakamara.de" target="_blank" rel="noreferrer noopener">yakamara.de</a></li>
             <li><a href="https://www.redaxo.org" target="_blank" rel="noreferrer noopener">redaxo.org</a></li>
-            <li class="rex-background-credits"><a href="https://unsplash.com/@ricvath" target="_blank" rel="noreferrer noopener">Photo by Richard Horvath on Unsplash</a></li>
+            <li class="rex-background-credits"><a href="https://unsplash.com/@cant89" target="_blank" rel="noreferrer noopener">Photo by Davide Cantelli on Unsplash</a></li>
         </ul>
     </nav>
 </footer>
