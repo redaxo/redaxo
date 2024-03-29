8666f5e3e17a7dde8b177e211698d3a70ddf24b4
tbaddade <thomas@redaxo.org>
2024-03-29 05:10:35 +0100

    .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -114,7 +114,7 @@ override(
     \rex_user::getComplexPerm(0),
     map([
         'clang' => \rex_clang_perm::class,
-        'media' => \rex_media_perm::class,
+        'media' => \Redaxo\Core\MediaPool\MediaPoolPermission::class,
         'modules' => \rex_module_perm::class,
         'structure' => \rex_structure_perm::class,
     ])
