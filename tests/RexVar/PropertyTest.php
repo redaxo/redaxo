78e872be7e5cc5aebe0d52e2f412f8399a7ff50a
tbaddade <thomas@redaxo.org>
2024-04-11 23:46:10 +0200

    PropertyTest verschoben

---
+++ tests/RexVar/PropertyTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Redaxo\Core\Tests\RexVar;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Redaxo\Core\Addon\Addon;
+use Redaxo\Core\Core;
+
+/**
+ * @internal
+ */
+final class PropertyTest extends RexVarTestBase
+{
+    protected function setUp(): void
+    {
+        Core::setProperty('myCoreProperty', 'myCorePropertyValue');
+        Addon::get('project')->setProperty('myPackageProperty', 'myPackagePropertyValue');
+    }
+
+    protected function tearDown(): void
+    {
+        Core::removeProperty('myCoreProperty');
+        Addon::get('project')->removeProperty('tests');
+    }
+
+    /** @return list<array{string, string}> */
+    public static function propertyReplaceProvider(): array
+    {
+        return [
+            ['REX_PROPERTY[key=myCoreProperty]', 'myCorePropertyValue'],
+            ['REX_PROPERTY[namespace=project key=myPackageProperty]', 'myPackagePropertyValue'],
+        ];
+    }
+
+    #[DataProvider('propertyReplaceProvider')]
+    public function testPropertyReplace(string $content, string $expectedOutput): void
+    {
+        $this->assertParseOutputEquals($expectedOutput, $content);
+    }
+}
