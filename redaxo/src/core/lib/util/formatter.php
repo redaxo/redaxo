93198f4e4b0bfcb6ceeacecf54f0efdbbcfd15b9
Norbert Micheel <nm@lautundschoen.de>
2024-04-11 18:23:02 +0200

    Update formatter.php
    Missing "t" in "dateime"

--- redaxo/src/core/lib/util/formatter.php
+++ redaxo/src/core/lib/util/formatter.php
@@ -521,7 +521,7 @@ abstract class rex_formatter
         }
 
         if (str_starts_with($value, '0000-00-00')) {
-            trigger_error(sprintf('%s: "%s" is not a valid dateime string.', __METHOD__, $value), E_USER_WARNING);
+            trigger_error(sprintf('%s: "%s" is not a valid datetime string.', __METHOD__, $value), E_USER_WARNING);
 
             return null;
         }
