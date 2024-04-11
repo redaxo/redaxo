fa58505c82ff81cc0bd344c0337459c60630387f
tbaddade <thomas@redaxo.org>
2024-04-11 10:25:28 +0200

    rector

--- rector.php
+++ rector.php
@@ -52,6 +52,7 @@ use Redaxo\Core\Mailer;
 use Redaxo\Core\MediaManager;
 use Redaxo\Core\MediaPool;
 use Redaxo\Core\MetaInfo;
+use Redaxo\Core\RexVar;
 use Redaxo\Core\Translation;
 use Redaxo\Core\Util;
 use Redaxo\Core\Validator;
@@ -262,6 +263,10 @@ return RectorConfig::configure()
         'rex_url' => Filesystem\Url::class,
         'rex_validator' => Validator\Validator::class,
         'rex_validation_rule' => Validator\ValidationRule::class,
+        'rex_var' => RexVar\AbstractRexVar::class,
+        'rex_var_media' => MediaPool\RexVar\Media::class,
+        'rex_var_medialist' => MediaPool\RexVar\MediaList::class,
+        'rex_var_property' => RexVar\Property::class,
         'rex_version' => Util\Version::class,
     ])
     ->withConfiguredRule(ArgumentAdderRector::class, [
