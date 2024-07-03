49cc19db58be50d99d0b1fa02b5efbbcda870de0
Gregor Harlan <mail@gh01.de>
2024-07-04 00:33:57 +0200

    php 8.3: fix php highlighting color (dark mode)

--- redaxo/src/core/lib/util/markdown.php
+++ redaxo/src/core/lib/util/markdown.php
@@ -202,6 +202,7 @@ final class rex_parsedown extends ParsedownExtra
         }
 
         // php 8.3 fix
+        $text = preg_replace('@<code style="color:[^"]+">@', '<code>', $text, 1);
         $text = preg_replace('@<span style="color:[^"]+">\n(<span style="color:[^"]+">)@', '$1', $text, 1);
         $text = preg_replace('@<\/span>\n(<\/span>\n<\/code>)$@', '$1', $text, 1);
 
