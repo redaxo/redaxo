ca5d1031f8f1c686bb29f5e93d3b729e93cfce28
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-25 17:10:14 +0200

    Update media_manager.php

--- redaxo/src/addons/media_manager/lib/media_manager.php
+++ redaxo/src/addons/media_manager/lib/media_manager.php
@@ -257,7 +257,10 @@ class rex_media_manager
     {
         assert(null !== $this->cachePath);
         assert(null !== $this->type);
-        $cachePath = rex_extension::registerPoint(new rex_extension_point('MEDIA_MANAGER_CACHE_FILENAME_PATH', $this->cachePath, $this));
+      
+        $cachePath = rex_extension::registerPoint(new rex_extension_point('MEDIA_MANAGER_CACHE_FILENAME_PATH', $this->cachePath, ['object' => $this,  
+     ]));
+
         return $cachePath . $this->type . '/' . $this->originalFilename;
     }
 
