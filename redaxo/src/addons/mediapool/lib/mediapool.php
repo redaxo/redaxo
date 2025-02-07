9d008697dcec6bf5a972bdc081fadb68e9dab7fa
Oliver Hörold <oliver@hoerold.de>
2025-02-07 11:26:56 +0100

    Mediapool: Check bzgl. gesperrter Dateiendungen korrigiert (#6213)
    Co-authored-by: Gregor Harlan <mail@gh01.de>

--- redaxo/src/addons/mediapool/lib/mediapool.php
+++ redaxo/src/addons/mediapool/lib/mediapool.php
@@ -122,7 +122,9 @@ final class rex_mediapool
         foreach ($blockedExtensions as $blockedExtension) {
             // $blockedExtensions extensions are not allowed within filenames, to prevent double extension vulnerabilities:
             // -> some webspaces execute files named file.php.txt as php
-            if (str_contains($filename, '.' . $blockedExtension)) {
+            if (str_ends_with($filename, '.' . $blockedExtension) // Prüfe ob der String mit der verbotenen Endung endet
+                || str_ends_with($filename, '.' . $blockedExtension . '.' . $fileExt) // prüfe ob es keine doppelte Endung der Form *.php.ext gibt
+            ) {
                 return false;
             }
         }
