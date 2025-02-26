7feb807c57a21c63213271cc10aea714db2c4b75
Gregor Harlan <mail@gh01.de>
2025-02-26 17:16:00 +0100

    PHPStan: PHP-Baseline-Files nutzen (#6250)

---
+++ .tools/phpstan/baseline/varTag.nativeType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+// total 1 error
+
+$ignoreErrors = [];
+$ignoreErrors[] = [
+    'message' => '#^PHPDoc tag @var with type class\\-string is not subtype of native type lowercase\\-string&non\\-falsy\\-string\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/../../../redaxo/src/core/lib/var/var.php',
+];
+
+return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
