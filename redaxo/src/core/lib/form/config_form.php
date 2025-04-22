e27cd0a82aabcd7decf9469410e073ed653cfab3
Thomas Skerbis <thomas.skerbis@mac.com>
2025-04-22 13:23:09 +0200

    config_form.php
    allow multiple buttons

--- redaxo/src/core/lib/form/config_form.php
+++ redaxo/src/core/lib/form/config_form.php
@@ -11,6 +11,9 @@ class rex_config_form extends rex_form_base
 
     /** @var string */
     private $namespace;
+    
+    /** @var array */
+    private $additionalButtons = [];
 
     /**
      * @param string $namespace `rex_config` namespace, usually the package key
@@ -46,11 +49,64 @@ class rex_config_form extends rex_form_base
     {
         parent::loadBackendConfig();
 
+        // Wir erzeugen die Buttons erst beim Anzeigen des Formulars,
+        // damit alle zusätzlichen Buttons erfasst werden können
+    }
+    
+    /**
+     * Adds a button to the form.
+     *
+     * @param string $name Name of the button (will be used as name attribute)
+     * @param string $label Label text of the button
+     * @param string $type Type of the button (submit, reset, button)
+     * @param array $attributes Additional HTML attributes for the button
+     * @return rex_form_element The created button element
+     */
+    public function addButton($name, $label, $type = 'submit', array $attributes = [])
+    {
+        $attr = array_merge(['type' => $type, 'internal::useArraySyntax' => false, 'internal::fieldSeparateEnding' => true], $attributes);
+        $button = $this->addField('button', $name, $label, $attr, false);
+        $this->additionalButtons[] = $button;
+        return $button;
+    }
+    
+    /**
+     * @return string
+     */
+    public function get()
+    {
+        // Wir erstellen das Kontrollfeld mit allen Buttons kurz vor der Ausgabe des Formulars
+        $this->createControlField();
+        
+        return parent::get();
+    }
+    
+    /**
+     * Creates the control field with the save button and any additional buttons
+     */
+    protected function createControlField()
+    {
+        // Standard-Speichern-Button
         $attr = ['type' => 'submit', 'internal::useArraySyntax' => false, 'internal::fieldSeparateEnding' => true];
-        $this->addControlField(
-            null,
-            $this->addField('button', 'save', rex_i18n::msg('form_save'), $attr, false),
-        );
+        $saveButton = $this->addField('button', 'save', rex_i18n::msg('form_save'), $attr, false);
+        
+        // Control-Element mit allen Buttons erstellen
+        if (empty($this->additionalButtons)) {
+            // Standardverhalten, wenn keine zusätzlichen Buttons vorhanden sind
+            $this->addControlField(null, $saveButton);
+        } else {
+            // Bei mindestens einem Button erstellen wir ein erweitertes Control-Element
+            $buttons = array_merge([$saveButton], $this->additionalButtons);
+            
+            // Die ersten fünf Buttons als Parameter übergeben
+            $saveElement = isset($buttons[0]) ? $buttons[0] : null;
+            $applyElement = isset($buttons[1]) ? $buttons[1] : null;
+            $deleteElement = isset($buttons[2]) ? $buttons[2] : null;
+            $resetElement = isset($buttons[3]) ? $buttons[3] : null;
+            $abortElement = isset($buttons[4]) ? $buttons[4] : null;
+            
+            $this->addControlField($saveElement, $applyElement, $deleteElement, $resetElement, $abortElement);
+        }
     }
 
     protected function getValue($name)
