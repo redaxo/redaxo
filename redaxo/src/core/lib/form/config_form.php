fe5862c5006a83c25c81adf0f904e2cfe194b026
skerbis <791247+skerbis@users.noreply.github.com>
2025-04-22 11:31:43 +0000

    Apply php-cs-fixer changes

--- redaxo/src/core/lib/form/config_form.php
+++ redaxo/src/core/lib/form/config_form.php
@@ -11,7 +11,7 @@ class rex_config_form extends rex_form_base
 
     /** @var string */
     private $namespace;
-    
+
     /** @var array */
     private $additionalButtons = [];
 
@@ -52,7 +52,7 @@ class rex_config_form extends rex_form_base
         // Wir erzeugen die Buttons erst beim Anzeigen des Formulars,
         // damit alle zusätzlichen Buttons erfasst werden können
     }
-    
+
     /**
      * Adds a button to the form.
      *
@@ -69,7 +69,7 @@ class rex_config_form extends rex_form_base
         $this->additionalButtons[] = $button;
         return $button;
     }
-    
+
     /**
      * @return string
      */
@@ -77,19 +77,19 @@ class rex_config_form extends rex_form_base
     {
         // Wir erstellen das Kontrollfeld mit allen Buttons kurz vor der Ausgabe des Formulars
         $this->createControlField();
-        
+
         return parent::get();
     }
-    
+
     /**
-     * Creates the control field with the save button and any additional buttons
+     * Creates the control field with the save button and any additional buttons.
      */
     protected function createControlField()
     {
         // Standard-Speichern-Button
         $attr = ['type' => 'submit', 'internal::useArraySyntax' => false, 'internal::fieldSeparateEnding' => true];
         $saveButton = $this->addField('button', 'save', rex_i18n::msg('form_save'), $attr, false);
-        
+
         // Control-Element mit allen Buttons erstellen
         if (empty($this->additionalButtons)) {
             // Standardverhalten, wenn keine zusätzlichen Buttons vorhanden sind
@@ -97,14 +97,14 @@ class rex_config_form extends rex_form_base
         } else {
             // Bei mindestens einem Button erstellen wir ein erweitertes Control-Element
             $buttons = array_merge([$saveButton], $this->additionalButtons);
-            
+
             // Die ersten fünf Buttons als Parameter übergeben
-            $saveElement = isset($buttons[0]) ? $buttons[0] : null;
-            $applyElement = isset($buttons[1]) ? $buttons[1] : null;
-            $deleteElement = isset($buttons[2]) ? $buttons[2] : null;
-            $resetElement = isset($buttons[3]) ? $buttons[3] : null;
-            $abortElement = isset($buttons[4]) ? $buttons[4] : null;
-            
+            $saveElement = $buttons[0] ?? null;
+            $applyElement = $buttons[1] ?? null;
+            $deleteElement = $buttons[2] ?? null;
+            $resetElement = $buttons[3] ?? null;
+            $abortElement = $buttons[4] ?? null;
+
             $this->addControlField($saveElement, $applyElement, $deleteElement, $resetElement, $abortElement);
         }
     }
