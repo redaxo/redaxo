9896efe277853ca27ca96caf49d9bd3ce2ba86f3
Gregor Harlan <mail@gh01.de>
2025-05-19 19:21:06 +0200

    Update tools

--- redaxo/src/core/lib/setup/setup.php
+++ redaxo/src/core/lib/setup/setup.php
@@ -8,6 +8,7 @@
 class rex_setup
 {
     // These values must be synchronized with the values in redaxo/src/core/update.php
+    /** @var string */
     public const MIN_PHP_VERSION = REX_MIN_PHP_VERSION;
     public const MIN_PHP_EXTENSIONS = ['ctype', 'fileinfo', 'filter', 'iconv', 'intl', 'mbstring', 'pcre', 'pdo', 'pdo_mysql', 'session', 'tokenizer'];
     public const MIN_MYSQL_VERSION = '5.6';
