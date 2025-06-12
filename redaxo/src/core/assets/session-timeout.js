c20092ec0d469753489e44f7fbda3f6cb68f25e6
Jan Kristinus <jan.kristinus@yakamara.de>
2025-06-12 09:35:40 +0200

    session timeout js nun ohne jquery und als lokale Funktionen

--- redaxo/src/core/assets/session-timeout.js
+++ redaxo/src/core/assets/session-timeout.js
@@ -16,186 +16,271 @@ rex.i18n.session_timeout_refresh_label
 
 if (1 === rex.session_logged_in && rex.session_keep_alive) {
 
-    (function ($) {
+    (function () {
+        'use strict';
 
-            'use strict';
+        // Session Timeout Manager als Closure
+        const createSessionTimeoutManager = function () {
+            // Private Variablen
+            let currentBrowserTime;
+            let serverTime;
+            let offsetTime;
+            let lastSessionUpdate;
+            let maxKeepAlive;
+            let keepAliveExpandtime = 15 * 60; // 15 Minuten in Sekunden
+            let sessionCheckInterval;
 
-            $.rex_sessionTimeout = function (options) {
-                rex.currentBrowserTime = new Date();
-                rex.serverTime = new Date(rex.time * 1000);
-                rex.offsetTime = rex.currentBrowserTime.getTime() - rex.serverTime.getTime();
-                rex.lastSessionUpdate = new Date(rex.time * 1000);
-                rex.maxKeepAlive = new Date((rex.time + rex.session_keep_alive) * 1000);
-                rex.keepAliveExpandtime = 15 * 60; // 15 Minuten in Sekunden
+            // Private Methoden
+            const init = function () {
+                currentBrowserTime = new Date();
+                serverTime = new Date(rex.time * 1000);
+                offsetTime = currentBrowserTime.getTime() - serverTime.getTime();
+                lastSessionUpdate = new Date(rex.time * 1000);
+                maxKeepAlive = new Date((rex.time + rex.session_keep_alive) * 1000);
 
                 if (rex.session_warning_time > rex.session_duration) {
-                    rex.session_warning_time = parseInt(rex.session_duration / 2); // Session duration halbiert
+                    rex.session_warning_time = parseInt(rex.session_duration / 2);
                 }
 
-                $.rex_sessionInterval();
+                startSessionInterval();
+            };
 
-            }
+            const startSessionInterval = function () {
+                sessionCheckInterval = setInterval(function () {
+                    const currentBrowserTimeNow = new Date();
+                    const currentServerTimeNow = new Date(currentBrowserTimeNow.getTime() - offsetTime);
 
-            $.rex_sessionInterval = function (options) {
-
-                var rex_sessionCheckInterval = setInterval(function () {
-
-                    rex.currentBrowserTime = new Date();
-                    rex.currentServerTime = new Date(rex.currentBrowserTime.getTime() - rex.offsetTime);
-
-                    // Overalltime is about to expire
-                    rex.overAllEndServerTimeWarningMoment = new Date((rex.session_start + rex.session_max_overall_duration - rex.session_warning_time) * 1000);
-                    if (rex.overAllEndServerTimeWarningMoment < rex.currentServerTime) {
-                        clearInterval(rex_sessionCheckInterval);
-                        $.rex_viewSessionOverallTimeoutDialog();
+                    // Überprüfung der Gesamtzeit
+                    const overAllEndServerTimeWarningMoment = new Date((rex.session_start + rex.session_max_overall_duration - rex.session_warning_time) * 1000);
+                    if (overAllEndServerTimeWarningMoment < currentServerTimeNow) {
+                        clearInterval(sessionCheckInterval);
+                        viewSessionOverallTimeoutDialog();
+                        return;
                     }
 
-                    // Current Session Check, if it is about to expire
-                    rex.nextWarningSessionDurationTime = new Date(rex.lastSessionUpdate.getTime() + (rex.session_warning_time * 1000));
-                    if (rex.nextWarningSessionDurationTime < rex.currentServerTime) {
-                        clearInterval(rex_sessionCheckInterval);
-                        $.rex_viewSessionExpandDialog();
+                    // Aktuelle Session überprüfen
+                    const nextWarningSessionDurationTime = new Date(lastSessionUpdate.getTime() + (rex.session_warning_time * 1000));
+                    if (nextWarningSessionDurationTime < currentServerTimeNow) {
+                        clearInterval(sessionCheckInterval);
+                        viewSessionExpandDialog();
+                        return;
                     }
 
-                    // KeepAlive, if not timed out
-                    if (rex.maxKeepAlive > rex.currentServerTime) {
-                        clearInterval(rex_sessionCheckInterval);
-                        $.ajax(rex.session_keep_alive_url, {
-                            cache: false
-                        }).done(function (data) {
-                            rex.lastSessionUpdate = new Date();
-                            rex.lastSessionUpdate.setTime(rex.lastSessionUpdate.getTime() - rex.offsetTime); // Adjust to server time
-                            $.rex_sessionInterval(); // Restart the session interval
-                        }).fail(function (data) {
-                            $.rex_viewSessionFailedDialog();
-                        });
+                    // KeepAlive wenn nicht abgelaufen
+                    if (maxKeepAlive > currentServerTimeNow) {
+                        clearInterval(sessionCheckInterval);
+                        performKeepAlive();
                     }
 
                 }, 15 * 1000);
+            };
 
-            }
+            const performKeepAlive = function () {
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', rex.session_keep_alive_url + '?_=' + new Date().getTime(), true);
 
-            $.rex_viewModalBox = function (options) {
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        lastSessionUpdate = new Date();
+                        lastSessionUpdate.setTime(lastSessionUpdate.getTime() - offsetTime);
+                        startSessionInterval();
+                    } else {
+                        viewSessionFailedDialog();
+                    }
+                };
 
-                // options.buttons auslesen und html erstellen
-                var buttonHTML = '';
+                xhr.onerror = function () {
+                    viewSessionFailedDialog();
+                };
+
+                xhr.send();
+            };
+
+            const createModalBox = function (options) {
+                // Entferne existierende Dialoge
+                const existingDialog = document.querySelector('.rex-session-timeout-dialog');
+                if (existingDialog) {
+                    existingDialog.remove();
+                }
+
+                // Erstelle Modal HTML
+                const modal = document.createElement('div');
+                modal.className = 'modal rex-session-timeout-dialog';
+
+                let buttonHTML = '';
                 if (options.buttons && options.buttons.length) {
                     options.buttons.forEach(function (button) {
-                        buttonHTML += '<button id="' + button.id + '" ' + button.attr + ' type="button" >' + button.label + '</button>';
+                        buttonHTML += '<button id="' + button.id + '" ' + button.attr + ' type="button">' + button.label + '</button>';
+                    });
+                }
+
+                modal.innerHTML = `
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title">${rex.i18n.session_timeout_title}</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>${options.message}</p>
+                            </div>
+                            <div class="modal-footer">
+                                ${buttonHTML}
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                document.body.appendChild(modal);
+
+                // Event Listener für Buttons hinzufügen
+                if (options.buttons && options.buttons.length) {
+                    options.buttons.forEach(function (button) {
                         if (button.click) {
-                            $(document).on('click', '#' + button.id, function () {
-                                button.click();
-                            });
+                            const btnElement = document.getElementById(button.id);
+                            if (btnElement) {
+                                btnElement.addEventListener('click', button.click);
+                            }
                         }
                     });
                 }
 
-                $('body')
-                    .find('.rex-session-timeout-dialog')
-                    .remove();
-                $('body')
-                    .append(
-                        '<div class="modal rex-session-timeout-dialog"> \
-                            <div class="modal-dialog"> \
-                                <div class="modal-content"> \
-                                    <div class="modal-header"> \
-                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> \
-                                        <h4 class="modal-title">' + rex.i18n.session_timeout_title + '</h4> \
-                                    </div> \
-                                    <div class="modal-body"> \
-                                        <p>' + options.message + '</p> \
-                                    </div> \
-                                    <div class="modal-footer"> \
-                                        ' + buttonHTML + ' \
-                                    </div> \
-                                </div> \
-                            </div> \
-                        </div>'
-                    );
+                // Close Button Funktionalität
+                const closeButton = modal.querySelector('.close');
+                if (closeButton) {
+                    closeButton.addEventListener('click', function () {
+                        modal.remove();
+                    });
+                }
 
-                $('.rex-session-timeout-dialog').on('hidden.bs.modal', function (e) {
-                    $('.rex-session-timeout-dialog').remove();
-                })
-                $('.rex-session-timeout-dialog').modal({
-                    backdrop: 'static',
+                // Bootstrap Modal anzeigen (falls Bootstrap vorhanden)
+                if (typeof $ !== 'undefined' && $.fn.modal) {
+                    $(modal).modal({
+                        backdrop: 'static'
+                    });
+                    $(modal).on('hidden.bs.modal', function () {
+                        modal.remove();
+                    });
+                } else {
+                    // Fallback ohne Bootstrap
+                    modal.style.display = 'block';
+                    modal.classList.add('in');
+
+                    // Backdrop hinzufügen
+                    const backdrop = document.createElement('div');
+                    backdrop.className = 'modal-backdrop fade in';
+                    document.body.appendChild(backdrop);
+                }
+            };
+
+            const viewSessionFailedDialog = function () {
+                const options = {
+                    title: rex.i18n.session_timeout_title,
+                    message: rex.i18n.session_timeout_message_failed,
+                    buttons: [
+                        {
+                            id: 'rex-session-timeout-dialog-logout',
+                            label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_login_label,
+                            attr: 'class="btn btn-default rex-session-timeout-dialog-logout"',
+                            click: function () {
+                                window.location.href = rex.session_logout_url;
+                            }
+                        }
+                    ]
+                };
+
+                createModalBox(options);
+            };
+
+            const viewSessionExpandDialog = function () {
+                const values = [parseInt(rex.session_warning_time / 60), parseInt(keepAliveExpandtime / 60)];
+                const message = rex.i18n.session_timeout_message_expand.replace(/{(\d+)}/g, function (match, index) {
+                    return values[Number(index)];
                 });
-            }
 
-            $.rex_viewSessionFailedDialog = function (options) {
-
-                options = options || {};
-                options.title = rex.i18n.session_timeout_title;
-                options.message = rex.i18n.session_timeout_message_failed;
-                options.buttons = [
-                    {
-                        id: 'rex-session-timeout-dialog-logout',
-                        label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_login_label,
-                        attr: ' class="btn btn-default rex-session-timeout-dialog-logout"',
-                        click: function () {
-                            window.location.href = rex.session_logout_url;
+                const options = {
+                    message: message,
+                    buttons: [
+                        {
+                            id: 'rex-session-timeout-dialog-logout',
+                            label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_logout_label,
+                            attr: 'class="btn btn-default rex-session-timeout-dialog-logout"',
+                            click: function () {
+                                window.location.href = rex.session_logout_url;
+                            }
+                        },
+                        {
+                            id: 'rex-session-timeout-dialog-refresh',
+                            label: '<i class="rex-icon rex-icon-refresh"></i> ' + rex.i18n.session_timeout_refresh_label,
+                            attr: 'class="btn btn-primary rex-session-timeout-dialog-refresh" data-dismiss="modal"',
+                            click: function () {
+                                maxKeepAlive = new Date(maxKeepAlive.getTime() + (keepAliveExpandtime * 1000));
+                                startSessionInterval();
+                                const dialog = document.querySelector('.rex-session-timeout-dialog');
+                                if (dialog) {
+                                    dialog.remove();
+                                }
+                                const backdrop = document.querySelector('.modal-backdrop');
+                                if (backdrop) {
+                                    backdrop.remove();
+                                }
+                            }
                         }
+                    ]
+                };
+
+                createModalBox(options);
+            };
+
+            const viewSessionOverallTimeoutDialog = function () {
+                const values = [parseInt(rex.session_warning_time / 60)];
+                const message = rex.i18n.session_timeout_message_expired.replace(/{(\d+)}/g, function (match, index) {
+                    return values[Number(index)];
+                });
+
+                const options = {
+                    message: message,
+                    buttons: [
+                        {
+                            id: 'rex-session-timeout-dialog-logout',
+                            attr: 'class="btn btn-default rex-session-timeout-dialog-logout"',
+                            label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_logout_label,
+                            click: function () {
+                                window.location.href = rex.session_logout_url;
+                            }
+                        }
+                    ]
+                };
+
+                createModalBox(options);
+            };
+
+            // Öffentliche API
+            return {
+                init: init,
+                stopInterval: function () {
+                    if (sessionCheckInterval) {
+                        clearInterval(sessionCheckInterval);
                     }
-                ];
+                }
+            };
+        };
 
-                $.rex_viewModalBox(options);
-            }
+        // Session Manager erstellen und initialisieren
+        const sessionManager = createSessionTimeoutManager();
 
-            $.rex_viewSessionExpandDialog = function (options) {
-
-                options = options || {};
-                var values = [parseInt(rex.session_warning_time/60), parseInt(rex.keepAliveExpandtime/60)];
-                options.message = rex.i18n.session_timeout_message_expand.replace(/{(\d+)}/g, (match, index) => values[Number(index)]);;
-
-                options.buttons = [
-                    {
-                        id: 'rex-session-timeout-dialog-logout',
-                        label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_logout_label,
-                        attr: ' class="btn btn-default rex-session-timeout-dialog-logout"',
-                        click: function () {
-                            window.location.href = rex.session_logout_url;
-                        }
-                    },
-                    {
-                        id: 'rex-session-timeout-dialog-refresh',
-                        label: '<i class="rex-icon rex-icon-refresh"></i> ' + rex.i18n.session_timeout_refresh_label,
-                        attr: ' class="btn btn-primary rex-session-timeout-dialog-refresh" data-dismiss="modal"',
-                        click: function () {
-                            rex.maxKeepAlive = new Date(rex.maxKeepAlive.getTime() + (rex.keepAliveExpandtime * 1000)); // 15 Minuten
-                            $.rex_sessionInterval(); // Restart the session interval
-                            $('.rex-session-timeout-dialog').remove();
-                        }
-                    }
-                ];
-
-                $.rex_viewModalBox(options);
-
-            }
-
-            $.rex_viewSessionOverallTimeoutDialog = function (options) {
-
-                options = options || {};
-                var values = [parseInt(rex.session_warning_time/60)];
-                options.message = rex.i18n.session_timeout_message_expired.replace(/{(\d+)}/g, (match, index) => values[Number(index)]);;
-
-                options.buttons = [
-                    {
-                        id: 'rex-session-timeout-dialog-logout',
-                        attr: ' class="btn btn-default rex-session-timeout-dialog-logout"',
-                        label: '<i class="rex-icon rex-icon-sign-out"></i> ' + rex.i18n.session_timeout_logout_label,
-                        click: function () {
-                            window.location.href = rex.session_logout_url;
-                        }
-                    }
-                ];
-                $.rex_viewModalBox(options);
-
-            }
-
+        // Warte bis DOM geladen ist
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', function () {
+                sessionManager.init();
+            });
+        } else {
+            sessionManager.init();
         }
-    )(jQuery);
 
-    $(document).ready(function () {
-        $.rex_sessionTimeout();
-    });
+        // Optional: Manager global verfügbar machen für Debugging
+        window.rexSessionManager = sessionManager;
+
+    })();
 
 }
