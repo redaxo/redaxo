c44533d594ac77e907190f841f1c6df7cf591fe6
Gregor Harlan <mail@gh01.de>
2025-09-04 15:31:03 +0200

    Login: neues Hintergrundbild (#6350)

--- redaxo/src/core/fragments/core/login_background.php
+++ redaxo/src/core/fragments/core/login_background.php
@@ -4,7 +4,7 @@
  * @psalm-scope-this rex_fragment
  */
 
-$pictureName = 'lucas-k-wQLAGv4_OYs-unsplash';
+$pictureName = 'and-machines-HErhYBxhreE-unsplash';
 
 $url2400Avif = rex_url::pluginAssets('be_style', 'redaxo', 'images/' . $pictureName . '-2400.avif');
 $url3500Avif = rex_url::pluginAssets('be_style', 'redaxo', 'images/' . $pictureName . '-3500.avif');
@@ -32,7 +32,7 @@ $url3500Webp = rex_url::pluginAssets('be_style', 'redaxo', 'images/' . $pictureN
 
 <style nonce="<?= rex_response::getNonce() ?>">
     #rex-page-login {
-        background-color: #032126;
+        background-color: #000000;
     }
     #rex-page-login .panel-default {
         background-color: rgba(33, 38, 41, 0.9);
@@ -44,7 +44,7 @@ $url3500Webp = rex_url::pluginAssets('be_style', 'redaxo', 'images/' . $pictureN
     }
     #rex-page-login .btn-primary:hover,
     #rex-page-login .btn-primary:focus {
-        background-color: #ff3c00;
+        background-color: #BB017A;
     }
 
     #rex-page-login .rex-global-footer {
@@ -70,7 +70,7 @@ $url3500Webp = rex_url::pluginAssets('be_style', 'redaxo', 'images/' . $pictureN
         <ul class="list-inline">
             <li><a href="https://www.yakamara.de" target="_blank" rel="noreferrer noopener">yakamara.de</a></li>
             <li><a href="https://www.redaxo.org" target="_blank" rel="noreferrer noopener">redaxo.org</a></li>
-            <li class="rex-background-credits">Photo by <a href="https://unsplash.com/de/@aznbokchoy" target="_blank" rel="noreferrer noopener">Lucas K</a> on <a href="https://unsplash.com/de/fotos/blauer-und-orangefarbener-rauch-wQLAGv4_OYs"  target="_blank" rel="noreferrer noopener">Unsplash</a></li>
+            <li class="rex-background-credits">Photo by <a href="https://unsplash.com/de/@and_machines" target="_blank" rel="noreferrer noopener">@and_machines</a> on <a href="https://unsplash.com/de/fotos/ein-schwarzer-hintergrund-mit-blauen-und-rosa-linien-HErhYBxhreE"  target="_blank" rel="noreferrer noopener">Unsplash</a></li>
         </ul>
     </nav>
 </footer>
